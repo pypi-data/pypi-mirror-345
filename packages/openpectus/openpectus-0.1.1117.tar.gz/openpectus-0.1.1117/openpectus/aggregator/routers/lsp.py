14400ca3bcc1faa2d6f1a97133bd0a70e94660b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-02 16:04:04 +0000

    Add package openpectus/0.1.1117/openpectus-0.1.1117.tar.gz

---
+++ packages/openpectus/openpectus-0.1.1117.tar.gz/openpectus-0.1.1117/openpectus/aggregator/routers/lsp.py
@@ -0,0 +1,73 @@
+import logging
+
+import openpectus.aggregator.deps as agg_deps
+import openpectus.aggregator.models as Mdl
+import openpectus.aggregator.routers.dto as Dto
+from fastapi import APIRouter, Depends, Response, HTTPException
+from openpectus.aggregator.aggregator import Aggregator
+from starlette.status import HTTP_404_NOT_FOUND
+
+
+logger = logging.getLogger(__name__)
+router = APIRouter(tags=["lsp"], include_in_schema=False)
+
+
+def get_registered_engine_data_or_fail(engine_id: str, agg: Aggregator) -> Mdl.EngineData:
+    engine_data = agg.get_registered_engine_data(engine_id)
+    if engine_data is None:
+        raise HTTPException(status_code=HTTP_404_NOT_FOUND)
+    return engine_data
+
+
+@router.get('/lsp/uod/{engine_id}', response_model_exclude_none=True)
+def get_uod_info(
+        engine_id: str,
+        response: Response,
+        agg: Aggregator = Depends(agg_deps.get_aggregator)
+        ) -> Dto.UodDefinition:
+    response.headers["Cache-Control"] = "no-store"
+    engine_data = get_registered_engine_data_or_fail(engine_id, agg)
+    uod_definition = engine_data.uod_definition
+    if uod_definition is None:
+        raise HTTPException(status_code=HTTP_404_NOT_FOUND)
+    return Dto.UodDefinition.from_model(uod_definition)
+
+
+@router.get('/uod/{engine_id}/pcode.tmLanguage.json', response_model_exclude_none=True)
+def get_pcode_tm_grammar(engine_id: str, agg: Aggregator = Depends(agg_deps.get_aggregator)):
+    engine_data = get_registered_engine_data_or_fail(engine_id, agg)
+    if engine_data.uod_definition is None:
+        logger.error(f"engine_data.uod_definition is none, {engine_id=}")
+        raise HTTPException(status_code=HTTP_404_NOT_FOUND)
+
+    sys_cmds = [c.name for c in engine_data.uod_definition.system_commands]
+    uod_cmds = [c.name for c in engine_data.uod_definition.commands]
+    tags = [t.name for t in engine_data.uod_definition.tags]
+
+    return {
+        "name": "PCode",
+        "scopeName": "source.pcode",
+        "fileTypes": "pcode",
+        "patterns": [
+            {
+                "name": "constant.language.pcode",  # color: blue, content: system commands
+                "match": f"\\b({'|'.join(sys_cmds)})\\b",
+            },
+            {
+                "name": "support.constant.color",  # color: lighter blue, content: uod commands
+                "match": f"\\b({'|'.join(uod_cmds)})\\b",
+            },
+            {
+                "name": "comment.control.pcode",  # color: green, content: comments
+                "begin": "#",
+                "end": "$"
+            },
+            {
+                "name": "support.type.property-name",  # color: bright red, content: thresholds
+                "match": "^\\s*\\d+(\\.\\d+)?\\s",
+            },            {
+                "name": "entity.name.class",  # color: blue/green, content: tags
+                "match": f"\\b({'|'.join(tags)})\\b",
+            },
+        ]
+    }
