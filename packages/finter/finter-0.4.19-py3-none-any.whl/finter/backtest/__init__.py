30d78a4f6fa8268ff7a9c1a8a72144c30e56b26c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-02 15:04:47 +0000

    Add package finter/0.4.19/finter-0.4.19-py3-none-any.whl

---
+++ packages/finter/finter-0.4.19-py3-none-any.whl/finter/backtest/__init__.py
@@ -0,0 +1,5 @@
+import finter.backtest.__legacy_support
+from finter.backtest.simulator import Simulator
+from finter.backtest.simulators.multi_account.main import MultiAccountSimulator
+
+__all__ = ["Simulator", "MultiAccountSimulator"]
