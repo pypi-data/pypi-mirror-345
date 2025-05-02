9163c59bff39a49c7045a8be4e82d55d3c3f2a2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-02 16:03:58 +0000

    Add package osl-dynamics/2.1.3/osl_dynamics-2.1.3.tar.gz

---
+++ packages/osl-dynamics/osl_dynamics-2.1.3.tar.gz/osl_dynamics-2.1.3/setup.py
@@ -0,0 +1,7 @@
+# This is the build script for setuptools.
+# See: https://packaging.python.org/en/latest/tutorials/packaging-projects/
+
+from setuptools import setup
+
+if __name__ == "__main__":
+    setup(long_description_content_type='text/markdown')
