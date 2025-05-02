e139d68ffb82d358dafef5f481fbb196e449e15a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-02 04:03:21 +0000

    Add package sequenzo/0.1.9/sequenzo-0.1.9.tar.gz

---
+++ packages/sequenzo/sequenzo-0.1.9.tar.gz/sequenzo-0.1.9/__init__.py
@@ -0,0 +1,57 @@
+"""
+@Author  : Yuqi Liang 梁彧祺
+@File    : __init__.py
+@Time    : 11/02/2025 16:42
+@Desc    : Sequenzo Package Initialization
+"""
+# sequenzo/__init__.py (Top-level)
+
+__version__ = "0.1.9"
+
+# Lazy import: public submodules
+from sequenzo import (datasets, visualization, clustering,
+                      dissimilarity_measures, big_data, define_sequence_data,
+                      multidomain)
+
+
+def __getattr__(name):
+    try:
+        if name == "datasets":
+            from sequenzo import datasets
+            return datasets
+        elif name == "visualization":
+            from sequenzo import visualization
+            return visualization
+        elif name == "clustering":
+            from sequenzo import clustering
+            return clustering
+        elif name == "dissimilarity_measures":
+            from sequenzo import dissimilarity_measures
+            return dissimilarity_measures
+        elif name == "SequenceData":
+            from sequenzo.define_sequence_data import SequenceData
+            return SequenceData
+        elif name == "big_data":
+            from sequenzo.big_data import big_data
+            return big_data
+        elif name == "multidomain":
+            from sequenzo.multidomain import multidomain
+            return multidomain
+    except ImportError as e:
+        raise AttributeError(f"Could not import {name}: {e}")
+
+    raise AttributeError(f"module 'sequenzo' has no attribute '{name}'")
+
+
+# Explicit re-export for IDE autocomplete
+SequenceData = define_sequence_data.SequenceData
+
+__all__ = [
+    'datasets',
+    'visualization',
+    'clustering',
+    'dissimilarity_measures',
+    'SequenceData',
+    'big_data',
+    'multidomain'
+]
