ea01f3d31b65d33c39160b0a40b496ce760f86f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-02 17:04:05 +0000

    Add package acmsg/0.2.1/acmsg-0.2.1-py3-none-any.whl

---
+++ packages/acmsg/acmsg-0.2.1-py3-none-any.whl/acmsg/config.py
@@ -0,0 +1,44 @@
+import os
+import yaml
+
+
+class Config:
+    def __init__(self):
+        self.config_dir = self.create_dir_if_not_exists()
+        self.config_file = self.create_file_if_not_exists()
+        self.model = self.get_option("model")
+        self.api_token = self.get_option("api_token")
+
+    def create_dir_if_not_exists(self):
+        home = os.getenv("HOME") or os.path.expanduser("~")
+        config_dir = f"{home}/.config/acmsg"
+        os.makedirs(config_dir, exist_ok=True)
+        return config_dir
+
+    def create_file_if_not_exists(self):
+        config_file = f"{self.config_dir}/config.yaml"
+        data = {"api_token": "", "model": "thudm/glm-4-32b:free"}
+
+        if not os.path.exists(config_file):
+            os.makedirs(self.config_dir, exist_ok=True)
+            with open(config_file, "w") as f:
+                yaml.dump(data, f)
+
+        return config_file
+
+    def set_option(self, option, value):
+        config_file = self.config_file
+        with open(config_file, "r") as f:
+            data = yaml.safe_load(f)
+
+        data[option] = value
+
+        with open(config_file, "w") as f:
+            yaml.dump(data, f, default_flow_style=False)
+
+    def get_option(self, option):
+        config_file = self.config_file
+        with open(config_file, "r") as f:
+            data = yaml.safe_load(f)
+
+        return data.get(option)
