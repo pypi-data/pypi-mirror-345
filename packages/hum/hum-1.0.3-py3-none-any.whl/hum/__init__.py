d6b4b9a1af869e5709b29aaf23131e718a817765
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-02 16:04:17 +0000

    Add package hum/1.0.3/hum-1.0.3-py3-none-any.whl

---
+++ packages/hum/hum-1.0.3-py3-none-any.whl/hum/__init__.py
@@ -0,0 +1,10 @@
+"""
+Tools to create audio
+"""
+
+from hum.pyo_util import synth, Synth, knob_params, knob_exclude
+from hum.utils import (
+    plot_wf,
+    disp_wf,
+)
+from hum.util import simple_chunker
