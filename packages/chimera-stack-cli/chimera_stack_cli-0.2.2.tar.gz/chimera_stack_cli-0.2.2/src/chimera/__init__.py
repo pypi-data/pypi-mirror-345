11a1c45ddd2b1b985ec4c1de5f35a6af2e3a5ae3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-02 16:04:25 +0000

    Add package chimera-stack-cli/0.2.2/chimera_stack_cli-0.2.2.tar.gz

---
+++ packages/chimera-stack-cli/chimera_stack_cli-0.2.2.tar.gz/chimera_stack_cli-0.2.2/src/chimera/__init__.py
@@ -0,0 +1,12 @@
+"""
+ChimeraStack CLI - A template-based development environment manager.
+
+A powerful tool for creating preconfigured development environments with Docker.
+"""
+
+__version__ = "0.2.2"
+__author__ = "Amirofcodes"
+__email__ = "amirofcodes20@gmail.com"
+__license__ = "MIT"
+__repository__ = "https://github.com/amirofcodes/ChimeraStack-CLI"
+__description__ = "A powerful development environment manager that helps you quickly set up and manage local development environments using pre-configured templates."
