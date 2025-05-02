2c899f18dbc5e2ef938e3408a5869b2a22730f8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-02 03:20:13 +0000

    Add package buildkite-sdk/0.2.2/buildkite_sdk-0.2.2.tar.gz

---
+++ packages/buildkite-sdk/buildkite_sdk-0.2.2.tar.gz/buildkite_sdk-0.2.2/tests/test_command_step.py
@@ -0,0 +1,18 @@
+from buildkite_sdk import Pipeline, CommandStep
+import json
+
+def test_command_step_simple():
+    pipeline = Pipeline()
+    pipeline.add_step(CommandStep(
+        commands="echo 'Hello, world!'"
+    ))
+
+    expected = {"steps": [{"commands": "echo 'Hello, world!'"}]}
+    assert pipeline.to_json() == json.dumps(expected, indent="    ")
+
+def test_command_step_typed_dict():
+    pipeline = Pipeline()
+    pipeline.add_step({ "commands": "echo 'Hello, world!'" })
+
+    expected = {"steps": [{"commands": "echo 'Hello, world!'"}]}
+    assert pipeline.to_json() == json.dumps(expected, indent="    ")
