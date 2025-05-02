a2ad2131f62d739061e9069961e55232853edad1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-02 15:05:49 +0000

    Add package molass-legacy/0.0.5/molass_legacy-0.0.5-py3-none-any.whl

---
+++ packages/molass-legacy/molass_legacy-0.0.5-py3-none-any.whl/molass_legacy/Trimming/__init__.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+"""
+    Trimming.__init__.py
+
+    Copyright (c) 2021, SAXS Team, KEK-PF
+"""
+from .CdLimit import CdLimit
+from .FlangeLimit import FlangeLimit
+from .GuinierLimit import GuinierLimit
+from .FlowChange import FlowChange
+from .DataRange import DataRangeDialog
+from .TrimmingInfo import *
+from .AutoRestrictor import AutoRestrictor
