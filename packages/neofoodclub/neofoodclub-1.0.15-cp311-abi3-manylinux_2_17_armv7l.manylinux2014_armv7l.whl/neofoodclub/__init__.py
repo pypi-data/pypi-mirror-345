96f3fa2e22b4d41b00c578f0a19ea0b471879ba9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-02 16:04:21 +0000

    Add package neofoodclub/1.0.15/neofoodclub-1.0.15-cp311-abi3-manylinux_2_17_armv7l.manylinux2014_armv7l.whl

---
+++ packages/neofoodclub/neofoodclub-1.0.15-cp311-abi3-manylinux_2_17_armv7l.manylinux2014_armv7l.whl/neofoodclub/__init__.py
@@ -0,0 +1,22 @@
+__title__ = "neofoodclub"
+__author__ = "diceroll123"
+__license__ = "MIT"
+__copyright__ = "Copyright 2021-present diceroll123"
+__version__ = "1.0.15"
+
+from .neofoodclub import *  # noqa
+from .probability_model import *  # noqa
+
+__all__ = (  # noqa
+    "Math",
+    "Arena",
+    "Arenas",
+    "Bets",
+    "Chance",
+    "NeoFoodClub",
+    "Odds",
+    "OddsChange",
+    "Pirate",
+    "PartialPirate",
+    "ProbabilityModel",
+)
