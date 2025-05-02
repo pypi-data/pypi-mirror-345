1bc3c0161e895544baaa465d11dad0fa20a0cf6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-02 04:03:35 +0000

    Add package graphos/0.2.1/graphos-0.2.1.tar.gz

---
+++ packages/graphos/graphos-0.2.1.tar.gz/graphos-0.2.1/graphos/src/constants.py
@@ -0,0 +1,11 @@
+
+
+class Ascii:
+    ROUND_UL_CORNDER = '╭'
+    ROUND_UR_CORNER = '╮'
+    ROUND_LL_CORNER = '╰'
+    ROUND_LR_CORNER = '╯'
+
+MOUSE_OUTPUT = "logging/mouse.log"
+LOG_OUTPUT = "logging/application.log"
+SAVE_OUTPUT = "user_data/state.json"
