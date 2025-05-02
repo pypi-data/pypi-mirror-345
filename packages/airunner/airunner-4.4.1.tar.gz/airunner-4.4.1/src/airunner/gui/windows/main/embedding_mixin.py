625354918ccd7ce3be947903a6ca585153d5af53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-02 14:03:58 +0000

    Add package airunner/4.4.1/airunner-4.4.1.tar.gz

---
+++ packages/airunner/airunner-4.4.1.tar.gz/airunner-4.4.1/src/airunner/gui/windows/main/embedding_mixin.py
@@ -0,0 +1,49 @@
+import os
+from airunner.enums import SignalCode
+from airunner.utils.models import scan_path_for_embeddings
+
+
+class EmbeddingMixin:
+    @property
+    def __embeddings(self):
+        return self.get_embeddings_by_version(self.generator_settings.version)
+
+    def get_embeddings(self, message: dict = None):
+        name_filter = message.get("name_filter") if message is not None else ""
+        embeddings = []
+
+        for embedding in self.__embeddings:
+            if name_filter == "":
+                embeddings.append(embedding)
+                continue
+            if name_filter in embedding["name"]:
+                embeddings.append(embedding)
+        self.emit_signal(
+            SignalCode.EMBEDDING_GET_ALL_RESULTS_SIGNAL,
+            {
+                "embeddings": embeddings
+            }
+        )
+        return embeddings
+
+    def delete_missing_embeddings(self):
+        embeddings = self.get_embeddings()
+        for embedding in embeddings:
+            if not os.path.exists(embedding["path"]):
+                self._delete_embedding(embedding)
+    
+    def _delete_embedding(self, embedding):
+        for index, _embedding in enumerate(self.embeddings):
+            if _embedding.name == embedding.name and _embedding.path == embedding.path:
+                self._delete_embedding(embedding)
+                return
+
+    def scan_for_embeddings(self):
+        embeddings = scan_path_for_embeddings(self.path_settings.base_path)
+        self.update_embeddings(embeddings)
+        self.emit_signal(
+            SignalCode.EMBEDDING_GET_ALL_RESULTS_SIGNAL,
+            {
+                "embeddings": self.get_embeddings_by_version(self.generator_settings.version)
+            }
+        )
