f76a294d1e3abf2303690ab2639cc0f5b1c48163
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-02 15:04:23 +0000

    Add package wthisj/0.2.1/wthisj-0.2.1-py3-none-any.whl

---
+++ packages/wthisj/wthisj-0.2.1-py3-none-any.whl/wthisj/__init__.py
@@ -0,0 +1,9 @@
+"""
+wthisj is a python package that can perform punching shear stress calculations.
+"""
+
+__version__ = "0.2.1"
+__author__ = "Robert Wang"
+__license__ = "MIT"
+
+from wthisj.punchingshearsection import PunchingShearSection
