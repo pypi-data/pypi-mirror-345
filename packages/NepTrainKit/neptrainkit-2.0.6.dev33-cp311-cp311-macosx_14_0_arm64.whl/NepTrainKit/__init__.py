d833e6154ef271b5aaf7d380a0d533bd09e63fda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-02 15:05:44 +0000

    Add package NepTrainKit/2.0.6.dev33/neptrainkit-2.0.6.dev33-cp311-cp311-macosx_14_0_arm64.whl

---
+++ packages/NepTrainKit/neptrainkit-2.0.6.dev33-cp311-cp311-macosx_14_0_arm64.whl/NepTrainKit/__init__.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*-
+# @Time    : 2024/11/28 12:52
+# @Author  :
+# @email    : 1747193328@qq.com
+
+import os
+import sys
+from loguru import logger
+
+
+try:
+    # Actual if statement not needed, but keeps code inspectors more happy
+    if __nuitka_binary_dir is not None:
+        is_nuitka_compiled = True
+except NameError:
+    is_nuitka_compiled = False
+
+
+
+if is_nuitka_compiled:
+
+
+    logger.add("./Log/{time:%Y-%m}.log",
+               level="DEBUG",
+                )
+    module_path="./"
+else:
+
+    module_path = os.path.dirname(__file__)
+
