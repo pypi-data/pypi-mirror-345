6dbd7f51cd5eba0bc9edd5990a1783d77c7ba5da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-02 16:03:58 +0000

    Add package LeityGram/0.2.3/LeityGram-0.2.3-py3-none-any.whl

---
+++ packages/LeityGram/LeityGram-0.2.3-py3-none-any.whl/leitygram/__init__.py
@@ -0,0 +1,10 @@
+from .bot import LeityBot
+from .types import Message, ReplyKeyboardMarkup, InlineKeyboardMarkup, CallbackQuery
+
+__all__ = [
+    'LeityBot',
+    'Message',
+    'ReplyKeyboardMarkup',
+    'InlineKeyboardMarkup',
+    'CallbackQuery'
+]
