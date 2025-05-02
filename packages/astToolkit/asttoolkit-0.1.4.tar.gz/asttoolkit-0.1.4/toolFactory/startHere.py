16e895c68ca4b913a22f862e2b219718643b07c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-02 14:03:56 +0000

    Add package astToolkit/0.1.4/asttoolkit-0.1.4.tar.gz

---
+++ packages/astToolkit/asttoolkit-0.1.4.tar.gz/asttoolkit-0.1.4/toolFactory/startHere.py
@@ -0,0 +1,11 @@
+from toolFactory import ast_Identifier, sys_version_infoMinimum
+import ast
+
+list_astDOTnewHARDCODED: list[ast_Identifier] = ['astDOTParamSpec', 'astDOTTryStar', 'astDOTTypeAlias', 'astDOTTypeVar', 'astDOTTypeVarTuple', 'astDOTtype_param']
+list_astDOTnew = list_astDOTnewHARDCODED
+
+# list_astDOTStuPyd: list[ast_Identifier] = []
+
+def Z0Z_typesSpecial(astModule: ast.Module):
+	global list_astDOTnew
+	pass
