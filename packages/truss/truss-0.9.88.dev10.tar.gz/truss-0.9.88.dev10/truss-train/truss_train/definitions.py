b30bc4c2fc3e2c0b3ee80e6662b14cee0ad3db9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-02 15:04:52 +0000

    Add package truss/0.9.88.dev10/truss-0.9.88.dev10.tar.gz

---
+++ packages/truss/truss-0.9.88.dev10.tar.gz/truss-0.9.88.dev10/truss-train/truss_train/definitions.py
@@ -0,0 +1,59 @@
+from typing import Dict, List, Optional, Union
+
+import pydantic
+
+from truss.base import custom_types, truss_config
+
+
+class SecretReference(custom_types.SafeModel):
+    name: str
+
+
+class Compute(custom_types.SafeModel):
+    node_count: int = 1
+    cpu_count: int = 1
+    memory: str = "2Gi"
+    accelerator: Optional[truss_config.AcceleratorSpec] = None
+
+    def model_dump(self, *args, **kwargs):
+        data = super().model_dump(*args, **kwargs)
+        if self.accelerator and self.accelerator.accelerator:
+            data["accelerator"] = {
+                "accelerator": self.accelerator.accelerator.value,
+                "count": self.accelerator.count,
+            }
+        return data
+
+
+class CheckpointingConfig(custom_types.SafeModel):
+    enabled: bool = False
+    checkpoint_path: Optional[str] = None
+
+
+class Runtime(custom_types.SafeModel):
+    start_commands: List[str] = []
+    environment_variables: Dict[str, Union[str, SecretReference]] = {}
+    enable_cache: bool = False
+    checkpointing_config: CheckpointingConfig = CheckpointingConfig()
+
+
+class Image(custom_types.SafeModel):
+    base_image: str
+
+
+class TrainingJob(custom_types.SafeModel):
+    image: Image
+    compute: Compute = Compute()
+    runtime: Runtime = Runtime()
+
+    def model_dump(self, *args, **kwargs):
+        data = super().model_dump(*args, **kwargs)
+        data["compute"] = self.compute.model_dump()
+        return data
+
+
+class TrainingProject(custom_types.SafeModel):
+    name: str
+    # TrainingProject is the wrapper around project config and job config. However, we exclude job
+    # in serialization so just TrainingProject metadata is included in API requests.
+    job: TrainingJob = pydantic.Field(exclude=True)
