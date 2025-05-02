905b94d0dd72b1513477dae7bb74b941da1518dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-02 14:03:42 +0000

    Add package types-aiobotocore-ce/2.22.0/types_aiobotocore_ce-2.22.0-py3-none-any.whl

---
+++ packages/types-aiobotocore-ce/types_aiobotocore_ce-2.22.0-py3-none-any.whl/types_aiobotocore_ce/__init__.pyi
@@ -0,0 +1,29 @@
+"""
+Main interface for ce service.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_ce/)
+
+Copyright 2025 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_ce import (
+        Client,
+        CostExplorerClient,
+    )
+
+    session = get_session()
+    async with session.create_client("ce") as client:
+        client: CostExplorerClient
+        ...
+
+    ```
+"""
+
+from .client import CostExplorerClient
+
+Client = CostExplorerClient
+
+__all__ = ("Client", "CostExplorerClient")
