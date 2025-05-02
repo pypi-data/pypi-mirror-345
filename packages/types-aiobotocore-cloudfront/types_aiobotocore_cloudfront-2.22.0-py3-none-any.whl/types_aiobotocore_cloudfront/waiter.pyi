d3bb9be60f13bd42cf7d49e2585575d7e044b20a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-02 14:03:43 +0000

    Add package types-aiobotocore-cloudfront/2.22.0/types_aiobotocore_cloudfront-2.22.0-py3-none-any.whl

---
+++ packages/types-aiobotocore-cloudfront/types_aiobotocore_cloudfront-2.22.0-py3-none-any.whl/types_aiobotocore_cloudfront/waiter.pyi
@@ -0,0 +1,90 @@
+"""
+Type annotations for cloudfront service client waiters.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_cloudfront/waiters/)
+
+Copyright 2025 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+
+    from types_aiobotocore_cloudfront.client import CloudFrontClient
+    from types_aiobotocore_cloudfront.waiter import (
+        DistributionDeployedWaiter,
+        InvalidationCompletedWaiter,
+        StreamingDistributionDeployedWaiter,
+    )
+
+    session = get_session()
+    async with session.create_client("cloudfront") as client:
+        client: CloudFrontClient
+
+        distribution_deployed_waiter: DistributionDeployedWaiter = client.get_waiter("distribution_deployed")
+        invalidation_completed_waiter: InvalidationCompletedWaiter = client.get_waiter("invalidation_completed")
+        streaming_distribution_deployed_waiter: StreamingDistributionDeployedWaiter = client.get_waiter("streaming_distribution_deployed")
+    ```
+"""
+
+from __future__ import annotations
+
+import sys
+
+from aiobotocore.waiter import AIOWaiter
+
+from .type_defs import (
+    GetDistributionRequestWaitTypeDef,
+    GetInvalidationRequestWaitTypeDef,
+    GetStreamingDistributionRequestWaitTypeDef,
+)
+
+if sys.version_info >= (3, 12):
+    from typing import Unpack
+else:
+    from typing_extensions import Unpack
+
+__all__ = (
+    "DistributionDeployedWaiter",
+    "InvalidationCompletedWaiter",
+    "StreamingDistributionDeployedWaiter",
+)
+
+class DistributionDeployedWaiter(AIOWaiter):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/cloudfront/waiter/DistributionDeployed.html#CloudFront.Waiter.DistributionDeployed)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_cloudfront/waiters/#distributiondeployedwaiter)
+    """
+    async def wait(  # type: ignore[override]
+        self, **kwargs: Unpack[GetDistributionRequestWaitTypeDef]
+    ) -> None:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/cloudfront/waiter/DistributionDeployed.html#CloudFront.Waiter.DistributionDeployed.wait)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_cloudfront/waiters/#distributiondeployedwaiter)
+        """
+
+class InvalidationCompletedWaiter(AIOWaiter):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/cloudfront/waiter/InvalidationCompleted.html#CloudFront.Waiter.InvalidationCompleted)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_cloudfront/waiters/#invalidationcompletedwaiter)
+    """
+    async def wait(  # type: ignore[override]
+        self, **kwargs: Unpack[GetInvalidationRequestWaitTypeDef]
+    ) -> None:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/cloudfront/waiter/InvalidationCompleted.html#CloudFront.Waiter.InvalidationCompleted.wait)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_cloudfront/waiters/#invalidationcompletedwaiter)
+        """
+
+class StreamingDistributionDeployedWaiter(AIOWaiter):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/cloudfront/waiter/StreamingDistributionDeployed.html#CloudFront.Waiter.StreamingDistributionDeployed)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_cloudfront/waiters/#streamingdistributiondeployedwaiter)
+    """
+    async def wait(  # type: ignore[override]
+        self, **kwargs: Unpack[GetStreamingDistributionRequestWaitTypeDef]
+    ) -> None:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/cloudfront/waiter/StreamingDistributionDeployed.html#CloudFront.Waiter.StreamingDistributionDeployed.wait)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_cloudfront/waiters/#streamingdistributiondeployedwaiter)
+        """
