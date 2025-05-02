ebd83ecceeda37a3eb27a1f5e6a11eb52ca07f9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-02 15:04:46 +0000

    Add package mlfastflow/0.1.22/mlfastflow-0.1.22.tar.gz

---
+++ packages/mlfastflow/mlfastflow-0.1.22.tar.gz/mlfastflow-0.1.22/mlfastflow/__init__.py
@@ -0,0 +1,15 @@
+"""MLFastFlow - packages for fast dataflow and workflow processing."""
+
+__version__ = "0.1.22"
+
+# Import core components
+from mlfastflow.core import Flow
+
+# Import sourcing functionality
+from mlfastflow.sourcing import Sourcing
+
+# Import BigQueryClient
+from mlfastflow.bigqueryclient import BigQueryClient
+
+# Make these classes available at the package level
+__all__ = ['Flow', 'Sourcing', 'BigQueryClient']
