de5216c08da21b21aec04f38b95f50310e44a74c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-02 04:03:34 +0000

    Add package python-dotenvx/0.2.4/python_dotenvx-0.2.4.tar.gz

---
+++ packages/python-dotenvx/python_dotenvx-0.2.4.tar.gz/python_dotenvx-0.2.4/CHANGELOG.md
@@ -0,0 +1,33 @@
+# Changelog
+
+All notable changes to this project will be documented in this file. See [standard-version](https://github.com/conventional-changelog/standard-version) for commit guidelines.
+
+## [Unreleased](https://github.com/dotenvx/python-dotenvx/compare/v0.2.4...main)
+
+## [0.2.4](https://github.com/dotenvx/dotenvx/compare/v0.2.3...v0.2.4)
+
+### Added
+
+* Add the ability to specify the `os` and `arch` on `dotenvx-postinstall`
+
+## [0.2.3](https://github.com/dotenvx/dotenvx/compare/v0.2.2...v0.2.3)
+
+### Changed
+
+* Patch `dotenvx` binary install across multiple edge cases
+
+## [0.2.2](https://github.com/dotenvx/dotenvx/compare/v0.2.1...v0.2.2)
+
+### Changed
+
+* Adjust installation of `dotenvx` binary on demand
+
+## [0.2.1](https://github.com/dotenvx/dotenvx/compare/v0.2.1...v0.2.1)
+
+### Added
+
+* Install the `dotenvx` binary on install of the python package
+
+## 0.2.0
+
+Please see commit history.
---
+++ packages/python-dotenvx/python_dotenvx-0.2.4.tar.gz/python_dotenvx-0.2.4/setup.py
@@ -0,0 +1,49 @@
+import os
+from setuptools import setup, find_packages
+
+src = {}
+dir = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(dir, "src/dotenvx", "__version__.py"), "r") as f:
+    exec(f.read(), src)
+
+def read_files(files):
+    data = []
+    for file in files:
+        with open(file, encoding='utf-8') as f:
+            data.append(f.read())
+    return "\n".join(data)
+
+readme = read_files(['README.md', 'CHANGELOG.md'])
+
+setup(
+    name='python-dotenvx',
+    description=src['__description__'],
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    version=src['__version__'],
+    license=src['__license__'],
+    author=src['__author__'],
+    author_email=src['__author_email__'],
+    include_package_data=True,
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url=src['__url__'],
+    keywords=[
+    'environment',
+    'environment variables',
+    'deployments',
+    'settings',
+    'env',
+    'dotenv',
+    'configurations',
+    'python',
+    'dotenvx'
+    ],
+    install_requires=[
+    ],
+    entry_points={
+        'console_scripts': [
+            'dotenvx-postinstall=dotenvx.main:postinstall',
+        ],
+    }
+)
