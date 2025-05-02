a62fbf630b306ebc5e06fd91470afefc92a99500
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-02 03:20:35 +0000

    Add package termux-api-python/0.1.1/termux_api_python-0.1.1.tar.gz

---
+++ packages/termux-api-python/termux_api_python-0.1.1.tar.gz/termux_api_python-0.1.1/termux_api_python/__init__.py
@@ -0,0 +1,10 @@
+"""
+Termux API Python Wrapper
+
+This module provides a Python interface to the Termux API commands.
+"""
+
+from .termux_api import TermuxAPI
+
+__version__ = "0.1.0"
+__all__ = ["TermuxAPI"]
