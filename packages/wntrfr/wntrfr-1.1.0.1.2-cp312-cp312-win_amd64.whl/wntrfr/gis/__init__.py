043f9dc296a9a541faad8fed40de2246d77d8750
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-02 04:03:24 +0000

    Add package wntrfr/1.1.0.1.2/wntrfr-1.1.0.1.2-cp312-cp312-win_amd64.whl

---
+++ packages/wntrfr/wntrfr-1.1.0.1.2-cp312-cp312-win_amd64.whl/wntrfr/gis/__init__.py
@@ -0,0 +1,7 @@
+"""
+The wntrfr.gis package contains methods to convert between water network models
+and GIS formatted data and geospatial functions to snap data and find intersections.
+"""
+from wntrfr.gis.network import WaterNetworkGIS
+from wntrfr.gis.geospatial import snap, intersect
+
---
+++ packages/wntrfr/wntrfr-1.1.0.1.2-cp312-cp312-win_amd64.whl/wntrfr/scenario/__init__.py
@@ -0,0 +1,6 @@
+"""
+The wntrfr.scenario package contains methods to define disaster scenarios and 
+fragility/survival curves.
+"""
+from wntrfr.scenario.earthquake import Earthquake
+from wntrfr.scenario.fragility_curve import FragilityCurve
