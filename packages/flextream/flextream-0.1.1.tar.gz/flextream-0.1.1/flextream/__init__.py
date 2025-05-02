1a5091dc41214e862b596b0727069d539ea4b563
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-02 16:04:40 +0000

    Add package flextream/0.1.1/flextream-0.1.1.tar.gz

---
+++ packages/flextream/flextream-0.1.1.tar.gz/flextream-0.1.1/flextream/__init__.py
@@ -0,0 +1,6 @@
+"""
+Flextream is a tool for handling streaming to eventhub with minimal bottlenecks
+"""
+
+from flextream.batch_handler import send_to_eventhub, BatchHandler  # noqa
+from flextream._version import __version__  # noqa
