cc73a59ce1c8febb618e65ada79c618455677bba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-02 04:03:22 +0000

    Add package dissect.target/3.22.dev52/dissect_target-3.22.dev52.tar.gz

---
+++ packages/dissect.target/dissect_target-3.22.dev52.tar.gz/dissect_target-3.22.dev52/tests/plugins/filesystem/test_walkfs.py
@@ -0,0 +1,39 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from dissect.target.filesystem import VirtualFile, VirtualFilesystem
+from dissect.target.plugins.filesystem.walkfs import WalkFSPlugin
+
+if TYPE_CHECKING:
+    from dissect.target.target import Target
+
+
+def test_walkfs_plugin(target_unix: Target, fs_unix: VirtualFilesystem) -> None:
+    fs_unix.map_file_entry("/path/to/some/file", VirtualFile(fs_unix, "file", None))
+    fs_unix.map_file_entry("/path/to/some/other/file.ext", VirtualFile(fs_unix, "file.ext", None))
+    fs_unix.map_file_entry("/root_file", VirtualFile(fs_unix, "root_file", None))
+    fs_unix.map_file_entry("/other_root_file.ext", VirtualFile(fs_unix, "other_root_file.ext", None))
+    fs_unix.map_file_entry("/.test/test.txt", VirtualFile(fs_unix, "test.txt", None))
+    fs_unix.map_file_entry("/.test/.more.test.txt", VirtualFile(fs_unix, ".more.test.txt", None))
+
+    target_unix.add_plugin(WalkFSPlugin)
+
+    results = list(target_unix.walkfs())
+    assert len(results) == 14
+    assert sorted([r.path for r in results]) == [
+        "/",
+        "/.test",
+        "/.test/.more.test.txt",
+        "/.test/test.txt",
+        "/etc",
+        "/other_root_file.ext",
+        "/path",
+        "/path/to",
+        "/path/to/some",
+        "/path/to/some/file",
+        "/path/to/some/other",
+        "/path/to/some/other/file.ext",
+        "/root_file",
+        "/var",
+    ]
