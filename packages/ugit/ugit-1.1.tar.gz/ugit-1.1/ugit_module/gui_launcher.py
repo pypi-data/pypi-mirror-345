f3841fe21936102c1b10a33173bac29a545bf668
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-02 04:03:24 +0000

    Add package ugit/1.1/ugit-1.1.tar.gz

---
+++ packages/ugit/ugit-1.1.tar.gz/ugit-1.1/ugit_module/gui_launcher.py
@@ -0,0 +1,7 @@
+def main():
+    try:
+        from ugit_module import ugit_gui  # Updated import path
+    except ImportError as e:
+        print("Error: Could not import ugit_module.ugit_gui.")
+        print("Make sure ugit_gui.py is inside the ugit_module package.")
+        print("Details:", e)
