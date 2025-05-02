0f87c9f61999ef5f8a097bd5c12074de8b6e0f63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-02 04:03:35 +0000

    Add package spryx-core/0.1.6/spryx_core-0.1.6.tar.gz

---
+++ packages/spryx-core/spryx_core-0.1.6.tar.gz/spryx_core-0.1.6/spryx_core/security/value_objects.py
@@ -0,0 +1,28 @@
+from enum import StrEnum
+
+from pydantic import BaseModel, Field
+
+from spryx_core.id import EntityId
+from spryx_core.security.permissions import Permission
+
+
+class PlatformPermission(StrEnum):
+    CREATE_ORGANIZATION = "create_organization"
+
+
+class OrganizationRole(BaseModel):
+    """Role of a user in an organization."""
+
+    id: EntityId = Field(..., description="ID of the role")
+    name: str = Field(..., description="Name of the role")
+    permissions: set[Permission] = Field(..., description="Permissions of the role")
+
+
+class CurrentOrganization(BaseModel):
+    """Current organization of a user."""
+
+    id: EntityId = Field(..., description="ID of the organization")
+    name: str = Field(..., description="Name of the organization")
+    role: OrganizationRole = Field(
+        ..., description="Role of the user in the organization"
+    )
