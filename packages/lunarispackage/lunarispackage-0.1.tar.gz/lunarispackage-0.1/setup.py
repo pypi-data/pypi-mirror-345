c02935ae0f94855ce34ed273ed94817d12eb5906
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-02 16:04:39 +0000

    Add package lunarispackage/0.1/lunarispackage-0.1.tar.gz

---
+++ packages/lunarispackage/lunarispackage-0.1.tar.gz/lunarispackage-0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup,find_packages
+
+setup(
+    name = "lunarispackage" ,
+    version = "0.1",
+    author = "WhizzyWhiznerd",
+    author_email= "example@gmail.com",
+    description= "This is Speech to Text Package created by WhizzyWhiznerd"
+)
+packages = find_packages(),
+install_requirements=[
+    'selenium',
+    'webdriver_manager'
+]
+
+
+#pypi-AgEIcHlwaS5vcmcCJGRlMGU3NzcwLTM1NTgtNDJhNS04YmQwLWU4N2ZjZTgwNzU0OAACKlszLCI2NzQzZDQ3YS1hMjMwLTRhODUtODBjOS0yYjMwODEzNDk3ZTkiXQAABiCqwxDna3Udprfm9o3yX6HZkFg-nesF6-ll42yEnbD3qQ
