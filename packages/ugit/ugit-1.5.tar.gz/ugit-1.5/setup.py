a1dddfc90ea708bdd541607f0dde08108c8b167e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-02 04:03:36 +0000

    Add package ugit/1.5/ugit-1.5.tar.gz

---
+++ packages/ugit/ugit-1.5.tar.gz/ugit-1.5/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='ugit',
+    version='1.5',
+    packages=['ugit_module'],
+    entry_points={
+        'console_scripts': [
+            'ugit = ugit_module.cli:main',
+            'ugit-gui = ugit_module.gui_launcher:main'
+        ]
+    }
+)
