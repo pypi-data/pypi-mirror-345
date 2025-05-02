7b097c2b4100fa4bbfef4bccd6510c455ab029ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-02 05:03:45 +0000

    Add package mockstack/0.5.2/mockstack-0.5.2-py3-none-any.whl

---
+++ packages/mockstack/mockstack-0.5.2-py3-none-any.whl/mockstack/display.py
@@ -0,0 +1,27 @@
+"""Display and logging functionality."""
+
+import logging
+
+from fastapi import FastAPI
+
+from mockstack.config import Settings
+
+
+def announce(app: FastAPI, settings: Settings):
+    """Log the startup message with the active settings."""
+    logger = logging.getLogger("uvicorn")
+    extra = {"markup": True}
+
+    logger.info(
+        f"[bold medium_purple]mockstack[/bold medium_purple] ready to roll. "
+        f"debug: [medium_purple]{settings.debug}[/medium_purple]. "
+        f"strategy: [medium_purple]{settings.strategy}[/medium_purple]. ",
+        extra=extra,
+    )
+    logger.info(str(app.state.strategy), extra=extra)
+    logger.info(
+        f"[medium_purple]OpenTelemetry[/medium_purple] enabled: [medium_purple]{settings.opentelemetry.enabled}[/medium_purple],\n "
+        f"endpoint: [medium_purple]{settings.opentelemetry.endpoint}[/medium_purple],\n "
+        f"capture_response_body: [medium_purple]{settings.opentelemetry.capture_response_body}[/medium_purple]",
+        extra=extra,
+    )
