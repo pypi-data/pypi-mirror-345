0218030d315af00a83fe1fe9dbbcc360174ac38f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-02 14:03:58 +0000

    Add package dependency-groups/1.3.1/dependency_groups-1.3.1.tar.gz

---
+++ packages/dependency-groups/dependency_groups-1.3.1.tar.gz/dependency_groups-1.3.1/CHANGELOG.rst
@@ -0,0 +1,83 @@
+CHANGELOG
+=========
+
+Unreleased
+----------
+
+1.3.1
+-----
+
+- Fix a bug in which names in includes were not normalized before comparisons,
+  resulting in spurious ``LookupError``\s.
+- Optimize the behavior of the ``resolve()`` function on multiple groups.
+
+1.3.0
+-----
+
+- Bugfix: raise a ``TypeError`` on non-list groups (was ``ValueError``).
+  Thanks :user:`henryiii`!
+
+- Several improvements to the CLI interfaces! Thanks :user:`henryiii`!
+
+  - Add support for a ``cli`` extra, as in
+    ``pip install "dependency-groups[cli]"``, which ensures that ``tomli`` is
+    present on older Pythons.
+
+  - Add support for ``dependency-groups`` as an entrypoint, as an alias of
+    ``python -m dependency_groups``.
+
+  - The ``dependency-groups`` command now supports a ``--list`` flag to list
+    groups instead of resolving them.
+
+1.2.0
+-----
+
+- Switch to ``flit-core`` as the build backend
+- Add support for supplying multiple dependency groups to the functional
+  ``resolve()`` API: ``resolve(dependency_groups, *groups: str)``. Thanks
+  :user:`henryiii`!
+
+1.1.0
+-----
+
+- Add support for Python 3.8
+
+1.0.0
+-----
+
+- Update metadata to 1.0.0 and "Production" status
+- Support Python 3.13
+
+0.3.0
+-----
+
+- Add a new command, ``pip-install-dependency-groups``, which is capable of
+  installing dependency groups by invoking ``pip``
+
+0.2.2
+-----
+
+- The pre-commit hook sets ``pass_filenames: false``
+- The error presentation in the lint CLI has been improved
+
+0.2.1
+-----
+
+- Bugfix to pre-commit config
+
+0.2.0
+-----
+
+- Add a new CLI component, ``lint-dependency-groups``, which can be used to lint
+  dependency groups.
+- Provide a pre-commit hook, named ``lint-dependency-groups``
+
+0.1.1
+-----
+
+- Fix a bug in cycle detection for nontrivial cycles
+
+0.1.0
+-----
+
+- Initial Release
---
+++ packages/dependency-groups/dependency_groups-1.3.1.tar.gz/dependency_groups-1.3.1/tests/test_resolve_func.py
@@ -0,0 +1,163 @@
+import pytest
+
+from dependency_groups import resolve
+
+
+def test_empty_group():
+    groups = {"test": []}
+    assert resolve(groups, "test") == ()
+
+
+def test_str_list_group():
+    groups = {"test": ["pytest"]}
+    assert resolve(groups, "test") == ("pytest",)
+
+
+def test_single_include_group():
+    groups = {
+        "test": [
+            "pytest",
+            {"include-group": "runtime"},
+        ],
+        "runtime": ["sqlalchemy"],
+    }
+    assert set(resolve(groups, "test")) == {"pytest", "sqlalchemy"}
+
+
+def test_sdual_include_group():
+    groups = {
+        "test": [
+            "pytest",
+        ],
+        "runtime": ["sqlalchemy"],
+    }
+    assert set(resolve(groups, "test", "runtime")) == {"pytest", "sqlalchemy"}
+
+
+def test_normalized_group_name():
+    groups = {
+        "TEST": ["pytest"],
+    }
+    assert resolve(groups, "test") == ("pytest",)
+
+
+def test_malformed_group_data():
+    groups = [{"test": ["pytest"]}]
+    with pytest.raises(TypeError, match="Dependency Groups table is not a mapping"):
+        resolve(groups, "test")
+
+
+def test_malformed_group_query():
+    groups = {"test": ["pytest"]}
+    with pytest.raises(TypeError, match="Dependency group name is not a str"):
+        resolve(groups, 0)
+
+
+def test_no_such_group_name():
+    groups = {
+        "test": ["pytest"],
+    }
+    with pytest.raises(LookupError, match="'testing' not found"):
+        resolve(groups, "testing")
+
+
+def test_duplicate_normalized_name():
+    groups = {
+        "test": ["pytest"],
+        "TEST": ["nose2"],
+    }
+    with pytest.raises(
+        ValueError,
+        match=r"Duplicate dependency group names: test \((test, TEST)|(TEST, test)\)",
+    ):
+        resolve(groups, "test")
+
+
+def test_cyclic_include():
+    groups = {
+        "group1": [
+            {"include-group": "group2"},
+        ],
+        "group2": [
+            {"include-group": "group1"},
+        ],
+    }
+    with pytest.raises(
+        ValueError,
+        match=(
+            "Cyclic dependency group include while resolving group1: "
+            "group1 -> group2, group2 -> group1"
+        ),
+    ):
+        resolve(groups, "group1")
+
+
+def test_cyclic_include_many_steps():
+    groups = {}
+    for i in range(100):
+        groups[f"group{i}"] = [{"include-group": f"group{i+1}"}]
+    groups["group100"] = [{"include-group": "group0"}]
+    with pytest.raises(
+        ValueError,
+        match="Cyclic dependency group include while resolving group0:",
+    ):
+        resolve(groups, "group0")
+
+
+def test_cyclic_include_self():
+    groups = {
+        "group1": [
+            {"include-group": "group1"},
+        ],
+    }
+    with pytest.raises(
+        ValueError,
+        match=(
+            "Cyclic dependency group include while resolving group1: "
+            "group1 includes itself"
+        ),
+    ):
+        resolve(groups, "group1")
+
+
+def test_cyclic_include_ring_under_root():
+    groups = {
+        "root": [
+            {"include-group": "group1"},
+        ],
+        "group1": [
+            {"include-group": "group2"},
+        ],
+        "group2": [
+            {"include-group": "group1"},
+        ],
+    }
+    with pytest.raises(
+        ValueError,
+        match=(
+            "Cyclic dependency group include while resolving root: "
+            "group1 -> group2, group2 -> group1"
+        ),
+    ):
+        resolve(groups, "root")
+
+
+def test_non_list_data():
+    groups = {"test": "pytest, coverage"}
+    with pytest.raises(TypeError, match="Dependency group 'test' is not a list"):
+        resolve(groups, "test")
+
+
+@pytest.mark.parametrize(
+    "item",
+    (
+        {},
+        {"foo": "bar"},
+        {"include-group": "testing", "foo": "bar"},
+        object(),
+    ),
+)
+def test_unknown_object_shape(item):
+    groups = {"test": [item]}
+    with pytest.raises(ValueError, match="Invalid dependency group item:"):
+        resolve(groups, "test")
