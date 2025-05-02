6c985a925ee134ecf639232a1cd9f300b4416b3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-02 14:04:05 +0000

    Add package propertiesio/0.0.1/propertiesio-0.0.1.tar.gz

---
+++ packages/propertiesio/propertiesio-0.0.1.tar.gz/propertiesio-0.0.1/README.md
@@ -0,0 +1,2 @@
+本程序由chenxi开发，csdn：https://blog.csdn.net/chenxi1412
+遵从GPL协议，github：https://github.com/zhangjiahuichenxi/propertiesio
---
+++ packages/propertiesio/propertiesio-0.0.1.tar.gz/propertiesio-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+	long_description = fh.read()
+
+setuptools.setup(
+	name="propertiesio",
+	version="0.0.1",
+	author="chenxi12",
+	author_email="chenxi201412@outlook.com",
+	description="properties",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	url="https://github.com/zhangjiahuichenxi/propertiesio",
+	packages=setuptools.find_packages(),
+	classifiers=[
+	"Programming Language :: Python :: 3",
+	"License :: OSI Approved :: BSD License",
+	"Operating System :: OS Independent",
+	],
+	python_requires='>=3.6',
+)
