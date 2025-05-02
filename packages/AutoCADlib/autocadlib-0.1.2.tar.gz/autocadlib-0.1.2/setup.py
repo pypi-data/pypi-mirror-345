a4428e47db9436b9a6f02f0d60cab0c26df0e9cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-02 16:04:04 +0000

    Add package AutoCADlib/0.1.2/autocadlib-0.1.2.tar.gz

---
+++ packages/AutoCADlib/autocadlib-0.1.2.tar.gz/autocadlib-0.1.2/setup.py
@@ -0,0 +1,33 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name="AutoCADlib",  # Name of your package
+    version="0.1.2",
+    packages=find_packages(),  # Automatically finds the 'cadvance' package
+    install_requires=[
+        'pywin32',
+        'psutil',
+    ],
+    entry_points={
+        'console_scripts': [
+            'AutoCADlib = AutoCADlib.__main__:main',  # Registers 'cadvance' as a command-line tool
+        ],
+    },
+    keywords=["autocad", "automation", "activex", "comtypes", "AutoCAD", "AutoCADlib"],
+    author="Jones Peter",
+    author_email="jonespetersoftware@gmail.com",
+    url="https://github.com/Jones-peter",
+    description="A professional AutoCAD automation package with many functions.",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "Operating System :: Microsoft :: Windows",
+    ],
+    include_package_data=True,  # Include non-Python files, like the README
+)
---
+++ packages/AutoCADlib/autocadlib-0.1.2.tar.gz/autocadlib-0.1.2/tests/test.py
@@ -0,0 +1,3 @@
+from AutoCADlib import AutoCAD
+
+cad = AutoCAD()
