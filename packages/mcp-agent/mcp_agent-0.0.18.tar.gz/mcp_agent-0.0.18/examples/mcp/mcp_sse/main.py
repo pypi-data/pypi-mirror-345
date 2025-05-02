72fe9010a7b5189fed0d55f0adf97d3d4123d985
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-02 15:05:48 +0000

    Add package mcp-agent/0.0.18/mcp_agent-0.0.18.tar.gz

---
+++ packages/mcp-agent/mcp_agent-0.0.18.tar.gz/mcp_agent-0.0.18/examples/mcp/mcp_sse/main.py
@@ -0,0 +1,32 @@
+import asyncio
+
+from dotenv import load_dotenv
+from rich import print
+
+from mcp_agent.agents.agent import Agent
+from mcp_agent.app import MCPApp
+
+load_dotenv()  # load environment variables from .env
+
+
+async def test_sse():
+    app: MCPApp = MCPApp(name="test-app")
+    async with app.run():
+        print("MCP App initialized.")
+
+        agent: Agent = Agent(
+            name="agent",
+            instruction="You are an assistant",
+            server_names=["mcp_test_server_sse"],
+        )
+
+        async with agent:
+            print(await agent.list_tools())
+            # call_tool_result: CallToolResult = await agent.call_tool('mcp_test_server_sse-get-magic-number')
+            #
+            # assert call_tool_result.text == "42"
+            # print("SSE test passed!")
+
+
+if __name__ == "__main__":
+    asyncio.run(test_sse())
