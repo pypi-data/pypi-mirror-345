d552f4dffaeaa719cd939f2063e736fc3fb04549
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-02 14:04:09 +0000

    Add package types-aiobotocore-full/2.22.0/types_aiobotocore_full-2.22.0.tar.gz

---
+++ packages/types-aiobotocore-full/types_aiobotocore_full-2.22.0.tar.gz/types_aiobotocore_full-2.22.0/types_aiobotocore_cloudfront/__init__.py
@@ -0,0 +1,75 @@
+"""
+Main interface for cloudfront service.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_cloudfront/)
+
+Copyright 2025 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_cloudfront import (
+        Client,
+        CloudFrontClient,
+        DistributionDeployedWaiter,
+        InvalidationCompletedWaiter,
+        ListCloudFrontOriginAccessIdentitiesPaginator,
+        ListDistributionsPaginator,
+        ListInvalidationsPaginator,
+        ListKeyValueStoresPaginator,
+        ListPublicKeysPaginator,
+        ListStreamingDistributionsPaginator,
+        StreamingDistributionDeployedWaiter,
+    )
+
+    session = get_session()
+    async with session.create_client("cloudfront") as client:
+        client: CloudFrontClient
+        ...
+
+
+    distribution_deployed_waiter: DistributionDeployedWaiter = client.get_waiter("distribution_deployed")
+    invalidation_completed_waiter: InvalidationCompletedWaiter = client.get_waiter("invalidation_completed")
+    streaming_distribution_deployed_waiter: StreamingDistributionDeployedWaiter = client.get_waiter("streaming_distribution_deployed")
+
+    list_cloud_front_origin_access_identities_paginator: ListCloudFrontOriginAccessIdentitiesPaginator = client.get_paginator("list_cloud_front_origin_access_identities")
+    list_distributions_paginator: ListDistributionsPaginator = client.get_paginator("list_distributions")
+    list_invalidations_paginator: ListInvalidationsPaginator = client.get_paginator("list_invalidations")
+    list_key_value_stores_paginator: ListKeyValueStoresPaginator = client.get_paginator("list_key_value_stores")
+    list_public_keys_paginator: ListPublicKeysPaginator = client.get_paginator("list_public_keys")
+    list_streaming_distributions_paginator: ListStreamingDistributionsPaginator = client.get_paginator("list_streaming_distributions")
+    ```
+"""
+
+from .client import CloudFrontClient
+from .paginator import (
+    ListCloudFrontOriginAccessIdentitiesPaginator,
+    ListDistributionsPaginator,
+    ListInvalidationsPaginator,
+    ListKeyValueStoresPaginator,
+    ListPublicKeysPaginator,
+    ListStreamingDistributionsPaginator,
+)
+from .waiter import (
+    DistributionDeployedWaiter,
+    InvalidationCompletedWaiter,
+    StreamingDistributionDeployedWaiter,
+)
+
+Client = CloudFrontClient
+
+
+__all__ = (
+    "Client",
+    "CloudFrontClient",
+    "DistributionDeployedWaiter",
+    "InvalidationCompletedWaiter",
+    "ListCloudFrontOriginAccessIdentitiesPaginator",
+    "ListDistributionsPaginator",
+    "ListInvalidationsPaginator",
+    "ListKeyValueStoresPaginator",
+    "ListPublicKeysPaginator",
+    "ListStreamingDistributionsPaginator",
+    "StreamingDistributionDeployedWaiter",
+)
---
+++ packages/types-aiobotocore-full/types_aiobotocore_full-2.22.0.tar.gz/types_aiobotocore_full-2.22.0/types_aiobotocore_network_firewall/__init__.pyi
@@ -0,0 +1,63 @@
+"""
+Main interface for network-firewall service.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_network_firewall/)
+
+Copyright 2025 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_network_firewall import (
+        Client,
+        GetAnalysisReportResultsPaginator,
+        ListAnalysisReportsPaginator,
+        ListFirewallPoliciesPaginator,
+        ListFirewallsPaginator,
+        ListRuleGroupsPaginator,
+        ListTLSInspectionConfigurationsPaginator,
+        ListTagsForResourcePaginator,
+        NetworkFirewallClient,
+    )
+
+    session = get_session()
+    async with session.create_client("network-firewall") as client:
+        client: NetworkFirewallClient
+        ...
+
+
+    get_analysis_report_results_paginator: GetAnalysisReportResultsPaginator = client.get_paginator("get_analysis_report_results")
+    list_analysis_reports_paginator: ListAnalysisReportsPaginator = client.get_paginator("list_analysis_reports")
+    list_firewall_policies_paginator: ListFirewallPoliciesPaginator = client.get_paginator("list_firewall_policies")
+    list_firewalls_paginator: ListFirewallsPaginator = client.get_paginator("list_firewalls")
+    list_rule_groups_paginator: ListRuleGroupsPaginator = client.get_paginator("list_rule_groups")
+    list_tls_inspection_configurations_paginator: ListTLSInspectionConfigurationsPaginator = client.get_paginator("list_tls_inspection_configurations")
+    list_tags_for_resource_paginator: ListTagsForResourcePaginator = client.get_paginator("list_tags_for_resource")
+    ```
+"""
+
+from .client import NetworkFirewallClient
+from .paginator import (
+    GetAnalysisReportResultsPaginator,
+    ListAnalysisReportsPaginator,
+    ListFirewallPoliciesPaginator,
+    ListFirewallsPaginator,
+    ListRuleGroupsPaginator,
+    ListTagsForResourcePaginator,
+    ListTLSInspectionConfigurationsPaginator,
+)
+
+Client = NetworkFirewallClient
+
+__all__ = (
+    "Client",
+    "GetAnalysisReportResultsPaginator",
+    "ListAnalysisReportsPaginator",
+    "ListFirewallPoliciesPaginator",
+    "ListFirewallsPaginator",
+    "ListRuleGroupsPaginator",
+    "ListTLSInspectionConfigurationsPaginator",
+    "ListTagsForResourcePaginator",
+    "NetworkFirewallClient",
+)
