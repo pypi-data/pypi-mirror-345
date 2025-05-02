64d4feb628dbb1d5ae2aa3a3146b724bda359ce2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-02 15:05:07 +0000

    Add package rdlab-dataset/0.4.1/rdlab_dataset-0.4.1.tar.gz

---
+++ packages/rdlab-dataset/rdlab_dataset-0.4.1.tar.gz/rdlab_dataset-0.4.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='rdlab_dataset',
+    version='0.4.1',
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        'rdlab_dataset': ['data/*.pkl', 'font/*.ttf', 'background/*.jpg'],
+    },
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='soyvitou',
+    author_email='soyvitoupro@gmail.com',
+    url='https://github.com/SoyVitouPro/rdlab_dataset',
+    install_requires=[
+        'matplotlib',
+        'Pillow',
+        'numpy',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
