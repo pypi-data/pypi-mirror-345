8410d6c7e0648267265a13a9208112d1e2ccc637
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-02 16:04:23 +0000

    Add package diblob/2.0.1/diblob-2.0.1-py3-none-any.whl

---
+++ packages/diblob/diblob-2.0.1-py3-none-any.whl/testing_criterions/NPathCoverage.py
@@ -0,0 +1,54 @@
+from diblob import DigraphManager
+from testing_criterions.SimplePathsCoverage import SimplePathsCoverage
+from diblob.factory import DiblobFactory
+from testing_criterions.exceptions import InvalidNPathException
+from testing_criterions.decorators import validate_source, validate_sink, validate_reachability
+
+class NPathCoverage:
+    @validate_reachability()
+    @validate_source()
+    @validate_sink()
+    def __init__(self, digraph_manager, n_paths) -> None:
+        if n_paths < 2:
+            raise InvalidNPathException("n_path argument should be at least equals to 2.")
+
+        for reduce in range(n_paths - 1):
+            digraph_manager = DiblobFactory.generate_edge_digraph(digraph_manager, reduce_value=reduce)
+        self.digraph_manager = digraph_manager
+        self.digraph_manager.add_nodes('S', 'T')
+
+        for node_id in self.digraph_manager.nodes:
+        
+            if node_id[:2] == 'S|':
+                self.digraph_manager.connect_nodes(('S', node_id))
+            if node_id[-2:] == '|T':
+                self.digraph_manager.connect_nodes((node_id, 'T'))
+   
+
+    def get_test_cases(self, max_number_of_cycles_in_single_test_case):
+        spc = SimplePathsCoverage(self.digraph_manager)
+
+        for test_case in spc.get_test_cases(max_number_of_cycles_in_single_test_case):
+            yield test_case[1].split('|') + [t.split('|')[-1] for t in test_case[2:-1]]
+
+
+from diblob.digraph_manager import DigraphManager
+digraph_manager = DigraphManager({'B0':{
+    "S": ["1"],
+    "1": ["2", "3", "4"],
+    "T": [],
+    "6": ["1"],
+    "4": ["5"],
+    "3": ["5"],
+    "5": ["6", "T"],
+    "2": ["5"],
+}})
+# digraph_manager.add_nodes('S', '1', '2', '3', '4', '5', 'T')
+# digraph_manager.connect_nodes(('S', '1'), ('1', '2'), ('2', 'T'), ('1', '3'), ('3', '4'), ('4', '5'), ('5', '4'), ('4', '1'))
+
+ 
+scc = NPathCoverage(digraph_manager, n_paths=3)
+for x in scc.get_test_cases(1):
+   print(x)
+
+print(digraph_manager.diblobs)
