dd8064a379b890d7a551aa0618dd94a65451b3ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-02 04:03:30 +0000

    Add package maidr/0.25.1/maidr-0.25.1.tar.gz

---
+++ packages/maidr/maidr-0.25.1.tar.gz/maidr-0.25.1/maidr/__init__.py
@@ -0,0 +1,23 @@
+__version__ = "0.25.1"
+
+from .api import close, render, save_html, show, stacked
+from .core import Maidr
+from .core.enum import PlotType
+from .patch import (
+    barplot,
+    boxplot,
+    clear,
+    heatmap,
+    highlight,
+    histogram,
+    lineplot,
+    scatterplot,
+)
+
+__all__ = [
+    "close",
+    "render",
+    "save_html",
+    "show",
+    "stacked",
+]
