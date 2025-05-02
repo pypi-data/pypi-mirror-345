9768de9935c297c0fabeb53f634ba67cd67cd1e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-02 04:03:35 +0000

    Add package ckanext-selfinfo/1.1.10/ckanext_selfinfo-1.1.10.tar.gz

---
+++ packages/ckanext-selfinfo/ckanext_selfinfo-1.1.10.tar.gz/ckanext_selfinfo-1.1.10/ckanext/selfinfo/logic/action/__init__.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+
+from typing import Any
+
+from . import get
+from . import update
+import ckanext.selfinfo.config as self_config
+
+
+def get_actions():
+    actions: dict[str, Any] = {
+        self_config.selfinfo_get_main_action_name(): get.get_selfinfo,
+        "update_last_module_check": update.update_last_module_check,
+        "selfinfo_get_ram": get.selfinfo_get_ram,
+    }
+
+    return actions
