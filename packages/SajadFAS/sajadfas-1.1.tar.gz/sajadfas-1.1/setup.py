f6eeebf3ac1aea9a365ae1732cf259748ee0d540
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-02 03:21:07 +0000

    Add package SajadFAS/1.1/sajadfas-1.1.tar.gz

---
+++ packages/SajadFAS/sajadfas-1.1.tar.gz/sajadfas-1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup,find_packages
+setup(
+	version='1.1',
+	name='SajadFAS',
+	author='sajad',
+	description='Lib Sajad SxTeam',
+	packages=find_packages(),
+	classifiers=['Programming Language :: Python :: 3']
+)
