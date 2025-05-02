7384d31d977ca4ae3a58d60d4820daf094045a3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-02 04:03:23 +0000

    Add package ephor-cli/0.3.4/ephor_cli-0.3.4.tar.gz

---
+++ packages/ephor-cli/ephor_cli-0.3.4.tar.gz/ephor_cli-0.3.4/src/ephor_cli/constant.py
@@ -0,0 +1,7 @@
+import os
+
+API_SERVER_URL = os.getenv("API_SERVER_URL", "https://mcp-hive.ti.trilogy.com/api")
+# API_SERVER_URL = os.getenv("API_SERVER_URL", "http://localhost:3000/api")
+
+AGENT_SERVER_URL = os.getenv("AGENT_SERVER_URL", "https://agents.ti.trilogy.com")
+# AGENT_SERVER_URL = os.getenv("AGENT_SERVER_URL", "http://localhost:10002")
