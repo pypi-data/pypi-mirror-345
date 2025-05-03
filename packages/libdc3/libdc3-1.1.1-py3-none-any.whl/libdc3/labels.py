c39e4c6c01cd472fcb1a393e50e2f7b1aedb6032
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-03 04:04:47 +0000

    Add package libdc3/1.1.1/libdc3-1.1.1-py3-none-any.whl

---
+++ packages/libdc3/libdc3-1.1.1-py3-none-any.whl/libdc3/labels.py
@@ -0,0 +1,23 @@
+LATEX_UNITS = {
+    "/b": "$\mathbf{b}^{-1}$",
+    "/mb": "$\mathbf{mb}^{-1}$",
+    "/ub": "$\mu\mathbf{b}^{-1}$",
+    "/nb": "$\mathbf{nb}^{-1}$",
+    "/pb": "$\mathbf{pb}^{-1}$",
+    "/fb": "$\mathbf{fb}^{-1}$",
+    "Hz/b": "$\mathbf{Hz/b}$",
+    "Hz/mb": "$\mathbf{Hz/mb}$",
+    "Hz/ub": "$\mathbf{Hz/}\mathbf{\mu}\mathbf{b}$",
+    "Hz/nb": "$\mathbf{Hz/nb}$",
+    "Hz/pb": "$\mathbf{Hz/pb}$",
+    "Hz/fb": "$\mathbf{Hz/fb}$",
+}
+
+PARTICLE_TYPES = {"PROTPHYS": "pp", "IONPHYS": "PbPb", "PAPHYS": "pPb", "ALLIONS": "PbPb+pPb"}
+
+SQRT_S = {
+    "IONPHYS": "$\mathbf{\sqrt{s_{NN}} =}$",
+    "PROTPHYS": "$\mathbf{\sqrt{s} =}$",
+    "PAPHYS": "$\mathbf{\sqrt{s} =}$",
+    "ALLIONS": "$\mathbf{\sqrt{s} =}$",
+}
