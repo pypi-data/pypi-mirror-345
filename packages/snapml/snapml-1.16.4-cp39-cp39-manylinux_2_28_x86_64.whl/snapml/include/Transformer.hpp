4a3a0139ee25f8ed016a7a900ad997a48ad6e077
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-03 03:19:52 +0000

    Add package snapml/1.16.4/snapml-1.16.4-cp39-cp39-manylinux_2_28_x86_64.whl

---
+++ packages/snapml/snapml-1.16.4-cp39-cp39-manylinux_2_28_x86_64.whl/snapml/include/Transformer.hpp
@@ -0,0 +1,22 @@
+/*********************************************************************
+ *
+ * Licensed Materials - Property of IBM
+ *
+ * (C) Copyright IBM Corp. 2022, 2023. All Rights Reserved.
+
+ * US Government Users Restricted Rights - Use, duplication or
+ * disclosure restricted by GSA ADP Schedule Contract with IBM Corp.
+ *
+ ********************************************************************/
+
+namespace snapml {
+
+//! @ingroup c-api
+class Transformer {
+public:
+    virtual void transform(snapml::AnyDataset& dataset) = 0;
+    virtual ~Transformer()                              = default;
+    bool index_list_valid(std::set<uint32_t>& index_list, uint32_t max);
+};
+
+}
