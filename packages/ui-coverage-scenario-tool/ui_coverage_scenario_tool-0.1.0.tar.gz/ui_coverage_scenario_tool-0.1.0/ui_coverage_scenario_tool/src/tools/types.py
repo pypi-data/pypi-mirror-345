27c7619e7ff3b06f8fe7034993f22a483d961bcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-03 16:04:42 +0000

    Add package ui-coverage-scenario-tool/0.1.0/ui_coverage_scenario_tool-0.1.0.tar.gz

---
+++ packages/ui-coverage-scenario-tool/ui_coverage_scenario_tool-0.1.0.tar.gz/ui_coverage_scenario_tool-0.1.0/ui_coverage_scenario_tool/src/tools/types.py
@@ -0,0 +1,9 @@
+from typing import NewType
+
+AppKey = NewType('AppKey', str)
+AppName = NewType('AppName', str)
+
+Selector = NewType('Selector', str)
+SelectorKey = NewType('SelectorKey', str)
+
+ScenarioName = NewType('ScenarioName', str)
