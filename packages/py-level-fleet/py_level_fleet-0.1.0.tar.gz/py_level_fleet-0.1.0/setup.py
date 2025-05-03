7afe2f30cd366e9986eb51d9c0cc9a8c059467dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-03 03:20:07 +0000

    Add package py-level-fleet/0.1.0/py_level_fleet-0.1.0.tar.gz

---
+++ packages/py-level-fleet/py_level_fleet-0.1.0.tar.gz/py_level_fleet-0.1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='py-level-fleet',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=['beautifulsoup4', 'jinja2'],
+    description='A Vue.js-like templating engine using Jinja2',
+    author='Vishnu prasad',
+    author_email='vishnuprasadowe@gmail.com',
+    url='https://github.com/py-level/fleet',
+)
