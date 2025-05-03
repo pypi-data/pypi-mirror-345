ca0cd60601c161f4f25b0e8e4b11e7d3a515e651
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-03 16:04:46 +0000

    Add package PowerText/0.5.0/powertext-0.5.0-py3-none-any.whl

---
+++ packages/PowerText/powertext-0.5.0-py3-none-any.whl/power_text/__init__.py
@@ -0,0 +1,11 @@
+# Licensed under the Apache License: http://www.apache.org/licenses/LICENSE-2.0
+# For details: https://github.com/xiaosuyyds/PowerText/blob/master/NOTICE
+
+__version__ = "0.5.0"
+
+from .power_text import Font, draw_text
+
+__all__ = [
+    "Font",
+    "draw_text"
+]
