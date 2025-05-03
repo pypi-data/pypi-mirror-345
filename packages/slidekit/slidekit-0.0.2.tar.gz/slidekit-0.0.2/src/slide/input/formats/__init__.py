ce4a4c823225db449ac7104ed58b625317217fdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-03 05:04:35 +0000

    Add package slidekit/0.0.2/slidekit-0.0.2.tar.gz

---
+++ packages/slidekit/slidekit-0.0.2.tar.gz/slidekit-0.0.2/src/slide/input/formats/__init__.py
@@ -0,0 +1,15 @@
+"""
+slide/input/formats
+~~~~~~~~~~~~~~~~~~~
+"""
+
+from slide.input.formats.load_array import LoadArray
+from slide.input.formats.load_csv import LoadCSV
+from slide.input.formats.load_cytoscape import LoadCytoscape
+from slide.input.formats.load_dataframe import LoadDataFrame
+from slide.input.formats.load_dict import LoadDict
+from slide.input.formats.load_excel import LoadExcel
+from slide.input.formats.load_gpickle import LoadGPickle
+from slide.input.formats.load_json import LoadJSON
+from slide.input.formats.load_networkx import LoadNetworkX
+from slide.input.formats.load_tsv import LoadTSV
