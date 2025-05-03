505040d68c00bfbba3fe7ce5296c1544aef1b177
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-03 15:04:21 +0000

    Add package file-dir-backup/0.1.1/file_dir_backup-0.1.1.tar.gz

---
+++ packages/file-dir-backup/file_dir_backup-0.1.1.tar.gz/file_dir_backup-0.1.1/file_dir_backup/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from .core import backup
+
+__version__ = '0.1.1'
+__author__ = 'hjl'
+__description__ = 'A simple backup tool'
+__license__ = 'MIT'
+__copyright__ = 'Copyright (c) 2023 hjl'
+__email__ = '1878324764@qq.com'
