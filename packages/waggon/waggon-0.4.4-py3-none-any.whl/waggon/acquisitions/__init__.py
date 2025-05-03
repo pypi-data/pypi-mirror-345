90e548c293898fd45bcd9ff07cf0cdf57db09759
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-03 03:19:26 +0000

    Add package waggon/0.4.4/waggon-0.4.4-py3-none-any.whl

---
+++ packages/waggon/waggon-0.4.4-py3-none-any.whl/waggon/acquisitions/__init__.py
@@ -0,0 +1,15 @@
+from .base import Acquisition
+from .acquisition import EI, CB, WU, WU_IDW, PI, ES, KG, BarycentreEI, GPBarycentreEI
+
+__all__ = [
+    'Acquisition',
+    'EI',
+    'CB',
+    'WU',
+    'WU_IDW',
+    'PI',
+    'ES',
+    'KG',
+    'BarycentreEI',
+    'GPBarycentreEI'
+]
