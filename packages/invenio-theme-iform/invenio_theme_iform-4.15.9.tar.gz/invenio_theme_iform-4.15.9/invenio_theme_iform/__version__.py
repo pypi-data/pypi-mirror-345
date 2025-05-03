37568bb76c14d72aca89ff05d2c31486569911ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-03 05:05:34 +0000

    Add package invenio-theme-iform/4.15.9/invenio_theme_iform-4.15.9.tar.gz

---
+++ packages/invenio-theme-iform/invenio_theme_iform-4.15.9.tar.gz/invenio_theme_iform-4.15.9/invenio_theme_iform/__version__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2025 I-Form Advanced Manufacturing Research Centre.
+#
+# invenio-theme-iform is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file for more
+# details.
+
+"""Version data for this python module."""
+
+version = "4.15.9"
