bb68f7700c30a074ed3f7106b77e5a520bfe0f60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-03 07:04:44 +0000

    Add package wisdem/3.21.0/wisdem-3.21.0-cp312-cp312-musllinux_1_2_aarch64.whl

---
+++ packages/wisdem/wisdem-3.21.0-cp312-cp312-musllinux_1_2_aarch64.whl/wisdem/test/test_gluecode/test_gc_modified_yaml.py
@@ -0,0 +1,38 @@
+"""
+A test where we can modify different parts of the modeling_options to ensure
+they work with a top-to-bottom WISDEM run.
+"""
+
+import os
+import unittest
+
+from wisdem.glue_code.runWISDEM import run_wisdem
+
+test_dir = (
+    os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__)))))
+    + os.sep
+    + "examples"
+    + os.sep
+    + "02_reference_turbines"
+    + os.sep
+)
+fname_analysis_options = test_dir + "analysis_options.yaml"
+this_dir = os.path.dirname(os.path.realpath(__file__))
+fname_modeling_options = this_dir + os.sep + "modified_modeling_options.yaml"
+
+
+class TestRegression(unittest.TestCase):
+    def test15MW(self):
+        ## IEA 15MW
+        fname_wt_input = test_dir + "IEA-15-240-RWT.yaml"
+        wt_opt, _, _ = run_wisdem(fname_wt_input, fname_modeling_options, fname_analysis_options)
+
+        self.assertAlmostEqual(wt_opt["rotorse.blade_mass"][0], 68208.64259481194, 1) # new value: improved interpolation
+        self.assertAlmostEqual(wt_opt["rotorse.rp.AEP"][0] * 1.0e-6, 78.17449814864315, 1)
+        self.assertAlmostEqual(wt_opt["financese.lcoe"][0] * 1.0e3, 76.90528582538761, 1)
+        self.assertAlmostEqual(wt_opt["rotorse.rs.tip_pos.tip_deflection"][0], 25.93986529952085, 1)
+        self.assertAlmostEqual(wt_opt["towerse.z_param"][-1], 144.386, 3)
+
+
+if __name__ == "__main__":
+    unittest.main()
