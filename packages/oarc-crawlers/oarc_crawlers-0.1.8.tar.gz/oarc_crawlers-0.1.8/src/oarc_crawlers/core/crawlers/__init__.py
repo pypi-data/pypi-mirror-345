d56ff236df92407a5ebfcf8486a7253f2da9ff5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-03 07:03:49 +0000

    Add package oarc-crawlers/0.1.8/oarc_crawlers-0.1.8.tar.gz

---
+++ packages/oarc-crawlers/oarc_crawlers-0.1.8.tar.gz/oarc_crawlers-0.1.8/src/oarc_crawlers/core/crawlers/__init__.py
@@ -0,0 +1,18 @@
+"""OARC-Crawlers core crawlers package.
+
+This package contains various specialized crawlers for different data sources.
+"""
+
+from .arxiv_crawler import ArxivCrawler
+from .ddg_crawler import DDGCrawler
+from .gh_crawler import GHCrawler
+from .web_crawler import WebCrawler
+from .yt_crawler import YTCrawler
+
+__all__ = [
+    "ArxivCrawler",
+    "DDGCrawler",
+    "GHCrawler",
+    "WebCrawler",
+    "YTCrawler",
+]
