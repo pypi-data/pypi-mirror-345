7d47240c5f8e32fdef2a3db0a703a63d164f0283
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-03 05:04:32 +0000

    Add package byzh-rc/1.3.33/byzh_rc-1.3.33-py3-none-any.whl

---
+++ packages/byzh-rc/byzh_rc-1.3.33-py3-none-any.whl/byzh_rc/B_deeplearning/B_utils/__init__.py
@@ -0,0 +1,6 @@
+from .grid_train import grid_trains_1d, grid_trains_2d
+from .get_device import get_device
+from .get_params import get_params
+from .get_flops import get_flops
+
+all = ['grid_trains_1d', 'grid_trains_2d', 'get_device', 'get_params', 'get_flops']
