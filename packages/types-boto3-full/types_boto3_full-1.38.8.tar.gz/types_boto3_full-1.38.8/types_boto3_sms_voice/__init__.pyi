8c7a8b70e310e7c76defc1fe557bc8806ea98287
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-03 14:04:38 +0000

    Add package types-boto3-full/1.38.8/types_boto3_full-1.38.8.tar.gz

---
+++ packages/types-boto3-full/types_boto3_full-1.38.8.tar.gz/types_boto3_full-1.38.8/types_boto3_sms_voice/__init__.pyi
@@ -0,0 +1,26 @@
+"""
+Main interface for sms-voice service.
+
+[Documentation](https://youtype.github.io/types_boto3_docs/types_boto3_sms_voice/)
+
+Copyright 2025 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from boto3.session import Session
+    from types_boto3_sms_voice import (
+        Client,
+        SMSVoiceClient,
+    )
+
+    session = Session()
+    client: SMSVoiceClient = session.client("sms-voice")
+    ```
+"""
+
+from .client import SMSVoiceClient
+
+Client = SMSVoiceClient
+
+__all__ = ("Client", "SMSVoiceClient")
