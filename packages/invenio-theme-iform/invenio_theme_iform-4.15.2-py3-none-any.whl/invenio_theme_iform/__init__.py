96e1e73e7d8b1da78ccde684f58dcc09c46273b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-03 03:19:36 +0000

    Add package invenio-theme-iform/4.15.2/invenio_theme_iform-4.15.2-py3-none-any.whl

---
+++ packages/invenio-theme-iform/invenio_theme_iform-4.15.2-py3-none-any.whl/invenio_theme_iform/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2020-2022 Graz University of Technology.
+#
+# invenio-theme-iform is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file for more
+# details.
+
+"""invenio module for I-Form theme."""
+
+from .__version__ import version as __version__
+from .ext import InvenioThemeIform
+
+
+__all__ = ("__version__", "InvenioThemeIform")
