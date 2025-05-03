eb7c61b1a899b1981772a9a0c88062b3501752dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-03 15:04:42 +0000

    Add package pyoneclick/0.0.3/pyoneclick-0.0.3.tar.gz

---
+++ packages/pyoneclick/pyoneclick-0.0.3.tar.gz/pyoneclick-0.0.3/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pyoneclick",
+    version="0.0.3",
+    author="zpjbtdjm",
+    description="This tool enables the automatic filtering of control variables, "
+                "similar to Stata's 'oneclick' command, while offering enhanced functionality.",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Microsoft :: Windows",
+    ],
+    install_requires=[
+        "stata_setup",
+        "tqdm"
+    ],
+    entry_points={
+        "console_scripts": [
+            "pyoneclick=src.main:main",
+        ],
+    },
+)
