f7141d7d0198b4957626706ac14ea12d5f43aab5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-03 07:03:55 +0000

    Add package sinapsis-framework-converter/0.1.4/sinapsis_framework_converter-0.1.4-py3-none-any.whl

---
+++ packages/sinapsis-framework-converter/sinapsis_framework_converter-0.1.4-py3-none-any.whl/sinapsis_framework_converter/templates/onnx_trt_converter_template.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+from typing import cast
+
+from sinapsis_framework_converter.framework_converter.framework_converter_trt import (
+    FrameworkConverterTRT,
+)
+from sinapsis_framework_converter.templates.framework_converter_base import (
+    FrameworkConverterBase,
+)
+
+
+class ONNXTRTConverter(FrameworkConverterBase):
+    """Template to convert between ONNX and TensorRT frameworks
+    This module inherits its functionality from the base template
+    FrameworkConverterBase.
+
+    Usage example:
+
+    agent:
+      name: my_test_agent
+    templates:
+    - template_name: InputTemplate
+      class_name: InputTemplate
+      attributes: {}
+    - template_name: ONNXTRTConverter
+      class_name: ONNXTRTConverter
+      template_input: InputTemplate
+      attributes:
+        model_name: 'name_of_the_model_to_convert'
+        save_model_path: false
+        force_compilation: true
+
+
+    """
+
+    _EXPORTER = FrameworkConverterTRT
+
+    def load_model(self) -> None:
+        """It returns None as there is no model to be
+        loaded prior to the export"""
+
+    def convert_model(self) -> None:
+        """Converts model using export_onnx_to_trt method from FrameworkConverterTRT"""
+        self.exporter = cast(FrameworkConverterTRT, self.exporter)
+        self.exporter.export_onnx_to_trt()
---
+++ packages/sinapsis-framework-converter/sinapsis_framework_converter-0.1.4-py3-none-any.whl/sinapsis_framework_converter/templates/tf_onnx_converter_template.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+from typing import Optional, cast
+
+from sinapsis_framework_converter.framework_converter.framework_converter_tf import (
+    FrameworkConverterTFONNX,
+)
+from sinapsis_framework_converter.templates.framework_converter_base import (
+    FrameworkConverterAttributes,
+    FrameworkConverterBase,
+)
+
+
+class TensorFlowONNXConverter(FrameworkConverterBase):
+    """Template to convert from TensorFlow to ONNX
+
+
+    Usage example:
+
+    agent:
+      name: my_test_agent
+    templates:
+    - template_name: InputTemplate
+      class_name: InputTemplate
+      attributes: {}
+    - template_name: ONNXTRTConverter
+      class_name: ONNXTRTConverter
+      template_input: InputTemplate
+      attributes:
+        model_name: 'name_of_the_model_to_convert'
+        save_model_path: false
+        force_compilation: true
+        opset_version: null
+
+    """
+
+    _EXPORTER = FrameworkConverterTFONNX
+
+    class AttributesBaseModel(FrameworkConverterAttributes):
+        """Attributes for TensorFlowONNXConverter template.
+
+        opset_version (Optional[int]) : The operator set version for ONNX."""
+
+        opset_version: Optional[int] = None
+
+    def load_model(self) -> None:
+        """It returns None as there is no model to be
+        loaded prior to the export"""
+
+    def convert_model(self) -> None:
+        """Method to convert the model using the export_tensorflow_to_onxx method
+        from FrameworkConverterTFONNX
+        """
+        self.exporter = cast(FrameworkConverterTFONNX, self.exporter)
+        self.exporter.export_tensorflow_to_onnx(self.attributes.opset_version)
