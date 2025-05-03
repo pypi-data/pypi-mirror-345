ed817b554b07636ebefb39184b655af3c168ba4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-03 07:04:33 +0000

    Add package vector2dggs/0.8.0/vector2dggs-0.8.0.tar.gz

---
+++ packages/vector2dggs/vector2dggs-0.8.0.tar.gz/vector2dggs-0.8.0/vector2dggs/katana.py
@@ -0,0 +1,75 @@
+"""
+Copyright (c) 2016, Joshua Arnott
+
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS “AS IS” AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+"""
+
+from shapely.geometry import (
+    box,
+    Polygon,
+    MultiPolygon,
+    LineString,
+    MultiLineString,
+    GeometryCollection,
+)
+from shapely.validation import explain_validity, make_valid
+
+
+def katana(geometry, threshold, count=0) -> GeometryCollection:
+    """
+    Split a geometry into two parts across its shortest dimension.
+    Invalid input `geometry` will silently be made valid (if possible).
+    """
+    if geometry is None:
+        # Empty geometry collection
+        return GeometryCollection([])
+    if not geometry.is_valid:
+        # print(explain_validity(geometry))
+        geometry = make_valid(geometry)
+        if geometry.geom_type == "GeometryCollection":
+            geometry.normalize()
+        geometry = geometry.buffer(0)
+    bounds = geometry.bounds
+    width = bounds[2] - bounds[0]
+    height = bounds[3] - bounds[1]
+    if max(width, height) <= threshold or count == 250:
+        # either the polygon is smaller than the threshold, or the maximum
+        # number of recursions has been reached
+        return [geometry]
+    if height >= width:
+        # split left to right
+        a = box(bounds[0], bounds[1], bounds[2], bounds[1] + height / 2)
+        b = box(bounds[0], bounds[1] + height / 2, bounds[2], bounds[3])
+    else:
+        # split top to bottom
+        a = box(bounds[0], bounds[1], bounds[0] + width / 2, bounds[3])
+        b = box(bounds[0] + width / 2, bounds[1], bounds[2], bounds[3])
+    result = []
+    for d in (
+        a,
+        b,
+    ):
+        c = geometry.intersection(d)
+        if not isinstance(c, GeometryCollection):
+            c = GeometryCollection([c])
+        for e in c.geoms:
+            if isinstance(e, (Polygon, MultiPolygon, LineString, MultiLineString)):
+                result.extend(katana(e, threshold, count + 1))
+    if count > 0:
+        return result
+    # convert multipart into singlepart
+    final_result = []
+    for g in result:
+        # if isinstance(g, MultiPolygon):
+        #     final_result.extend(g)
+        # else:
+        # final_result.append(g)
+        final_result.append(g)
+    return final_result
