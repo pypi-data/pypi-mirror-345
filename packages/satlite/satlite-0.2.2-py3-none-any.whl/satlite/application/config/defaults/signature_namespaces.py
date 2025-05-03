8f80e09a49c4466deaecb9f1c2e5ecd82b30ce37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-03 14:04:41 +0000

    Add package satlite/0.2.2/satlite-0.2.2-py3-none-any.whl

---
+++ packages/satlite/satlite-0.2.2-py3-none-any.whl/satlite/application/config/defaults/signature_namespaces.py
@@ -0,0 +1,22 @@
+from typing import Any
+from uuid import UUID
+
+from litestar.contrib.jwt import OAuth2Login
+from litestar.enums import RequestEncodingType
+from litestar.security.jwt import Token
+
+
+def get_default_signature_namespaces() -> dict[str, Any]:
+    return {
+        'Token': Token,
+        'OAuth2Login': OAuth2Login,
+        'RequestEncodingType': RequestEncodingType,
+        'UUID': UUID,
+        # 'Body': Body,
+        # 'm': m,
+        # 'UserService': UserService,
+        # 'RoleService': RoleService,
+        # 'TeamService': TeamService,
+        # 'TeamMemberService': TeamMemberService,
+        # 'UserRoleService': UserRoleService,
+    }
