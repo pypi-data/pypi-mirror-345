56acfb89e446459d822629523287121c2c6c6839
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-03 15:04:15 +0000

    Add package LeityGram/0.3.1/LeityGram-0.3.1.tar.gz

---
+++ packages/LeityGram/LeityGram-0.3.1.tar.gz/LeityGram-0.3.1/LeityGram/__init__.py
@@ -0,0 +1,6 @@
+from .bot import LeityBot
+from .types import Message, User, Chat
+from .keyboards import Keyboard, Button
+from .storage import SQLiteStorage
+
+__all__ = ['LeityBot', 'Message', 'User', 'Chat', 'Keyboard', 'Button', 'SQLiteStorage']
---
+++ packages/LeityGram/LeityGram-0.3.1.tar.gz/LeityGram-0.3.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="LeityGram",
+    version="0.3.1",
+    packages=find_packages(),
+)
