104b694dc5e1b1cbc635b6157a495969255e7f87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-03 15:04:20 +0000

    Add package autolibs/0.1.0/autolibs-0.1.0.tar.gz

---
+++ packages/autolibs/autolibs-0.1.0.tar.gz/autolibs-0.1.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='autolibs',
+    version='0.1.0',
+    description='Auto-import numpy, pandas, matplotlib, seaborn, and other libraries',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='HARI_PRASAD_S',
+    author_email='hariprasad2k06@gmail.com',
+    url='https://github.com/hsnaidu/autolibs',
+    packages=find_packages(),
+    license='MIT',
+    install_requires=[
+        'numpy',
+        'pandas',
+        'matplotlib',
+        'seaborn',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',
+)
