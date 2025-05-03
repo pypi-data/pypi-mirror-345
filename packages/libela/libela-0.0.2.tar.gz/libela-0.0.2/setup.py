992b11fb3d6c0fd12874728e03f04d8112737d8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-03 05:04:35 +0000

    Add package libela/0.0.2/libela-0.0.2.tar.gz

---
+++ packages/libela/libela-0.0.2.tar.gz/libela-0.0.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+setup(
+    name='libela',  # Replace with the desired package name
+    version='0.0.2',
+    packages=find_packages(),
+    description='A brief description of your package',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Arya Amiri',
+    author_email='aryamiri@outlook.com',
+    url='https://lib-ela.org',  # Optional
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',  # Adjust the license type
+        'Operating System :: OS Independent',
+    ],
+)
