b556c48f5dff995350e67d5f8136d877312f14b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-03 16:04:48 +0000

    Add package ncnn/1.0.20250503/ncnn-1.0.20250503-cp36-cp36m-win32.whl

---
+++ packages/ncnn/ncnn-1.0.20250503-cp36-cp36m-win32.whl/ncnn/__init__.py
@@ -0,0 +1,46 @@
+
+
+""""""# start delvewheel patch
+def _delvewheel_init_patch_1_1_4():
+    import ctypes
+    import os
+    import platform
+    import sys
+    libs_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, 'ncnn.libs'))
+    is_pyinstaller = getattr(sys, 'frozen', False) and hasattr(sys, '_MEIPASS')
+    is_conda_cpython = platform.python_implementation() == 'CPython' and (hasattr(ctypes.pythonapi, 'Anaconda_GetVersion') or 'packaged by conda-forge' in sys.version)
+    if sys.version_info[:2] >= (3, 8) and not is_conda_cpython or sys.version_info[:2] >= (3, 10):
+        if not is_pyinstaller or os.path.isdir(libs_dir):
+            os.add_dll_directory(libs_dir)
+    else:
+        load_order_filepath = os.path.join(libs_dir, '.load-order-ncnn-1.0.20250503')
+        if not is_pyinstaller or os.path.isfile(load_order_filepath):
+            with open(os.path.join(libs_dir, '.load-order-ncnn-1.0.20250503')) as file:
+                load_order = file.read().split()
+            for lib in load_order:
+                lib_path = os.path.join(os.path.join(libs_dir, lib))
+                if not is_pyinstaller or os.path.isfile(lib_path):
+                    ctypes.WinDLL(lib_path)
+
+
+_delvewheel_init_patch_1_1_4()
+del _delvewheel_init_patch_1_1_4
+# end delvewheel patch
+
+# Tencent is pleased to support the open source community by making ncnn available.
+#
+# Copyright (C) 2020 THL A29 Limited, a Tencent company. All rights reserved.
+#
+# Licensed under the BSD 3-Clause License (the "License"); you may not use this file except
+# in compliance with the License. You may obtain a copy of the License at
+#
+# https://opensource.org/licenses/BSD-3-Clause
+#
+# Unless required by applicable law or agreed to in writing, software distributed
+# under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+# CONDITIONS OF ANY KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations under the License.
+
+from .ncnn import *
+
+__version__ = ncnn.__version__
