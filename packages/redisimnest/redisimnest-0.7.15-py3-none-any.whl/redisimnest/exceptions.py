64059640da53d1f148a009c04d7fbb64bd4b4912
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-03 06:04:33 +0000

    Add package redisimnest/0.7.15/redisimnest-0.7.15-py3-none-any.whl

---
+++ packages/redisimnest/redisimnest-0.7.15-py3-none-any.whl/redisimnest/exceptions.py
@@ -0,0 +1,21 @@
+class MissingParameterError(Exception):
+    pass
+
+class InvalidPrefixTemplateError(Exception):
+    pass
+
+class ParameterValidationError(ValueError): 
+    pass
+
+class KeyTypeValidationError(TypeError):
+    pass
+
+class MissingEncryptionKeyError(Exception):
+    pass
+
+class MissingDependencyError(ImportError):
+    pass
+
+
+class AccessDeniedError(Exception):
+    pass
