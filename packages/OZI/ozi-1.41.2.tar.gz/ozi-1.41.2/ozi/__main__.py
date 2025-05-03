7b0086b15eeb186fe3f5e213b187fc226d0feeee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-03 15:04:26 +0000

    Add package OZI/1.41.2/ozi-1.41.2.tar.gz

---
+++ packages/OZI/ozi-1.41.2.tar.gz/ozi-1.41.2/ozi/__main__.py
@@ -0,0 +1,14 @@
+# ozi/__main__.py
+# Part of the OZI Project, under the Apache License v2.0 with LLVM Exceptions.
+# See LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+"""``ozi`` console application."""  # pragma: no cover
+
+from ozi_core.__main__ import main  # pyright: ignore
+from ozi_core.__main__ import setup_parser  # pyright: ignore
+
+from ozi import __version__
+
+if __name__ == '__main__':
+    setup_parser(__version__)
+    main()  # pyright: ignore
