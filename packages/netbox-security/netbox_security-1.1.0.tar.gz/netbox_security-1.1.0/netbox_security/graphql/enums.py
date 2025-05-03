f14b87c564d777edd96b64ade0b1cee7153399f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-03 03:19:37 +0000

    Add package netbox-security/1.1.0/netbox_security-1.1.0.tar.gz

---
+++ packages/netbox-security/netbox_security-1.1.0.tar.gz/netbox_security-1.1.0/netbox_security/graphql/enums.py
@@ -0,0 +1,43 @@
+import strawberry
+
+from netbox_security.choices import (
+    FamilyChoices,
+    FirewallRuleFromSettingChoices,
+    FirewallRuleThenSettingChoices,
+    PoolTypeChoices,
+    RuleDirectionChoices,
+    NatTypeChoices,
+    RuleStatusChoices,
+    CustomInterfaceChoices,
+    AddressTypeChoices,
+    ActionChoices,
+)
+
+__all__ = (
+    "NetBoxSecurityFamilyEnum",
+    "NetBoxSecurityFirewallRuleFromSettingEnum",
+    "NetBoxSecurityFirewallRuleThenSettingEnum",
+    "NetBoxSecurityPoolTypeEnum",
+    "NetBoxSecurityRuleDirectionEnum",
+    "NetBoxSecurityNatTypeEnum",
+    "NetBoxSecurityRuleStatusEnum",
+    "NetBoxSecurityCustomInterfaceEnum",
+    "NetBoxSecurityAddressTypeEnum",
+    "NetBoxSecurityActionEnum",
+)
+
+
+NetBoxSecurityFamilyEnum = strawberry.enum(FamilyChoices.as_enum())
+NetBoxSecurityFirewallRuleFromSettingEnum = strawberry.enum(
+    FirewallRuleFromSettingChoices.as_enum()
+)
+NetBoxSecurityFirewallRuleThenSettingEnum = strawberry.enum(
+    FirewallRuleThenSettingChoices.as_enum()
+)
+NetBoxSecurityPoolTypeEnum = strawberry.enum(PoolTypeChoices.as_enum())
+NetBoxSecurityRuleDirectionEnum = strawberry.enum(RuleDirectionChoices.as_enum())
+NetBoxSecurityNatTypeEnum = strawberry.enum(NatTypeChoices.as_enum())
+NetBoxSecurityRuleStatusEnum = strawberry.enum(RuleStatusChoices.as_enum())
+NetBoxSecurityCustomInterfaceEnum = strawberry.enum(CustomInterfaceChoices.as_enum())
+NetBoxSecurityAddressTypeEnum = strawberry.enum(AddressTypeChoices.as_enum())
+NetBoxSecurityActionEnum = strawberry.enum(ActionChoices.as_enum())
