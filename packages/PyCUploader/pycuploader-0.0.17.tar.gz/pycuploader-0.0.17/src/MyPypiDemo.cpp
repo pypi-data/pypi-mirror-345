df0cb71074afe8c5dba58e547e30153295623bad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-03 05:04:31 +0000

    Add package PyCUploader/0.0.17/pycuploader-0.0.17.tar.gz

---
+++ packages/PyCUploader/pycuploader-0.0.17.tar.gz/pycuploader-0.0.17/src/MyPypiDemo.cpp
@@ -0,0 +1,17 @@
+/***************************
+@Author: Chunel
+@Contact: chunel@foxmail.com
+@File: MyPypiDemo.cpp
+@Time: 2025/5/2 14:50
+@Desc: 
+***************************/
+
+//
+// Created by Chunel on 2025/5/2.
+//
+
+#include "MyPypiDemo.h"
+
+int MyPypiDemo::add(int a, int b) {
+    return a + b;
+}
