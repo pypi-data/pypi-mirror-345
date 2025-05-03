5095c09f5df2818216d71dc24c444908cc188bcb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-03 14:04:49 +0000

    Add package aioqzone/1.9.5/aioqzone-1.9.5.tar.gz

---
+++ packages/aioqzone/aioqzone-1.9.5.tar.gz/aioqzone-1.9.5/src/aioqzone/message.py
@@ -0,0 +1,33 @@
+import typing as t
+
+from qqqr.message import *
+from tylisten import hookdef
+
+__all__ = [
+    "qr_cancelled",
+    "qr_fetched",
+    "sms_code_input",
+    "solve_select_captcha",
+    "solve_slide_captcha",
+    "login_success",
+    "login_failed",
+]
+
+
+@hookdef
+def login_success(uin: int) -> t.Any:
+    """Login success.
+
+    :param uin: login uin
+    """
+    ...
+
+
+@hookdef
+def login_failed(uin: int, exc: t.Union[BaseException, str]) -> t.Any:
+    """Login failed.
+
+    :param uin: login uin
+    :param exc: exception or error message
+    """
+    ...
