646a80fff646268abe630e11f080b748e2ee7cbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-03 05:04:32 +0000

    Add package dv-flow-mgr/1.6.14799059072rc0/dv_flow_mgr-1.6.14799059072rc0-py3-none-any.whl

---
+++ packages/dv-flow-mgr/dv_flow_mgr-1.6.14799059072rc0-py3-none-any.whl/dv_flow/mgr/config.py
@@ -0,0 +1,7 @@
+
+import dataclasses as dc
+from typing import List
+
+@dc.dataclass
+class Config(object):
+    overrides : List
---
+++ packages/dv-flow-mgr/dv_flow_mgr-1.6.14799059072rc0-py3-none-any.whl/dv_flow/mgr/config_def.py
@@ -0,0 +1,33 @@
+
+import pydantic.dataclasses as pdc
+from pydantic import BaseModel
+from typing import List, Union, Any
+from .extend_def import ExtendDef
+from .param_def import ParamDef
+
+class OverrideDef(BaseModel):
+    """Override definition"""
+    package : Union[str, None] = pdc.Field(
+        description="Package to override")
+    task : Union[str, None] = pdc.Field(
+        description="Task to override")
+    value : str = pdc.Field(
+        description="Override to use",
+        alias="with")
+
+class ConfigDef(BaseModel):
+    name : str = pdc.Field(
+        description="Name of the configuration")
+    params : List[ParamDef] = pdc.Field(
+        default_factory=list,
+        description="List of configuration parameters",
+        alias="with")
+    uses : str = pdc.Field(
+        default=None,
+        description="Name of the configuration to use as a base")
+    overrides : List[OverrideDef] = pdc.Field(
+        default_factory=list,
+        description="List of package overrides")
+    extensions : List[ExtendDef] = pdc.Field(
+        default_factory=list,
+        description="List of extensions to apply")
