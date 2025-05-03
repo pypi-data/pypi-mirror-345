20f424d0f2a089eb43bae800ad0eefafdd81185c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-03 06:04:53 +0000

    Add package pytauri-plugin-notification/0.5.0/pytauri_plugin_notification-0.5.0.tar.gz

---
+++ packages/pytauri-plugin-notification/pytauri_plugin_notification-0.5.0.tar.gz/pytauri_plugin_notification-0.5.0/src/pytauri_plugin_notification/__init__.py
@@ -0,0 +1,13 @@
+"""[tauri_plugin_notification::self](https://docs.rs/tauri-plugin-notification/latest/tauri_plugin_notification/index.html)"""
+
+from pytauri_plugin_notification.ffi import (
+    ImplNotificationExt,
+    NotificationBuilder,
+    NotificationExt,
+)
+
+__all__ = [
+    "ImplNotificationExt",
+    "NotificationBuilder",
+    "NotificationExt",
+]
