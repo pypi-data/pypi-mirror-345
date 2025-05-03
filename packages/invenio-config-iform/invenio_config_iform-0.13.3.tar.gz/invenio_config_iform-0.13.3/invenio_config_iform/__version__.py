171fed9d5be10c1dcfe5c1ce3709654600b34a6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-03 03:20:03 +0000

    Add package invenio-config-iform/0.13.3/invenio_config_iform-0.13.3.tar.gz

---
+++ packages/invenio-config-iform/invenio_config_iform-0.13.3.tar.gz/invenio_config_iform-0.13.3/invenio_config_iform/__version__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2025 I-Form Advanced Manufacturing Research Centre.
+#
+# invenio-config-iform is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file for more
+# details.
+
+"""Version data for this python module."""
+
+version = "0.13.3"
