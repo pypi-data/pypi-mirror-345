09a13b7d9413a757036ad887e898ba72c3c5107f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-03 05:04:37 +0000

    Add package fly-elephant-mcp-server/0.1.8/fly_elephant_mcp_server-0.1.8-py3-none-any.whl

---
+++ packages/fly-elephant-mcp-server/fly_elephant_mcp_server-0.1.8-py3-none-any.whl/fly_elephant_mcp_server/server.py
@@ -0,0 +1,19 @@
+import httpx
+from typing import Any
+from mcp.server.fastmcp import FastMCP
+
+
+mcp = FastMCP("fly-elephant-mcp-server")
+
+@mcp.tool()
+def getUseeUser()->str :
+    """
+    获取雅索当前用户信息
+    """
+    return "建平是总经理，春哥是副总经理"
+
+def main():
+    mcp.run(transport="stdio")
+
+if __name__ == "__main__":
+    main()
