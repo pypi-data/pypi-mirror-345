927786f86c3556a37ea7f0d0ecb69c9e0b656dee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-03 04:05:09 +0000

    Add package hogql-parser/1.1.2/hogql_parser-1.1.2-cp311-cp311-macosx_13_0_arm64.whl

---
+++ packages/hogql-parser/hogql_parser-1.1.2-cp311-cp311-macosx_13_0_arm64.whl/hogql_parser-stubs/__init__.pyi
@@ -0,0 +1,51 @@
+from posthog.hogql.ast import SelectQuery, SelectSetQuery, Program, CreateQuery
+from posthog.hogql.base import AST
+
+def parse_expr(expr: str, /, *, is_internal: bool = False) -> AST:
+    """Parse the HogQL expression string into an AST.
+
+    If the expr `is_internal`, spans and notices won't be included in the AST.
+    """
+    ...
+
+def parse_order_expr(expr: str, /, *, is_internal: bool = False) -> AST:
+    """Parse the ORDER BY clause string into an AST.
+
+    If the expr `is_internal`, spans and notices won't be included in the AST.
+    """
+    ...
+
+def parse_select(expr: str, /, *, is_internal: bool = False) -> SelectQuery | SelectSetQuery:
+    """Parse the HogQL SELECT statement string into an AST.
+
+    If the expr `is_internal`, spans and notices won't be included in the AST.
+    """
+    ...
+
+def parse_full_template_string(expr: str, /, *, is_internal: bool = False) -> AST:
+    """Parse a Hog template string into an AST.
+
+    If the expr `is_internal`, spans and notices won't be included in the AST.
+    """
+    ...
+
+def parse_string_literal_text(value: str, /) -> str:
+    """Unquote the string (an identifier or a string literal).
+
+    If the expr is `internal`, spans and notices won't be included in the AST.
+    """
+    ...
+
+def parse_program(source: str, /, *, is_internal: bool = False) -> Program:
+    """Parse a Hog program.
+
+    If the expr `is_internal`, spans and notices won't be included in the AST.
+    """
+    ...
+
+def parse_create(expr: str, /, *, is_internal: bool = False) -> CreateQuery:
+    """Parse the CREATE TABLE statement string into an AST.
+
+    If the expr `is_internal`, spans and notices won't be included in the AST.
+    """
+    ...
