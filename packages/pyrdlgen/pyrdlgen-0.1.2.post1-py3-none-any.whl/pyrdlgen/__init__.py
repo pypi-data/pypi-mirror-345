160705a49422b2564a890ef9ca48e1fd4ee18e20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-03 03:18:48 +0000

    Add package pyrdlgen/0.1.2.post1/pyrdlgen-0.1.2.post1-py3-none-any.whl

---
+++ packages/pyrdlgen/pyrdlgen-0.1.2.post1-py3-none-any.whl/pyrdlgen/__init__.py
@@ -0,0 +1,10 @@
+from .core import render_report
+__version__ = "0.1.0"
+
+__all__ = ["render_report"]
+
+__author__ = "MobsterDev"
+
+__license__ = "MIT"
+
+__url__ = "https://github.com/mobster-dev/templates/pyrdlgen.html"
