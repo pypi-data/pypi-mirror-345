62b17471c23b6dc2abfbf556c850ef2b43de445e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-03 04:04:20 +0000

    Add package yolo9/1.0.8/yolo9-1.0.8-py3-none-any.whl

---
+++ packages/yolo9/yolo9-1.0.8-py3-none-any.whl/yolo9/data/class_names.py
@@ -0,0 +1,19 @@
+import os
+import yaml
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+with open(os.path.join(current_dir, "coco.yaml"), "r") as f:
+    coco_names = yaml.load(f, Loader=yaml.FullLoader)["names"]
+
+
+hardhat_names = {
+    0: "head",
+    1: "helmet",
+    2: "person"
+}
+
+
+carplate_names = {
+    0: "car plate"
+}
