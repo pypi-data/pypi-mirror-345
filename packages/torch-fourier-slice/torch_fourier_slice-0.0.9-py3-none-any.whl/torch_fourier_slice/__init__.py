67589e3817be7c7ea52c1623bf5f5b0df1ced67c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-03 03:20:07 +0000

    Add package torch-fourier-slice/0.0.9/torch_fourier_slice-0.0.9-py3-none-any.whl

---
+++ packages/torch-fourier-slice/torch_fourier_slice-0.0.9-py3-none-any.whl/torch_fourier_slice/__init__.py
@@ -0,0 +1,31 @@
+"""Fourier slicing on images and volumes.
+
+Fourier slice slice_extraction/slice_insertion from 2D images and 3D
+volumes in PyTorch.
+"""
+
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("torch-fourier-slice")
+except PackageNotFoundError:
+    __version__ = "uninstalled"
+__author__ = "Alister Burt"
+__email__ = "alisterburt@gmail.com"
+
+from .backproject import backproject_2d_to_3d
+from .project import project_2d_to_1d, project_3d_to_2d
+from .slice_extraction import (
+    extract_central_slices_rfft_2d,
+    extract_central_slices_rfft_3d,
+)
+from .slice_insertion import insert_central_slices_rfft_3d
+
+__all__ = [
+    "backproject_2d_to_3d",
+    "project_3d_to_2d",
+    "project_2d_to_1d",
+    "extract_central_slices_rfft_3d",
+    "extract_central_slices_rfft_2d",
+    "insert_central_slices_rfft_3d",
+]
