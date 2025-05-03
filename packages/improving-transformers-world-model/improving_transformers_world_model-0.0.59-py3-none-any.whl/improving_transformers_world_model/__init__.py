691662c932dae370c315c5321d37318abf287256
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-03 05:04:33 +0000

    Add package improving-transformers-world-model/0.0.59/improving_transformers_world_model-0.0.59-py3-none-any.whl

---
+++ packages/improving-transformers-world-model/improving_transformers_world_model-0.0.59-py3-none-any.whl/improving_transformers_world_model/__init__.py
@@ -0,0 +1,11 @@
+from improving_transformers_world_model.world_model import (
+    BlockCausalAttention,
+    BlockCausalTransformer,
+    NearestNeighborTokenizer,
+    WorldModel
+)
+
+from improving_transformers_world_model.agent import (
+    Agent,
+    Impala
+)
