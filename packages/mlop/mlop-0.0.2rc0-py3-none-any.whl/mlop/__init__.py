e153d51a1f9bfd95bcee2ff3332ea055d01507ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-03 07:04:38 +0000

    Add package mlop/0.0.2rc0/mlop-0.0.2rc0-py3-none-any.whl

---
+++ packages/mlop/mlop-0.0.2rc0-py3-none-any.whl/mlop/__init__.py
@@ -0,0 +1,31 @@
+from .auth import login, logout
+from .data import Data, Graph, Histogram
+from .file import Audio, File, Image, Video
+from .init import finish, init
+from .sets import Settings
+from .sys import System
+
+# TODO: setup preinit
+
+_hooks = []
+ops, log, watch, alert = None, None, None, None
+
+__all__ = (
+    "Data",
+    "Graph",
+    "Histogram",
+    "File",
+    "Image",
+    "Audio",
+    "Video",
+    "System",
+    "Settings",
+    "alert",
+    "init",
+    "login",
+    "logout",
+    "watch",
+    "finish",
+)
+
+__version__ = "0.0.2"
---
+++ packages/mlop/mlop-0.0.2rc0-py3-none-any.whl/mlop/compat/__init__.py
@@ -0,0 +1,3 @@
+from .torch import _watch_torch
+
+__all__ = ['_watch_torch']
