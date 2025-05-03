d39afe10949d6cde44d50eddcb457eafcf2cc26b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-03 06:04:47 +0000

    Add package malevich-app/0.3.22/malevich_app-0.3.22.tar.gz

---
+++ packages/malevich-app/malevich_app-0.3.22.tar.gz/malevich_app-0.3.22/malevich_app/export/secondary/jls_imported.py
@@ -0,0 +1,20 @@
+imported = False
+imported2 = False
+imported3 = False
+
+# FIXME
+
+
+def set_imported(imported_new: bool):
+    global imported
+    imported = imported_new
+
+
+def set_imported2(imported_new: bool):
+    global imported2
+    imported2 = imported_new
+
+
+def set_imported3(imported_new: bool):
+    global imported3
+    imported3 = imported_new
