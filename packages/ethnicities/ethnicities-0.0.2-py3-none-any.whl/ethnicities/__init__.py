96ec54a7c557d29a31303249c066e634264de32f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-03 05:04:36 +0000

    Add package ethnicities/0.0.2/ethnicities-0.0.2-py3-none-any.whl

---
+++ packages/ethnicities/ethnicities-0.0.2-py3-none-any.whl/ethnicities/__init__.py
@@ -0,0 +1,11 @@
+from .all_ethnicities import ALL_ETHNICITIES as all_ethnicities
+from .common import COMMON as common
+from .by_region import BY_REGION as by_region
+from .common_and_by_region import COMMON_AND_BY_REGION as common_and_by_region
+
+__all__ = [
+    "all_ethnicities",
+    "common",
+    "by_region",
+    "common_and_by_region",
+]
---
+++ packages/ethnicities/ethnicities-0.0.2-py3-none-any.whl/ethnicities/common.py
@@ -0,0 +1,7 @@
+# Top ~20 ethnicities by global population
+COMMON = [
+    "indian", "chinese", "american", "indonesian", "pakistani",
+    "nigerian", "brazilian", "bangladeshi", "russian", "mexican",
+    "japanese", "filipino", "ethiopian", "congolese", "egyptian",
+    "vietnamese", "iranian", "turkish", "german", "french"
+]
