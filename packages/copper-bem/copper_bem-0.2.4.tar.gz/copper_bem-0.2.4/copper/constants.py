6919723093ea91f6f71dae7b5f7148b8da3d9249
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-03 14:04:22 +0000

    Add package copper-bem/0.2.4/copper_bem-0.2.4.tar.gz

---
+++ packages/copper-bem/copper_bem-0.2.4.tar.gz/copper_bem-0.2.4/copper/constants.py
@@ -0,0 +1,16 @@
+"""
+constants.py
+====================================
+Holds all the constants referenced in copper.
+"""
+
+import os
+
+LOGGING_FORMAT = "%(asctime)s - %(levelname)s - %(message)s"
+ROOT_DIR = os.path.dirname(__file__)
+CHILLER_SCHEMA_PATH = f"{ROOT_DIR}/data/schema/copper.chiller.schema.json"
+CHILLER_GENE_SCHEMA_PATH = (
+    f"{ROOT_DIR}/data/schema/copper.chiller.generate_set_of_curves.schema.json"
+)
+CHILLER_ACTION_SCHEMA_PATH = f"{ROOT_DIR}/data/schema/copper.chiller.action.schema.json"
+SCHEMA_PATH = f"{ROOT_DIR}/data/schema/copper.schema.json"
