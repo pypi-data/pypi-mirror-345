cb6275c7c2e618c4165997eb235de58cdbe00d8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-03 15:04:49 +0000

    Add package nextperm/0.1.0/nextperm-0.1.0-py3-none-any.whl

---
+++ packages/nextperm/nextperm-0.1.0-py3-none-any.whl/nextperm/__init__.py
@@ -0,0 +1,40 @@
+from .core import (
+    next_permutation,
+    prev_permutation,
+    is_sorted,
+    is_sorted_until,
+    rotate_left,
+    rotate_right,
+    lower_bound,
+    upper_bound,
+    count_permutations,
+    nth_permutation,
+    reverse,
+    shuffle,
+    swap,
+    generate_combinations,
+    factorial,
+    nCr,
+    nPr
+)
+
+__all__ = [
+    "next_permutation",
+    "prev_permutation",
+    "is_sorted",
+    "is_sorted_until",
+    "rotate_left",
+    "rotate_right",
+    "lower_bound",
+    "upper_bound",
+    "count_permutations",
+    "nth_permutation",
+    "reverse",
+    "shuffle",
+    "swap",
+    "generate_combinations",
+    "factorial",
+    "nCr",
+    "nPr"
+]
+s
