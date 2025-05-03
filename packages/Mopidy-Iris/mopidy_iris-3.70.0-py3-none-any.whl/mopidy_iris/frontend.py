5211530560c511a5f3a209e453429af0fde5b4dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-03 16:04:46 +0000

    Add package Mopidy-Iris/3.70.0/mopidy_iris-3.70.0-py3-none-any.whl

---
+++ packages/Mopidy-Iris/mopidy_iris-3.70.0-py3-none-any.whl/mopidy_iris/frontend.py
@@ -0,0 +1,39 @@
+import pykka
+import logging
+import functools
+from mopidy.core import CoreListener
+from .mem import iris
+
+# import logger
+logger = logging.getLogger(__name__)
+
+
+class IrisFrontend(pykka.ThreadingActor, CoreListener):
+    def __init__(self, config, core):
+        super().__init__()
+
+        # Pass our Mopidy config and core to the IrisCore instance
+        iris.config = config
+        iris.core = core
+
+    def on_start(self):
+        iris.start()
+
+    def on_stop(self):
+        iris.stop()
+
+    def track_playback_ended(self, tl_track, time_position):
+        if (
+            iris.ioloop
+            and "spotify" in iris.config
+            and iris.config["spotify"]["enabled"]
+        ):
+            iris.ioloop.add_callback(
+                functools.partial(iris.check_for_radio_update)
+            )
+
+    def tracklist_changed(self):
+        if iris.ioloop:
+            iris.ioloop.add_callback(
+                functools.partial(iris.clean_queue_metadata)
+            )
