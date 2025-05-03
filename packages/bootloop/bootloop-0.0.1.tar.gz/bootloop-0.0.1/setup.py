a2e2ab7477b3aff9b291ab2590d2d95ada7b26ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-03 15:04:15 +0000

    Add package bootloop/0.0.1/bootloop-0.0.1.tar.gz

---
+++ packages/bootloop/bootloop-0.0.1.tar.gz/bootloop-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bootloop",
+    version="0.0.1",
+    packages=find_packages(),
+    description="BootLoop",
+    author="Noah Pacik-Nelson",
+    author_email="noah@bootloop.ai",
+    python_requires=">=3.6",
+) 
