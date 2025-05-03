e9b358cde38d62d75a19cbafb471493a1af13352
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-03 03:19:47 +0000

    Add package EbookLib/0.19/ebooklib-0.19.tar.gz

---
+++ packages/EbookLib/ebooklib-0.19.tar.gz/ebooklib-0.19/samples/README.md
@@ -0,0 +1,38 @@
+EbookLib Samples
+================
+
+* 01_sample_create
+
+  Create simple EPUB with two chapters and custom CSS for the navigation.
+
+* 02_cover_create
+
+  Same as 01_sample_create but with cover page.
+
+* 03_advanced_create
+
+   Creates EPUB but uses some advanced options from the library.
+
+* 04_markdown_parse
+
+  Simple script which creates static markdown files + images from your EPUB.
+
+* 05_plugins_create
+
+  How to create sample plugin.
+
+* 06_parse
+
+  Basing parsing of EPUB file.
+
+* 07_pagebreaks
+
+  Create simple EPUB3 with both visible and invisible pagebreaks.
+
+* 08_SMIL
+
+  How to integrate Synchronized Multimedia Integration Language (SMIL) into your EPUB.
+
+* 09_create_image
+
+  Creates chapter with image.
---
+++ packages/EbookLib/ebooklib-0.19.tar.gz/ebooklib-0.19/setup.py
@@ -0,0 +1,42 @@
+import io
+import re
+
+from setuptools import setup
+
+
+def read(path):
+    with io.open(path, mode="r", encoding="utf-8") as fd:
+        content = fd.read()
+    # Convert Markdown links to reStructuredText links
+    return re.sub(r"\[([^]]+)\]\(([^)]+)\)", r"`\1 <\2>`_", content)
+
+
+setup(
+    name = 'EbookLib',
+    version = '0.19',
+    author = 'Aleksandar Erkalovic',
+    author_email = 'aerkalov@gmail.com',
+    packages = ['ebooklib', 'ebooklib.plugins'],
+    url = 'https://github.com/aerkalov/ebooklib',
+    license = 'GNU Affero General Public License',
+    description = 'Ebook library which can handle EPUB2/EPUB3 and Kindle format',
+    long_description = read('README.md'),
+    long_description_content_type = 'text/markdown',
+    keywords = ['ebook', 'epub', 'kindle'],
+    classifiers = [
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ],
+    install_requires = [
+       "lxml", "six"
+    ]
+)
+
