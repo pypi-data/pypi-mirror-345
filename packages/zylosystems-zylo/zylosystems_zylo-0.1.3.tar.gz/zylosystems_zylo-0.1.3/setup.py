95529428e8e695ac900fcee638a9139e59536e8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-03 15:04:50 +0000

    Add package zylosystems-zylo/0.1.3/zylosystems_zylo-0.1.3.tar.gz

---
+++ packages/zylosystems-zylo/zylosystems_zylo-0.1.3.tar.gz/zylosystems_zylo-0.1.3/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages  # ✅ 꼭 필요함
+
+setup(
+    name='zylosystems-zylo',
+    version='0.1.3',
+    packages=find_packages(include=["zylo", "zylo.*"]),
+    include_package_data=True,
+    package_data={'zylo': ['static/docs/**/*']},
+    install_requires=['Flask'],
+    entry_points={
+        'console_scripts': [
+            'zylo=zylo.cli:main',  # ← 여기서 'zylo' 명령어를 main()에 연결!
+        ],
+    },
+)
