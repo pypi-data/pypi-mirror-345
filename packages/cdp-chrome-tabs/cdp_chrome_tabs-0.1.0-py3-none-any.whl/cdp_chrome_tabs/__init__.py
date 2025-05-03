3607cbb4d592547d339de765a1d4738aafb02f13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-03 07:04:05 +0000

    Add package cdp-chrome-tabs/0.1.0/cdp_chrome_tabs-0.1.0-py3-none-any.whl

---
+++ packages/cdp-chrome-tabs/cdp_chrome_tabs-0.1.0-py3-none-any.whl/cdp_chrome_tabs/__init__.py
@@ -0,0 +1,7 @@
+"""
+CDP Chrome Tabs - A package to interact with Chrome tabs via Chrome DevTools Protocol
+"""
+
+from .chrome_tabs import ChromeTabs, ChromeTab
+
+__all__ = ["ChromeTabs", "ChromeTab"]
