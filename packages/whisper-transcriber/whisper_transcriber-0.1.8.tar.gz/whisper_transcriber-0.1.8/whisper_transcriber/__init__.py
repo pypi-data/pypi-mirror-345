6e9ebbc1e8b65a2e61d855da38ead2b24451fb35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-03 05:04:37 +0000

    Add package whisper-transcriber/0.1.8/whisper_transcriber-0.1.8.tar.gz

---
+++ packages/whisper-transcriber/whisper_transcriber-0.1.8.tar.gz/whisper_transcriber-0.1.8/whisper_transcriber/__init__.py
@@ -0,0 +1,17 @@
+"""
+Whisper Transcriber - A library for transcribing audio files using Whisper models
+"""
+
+__version__ = "0.1.0"
+
+from .transcriber import WhisperTranscriber
+from .audio_processing import analyze_audio_levels, analyze_full_audio_for_silence
+from .utils import format_timestamp, format_srt_timestamp
+
+__all__ = [
+    'WhisperTranscriber',
+    'analyze_audio_levels',
+    'analyze_full_audio_for_silence',
+    'format_timestamp',
+    'format_srt_timestamp'
+]
