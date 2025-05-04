53301ec35fb170b5e4a55c6e94cf1d5f842a6e8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-04 03:26:53 +0000

    Add package mermaid-magic/0.1.0/mermaid_magic-0.1.0.tar.gz

---
+++ packages/mermaid-magic/mermaid_magic-0.1.0.tar.gz/mermaid_magic-0.1.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name="mermaid-magic",
+    version="0.1.0",
+    description="IPython magic for rendering Mermaid diagrams",
+    author="Vijay Balasubramaniam",
+    author_email="vbalasu@gmail.com",
+    py_modules=["mermaid_magic"],
+    install_requires=[
+        "ipython",
+    ],
+    project_urls={
+        "Source": "https://github.com/vbalasu/mermaid-magic",
+        "Bug Reports": "https://github.com/vbalasu/mermaid-magic/issues",
+    },
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+) 
