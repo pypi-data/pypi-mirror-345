32a6ecc6c4162d2189b1b3e1490f9236a4456a1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-04 03:25:32 +0000

    Add package mula/1.3.2/mula-1.3.2-py3-none-any.whl

---
+++ packages/mula/mula-1.3.2-py3-none-any.whl/mula/param.py
@@ -0,0 +1,8 @@
+class CommonParam:
+    def __init__(self):
+        self.duedate: str | None = None
+        self.maxfiles: int | None = None
+        self.visible: bool | None = None
+        self.exec: bool | None = None
+        self.info: bool | None = None
+        self.drafts: str | None = None
