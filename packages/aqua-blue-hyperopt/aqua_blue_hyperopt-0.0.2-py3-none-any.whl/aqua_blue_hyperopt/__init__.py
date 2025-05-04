dfd44bab67f88aad2d41b56ce51bc2ac541e7cc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-04 15:04:54 +0000

    Add package aqua-blue-hyperopt/0.0.2/aqua_blue_hyperopt-0.0.2-py3-none-any.whl

---
+++ packages/aqua-blue-hyperopt/aqua_blue_hyperopt-0.0.2-py3-none-any.whl/aqua_blue_hyperopt/__init__.py
@@ -0,0 +1,10 @@
+__version__ = "0.0.2"
+__authors__ = [
+    "Hrishikesh Belagali"
+]
+__author_emails__ = [
+    "hrishikesh.belagali@ccmc.global",
+]
+__url__ = "https://github.com/Chicago-Club-Management-Company/aqua-blue-hyperopt"
+
+from . import hyper as hyper
