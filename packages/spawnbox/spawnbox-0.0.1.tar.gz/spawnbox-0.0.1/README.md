fac05f7135458e7f81729c7cbc9c2275254a132f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-04 14:05:05 +0000

    Add package spawnbox/0.0.1/spawnbox-0.0.1.tar.gz

---
+++ packages/spawnbox/spawnbox-0.0.1.tar.gz/spawnbox-0.0.1/README.md
@@ -0,0 +1,5 @@
+# SpawnBox
+
+A (decently) simple game framework based on pysdl2 and [MandawEngineSDL.](https://github.com/mandaw2014/MandawEngineSDL)
+
+Documentation is on [gitbook.io](https://modguy07.gitbook.io/spawnbox).
---
+++ packages/spawnbox/spawnbox-0.0.1.tar.gz/spawnbox-0.0.1/src/spawnbox/__init__.py
@@ -0,0 +1,9 @@
+from spawnbox.main import SpawnBox
+from spawnbox.entity import Entity
+from spawnbox.input import Input
+
+'''
+  This Source Code Form is subject to the terms of the Mozilla Public
+  License, v. 2.0. If a copy of the MPL was not distributed with this
+  file, You can obtain one at https://mozilla.org/MPL/2.0/.
+'''
