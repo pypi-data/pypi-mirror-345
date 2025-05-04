78f787aa982b3578b629b12b86f08c6f2440e1bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-04 15:04:43 +0000

    Add package feng-tools/0.1.6/feng_tools-0.1.6-py3-none-any.whl

---
+++ packages/feng-tools/feng_tools-0.1.6-py3-none-any.whl/feng_tools/fastapi/__init__.py
@@ -0,0 +1,9 @@
+"""
+pip install python-dateutil
+pip install pydantic
+pip install pydantic-settings
+pip install fastapi
+pip install uvicorn
+pip install sqlmodel
+pip install python-multipart -i https://pypi.tuna.tsinghua.edu.cn/simple/
+"""
