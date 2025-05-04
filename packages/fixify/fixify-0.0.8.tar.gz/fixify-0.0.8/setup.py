3feff35674bb4a58b549072c3ebe469fa87dbc46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-04 03:26:19 +0000

    Add package fixify/0.0.8/fixify-0.0.8.tar.gz

---
+++ packages/fixify/fixify-0.0.8.tar.gz/fixify-0.0.8/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+import os
+
+# Safely open README.md
+long_description = ""
+if os.path.exists("README.md"):
+    with open("README.md", encoding="utf-8") as f:
+        long_description = f.read()
+
+setup(
+    name="fixify",
+    version="0.0.8",
+    packages=find_packages(),
+    install_requires=[
+        'pandas', 'numpy', 'IPython', 'together', 'Click',  # Make sure Click is included
+    ],
+    author='YellowForest',
+    description='Terminal AI for Code Explanation & Correction',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Mathematics',
+    ],
+    python_requires='>=3.6',  # Add this to specify minimum Python version
+    entry_points={
+        'console_scripts': [
+            'fixify = cli:cli',  # This points to the `cli()` function in cli.py
+        ],
+    },
+)
