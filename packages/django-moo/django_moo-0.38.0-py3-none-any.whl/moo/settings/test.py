6ffbf48a7e4146356fd1a89865c9abf677755e20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-04 03:27:00 +0000

    Add package django-moo/0.38.0/django_moo-0.38.0-py3-none-any.whl

---
+++ packages/django-moo/django_moo-0.38.0-py3-none-any.whl/moo/settings/test.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+from .base import *  # pylint: disable=wildcard-import
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-061+p62f39ohlfrgu&)%1lxo%%#_-$rc5l_zsrlx6jqy)sw(=r"
