007c11f9c7e5a8ed0615ae4f3da44d0935feddfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-04 03:26:08 +0000

    Add package chercher-plugin-docx/0.1.3/chercher_plugin_docx-0.1.3.tar.gz

---
+++ packages/chercher-plugin-docx/chercher_plugin_docx-0.1.3.tar.gz/chercher_plugin_docx-0.1.3/README.md
@@ -0,0 +1,14 @@
+# chercher-plugin-docx
+
+[Chercher](https://github.com/dnlzrgz/chercher) plugin that adds support for DOCX files thanks to [PyMuPDF](https://pymupdf.readthedocs.io/en/latest/).
+
+## Installation
+
+```bash
+uv tool install --with chercher-plugin-docx chercher
+```
+
+## Settings
+
+> [!NOTE]
+> If you plugin needs additional settings, add the instructions here.
---
+++ packages/chercher-plugin-docx/chercher_plugin_docx-0.1.3.tar.gz/chercher_plugin_docx-0.1.3/tests/test_chercher_plugin_docx.py
@@ -0,0 +1,59 @@
+from pathlib import Path
+import pytest
+from chercher_plugin_docx import ingest, prune
+from chercher import Document
+
+
+@pytest.fixture
+def sample_files():
+    samples_dir = Path(__file__).parent / "samples"
+    return [file.as_uri() for file in samples_dir.iterdir() if file.is_file()]
+
+
+def test_ingest_valid_file(sample_files):
+    for uri in sample_files:
+        documents = list(ingest(uri=uri))
+        assert documents != []
+
+        for doc in documents:
+            assert isinstance(doc, Document)
+            assert doc.uri == uri
+            assert doc.body != ""
+            assert doc.metadata
+
+
+def test_ingest_invalid_file(tmp_path):
+    p = tmp_path / "test.txt"
+    p.write_text("Test")
+
+    uri = p.as_uri()
+    documents = list(ingest(uri=uri))
+    assert documents == []
+
+
+def test_ingest_missing_file(tmp_path):
+    p = tmp_path / "missingno.epub"
+    documents = list(ingest(uri=p.as_uri()))
+    assert documents == []
+
+
+def test_ingest_invalid_uri():
+    uri = "https://drive.com/haiku.docx"
+    documents = list(ingest(uri=uri))
+    assert documents == []
+
+
+def test_prune_valid_file(sample_files):
+    uri = sample_files[0]
+    assert prune(uri=uri) is None
+
+
+def test_prune_missing_file(tmp_path):
+    p = tmp_path / "missingno.docx"
+    uri = p.as_uri()
+    assert prune(uri=uri)
+
+
+def test_prune_invalid_uri():
+    uri = "https://drive.com/haiku.docx"
+    assert prune(uri=uri) is None
