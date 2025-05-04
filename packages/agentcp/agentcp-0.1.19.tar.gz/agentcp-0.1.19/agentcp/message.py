175427ef2341127102ae60dc6de9b882ab3046e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-04 15:05:43 +0000

    Add package agentcp/0.1.19/agentcp-0.1.19.tar.gz

---
+++ packages/agentcp/agentcp-0.1.19.tar.gz/agentcp-0.1.19/agentcp/message.py
@@ -0,0 +1,29 @@
+from dataclasses import dataclass
+from typing import Dict, List, Literal, Optional, Union
+
+
+@dataclass
+class Artifact:
+    identifier: str
+    title: str
+    type: Literal[
+        "application/vnd.ant.code",
+        "text/markdown",
+        "text/html",
+        "image/svg+xml",
+        "application/vnd.ant.mermaid",
+        "application/vnd.ant.react",
+    ]
+    language: Optional[str] = None
+
+
+@dataclass
+class AssistantMessageBlock:
+    type: Literal["text", "content", "search", "reasoning_content", "error"]
+    status: Literal["success", "loading", "cancel", "error", "reading", "optimizing"]
+    timestamp: int
+    content: Optional[str] = None
+    stream: Optional[bool] = False
+    prompt: Optional[str] = None  # Add prompt field to AssistantMessageBlock
+    extra: Optional[Dict[str, Union[str, int, List[dict]]]] = None
+    artifact: Optional[Artifact] = None
