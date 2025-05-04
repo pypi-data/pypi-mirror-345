4ca572f4ebeba1c2eaa7fb63172a73c529ee3dbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-04 15:05:31 +0000

    Add package stdqt/0.0.2/stdqt-0.0.2-py3-none-any.whl

---
+++ packages/stdqt/stdqt-0.0.2-py3-none-any.whl/stdqt/_all.py
@@ -0,0 +1,71 @@
+from PyQt5 import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtMultimedia import *
+from PyQt5.QtNetwork import *
+from PyQt5.QtWebEngine import *
+from PyQt5.QtWebEngineWidgets import *
+from PyQt5.QtWebEngineCore import *
+from PyQt5.QtXml import *
+from PyQt5.QtWinExtras import *
+from PyQt5.QtWebSockets import *
+from PyQt5.QtWebChannel import *
+from PyQt5.QtTextToSpeech import *
+from PyQt5.QtTest import *
+from PyQt5.QtSvg import *
+from PyQt5.QtSql import *
+from PyQt5.QtSerialPort import *
+from PyQt5.QtSensors import *
+from PyQt5.QtRemoteObjects import *
+from PyQt5.QtQuick import *
+from PyQt5.QtQuickWidgets import *
+from PyQt5.QtQuick3D import *
+from PyQt5.QtQml import *
+from PyQt5.QtPrintSupport import *
+from PyQt5.QtPositioning import *
+from PyQt5.QtOpenGL import *
+from PyQt5.QtNfc import *
+from PyQt5.QtMultimediaWidgets import *
+from PyQt5.QtLocation import *
+from PyQt5.QtHelp import *
+from PyQt5.QtDesigner import *
+from PyQt5.QtDBus import *
+from PyQt5.QtBluetooth import *
+
+
+from PyQt5 import (
+    QtGui, 
+    QtWidgets, 
+    QtCore, 
+    QtMultimedia, 
+    QtNetwork, 
+    QtWebEngine, 
+    QtWebEngineWidgets, 
+    QtWebEngineCore, 
+    QtXml, 
+    QtWinExtras, 
+    QtWebSockets, 
+    QtWebChannel,
+    QtTextToSpeech,
+    QtDesigner,
+    QtHelp,
+    QtDBus,
+    QtBluetooth,
+    QtLocation,
+    QtOpenGL,
+    QtNfc,
+    QtMultimediaWidgets,
+    QtPositioning,
+    QtPrintSupport,
+    QtQml,
+    QtQuick3D,
+    QtQuickWidgets,
+    QtQuick,
+    QtRemoteObjects,
+    QtSensors,
+    QtSerialPort,
+    QtTest,
+    QtSql,
+    QtSvg
+)
