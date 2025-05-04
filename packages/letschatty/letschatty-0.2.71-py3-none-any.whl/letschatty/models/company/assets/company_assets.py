e3b2b56df44e3aef6162beab6758aea98440213c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-04 04:06:36 +0000

    Add package letschatty/0.2.71/letschatty-0.2.71-py3-none-any.whl

---
+++ packages/letschatty/letschatty-0.2.71-py3-none-any.whl/letschatty/models/company/assets/company_assets.py
@@ -0,0 +1,17 @@
+from enum import StrEnum
+
+
+class CompanyAssetType(StrEnum):
+    USERS = "users"
+    BUSINESS_AREAS = "business_areas"
+    FUNNELS = "funnels"
+    PRODUCTS = "products"
+    SALES = "sales"
+    TAGS = "tags"
+    SOURCES = "sources"
+    TEMPLATES = "templates"
+    FAST_ANSWERS = "fast_answers"
+    ANALYTICS = "analytics"
+    CHATS = "chats"
+    TOPICS = "topics"
+    COMPANY = "company"
