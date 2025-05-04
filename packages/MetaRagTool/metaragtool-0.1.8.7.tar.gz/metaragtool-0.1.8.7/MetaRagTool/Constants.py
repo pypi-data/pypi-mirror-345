873e4645709124132dcb5801fefed7f9f81450eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-04 15:04:49 +0000

    Add package MetaRagTool/0.1.8.7/metaragtool-0.1.8.7.tar.gz

---
+++ packages/MetaRagTool/metaragtool-0.1.8.7.tar.gz/metaragtool-0.1.8.7/MetaRagTool/Constants.py
@@ -0,0 +1,32 @@
+﻿use_wandb = False
+local_mode = True
+trust_remote_code_SentenceTransformer = False
+
+# import os
+# WandbToken=os.getenv('WandbToken')
+# HFToken=os.getenv('HFToken')
+# GitHubToken=os.getenv('GitHubToken')
+
+
+WandbToken = ''
+HFToken = ''
+GitHubToken = ''
+API_KEY_GEMINI = ""
+API_KEY_OPENAI = ""
+
+
+def SetTokens(t_wandb=None, t_hf=None, t_github=None,
+              t_gemini=None, t_openai=None):
+    global WandbToken, HFToken, GitHubToken
+    global API_KEY_GEMINI, API_KEY_OPENAI
+
+    if t_wandb is not None:
+        WandbToken = t_wandb
+    if t_hf is not None:
+        HFToken = t_hf
+    if t_github is not None:
+        GitHubToken = t_github
+    if t_gemini is not None:
+        API_KEY_GEMINI = t_gemini
+    if t_openai is not None:
+        API_KEY_OPENAI = t_openai
---
+++ packages/MetaRagTool/metaragtool-0.1.8.7.tar.gz/metaragtool-0.1.8.7/MetaRagTool/__init__.py
@@ -0,0 +1,27 @@
+import MetaRagTool.Constants
+import MetaRagTool.LLM
+import MetaRagTool.Encoders
+import MetaRagTool.RAG
+import MetaRagTool.Utils
+import MetaRagTool.Evaluations
+import MetaRagTool.Apps
+
+
+from MetaRagTool.RAG.MetaRAG import MetaRAG
+from MetaRagTool.LLM.GoogleGemini import Gemini
+from MetaRagTool.Encoders.SentenceTransformerEncoder import SentenceTransformerEncoder
+from MetaRagTool.Evaluations import Evals
+
+
+
+
+
+
+
+
+
+
+
+
+__version__ = "0.1.8"
+print("MetaRagTool imported")
