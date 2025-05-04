90cc181a326463609b37ff1577c5c3c372ca72c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-04 03:26:25 +0000

    Add package doFolder/2.0.3/dofolder-2.0.3-py3-none-any.whl

---
+++ packages/doFolder/dofolder-2.0.3-py3-none-any.whl/doFolder/globalType.py
@@ -0,0 +1,43 @@
+"""
+This module ensures the support for the static type system in different versions
+
+It is generally named _tt in other modules
+"""
+
+# pylint: disable=unused-import
+
+from typing_extensions import (
+    List,
+    Dict,
+    Tuple,
+    Set,
+    Optional,
+    Iterator,
+    Iterable,
+    Any,
+    Union,
+    Literal,
+    overload,
+    TypeAlias,
+    IO,
+    BinaryIO,
+    cast,
+    Never,
+    NoReturn,
+    TypeVar,
+    Sequence,
+    Self
+)
+from .path import Path
+from abc import ABC
+
+import sys
+
+if sys.version_info >= (3, 10):
+    from collections.abc import Callable
+else:
+    from typing import Callable
+
+EVENT_TYPE = Literal["CREATED", "DELETED", "MODIFIED"]
+
+Pathable = Union[str, Path]
