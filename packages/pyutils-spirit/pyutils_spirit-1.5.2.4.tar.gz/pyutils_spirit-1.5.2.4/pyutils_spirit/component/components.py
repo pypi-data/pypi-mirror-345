1177f51074fa8983617a948baaaf5a9526ca414c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-04 04:06:33 +0000

    Add package pyutils-spirit/1.5.2.4/pyutils_spirit-1.5.2.4.tar.gz

---
+++ packages/pyutils-spirit/pyutils_spirit-1.5.2.4.tar.gz/pyutils_spirit-1.5.2.4/pyutils_spirit/component/components.py
@@ -0,0 +1,11 @@
+from pyutils_spirit.spirit_container.annotation import component
+
+from pyutils_spirit.util.cities import get_provinces, get_cities
+
+
+@component(signature="cities")
+class CitiesComponent:
+
+    def __init__(self):
+        self.get_cities: callable = get_cities
+        self.get_provinces: callable = get_provinces
