7bcaa05ed6b383ebc4a160039d715d2c019b5668
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-04 04:06:48 +0000

    Add package fapp/1.0.1/fapp-1.0.1.tar.gz

---
+++ packages/fapp/fapp-1.0.1.tar.gz/fapp-1.0.1/fapp/__init__.py
@@ -0,0 +1,7 @@
+"""
+fapp (frame analysis program in python) is a simple, easy-to-use implementation of the direct stiffness method.
+"""
+
+__version__ = "1.0.1"
+__author__ = "Robert Wang"
+__license__ = "MIT"
