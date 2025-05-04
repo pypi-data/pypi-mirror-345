88b6f1973ef5e96c770c85e26d173abfd1a76d53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-04 15:05:13 +0000

    Add package agentask/0.1.3/agentask-0.1.3.tar.gz

---
+++ packages/agentask/agentask-0.1.3.tar.gz/agentask-0.1.3/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="agentask",
+    version="0.1.3",
+    author="TaskFlow Team",
+    author_email="example@example.com",
+    description="An AI-powered task planning assistant with calendar generation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/taskflow",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+    install_requires=[
+        "mistralai",
+        "google-generativeai",
+        "python-dotenv",
+        "icalendar",
+        "grpcio==1.60.1",
+    ],
+    entry_points={
+        "console_scripts": [
+            "understand=agentask.understand_main:main",
+            "plan=agentask.plan_main:main",
+            "generate-calendar=agentask.calendar_main:main",
+        ],
+    },
+)
