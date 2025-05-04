3f202d6a35e4ed50197c7c6f42f859d41526bab7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-04 03:26:46 +0000

    Add package simple-bcp/0.8.0/simple_bcp-0.8.0-py3-none-any.whl

---
+++ packages/simple-bcp/simple_bcp-0.8.0-py3-none-any.whl/simple_bcp/__init__.py
@@ -0,0 +1,4 @@
+from ._bcp import BCP, MsSqlDatabaseParameters
+from ._encoding import FieldEncodingType, BcpEncodingSettings
+
+__all__: list[str] = ["BCP", "MsSqlDatabaseParameters", "FieldEncodingType", "BcpEncodingSettings"]
