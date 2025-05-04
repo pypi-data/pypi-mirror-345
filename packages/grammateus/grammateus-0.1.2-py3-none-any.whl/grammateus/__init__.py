da7ba9d19c4f038b9b89f455cfe8499c7f4968ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-04 14:04:44 +0000

    Add package grammateus/0.1.2/grammateus-0.1.2-py3-none-any.whl

---
+++ packages/grammateus/grammateus-0.1.2-py3-none-any.whl/grammateus/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Python
+
+"""Copyright (c) Alexander Fedotov.
+This source code is licensed under the license found in the
+LICENSE file in the root directory of this source tree.
+"""
+
+from .entities import Grammateus, Scribe
+
+__all__ = [
+    'Grammateus',
+    'Scribe'
+]
