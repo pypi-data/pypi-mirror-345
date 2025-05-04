e026ef01f144001aa900fd08c5a6a5776847ed8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-04 04:06:31 +0000

    Add package pyutils-spirit/1.5.3.0/pyutils_spirit-1.5.3.0.tar.gz

---
+++ packages/pyutils-spirit/pyutils_spirit-1.5.3.0.tar.gz/pyutils_spirit-1.5.3.0/pyutils_spirit/component/__init__.py
@@ -0,0 +1,5 @@
+from pyutils_spirit.component.components import CitiesComponent
+
+from pyutils_spirit.util.cities import get_provinces, get_cities
+
+CitiesComponent(get_cities_func=get_cities, get_provinces_func=get_provinces)
