a691fe6ad2efe2ab8c6869f9a4792726a36261d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-04 04:06:48 +0000

    Add package ncon-torch/0.2.8/ncon_torch-0.2.8.tar.gz

---
+++ packages/ncon-torch/ncon_torch-0.2.8.tar.gz/ncon_torch-0.2.8/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    readme = f.read()
+
+setuptools.setup(
+    name="ncon-torch",
+    version="0.2.8",
+    author="Faisal Alam",
+    author_email="mfalam2@illinois.edu",
+    description="Tensor network contraction function with GPU and autograd support via PyTorch.",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/alam-faisal/ncon-torch",
+    packages=setuptools.find_packages("src"),
+    package_dir={"": "src"},
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering",
+    ],
+    keywords=["tensor networks"],
+    install_requires=[
+        "torch>=1.12",
+        "numpy>=1.20"],
+    extras_require={"tests": ["pytest", "coverage", "pytest-cov"]},
+    python_requires=">=3.8",
+)
