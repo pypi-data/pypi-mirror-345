a3dd3a4e1f0cf1331fc4f4798e57b1794b985e2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-04 14:04:57 +0000

    Add package model-compose/0.0.1/model_compose-0.0.1.tar.gz

---
+++ packages/model-compose/model_compose-0.0.1.tar.gz/model_compose-0.0.1/src/mindor/cli/commands/down.py
@@ -0,0 +1,6 @@
+
+import click
+
+@click.command(name="down")
+def down_command():
+    click.echo("🚀 Pipeline is now running in background (stub).")
---
+++ packages/model-compose/model_compose-0.0.1.tar.gz/model_compose-0.0.1/src/mindor/cli/commands/up.py
@@ -0,0 +1,6 @@
+
+import click
+
+@click.command(name="up")
+def up_command():
+    click.echo("🚀 Pipeline is now running in background (stub).")
