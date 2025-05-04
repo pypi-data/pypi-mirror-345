232e9d66e5351d25e3dd75a567ac25ad0aca54f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-04 14:04:49 +0000

    Add package fed-rag/0.0.11/fed_rag-0.0.11.tar.gz

---
+++ packages/fed-rag/fed_rag-0.0.11.tar.gz/fed_rag-0.0.11/src/fed_rag/exceptions/__init__.py
@@ -0,0 +1,73 @@
+from .common import MissingExtraError
+from .core import FedRAGError
+from .data_collator import DataCollatorError
+from .fl_tasks import (
+    FLTaskError,
+    MissingFLTaskConfig,
+    MissingRequiredNetParam,
+    NetTypeMismatch,
+)
+from .inspectors import (
+    InspectorError,
+    InspectorWarning,
+    InvalidReturnType,
+    MissingDataParam,
+    MissingMultipleDataParams,
+    MissingNetParam,
+    MissingTesterSpec,
+    MissingTrainerSpec,
+    UnequalNetParamWarning,
+)
+from .knowledge_stores import KnowledgeStoreError, KnowledgeStoreNotFoundError
+from .trainer import (
+    InconsistentDatasetError,
+    InvalidDataCollatorError,
+    InvalidLossError,
+    MissingInputTensor,
+    TrainerError,
+)
+from .trainer_manager import (
+    InconsistentRAGSystems,
+    RAGTrainerManagerError,
+    UnspecifiedGeneratorTrainer,
+    UnspecifiedRetrieverTrainer,
+    UnsupportedTrainerMode,
+)
+
+__all__ = [
+    # core
+    "FedRAGError",
+    # common
+    "MissingExtraError",
+    "DataCollatorError",
+    # fl_tasks
+    "FLTaskError",
+    "MissingFLTaskConfig",
+    "MissingRequiredNetParam",
+    "NetTypeMismatch",
+    # inspectors
+    "InspectorError",
+    "InspectorWarning",
+    "MissingNetParam",
+    "MissingMultipleDataParams",
+    "MissingDataParam",
+    "MissingTrainerSpec",
+    "MissingTesterSpec",
+    "UnequalNetParamWarning",
+    "InvalidReturnType",
+    # knowledge stores
+    "KnowledgeStoreError",
+    "KnowledgeStoreNotFoundError",
+    # rag trainer manager
+    "RAGTrainerManagerError",
+    "UnspecifiedGeneratorTrainer",
+    "UnspecifiedRetrieverTrainer",
+    "UnsupportedTrainerMode",
+    "InconsistentRAGSystems",
+    # trainer
+    "TrainerError",
+    "InvalidLossError",
+    "MissingInputTensor",
+    "InvalidDataCollatorError",
+    "InconsistentDatasetError",
+]
