877eaf3e954ad60c9d9311b6bcbf94bd1dbc7bbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-04 15:05:22 +0000

    Add package jasmit_firestarter/0.1.5/jasmit_firestarter-0.1.5.tar.gz

---
+++ packages/jasmit_firestarter/jasmit_firestarter-0.1.5.tar.gz/jasmit_firestarter-0.1.5/README.md
@@ -0,0 +1,4 @@
+
+# Fire-Starter
+
+This project is a template to clone and transform into a Python project with all the trimmings.
---
+++ packages/jasmit_firestarter/jasmit_firestarter-0.1.5.tar.gz/jasmit_firestarter-0.1.5/src/std_app.py
@@ -0,0 +1,47 @@
+"""
+std_app.py
+"""
+
+import configparser
+
+#  from .std_logging import StdLogging, function_logger
+from std_logging import StdLogging, function_logger
+
+
+#  =============================================================================
+class StdApp:
+    #  -----------------------------------------------------------------------------
+    def __init__(self, app_name="", version="0.0.0") -> None:
+        self._logger = StdLogging(f"logs/{app_name}.log")
+        self._logger.info(f"Begin 'StdApp.__init__         ' arguments - ({app_name=}, {version=})")
+
+        self._app_name = app_name
+        #  self.__version__ = version
+
+        self.cmdline_params = self.set_cmdline_params()
+        self.cfg_file_params = self.set_cfg_file_params()
+
+        self._logger.info("End   'StdApp.__init__         ' returns   - None\n")
+
+    # ---------------------------------------------------------------------------------------------------------------------
+    def __str__(self):
+        return "StdApp"
+
+    __repr__ = __str__
+
+    #  -----------------------------------------------------------------------------
+    @function_logger
+    def set_cmdline_params(self):
+        raise NotImplementedError("Please define set_cmdline_params in the derived class.")
+
+    #  -----------------------------------------------------------------------------
+    @function_logger
+    def set_cfg_file_params(self):
+        environment = self.cmdline_params.get("environment")
+
+        cfgfile_path = self.cmdline_params.get("cfgfile")
+        config = configparser.ConfigParser()
+        with open(cfgfile_path) as file:
+            config.read_file(file)
+
+        return config[environment]
