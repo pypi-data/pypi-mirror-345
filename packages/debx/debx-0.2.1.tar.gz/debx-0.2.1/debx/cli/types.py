cffab1190a076b4993977e328a5186e732774efb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-04 14:04:51 +0000

    Add package debx/0.2.1/debx-0.2.1.tar.gz

---
+++ packages/debx/debx-0.2.1.tar.gz/debx-0.2.1/debx/cli/types.py
@@ -0,0 +1,43 @@
+import tarfile
+from argparse import ArgumentDefaultsHelpFormatter, RawTextHelpFormatter
+from enum import Enum
+from pathlib import PurePosixPath
+from typing import Optional, TypedDict
+
+
+class Formatter(RawTextHelpFormatter, ArgumentDefaultsHelpFormatter):
+    pass
+
+
+class CLIFile(TypedDict, total=False):
+    src: bytes
+    dest: PurePosixPath
+    mode: int
+    uid: int
+    gid: int
+    mtime: int
+
+
+class TarInfoType(bytes, Enum):
+    regular = tarfile.REGTYPE
+    hardlink = tarfile.LNKTYPE
+    symlink = tarfile.SYMTYPE
+    char = tarfile.CHRTYPE
+    block = tarfile.BLKTYPE
+    directory = tarfile.DIRTYPE
+    fifo = tarfile.FIFOTYPE
+    contiguous = tarfile.CONTTYPE
+
+
+class InspectItem(TypedDict):
+    file: str
+    size: int
+    type: Optional[str]
+    mode: Optional[int]
+    uid: Optional[int]
+    gid: Optional[int]
+    mtime: Optional[int]
+    path: Optional[str]
+
+
+TAR_EXTENSIONS = (".tar.xz", ".tar.gz", ".tar.bz2", ".tar")
