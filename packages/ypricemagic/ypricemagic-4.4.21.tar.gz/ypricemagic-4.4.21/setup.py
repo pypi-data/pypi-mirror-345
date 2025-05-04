e1a88b0c0e9bd2e54fe0cd618dd76e03d33a88fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-04 04:06:28 +0000

    Add package ypricemagic/4.4.21/ypricemagic-4.4.21.tar.gz

---
+++ packages/ypricemagic/ypricemagic-4.4.21.tar.gz/ypricemagic-4.4.21/setup.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+from setuptools import find_packages, setup
+
+with open("requirements.txt", "r") as f:
+    requirements = list(map(str.strip, f.read().split("\n")))[:-1]
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name="ypricemagic",
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    use_scm_version={
+        "root": ".",
+        "relative_to": __file__,
+        "local_scheme": "no-local-version",
+        "version_scheme": "python-simplified-semver",
+    },
+    description="Use this tool to extract historical on-chain price data from an archive node. Shoutout to @bantg and @nymmrx for their awesome work on yearn-exporter that made this library possible.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="BobTheBuidler",
+    author_email="bobthebuidlerdefi@gmail.com",
+    url="https://github.com/BobTheBuidler/ypricemagic",
+    license="MIT",
+    install_requires=requirements,
+    setup_requires=[
+        "setuptools_scm",
+    ],
+    package_data={"y": ["py.typed"]},
+)
