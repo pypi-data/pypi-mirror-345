79e73dc754c5821baab2d98569ca043fd548ceac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-04 04:06:59 +0000

    Add package cbtope-2/1.1/cbtope_2-1.1.tar.gz

---
+++ packages/cbtope-2/cbtope_2-1.1.tar.gz/cbtope_2-1.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cbtope_2',
+    version='1.1',
+    packages=find_packages(),
+    install_requires=[
+        'joblib',
+        'scikit-learn',
+        'numpy',
+        'openpyxl',
+        'Bio'
+    ],
+    author='Nishant Kumar',
+    author_email='nishantk@iiitd.ac.in',
+    description='CBTope2: A tool for predicting interactivity of B-cell epitopes',
+    url='https://github.com/Megha2k/cbtope_2',
+    python_requires='>=3.7, <3.9',
+    include_package_data=True,
+    package_data={
+        'cbtope_2.models': ['*.pkl'],
+    },
+    entry_points={
+        'console_scripts': [
+            'cbtope2 = cbtope_2.standalone:main'
+        ],
+    },
+)
