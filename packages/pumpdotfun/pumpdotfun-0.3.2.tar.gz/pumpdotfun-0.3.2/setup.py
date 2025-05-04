47ff32eb9db66ee034ec4246db2f95d24e42f46c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-04 04:07:07 +0000

    Add package pumpdotfun/0.3.2/pumpdotfun-0.3.2.tar.gz

---
+++ packages/pumpdotfun/pumpdotfun-0.3.2.tar.gz/pumpdotfun-0.3.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pumpdotfun',
+    version='0.3.2',
+    packages=find_packages(),
+    install_requires=open('requirements.txt').read().splitlines(),
+    license='MIT',
+    description='pump.fun frontend API Wrapper for Python',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Melvin Luis',
+    author_email='whappiness183@gmail.com',
+    url='https://github.com/izzulafifteam/py-python.git',
+    project_urls={
+        "Source": "https://github.com/izzulafifteam/pumpdotfun",
+        "Documentation": "https://frontend-api.pump.fun/api"
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
