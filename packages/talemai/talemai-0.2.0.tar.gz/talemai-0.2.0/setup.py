9743bd871fd156cadeed066097767b296afb6b39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-04 15:05:45 +0000

    Add package talemai/0.2.0/talemai-0.2.0.tar.gz

---
+++ packages/talemai/talemai-0.2.0.tar.gz/talemai-0.2.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='talemai',
+    version='0.2.0',
+    author='Hemit Patel',
+    description='A description of your project',
+    packages=find_packages(include=('talemai')),
+    include_package_data=True,
+    python_requires='>=3.8',
+    install_requires=[
+        'click',
+        'aiofiles',
+        'PyPDF2',
+        'pypdf',
+        'pyfiglet',
+        'langchain',
+        'langchain-community',
+        'langchain-astradb',
+        'langchain-huggingface',
+    ],
+    entry_points={
+        'console_scripts': [
+            'talemai = talemai.__main__.py:main',  # Adjust path as needed
+        ],
+    },
+)
