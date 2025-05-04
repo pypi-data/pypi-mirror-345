f48d25c9a29b14a1a9cd319a118db243d7a95ea5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-04 03:26:36 +0000

    Add package artemis-model/0.1.89/artemis_model-0.1.89.tar.gz

---
+++ packages/artemis-model/artemis_model-0.1.89.tar.gz/artemis_model-0.1.89/artemis_model/redis/__init__.py
@@ -0,0 +1,5 @@
+"""Redis models."""
+
+from .zone_state import ZoneState, NowPlaying, SessionId, BucketId
+
+__all__ = ["ZoneState", "NowPlaying", "SessionId", "BucketId"]
---
+++ packages/artemis-model/artemis_model-0.1.89.tar.gz/artemis_model-0.1.89/artemis_model/schedule.py
@@ -0,0 +1,54 @@
+import uuid
+
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy.types import JSON, UUID
+
+from artemis_model.base import CustomBase, TimeStampMixin, AuditMixin, CustomSyncBase
+
+from sqlalchemy.ext.declarative import declared_attr
+
+
+class ScheduleMixin(TimeStampMixin, AuditMixin):
+    """
+    This table is used to store the schedule of a zone.
+    """
+
+    id: Mapped[int] = mapped_column(autoincrement=True, primary_key=True, index=True)
+    name: Mapped[str] = mapped_column(nullable=False)
+    zone_id: Mapped[int] = mapped_column(ForeignKey("zone.id"), index=True)
+    is_active: Mapped[bool] = mapped_column(default=True, nullable=False)
+    timesheet: Mapped[JSON] = mapped_column(JSON, nullable=True)
+    locked_by: Mapped[str] = mapped_column(default=None, nullable=True)
+    legacy_id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), nullable=True)
+
+    @declared_attr
+    def zone(cls) -> Mapped["Zone"]:
+        return relationship(back_populates="schedules")
+
+
+class ScheduleSync(CustomSyncBase, ScheduleMixin):
+    pass
+
+
+class Schedule(CustomBase, ScheduleMixin):
+    pass
+
+
+class SchedulePresetMixin(TimeStampMixin, AuditMixin):
+    """
+    This table is used to store preset schedules regarding location type.
+    """
+
+    id: Mapped[int] = mapped_column(autoincrement=True, primary_key=True, index=True)
+    type: Mapped[str] = mapped_column(nullable=False)
+    is_active: Mapped[bool] = mapped_column(default=True, nullable=False)
+    timesheet: Mapped[JSON] = mapped_column(JSON, nullable=True)
+
+
+class SchedulePresetSync(CustomSyncBase, SchedulePresetMixin):
+    pass
+
+
+class SchedulePreset(CustomBase, SchedulePresetMixin):
+    pass
