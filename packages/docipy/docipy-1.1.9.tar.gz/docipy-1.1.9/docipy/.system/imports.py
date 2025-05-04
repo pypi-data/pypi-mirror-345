3d7f346211c08a69abd3dcc3c73b7bfac2bec4ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-04 03:26:27 +0000

    Add package docipy/1.1.9/docipy-1.1.9.tar.gz

---
+++ packages/docipy/docipy-1.1.9.tar.gz/docipy-1.1.9/docipy/.system/imports.py
@@ -0,0 +1,14 @@
+from clight.system.importer import cli  # DON'T REMOVE THIS LINE
+
+import os
+import re
+import sys
+import yaml
+import json
+import time
+import string
+import shutil
+import random
+import datetime
+import markdown
+import webbrowser
