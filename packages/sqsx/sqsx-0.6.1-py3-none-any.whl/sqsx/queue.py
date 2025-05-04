ddcd08c0ff7c8f8d338f42ef6c2f5bcd430bbc7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-04 03:26:00 +0000

    Add package sqsx/0.6.1/sqsx-0.6.1-py3-none-any.whl

---
+++ packages/sqsx/sqsx-0.6.1-py3-none-any.whl/sqsx/queue.py
@@ -0,0 +1,200 @@
+import logging
+import signal
+import time
+from concurrent.futures import ThreadPoolExecutor, wait
+from types import FrameType
+from typing import Any, Callable, Optional
+
+from pydantic import BaseModel, Field, PrivateAttr
+
+from sqsx.exceptions import NoRetry, Retry
+from sqsx.helper import backoff_calculator_seconds, base64_to_dict, dict_to_base64
+
+logger = logging.getLogger(__name__)
+queue_url_regex = r"(http|https)[:][\/]{2}[a-zA-Z0-9-_:.]+[\/][0-9]{12}[\/]{1}[a-zA-Z0-9-_]{0,80}"
+
+
+class BaseQueueMixin:
+    url: str
+    sqs_client: Any
+    min_backoff_seconds: int
+    max_backoff_seconds: int
+    _consume_message: Any
+
+    def consume_messages(
+        self,
+        max_messages: int = 1,
+        max_threads: int = 1,
+        wait_seconds: int = 10,
+        polling_wait_seconds: int = 10,
+        run_forever: bool = True,
+        enable_signal_to_exit_gracefully: bool = True,
+    ) -> None:
+        logger.info(f"Starting consuming tasks, queue_url={self.url}")
+
+        if enable_signal_to_exit_gracefully:
+            signal.signal(signal.SIGINT, self._exit_gracefully_from_signal)
+            signal.signal(signal.SIGTERM, self._exit_gracefully_from_signal)
+
+        while True:
+            if self._should_consume_tasks_stop:
+                logger.info(f"Stopping consuming tasks, queue_url={self.url}")
+                break
+
+            response = self.sqs_client.receive_message(
+                QueueUrl=self.url,
+                AttributeNames=["All"],
+                MaxNumberOfMessages=min(max_messages, 10),
+                MessageAttributeNames=["All"],
+                WaitTimeSeconds=polling_wait_seconds,
+            )
+
+            sqs_messages = response.get("Messages", [])
+            if not sqs_messages:
+                logger.debug(
+                    f"Waiting some seconds because no message was received, wait_seconds={wait_seconds}, "
+                    f"polling_wait_seconds={polling_wait_seconds}, queue_url={self.url}"
+                )
+                time.sleep(wait_seconds)
+                continue
+
+            with ThreadPoolExecutor(max_workers=max_threads) as executor:
+                futures = []
+                for sqs_message in sqs_messages:
+                    futures.append(executor.submit(self._consume_message, sqs_message))
+                wait(futures)
+
+            if not run_forever:
+                break
+
+    def exit_gracefully(self) -> None:
+        logger.info(f"Starting graceful shutdown process, queue_url={self.url}")
+        self._should_consume_tasks_stop = True
+
+    def _exit_gracefully_from_signal(self, signal: int, frame: Optional[FrameType]):
+        self.exit_gracefully()
+
+    def _message_ack(self, sqs_message: dict) -> None:
+        receipt_handle = sqs_message["ReceiptHandle"]
+        self.sqs_client.delete_message(QueueUrl=self.url, ReceiptHandle=receipt_handle)
+
+    def _message_nack(
+        self,
+        sqs_message: dict,
+        min_backoff_seconds: Optional[int] = None,
+        max_backoff_seconds: Optional[int] = None,
+    ) -> None:
+        min_backoff_seconds = min_backoff_seconds if min_backoff_seconds else self.min_backoff_seconds
+        max_backoff_seconds = max_backoff_seconds if max_backoff_seconds else self.max_backoff_seconds
+        receipt_handle = sqs_message["ReceiptHandle"]
+        receive_count = int(sqs_message["Attributes"]["ApproximateReceiveCount"]) - 1
+        timeout = backoff_calculator_seconds(receive_count, min_backoff_seconds, max_backoff_seconds)
+        self.sqs_client.change_message_visibility(
+            QueueUrl=self.url, ReceiptHandle=receipt_handle, VisibilityTimeout=timeout
+        )
+
+
+class Queue(BaseModel, BaseQueueMixin):
+    url: str = Field(pattern=queue_url_regex)
+    sqs_client: Any
+    min_backoff_seconds: int = Field(default=30)
+    max_backoff_seconds: int = Field(default=900)
+    _handlers: dict[str, Callable] = PrivateAttr(default={})
+    _should_consume_tasks_stop: bool = PrivateAttr(default=False)
+
+    def add_task(self, task_name: str, **task_kwargs) -> dict:
+        return self.sqs_client.send_message(
+            QueueUrl=self.url,
+            MessageAttributes={"TaskName": {"DataType": "String", "StringValue": task_name}},
+            MessageBody=dict_to_base64({"kwargs": task_kwargs}),
+        )
+
+    def add_task_handler(self, task_name: str, task_handler_function: Callable) -> None:
+        self._handlers.update({task_name: task_handler_function})
+
+    def _consume_message(self, sqs_message: dict) -> None:
+        message_id = sqs_message["MessageId"]
+        task_name_attribute = sqs_message["MessageAttributes"].get("TaskName")
+        if task_name_attribute is None:
+            logger.warning(f"Message without TaskName attribute, message_id={message_id}")
+            return self._message_nack(sqs_message)
+
+        task_name = task_name_attribute["StringValue"]
+        task_handler_function = self._handlers.get(task_name)
+        if task_handler_function is None:
+            logger.warning(f"Task handler not found, message_id={message_id}, task_name={task_name}")
+            return self._message_nack(sqs_message)
+
+        try:
+            message_data = base64_to_dict(sqs_message["Body"])
+        except Exception:
+            logger.exception(f"Invalid message body, message_id={message_id}, task_name={task_name}")
+            return self._message_nack(sqs_message)
+
+        kwargs = message_data["kwargs"]
+        context = {
+            "queue_url": self.url,
+            "task_name": task_name,
+            "sqs_message": sqs_message,
+        }
+
+        try:
+            task_handler_function(context, **kwargs)
+        except Retry as exc:
+            logger.info(
+                f"Received an sqsx.Retry, setting a custom backoff policy, message_id={message_id}, task_name={task_name}"
+            )
+            return self._message_nack(
+                sqs_message,
+                min_backoff_seconds=exc.min_backoff_seconds,
+                max_backoff_seconds=exc.max_backoff_seconds,
+            )
+        except NoRetry:
+            logger.info(
+                f"Received an sqsx.NoRetry, removing the task, message_id={message_id}, task_name={task_name}"
+            )
+            return self._message_ack(sqs_message)
+        except Exception:
+            logger.exception(f"Error while processing, message_id={message_id}, task_name={task_name}")
+            return self._message_nack(sqs_message)
+
+        self._message_ack(sqs_message)
+
+
+class RawQueue(BaseModel, BaseQueueMixin):
+    url: str = Field(pattern=queue_url_regex)
+    message_handler_function: Callable
+    sqs_client: Any
+    min_backoff_seconds: int = Field(default=30)
+    max_backoff_seconds: int = Field(default=900)
+    _should_consume_tasks_stop: bool = PrivateAttr(default=False)
+
+    def add_message(self, message_body: str, message_attributes: Optional[dict] = None) -> dict:
+        if message_attributes is None:
+            message_attributes = {}
+        return self.sqs_client.send_message(
+            QueueUrl=self.url,
+            MessageAttributes=message_attributes,
+            MessageBody=message_body,
+        )
+
+    def _consume_message(self, sqs_message: dict) -> None:
+        message_id = sqs_message["MessageId"]
+
+        try:
+            self.message_handler_function(self.url, sqs_message)
+        except Retry as exc:
+            logger.info(f"Received an sqsx.Retry, setting a custom backoff policy, message_id={message_id}")
+            return self._message_nack(
+                sqs_message,
+                min_backoff_seconds=exc.min_backoff_seconds,
+                max_backoff_seconds=exc.max_backoff_seconds,
+            )
+        except NoRetry:
+            logger.info(f"Received an sqsx.NoRetry, removing the message, message_id={message_id}")
+            return self._message_ack(sqs_message)
+        except Exception:
+            logger.exception(f"Error while processing, message_id={message_id}")
+            return self._message_nack(sqs_message)
+
+        self._message_ack(sqs_message)
---
+++ packages/sqsx/sqsx-0.6.1-py3-none-any.whl/tests/test_queue.py
@@ -0,0 +1,300 @@
+import os
+import re
+import signal
+import threading
+import time
+from unittest import mock
+
+import pytest
+
+from sqsx.exceptions import NoRetry, Retry
+from sqsx.queue import queue_url_regex
+
+
+def task_handler(context, a, b, c):
+    print(f"context={context}, a={a}, b={b}, c={c}")
+
+
+def exception_handler(context, a, b, c):
+    raise Exception("BOOM!")
+
+
+def retry_exception_handler(context, a, b, c):
+    raise Retry(min_backoff_seconds=100, max_backoff_seconds=200)
+
+
+def no_retry_exception_handler(context, a, b, c):
+    raise NoRetry()
+
+
+def raw_exception_handler(queue_url, sqs_message):
+    raise Exception("BOOM!")
+
+
+def raw_retry_exception_handler(queue_url, sqs_message):
+    raise Retry(min_backoff_seconds=100, max_backoff_seconds=200)
+
+
+def raw_no_retry_exception_handler(queue_url, sqs_message):
+    raise NoRetry()
+
+
+def trigger_signal():
+    pid = os.getpid()
+    time.sleep(0.2)
+    os.kill(pid, signal.SIGINT)
+
+
+class SumHandler:
+    result_sum = 0
+
+    def __call__(self, context, a, b, c):
+        self.result_sum += a + b + c
+
+
+class CallCountHandler:
+    call_count = 0
+
+    def __call__(self, queue_url, sqs_message):
+        self.call_count += 1
+
+
+@pytest.mark.parametrize(
+    "queue_url,expected",
+    [
+        ("https://sqs.us-east-1.amazonaws.com/177715257436", False),
+        ("https://sqs.us-east-1.amazonaws.com/1/MyQueue", False),
+        ("https://sqs.us-east-1.amazonaws.com/MyQueue", False),
+        ("http://localhost:9324/000000000000/tests", True),
+        ("https://localhost:9324/000000000000/tests", True),
+        ("https://sqs.us-east-1.amazonaws.com/177715257436/MyQueue", True),
+    ],
+)
+def test_queue_url_regex(queue_url, expected):
+    result = True if re.search(queue_url_regex, queue_url) else False
+    assert result == expected
+
+
+def test_queue_add_task_handler(queue):
+    assert queue._handlers == {}
+
+    queue.add_task_handler("my_task", task_handler)
+    assert queue._handlers == {"my_task": task_handler}
+
+    queue.add_task_handler("my_other_task", task_handler)
+    assert queue._handlers == {"my_task": task_handler, "my_other_task": task_handler}
+
+    queue.add_task_handler("my_task", task_handler)
+    assert queue._handlers == {"my_task": task_handler, "my_other_task": task_handler}
+
+
+def test_queue_add_task(queue):
+    expected_md5_message_body = "8087eb7436895841c5d646156a8a469f"
+    expected_md5_message_attribute = "5346f2cd7c539a880febaf9112a86921"
+    response = queue.add_task("my_task", a=1, b=2, c=3)
+
+    assert response["ResponseMetadata"]["HTTPStatusCode"] == 200
+    assert response["MD5OfMessageBody"] == expected_md5_message_body
+    assert response["MD5OfMessageAttributes"] == expected_md5_message_attribute
+
+
+def test_queue_consume_message_without_task_name_attribute(queue, sqs_message, caplog):
+    queue._message_nack = mock.MagicMock()
+    sqs_message["MessageAttributes"].pop("TaskName")
+
+    queue._consume_message(sqs_message)
+
+    queue._message_nack.assert_called_once_with(sqs_message)
+    assert caplog.record_tuples == [
+        (
+            "sqsx.queue",
+            30,
+            "Message without TaskName attribute, message_id=33425f12-50e6-4f93-ac26-7ae7a069cf88",
+        )
+    ]
+
+
+def test_queue_consume_message_without_task_handler(queue, sqs_message, caplog):
+    queue._message_nack = mock.MagicMock()
+
+    queue._consume_message(sqs_message)
+
+    queue._message_nack.assert_called_once_with(sqs_message)
+    assert caplog.record_tuples == [
+        (
+            "sqsx.queue",
+            30,
+            "Task handler not found, message_id=33425f12-50e6-4f93-ac26-7ae7a069cf88, task_name=my_task",
+        )
+    ]
+
+
+def test_queue_consume_message_with_invalid_body(queue, sqs_message, caplog):
+    queue._message_nack = mock.MagicMock()
+    sqs_message["Body"] = "invalid-body"
+
+    queue.add_task_handler("my_task", task_handler)
+    queue._consume_message(sqs_message)
+
+    queue._message_nack.assert_called_once_with(sqs_message)
+    assert caplog.record_tuples == [
+        (
+            "sqsx.queue",
+            40,
+            "Invalid message body, message_id=33425f12-50e6-4f93-ac26-7ae7a069cf88, task_name=my_task",
+        )
+    ]
+
+
+def test_queue_consume_message_with_task_handler_exception(queue, sqs_message, caplog):
+    queue._message_nack = mock.MagicMock()
+
+    queue.add_task_handler("my_task", exception_handler)
+    queue._consume_message(sqs_message)
+
+    queue._message_nack.assert_called_once_with(sqs_message)
+    assert caplog.record_tuples == [
+        (
+            "sqsx.queue",
+            40,
+            "Error while processing, message_id=33425f12-50e6-4f93-ac26-7ae7a069cf88, task_name=my_task",
+        )
+    ]
+
+
+def test_queue_consume_messages(queue):
+    handler = SumHandler()
+
+    queue.add_task_handler("my_task", handler)
+    queue.add_task("my_task", a=1, b=2, c=3)
+    queue.add_task("my_task", a=1, b=2, c=3)
+
+    queue.consume_messages(max_messages=2, max_threads=2, run_forever=False)
+
+    assert handler.result_sum == 12
+
+
+def test_queue_consume_messages_with_task_handler_exception(queue, caplog):
+    queue.add_task_handler("my_task", exception_handler)
+    queue.add_task("my_task", a=1, b=2, c=3)
+
+    queue.consume_messages(run_forever=False)
+
+    assert caplog.record_tuples[1][0] == "sqsx.queue"
+    assert caplog.record_tuples[1][1] == 40
+    assert "Error while processing" in caplog.record_tuples[1][2]
+
+
+def test_queue_consume_messages_with_task_handler_retry_exception(queue, caplog):
+    queue.add_task_handler("my_task", retry_exception_handler)
+    queue.add_task("my_task", a=1, b=2, c=3)
+
+    queue.consume_messages(run_forever=False)
+
+    assert caplog.record_tuples[1][0] == "sqsx.queue"
+    assert caplog.record_tuples[1][1] == 20
+    assert "Received an sqsx.Retry, setting a custom backoff policy" in caplog.record_tuples[1][2]
+
+
+def test_queue_consume_messages_with_task_handler_no_retry_exception(queue, caplog):
+    queue.add_task_handler("my_task", no_retry_exception_handler)
+    queue.add_task("my_task", a=1, b=2, c=3)
+
+    queue.consume_messages(run_forever=False)
+
+    assert caplog.record_tuples[1][0] == "sqsx.queue"
+    assert caplog.record_tuples[1][1] == 20
+    assert "Received an sqsx.NoRetry, removing the task" in caplog.record_tuples[1][2]
+
+
+def test_queue_exit_gracefully(queue):
+    thread = threading.Thread(target=trigger_signal)
+    thread.daemon = True
+    thread.start()
+    handler = SumHandler()
+
+    queue.add_task_handler("my_task", handler)
+    queue.add_task("my_task", a=1, b=2, c=3)
+
+    queue.consume_messages(
+        wait_seconds=1, polling_wait_seconds=0, run_forever=True, enable_signal_to_exit_gracefully=True
+    )
+
+    assert handler.result_sum == 6
+
+
+def test_raw_queue_add_message(raw_queue):
+    expected_md5_message_body = "069840f6917e85a02167febb964f0041"
+    expected_md5_message_attribute = "90f34a800b9d242c1b32320e4a3ed630"
+    response = raw_queue.add_message(
+        message_body="My Message",
+        message_attributes={"Attr1": {"DataType": "String", "StringValue": "Attr1"}},
+    )
+
+    assert response["ResponseMetadata"]["HTTPStatusCode"] == 200
+    assert response["MD5OfMessageBody"] == expected_md5_message_body
+    assert response["MD5OfMessageAttributes"] == expected_md5_message_attribute
+
+
+def test_raw_queue_consume_messages(raw_queue):
+    handler = CallCountHandler()
+    raw_queue.message_handler_function = handler
+
+    raw_queue.add_message(message_body="Message Body")
+    raw_queue.add_message(message_body="Message Body")
+    raw_queue.add_message(message_body="Message Body")
+
+    raw_queue.consume_messages(max_messages=3, max_threads=3, run_forever=False)
+
+    assert handler.call_count == 3
+
+
+def test_raw_queue_consume_messages_with_message_handler_exception(raw_queue, caplog):
+    raw_queue.message_handler_function = raw_exception_handler
+
+    raw_queue.add_message(message_body="Message Body")
+    raw_queue.consume_messages(run_forever=False)
+
+    assert caplog.record_tuples[1][0] == "sqsx.queue"
+    assert caplog.record_tuples[1][1] == 40
+    assert "Error while processing" in caplog.record_tuples[1][2]
+
+
+def test_raw_queue_consume_messages_with_message_handler_retry_exception(raw_queue, caplog):
+    raw_queue.message_handler_function = raw_retry_exception_handler
+
+    raw_queue.add_message(message_body="Message Body")
+    raw_queue.consume_messages(run_forever=False)
+
+    assert caplog.record_tuples[1][0] == "sqsx.queue"
+    assert caplog.record_tuples[1][1] == 20
+    assert "Received an sqsx.Retry, setting a custom backoff policy" in caplog.record_tuples[1][2]
+
+
+def test_raw_queue_consume_messages_with_message_handler_no_retry_exception(raw_queue, caplog):
+    raw_queue.message_handler_function = raw_no_retry_exception_handler
+
+    raw_queue.add_message(message_body="Message Body")
+    raw_queue.consume_messages(run_forever=False)
+
+    assert caplog.record_tuples[1][0] == "sqsx.queue"
+    assert caplog.record_tuples[1][1] == 20
+    assert "Received an sqsx.NoRetry, removing the message" in caplog.record_tuples[1][2]
+
+
+def test_raw_queue_exit_gracefully(raw_queue):
+    thread = threading.Thread(target=trigger_signal)
+    thread.daemon = True
+    thread.start()
+    handler = handler = CallCountHandler()
+    raw_queue.message_handler_function = handler
+
+    raw_queue.add_message(message_body="Message Body")
+    raw_queue.add_message(message_body="Message Body")
+    raw_queue.add_message(message_body="Message Body")
+
+    raw_queue.consume_messages(
+        wait_seconds=1, polling_wait_seconds=0, run_forever=True, enable_signal_to_exit_gracefully=True
+    )
+
+    assert handler.call_count == 3
