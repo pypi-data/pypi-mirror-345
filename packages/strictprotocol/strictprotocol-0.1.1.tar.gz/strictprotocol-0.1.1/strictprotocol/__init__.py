029100294745b67c77b01e07743720e9a982f6f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-04 04:06:43 +0000

    Add package strictprotocol/0.1.1/strictprotocol-0.1.1.tar.gz

---
+++ packages/strictprotocol/strictprotocol-0.1.1.tar.gz/strictprotocol-0.1.1/strictprotocol/__init__.py
@@ -0,0 +1,3 @@
+from .core import StrictProtocol, is_signature_compatible, CheckMode
+from .safe_typing import safe_subtype, safe_isinstance
+__all__ = ["StrictProtocol", "is_signature_compatible", "CheckMode", "safe_subtype", "safe_isinstance"]
