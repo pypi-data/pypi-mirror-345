fed8497e6b60e72c4d1a0cec9f17d319e2062247
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-04 03:26:27 +0000

    Add package fixify/0.0.7/fixify-0.0.7-py3-none-any.whl

---
+++ packages/fixify/fixify-0.0.7-py3-none-any.whl/fixify/cli.py
@@ -0,0 +1,17 @@
+import click
+from fixify import fixify  # assuming your class is in fixify.py
+
+@click.command()
+@click.option('--heyai', help='Ask something to your AI buddy')
+@click.option('--explain', nargs=3, type=str, help='Explain code from a file: filepath start_line end_line')
+def cli(heyai, explain):
+    fix = fixify()
+
+    if heyai:
+        fix.heyAI(heyai)
+    elif explain:
+        filepath, start_line, end_line = explain
+        fix.explainFromFile(filepath, int(start_line), int(end_line))
+
+if __name__ == '__main__':
+    cli()
