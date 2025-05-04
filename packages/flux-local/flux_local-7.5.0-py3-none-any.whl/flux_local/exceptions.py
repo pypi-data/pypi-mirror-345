13534594dc5016763b16842c1af315c23769c1a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-04 04:06:31 +0000

    Add package flux-local/7.5.0/flux_local-7.5.0-py3-none-any.whl

---
+++ packages/flux-local/flux_local-7.5.0-py3-none-any.whl/flux_local/exceptions.py
@@ -0,0 +1,39 @@
+"""Exceptions related to flux-local."""
+
+__all__ = [
+    "FluxException",
+    "InputException",
+    "CommandException",
+]
+
+
+class FluxException(Exception):
+    """Generic base exception used for this library."""
+
+
+class InputException(FluxException):
+    """Raised when the input files or values are not formatted as expected."""
+
+
+class CommandException(FluxException):
+    """Raised when there is a failure running a subcommand."""
+
+
+class KustomizeException(CommandException):
+    """Raised when there is a failure running a kustomize command."""
+
+
+class KustomizePathException(CommandException):
+    """Raised a Kustomization points to a path that does not exist."""
+
+
+class HelmException(CommandException):
+    """Raised when there is a failure running a helm command."""
+
+
+class InvalidValuesReference(FluxException):
+    """Exception raised for an unsupported ValuesReference."""
+
+
+class InvalidSubstituteReference(FluxException):
+    """Exception raised for an unsupported SubstituteReference."""
