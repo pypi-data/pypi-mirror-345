10295a833e4eccf88c1b083901c4727e8224342f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-05 14:04:48 +0000

    Add package agentize/0.0.8/agentize-0.0.8-py3-none-any.whl

---
+++ packages/agentize/agentize-0.0.8-py3-none-any.whl/agentize/__init__.py
@@ -0,0 +1,10 @@
+import os
+import sys
+from typing import Final
+
+from loguru import logger
+
+from .lazy import lazy_run
+
+LOGURU_LEVEL: Final[str] = os.getenv("LOGURU_LEVEL", "INFO")
+logger.configure(handlers=[{"sink": sys.stderr, "level": LOGURU_LEVEL}])
