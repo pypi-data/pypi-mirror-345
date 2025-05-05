7c654aa82e546aa48e392a4a42f1e7f7fa5949ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-05 14:04:47 +0000

    Add package finalsa-s3-client/0.1.6/finalsa_s3_client-0.1.6-py3-none-any.whl

---
+++ packages/finalsa-s3-client/finalsa_s3_client-0.1.6-py3-none-any.whl/finalsa/s3/client/__init__.py
@@ -0,0 +1,10 @@
+from finalsa.s3.client.client import S3ClientImpl
+from finalsa.s3.client.tests import S3ClientTest, Document
+from finalsa.s3.client.interface import S3Client
+
+__all__ = [
+    "S3ClientImpl",
+    "S3ClientTest",
+    "S3Client",
+    "Document",
+]
