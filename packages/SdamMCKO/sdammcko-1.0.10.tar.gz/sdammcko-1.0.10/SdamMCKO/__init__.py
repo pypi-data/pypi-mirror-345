f318a71665fd80abbd35f5b47f92414a5f51cdbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-05 05:04:19 +0000

    Add package SdamMCKO/1.0.10/sdammcko-1.0.10.tar.gz

---
+++ packages/SdamMCKO/sdammcko-1.0.10.tar.gz/sdammcko-1.0.10/SdamMCKO/__init__.py
@@ -0,0 +1,26 @@
+f"""
+
+"""
+
+def q_help(N:int) -> str:
+    """
+
+    """
+    try:
+        file = open('Guide.md', 'r', encoding='utf-8')
+        content = f"".join(file.read()).split("##")
+        print(content[N])
+    except:
+        try:
+            file =  open('SdamMCKO/Guide.md', 'r', encoding='utf-8')
+            content = f"".join(file.read()).split("##")
+            print(content[N])
+        except FileNotFoundError as er:
+            print(f"Немного не повезло... Попытайтесь открыть папку из следующего сообщения /n"+ f"{er}")
+
+
+
+if __name__ == "__main__":
+
+    q_help(2)
+
