79488f4a51025ddad9cf4b4fb0f8976adaa150b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-05 04:06:03 +0000

    Add package yoreek-homeassistant/2025.4.5b1/yoreek_homeassistant-2025.4.5b1-py3-none-any.whl

---
+++ packages/yoreek-homeassistant/yoreek_homeassistant-2025.4.5b1-py3-none-any.whl/homeassistant/components/time/const.py
@@ -0,0 +1,9 @@
+"""Provides the constants needed for the component."""
+
+DOMAIN = "time"
+
+SERVICE_SET_VALUE = "set_value"
+
+ATTR_ENABLE_SECOND = "enable_second"
+
+DEFAULT_ENABLE_SECOND_VALUE = False
