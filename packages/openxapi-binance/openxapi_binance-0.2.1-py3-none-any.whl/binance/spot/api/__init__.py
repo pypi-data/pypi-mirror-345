1dc91c796ec92e010ec70847ed703ead06ef55a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-05 14:04:50 +0000

    Add package openxapi-binance/0.2.1/openxapi_binance-0.2.1-py3-none-any.whl

---
+++ packages/openxapi-binance/openxapi_binance-0.2.1-py3-none-any.whl/binance/spot/api/__init__.py
@@ -0,0 +1,14 @@
+# flake8: noqa
+
+# import apis into api package
+from binance.spot.api.algo_trading_api import AlgoTradingApi
+from binance.spot.api.binance_link_api import BinanceLinkApi
+from binance.spot.api.convert_api import ConvertApi
+from binance.spot.api.copy_trading_api import CopyTradingApi
+from binance.spot.api.futures_data_api import FuturesDataApi
+from binance.spot.api.margin_trading_api import MarginTradingApi
+from binance.spot.api.portfolio_margin_pro_api import PortfolioMarginProApi
+from binance.spot.api.spot_trading_api import SpotTradingApi
+from binance.spot.api.sub_account_api import SubAccountApi
+from binance.spot.api.wallet_api import WalletApi
+
