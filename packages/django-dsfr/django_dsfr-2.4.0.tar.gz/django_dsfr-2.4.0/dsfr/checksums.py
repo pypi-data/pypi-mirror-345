a7a8ef41370397d3bfac27472d5ee1f48419bfb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-05 16:05:13 +0000

    Add package django-dsfr/2.4.0/django_dsfr-2.4.0.tar.gz

---
+++ packages/django-dsfr/django_dsfr-2.4.0.tar.gz/django_dsfr-2.4.0/dsfr/checksums.py
@@ -0,0 +1,24 @@
+# Integrity checks for the css/js/favicon files
+# Do not update manually!
+# Generated with the following command:
+# python manage.py integrity_checksums
+
+# dsfr/static/dsfr/dist/dsfr/dsfr.module.min.js
+INTEGRITY_JS_MODULE = (
+    "sha384-l8g9Hsy5GI9+gVYLHCrBoaD2RBoVqjZ189s6ycWnyUMlBc4fJHgDv6Vy6srjghNj"
+)
+
+# dsfr/static/dsfr/dist/dsfr/dsfr.nomodule.min.js
+INTEGRITY_JS_NOMODULE = (
+    "sha384-n3DkLEpXk5jDW26QT+Rsm0VCNrSPDKpmFcCVIwlEkl9I6MgKB89KDVWuDiJpFktn"
+)
+
+# dsfr/static/dsfr/dist/dsfr/dsfr.min.css
+INTEGRITY_CSS = (
+    "sha384-hglg7vJT63v4rqjYrTSDzREjGYZb//5qqtcs8/7Ng8v1ihSg4LgdB+5lYdzAZXOt"
+)
+
+# dsfr/static/dsfr/dist/utility/utility.min.css
+INTEGRITY_UTILITY_CSS = (
+    "sha384-OF3yqHNd9CODR/aJcL4+uKOtkKyHvJxfNMO3eBRtIZpFy/iMuNJCyd0HbVArmrUC"
+)
