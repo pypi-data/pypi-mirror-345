9f044f4e483ba3925db2db1b66043ec282051fb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-05 15:04:59 +0000

    Add package localcosmos-app-kit-taxonomy/0.2.3/localcosmos_app_kit_taxonomy-0.2.3.tar.gz

---
+++ packages/localcosmos-app-kit-taxonomy/localcosmos_app_kit_taxonomy-0.2.3.tar.gz/localcosmos_app_kit_taxonomy-0.2.3/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+install_requires = [
+
+]
+
+setup(
+    name='localcosmos_app_kit_taxonomy',
+    version='0.2.3',
+    description='Local Cosmos Taxonomy django app',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='The MIT License',
+    platforms=['OS Independent'],
+    keywords='django, localcosmos, localcosmos server, biodiversity, taxonomy',
+    author='Thomas Uher',
+    author_email='thomas.uher@sisol-systems.com',
+    url='https://github.com/localcosmos/app-kit',
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    include_package_data=True,
+    install_requires=install_requires,
+)
