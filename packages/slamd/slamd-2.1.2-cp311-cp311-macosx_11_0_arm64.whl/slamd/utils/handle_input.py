2ae03d01df07cd563823b65d9ad5d1423cb74274
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-05 04:05:44 +0000

    Add package slamd/2.1.2/slamd-2.1.2-cp311-cp311-macosx_11_0_arm64.whl

---
+++ packages/slamd/slamd-2.1.2-cp311-cp311-macosx_11_0_arm64.whl/slamd/utils/handle_input.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+
+def process_radii(
+    radii: np.ndarray | float, 
+    n: int
+) -> np.ndarray:
+    if isinstance(radii, float) or isinstance(radii, int):
+        radii_np = np.full((n,), float(radii), dtype=np.float32)
+    elif isinstance(radii, np.ndarray):
+        if radii.shape == (n,):
+            radii_np = radii.astype(np.float32)
+        else:
+            raise ValueError(f"Invalid radii shape: {radii.shape}")
+    else:
+        raise TypeError("radii must be a float or a 1D np.ndarray")
+
+    return radii_np
+
+
+def process_color(
+    colors: np.ndarray | tuple[int, int, int], 
+    n: int
+) -> np.ndarray:
+    if isinstance(colors, tuple):
+        colors_np = np.tile(np.array(colors, dtype=np.float32) / 255.0, (n, 1))
+    elif isinstance(colors, np.ndarray):
+        colors = colors.astype(np.float32)
+        if colors.max() > 1.0:
+            colors /= 255.0
+        if colors.shape == (3,) or colors.shape == (1, 3):
+            colors_np = np.tile(colors.reshape(1, 3), (n, 1))
+        elif colors.shape == (n, 3):
+            colors_np = colors
+        else:
+            raise ValueError(f"Invalid color shape: {colors.shape}")
+    else:
+        raise TypeError("colors must be an np.ndarray or a (R, G, B) tuple")
+
+    return colors_np
