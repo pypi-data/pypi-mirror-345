4bc0eb01c031770524c0ed0420f73a1d3ce81665
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-05 03:23:46 +0000

    Add package ht-serve/0.0.3/ht_serve-0.0.3.tar.gz

---
+++ packages/ht-serve/ht_serve-0.0.3.tar.gz/ht_serve-0.0.3/ht_serve/__init__.py
@@ -0,0 +1,15 @@
+"""
+HT-Serve Secure HTTPS Dev Server
+
+Author: HermiTech Labs
+License: MIT
+"""
+
+__version__ = "1.0.2"
+
+def info():
+    return {
+        "name": "HT-Serve",
+        "version": __version__,
+        "description": "Secure HTTPS live reload server for dashboard prototyping."
+    }
