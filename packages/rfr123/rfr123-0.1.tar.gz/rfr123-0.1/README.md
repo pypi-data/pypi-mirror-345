c19603e8abfcf3a5b3565af1842c5ce7189244d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-05 04:05:43 +0000

    Add package rfr123/0.1/rfr123-0.1.tar.gz

---
+++ packages/rfr123/rfr123-0.1.tar.gz/rfr123-0.1/README.md
@@ -0,0 +1,10 @@
+# rfr123
+
+Downloads `RFR.Java` from GitHub to your current folder.
+
+## Usage
+
+```bash
+pip install rfr123
+rfr123
+```
---
+++ packages/rfr123/rfr123-0.1.tar.gz/rfr123-0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name="rfr123",
+    version="0.1",
+    packages=["rfr123"],
+    entry_points={
+        'console_scripts': [
+            'rfr123=rfr123.download_file:download',
+        ],
+    },
+    author="Rajesh",
+    description="Download RFR.Java from GitHub",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    license="MIT",
+    python_requires=">=3.6",
+)
