d7bfe8af7d3cb05f5dbd6e649cd46e7e8ca0c126
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-05 03:23:33 +0000

    Add package octodns-etchosts/1.0.0/octodns_etchosts-1.0.0.tar.gz

---
+++ packages/octodns-etchosts/octodns_etchosts-1.0.0.tar.gz/octodns_etchosts-1.0.0/octodns_etchosts/__init__.py
@@ -0,0 +1,181 @@
+#
+#
+#
+
+import re
+from collections import defaultdict
+from logging import getLogger
+from os import makedirs, path
+from os.path import isdir
+
+from octodns.provider.base import BaseProvider
+
+# TODO: remove __VERSION__ with the next major version release
+__version__ = __VERSION__ = '1.0.0'
+
+
+def _wildcard_match(fqdn, wildcards):
+    for _, _, regex, record in wildcards:
+        if regex.match(fqdn):
+            return record
+    return None
+
+
+class EtcHostsProvider(BaseProvider):
+    SUPPORTS_GEO = False
+    SUPPORTS_DYNAMIC = False
+    SUPPORTS = set(('A', 'AAAA', 'ALIAS', 'CNAME'))
+
+    def __init__(self, id, directory, *args, **kwargs):
+        self.log = getLogger(f'EtcHostsProvider[{id}]')
+        self.log.debug('__init__: id=%s, directory=%s', id, directory)
+        super().__init__(id, *args, **kwargs)
+        self.directory = directory
+
+        self._expected_zones = set()
+        self._records = defaultdict(list)
+        self._wildcards = []
+        self._zones = []
+
+        self._a_values = {}
+        self._aaaa_values = {}
+        self._cname_values = {}
+        self._a_wildcards = []
+        self._aaaa_wildcards = []
+        self._cname_wildcards = []
+
+    def populate(self, zone, target=False, lenient=False):
+        self.log.debug(
+            'populate: name=%s, target=%s, lenient=%s',
+            zone.name,
+            target,
+            lenient,
+        )
+
+        self._expected_zones.add(zone.name)
+
+        # We never act as a source, at least for now, if/when we do we still
+        # need to noop `if target`
+        return False
+
+    def _write(self):
+        if not isdir(self.directory):
+            makedirs(self.directory)
+
+        # Resolve all the records
+        for zone in self._zones:
+            name = zone.name
+            filepath = path.join(self.directory, name)
+            filename = f'{filepath}hosts'
+            self.log.info('_apply: filename=%s', filename)
+            with open(filename, 'w') as fh:
+                fh.write('##################################################\n')
+                fh.write(f'# octoDNS {self.id} {name}\n')
+                fh.write(
+                    '##################################################\n\n'
+                )
+
+                seen = set()
+                for record in sorted(zone.records):
+                    # Ignore AAAAs when we've seen an A with the same fqdn
+                    fqdn = record.fqdn
+                    if fqdn in seen:
+                        continue
+                    seen.add(fqdn)
+
+                    # Follow any symlinks
+                    current = record
+                    stack = [current]
+                    looped = False
+                    while current and current._type in ('ALIAS', 'CNAME'):
+                        value = current.value
+                        try:
+                            current = self._records[value][0]
+                        except (IndexError, KeyError):
+                            # No exact match, look for wildcards
+                            current = _wildcard_match(value, self._wildcards)
+
+                        if current:
+                            if current in stack:
+                                # Loop, break...
+                                looped = True
+                                break
+                            stack.append(current)
+
+                    # Walk the stack/path
+                    for node in stack:
+                        if node._type in ('ALIAS', 'CNAME'):
+                            fh.write(f'# {node.fqdn} -> {node.value}\n')
+                    # `node` will be the last element in the stack
+
+                    if looped:
+                        # We detected a loop, indicate it
+                        fh.write('# ** loop detected **\n')
+                    elif node._type in ('ALIAS', 'CNAME'):
+                        # We didn't make it all the way to an A/AAAA
+                        fh.write('# ** unavailable **\n')
+                    elif fqdn[0] == '*':
+                        # the record is a wildcard, just add a comment with
+                        # info about it
+                        fh.write(f'# {node.values[0]} -> {fqdn}\n')
+                        fh.write('# ** wildcard **\n')
+                    elif node.fqdn[0] == '*':
+                        # The last node is a wildcard, note that in a commend
+                        # and print the value
+                        fh.write(f'# {node.fqdn}\n')
+                        fh.write(f'{node.values[0]}\t{fqdn}\n')
+                    else:
+                        # The last node is a value node, just print it
+                        fh.write(f'{node.values[0]}\t{fqdn}\n')
+
+                    fh.write('\n')
+
+        return
+
+    def _apply(self, plan):
+        # Store the zone with its records
+        desired = plan.desired
+        name = desired.name
+
+        self.log.debug(
+            '_apply: zone=%s, num_records=%d', name, len(plan.changes)
+        )
+
+        # Store it
+        self._zones.append(desired)
+
+        # Add all of its records to our maps
+        for record in desired.records:
+            fqdn = record.fqdn
+            if fqdn[0] == '*':
+                regex = fqdn.replace('.', '\\.')
+                regex = re.compile(rf'^.{regex}$')
+                # We want longest match first, preferring A over AAAA so we'll
+                # prepend some bits to sort by here, the `-` is to reverse
+                # length and still allow type/the sort to be reverse=False
+                n = 1024 - len(fqdn)
+                self._wildcards.append((n, record._type, regex, record))
+            else:
+                self._records[fqdn].append(record)
+
+        # Mark it as seen
+        try:
+            self._expected_zones.remove(name)
+        except KeyError:
+            pass
+
+        if not self._expected_zones:
+            # We've seen everything and we're ready to write out our data
+            self.log.debug('_apply: all zone data collected')
+
+            # Sort A before AAAA, as we prefer A when available. CNAME should
+            # always stand alone
+            for records in self._records.values():
+                records.sort(key=lambda r: r._type)
+
+            # Sort wildcards longest first so that we match most specific
+            self._wildcards.sort(key=lambda w: w[0:2])
+
+            self._write()
+
+        return True
---
+++ packages/octodns-etchosts/octodns_etchosts-1.0.0.tar.gz/octodns_etchosts-1.0.0/tests/test_octodns_provider_etchosts.py
@@ -0,0 +1,356 @@
+#
+#
+#
+
+from os import path
+from os.path import isfile
+from shutil import rmtree
+from tempfile import mkdtemp
+from unittest import TestCase
+
+from octodns.provider.plan import Plan
+from octodns.record import Record
+from octodns.zone import Zone
+
+from octodns_etchosts import EtcHostsProvider
+
+
+class TemporaryDirectory(object):
+    def __init__(self, delete_on_exit=True):
+        self.delete_on_exit = delete_on_exit
+
+    def __enter__(self):
+        self.dirname = mkdtemp()
+        return self
+
+    def __exit__(self, *args, **kwargs):
+        if self.delete_on_exit:
+            rmtree(self.dirname)
+        else:
+            raise Exception(self.dirname)
+
+
+class TestEtcHostsProvider(TestCase):
+    def test_provider(self):
+        source = EtcHostsProvider('test', 'not-used')
+
+        zone = Zone('unit.tests.', [])
+
+        # We never populate anything, when acting as a source
+        source.populate(zone, target=source)
+        self.assertEqual(0, len(zone.records))
+        # Same if we're acting as a target
+        source.populate(zone)
+        self.assertEqual(0, len(zone.records))
+
+        record = Record.new(
+            zone, '', {'ttl': 60, 'type': 'ALIAS', 'value': 'www.unit.tests.'}
+        )
+        zone.add_record(record)
+
+        record = Record.new(
+            zone,
+            'www',
+            {'ttl': 60, 'type': 'AAAA', 'value': '2001:4860:4860::8888'},
+        )
+        zone.add_record(record)
+        record = Record.new(
+            zone,
+            'www',
+            {'ttl': 60, 'type': 'A', 'values': ['1.1.1.1', '2.2.2.2']},
+        )
+        zone.add_record(record)
+
+        record = record.new(
+            zone,
+            'v6',
+            {'ttl': 60, 'type': 'AAAA', 'value': '2001:4860:4860::8844'},
+        )
+        zone.add_record(record)
+
+        record = record.new(
+            zone,
+            'start',
+            {'ttl': 60, 'type': 'CNAME', 'value': 'middle.unit.tests.'},
+        )
+        zone.add_record(record)
+        record = record.new(
+            zone, 'middle', {'ttl': 60, 'type': 'CNAME', 'value': 'unit.tests.'}
+        )
+        zone.add_record(record)
+
+        record = record.new(
+            zone, 'ext', {'ttl': 60, 'type': 'CNAME', 'value': 'github.com.'}
+        )
+        zone.add_record(record)
+
+        record = record.new(
+            zone, '*', {'ttl': 60, 'type': 'A', 'value': '3.3.3.3'}
+        )
+        zone.add_record(record)
+
+        with TemporaryDirectory() as td:
+            # Add some subdirs to make sure that it can create them
+            directory = path.join(td.dirname, 'sub', 'dir')
+            hosts_file = path.join(directory, 'unit.tests.hosts')
+            target = EtcHostsProvider('test', directory)
+
+            # We add everything
+            plan = target.plan(zone)
+            self.assertEqual(len(zone.records), len(plan.changes))
+            self.assertFalse(isfile(hosts_file))
+
+            # Now actually do it
+            self.assertEqual(len(zone.records), target.apply(plan))
+            self.assertTrue(isfile(hosts_file))
+
+            with open(hosts_file) as fh:
+                data = fh.read()
+                # v6
+                self.assertTrue('2001:4860:4860::8844\tv6.unit.tests.' in data)
+                # www
+                self.assertTrue('1.1.1.1\twww.unit.tests.' in data)
+                # root ALIAS
+                self.assertTrue('# unit.tests. -> www.unit.tests.' in data)
+                self.assertTrue('1.1.1.1\tunit.tests.' in data)
+
+                self.assertTrue(
+                    '# start.unit.tests. -> middle.unit.tests.' in data
+                )
+                self.assertTrue('# middle.unit.tests. -> unit.tests.' in data)
+                self.assertTrue('# unit.tests. -> www.unit.tests.' in data)
+                self.assertTrue('1.1.1.1	start.unit.tests.' in data)
+
+            # second empty run that won't create dirs and overwrites file
+            plan = Plan(zone, zone, [], True)
+            self.assertEqual(0, target.apply(plan))
+
+    def test_cname_other_zone(self):
+        zone = Zone('unit.tests.', [])
+
+        record = Record.new(
+            zone,
+            'source',
+            {'ttl': 60, 'type': 'CNAME', 'value': 'target.other.tests.'},
+        )
+        zone.add_record(record)
+
+        other_zone = Zone('other.tests.', [])
+
+        record = Record.new(
+            other_zone,
+            'target',
+            {'ttl': 60, 'type': 'A', 'values': ['1.1.1.1', '2.2.2.2']},
+        )
+        other_zone.add_record(record)
+
+        with TemporaryDirectory() as td:
+            # Add some subdirs to make sure that it can create them
+            directory = path.join(td.dirname, 'hosts')
+            hosts_file = path.join(directory, 'unit.tests.hosts')
+            target = EtcHostsProvider('test', directory)
+
+            # We add everything
+            plan = target.plan(zone)
+            self.assertEqual(len(zone.records), len(plan.changes))
+            other_plan = target.plan(other_zone)
+            self.assertEqual(len(other_zone.records), len(other_plan.changes))
+
+            self.assertFalse(isfile(hosts_file))
+
+            # Apply the first one, nothing will be written yet
+            self.assertEqual(len(zone.records), target.apply(plan))
+            self.assertFalse(isfile(hosts_file))
+            # Apply the second one, now both will be written
+            self.assertEqual(len(other_zone.records), target.apply(other_plan))
+            self.assertTrue(isfile(hosts_file))
+
+            with open(hosts_file) as fh:
+                data = fh.read()
+                # CNAME to other zone
+                self.assertTrue(
+                    '# source.unit.tests. -> target.other.tests.\n'
+                    '1.1.1.1\tsource.unit.tests.' in data
+                )
+
+    def test_cnames_and_wildcards(self):
+        zone = Zone('unit.tests.', [])
+
+        record = Record.new(
+            zone, '*.sub', {'ttl': 60, 'type': 'A', 'value': '1.2.3.4'}
+        )
+        zone.add_record(record)
+
+        # One that matches a wildcard
+        record = Record.new(
+            zone,
+            'yep',
+            {'ttl': 60, 'type': 'CNAME', 'value': 'foo.sub.unit.tests.'},
+        )
+        zone.add_record(record)
+
+        # One that doesn't match anything
+        record = Record.new(
+            zone,
+            'nope',
+            {'ttl': 60, 'type': 'CNAME', 'value': 'foo.not.unit.tests.'},
+        )
+        zone.add_record(record)
+
+        # Wildcard CNAME
+        record = Record.new(
+            zone,
+            '*.cname',
+            {'ttl': 60, 'type': 'CNAME', 'value': 'foo.sub.unit.tests.'},
+        )
+        zone.add_record(record)
+
+        # Something that matches the CNAME wildcard
+        record = Record.new(
+            zone,
+            'yes',
+            {'ttl': 60, 'type': 'CNAME', 'value': 'foo.cname.unit.tests.'},
+        )
+        zone.add_record(record)
+
+        with TemporaryDirectory() as td:
+            # Add some subdirs to make sure that it can create them
+            directory = path.join(td.dirname, 'hosts')
+            hosts_file = path.join(directory, 'unit.tests.hosts')
+            target = EtcHostsProvider('test', directory)
+
+            # We add everything
+            plan = target.plan(zone)
+            self.assertEqual(len(zone.records), len(plan.changes))
+            self.assertFalse(isfile(hosts_file))
+
+            # Apply the first one, nothing will be written yet
+            self.assertEqual(len(zone.records), target.apply(plan))
+            self.assertTrue(isfile(hosts_file))
+
+            with open(hosts_file) as fh:
+                data = fh.read()
+                # wildcard is there
+                self.assertTrue(
+                    '# 1.2.3.4 -> *.sub.unit.tests.\n'
+                    '# ** wildcard **' in data
+                )
+                # CNAME that doesn't match anything
+                self.assertTrue(
+                    '# nope.unit.tests. -> foo.not.unit.tests.\n'
+                    '# ** unavailable **' in data
+                )
+                # CNAME that matches a wildcard
+                self.assertTrue(
+                    '# yep.unit.tests. -> foo.sub.unit.tests.\n'
+                    '# *.sub.unit.tests.\n'
+                    '1.2.3.4\tyep.unit.tests' in data
+                )
+                # CNAME wildcard in the middle of the path, and ends with an A
+                # wildcard
+                self.assertTrue(
+                    '# yes.unit.tests. -> foo.cname.unit.tests.\n'
+                    '# *.cname.unit.tests. -> '
+                    'foo.sub.unit.tests.\n'
+                    '# *.sub.unit.tests.\n'
+                    '1.2.3.4\tyes.unit.tests.'
+                )
+
+    def test_cname_loop(self):
+        source = EtcHostsProvider('test', 'not-used')
+
+        zone = Zone('unit.tests.', [])
+
+        # We never populate anything, when acting as a source
+        source.populate(zone, target=source)
+        self.assertEqual(0, len(zone.records))
+        # Same if we're acting as a target
+        source.populate(zone)
+        self.assertEqual(0, len(zone.records))
+
+        record = Record.new(
+            zone,
+            'start',
+            {'ttl': 60, 'type': 'CNAME', 'value': 'middle.unit.tests.'},
+        )
+        zone.add_record(record)
+        record = Record.new(
+            zone,
+            'middle',
+            {'ttl': 60, 'type': 'CNAME', 'value': 'loop.unit.tests.'},
+        )
+        zone.add_record(record)
+        record = Record.new(
+            zone,
+            'loop',
+            {'ttl': 60, 'type': 'CNAME', 'value': 'start.unit.tests.'},
+        )
+        zone.add_record(record)
+
+        with TemporaryDirectory() as td:
+            # Add some subdirs to make sure that it can create them
+            directory = path.join(td.dirname, 'sub', 'dir')
+            hosts_file = path.join(directory, 'unit.tests.hosts')
+            target = EtcHostsProvider('test', directory)
+
+            # We add everything
+            plan = target.plan(zone)
+            self.assertEqual(len(zone.records), len(plan.changes))
+            self.assertFalse(isfile(hosts_file))
+
+            # Now actually do it
+            self.assertEqual(len(zone.records), target.apply(plan))
+            self.assertTrue(isfile(hosts_file))
+
+            with open(hosts_file) as fh:
+                data = fh.read()
+                print(data)
+                self.assertTrue(
+                    '# loop.unit.tests. -> start.unit.tests.\n'
+                    '# ** loop detected **' in data
+                )
+                self.assertTrue(
+                    '# middle.unit.tests. -> loop.unit.tests.\n'
+                    '# ** loop detected **' in data
+                )
+                self.assertTrue(
+                    '# start.unit.tests. -> middle.unit.tests.\n'
+                    '# ** loop detected **' in data
+                )
+
+    def test_wildcard_same_length(self):
+        source = EtcHostsProvider('test', 'not-used')
+
+        zone = Zone('unit.tests.', [])
+
+        # We never populate anything, when acting as a source
+        source.populate(zone, target=source)
+        self.assertEqual(0, len(zone.records))
+        # Same if we're acting as a target
+        source.populate(zone)
+        self.assertEqual(0, len(zone.records))
+
+        record = Record.new(
+            zone, '*.www', {'ttl': 60, 'type': 'A', 'value': '3.3.3.3'}
+        )
+        zone.add_record(record)
+
+        record = Record.new(
+            zone, '*.ww2', {'ttl': 60, 'type': 'A', 'value': '3.3.3.3'}
+        )
+        zone.add_record(record)
+
+        with TemporaryDirectory() as td:
+            # Add some subdirs to make sure that it can create them
+            directory = path.join(td.dirname, 'sub', 'dir')
+            hosts_file = path.join(directory, 'unit.tests.hosts')
+            target = EtcHostsProvider('test', directory)
+
+            # We add everything
+            plan = target.plan(zone)
+            self.assertEqual(len(zone.records), len(plan.changes))
+            self.assertFalse(isfile(hosts_file))
+
+            # Now actually do it
+            self.assertEqual(len(zone.records), target.apply(plan))
+            self.assertTrue(isfile(hosts_file))
