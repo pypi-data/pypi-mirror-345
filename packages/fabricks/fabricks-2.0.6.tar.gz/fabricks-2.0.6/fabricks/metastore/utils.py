b619a1c6387ab1918ca39b70f8c1573f7ea5f60a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-05 15:05:34 +0000

    Add package fabricks/2.0.6/fabricks-2.0.6.tar.gz

---
+++ packages/fabricks/fabricks-2.0.6.tar.gz/fabricks-2.0.6/fabricks/metastore/utils.py
@@ -0,0 +1,46 @@
+from pyspark.sql import DataFrame
+
+from fabricks.context import SPARK
+
+
+def get_tables(schema: str) -> DataFrame:
+    table_df = SPARK.sql(f"show tables in {schema}")
+    view_df = SPARK.sql(f"show views in {schema}")
+
+    df = SPARK.sql(
+        """
+        select 
+            database,
+            concat_ws('.', database, tableName) as table
+        from 
+            {tables} 
+            left anti join {views} on tableName = viewName
+        """,
+        tables=table_df,
+        views=view_df,
+    )
+
+    if df.isEmpty():
+        return SPARK.sql("select null::string as database, null::string as table")
+
+    return df
+
+
+def get_views(schema: str) -> DataFrame:
+    view_df = SPARK.sql(f"show views in {schema}")
+
+    df = SPARK.sql(
+        """
+        select 
+            namespace as database,
+            concat_ws('.', namespace, viewName) as view
+        from 
+            {views}
+        """,
+        views=view_df,
+    )
+
+    if df.isEmpty():
+        return SPARK.sql("select null::string as database, null::string as view")
+
+    return df
