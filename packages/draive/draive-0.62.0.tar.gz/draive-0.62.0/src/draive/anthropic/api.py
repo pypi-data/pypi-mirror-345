12c4d96ac78a8fbbbda44f65886a5b8228988c0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-05 15:05:38 +0000

    Add package draive/0.62.0/draive-0.62.0.tar.gz

---
+++ packages/draive/draive-0.62.0.tar.gz/draive-0.62.0/src/draive/anthropic/api.py
@@ -0,0 +1,56 @@
+from typing import Literal, cast
+
+from anthropic import AsyncAnthropic
+from haiway import getenv_str
+
+__all__ = ("AnthropicAPI",)
+
+
+class AnthropicAPI:
+    def __init__(
+        self,
+        provider: Literal["anthropic", "bedrock"] = "anthropic",
+        /,
+        *,
+        base_url: str | None = None,
+        api_key: str | None = None,
+        timeout: float = 60.0,
+    ) -> None:
+        self._provider: Literal["anthropic", "bedrock"] = provider
+        self._base_url: str | None = base_url
+        self._api_key: str | None = api_key or getenv_str("ANTHROPIC_API_KEY")
+        self._timeout: float = timeout
+        self._client: AsyncAnthropic = self._prepare_client()
+
+    def _prepare_client(self) -> AsyncAnthropic:
+        match self._provider:
+            case "anthropic":
+                return AsyncAnthropic(
+                    base_url=self._base_url,
+                    api_key=self._api_key,
+                    timeout=self._timeout,
+                    max_retries=0,  # disable library retries
+                )
+
+            case "bedrock":
+                from anthropic import AsyncAnthropicBedrock
+
+                return cast(
+                    AsyncAnthropic,  # it is not actually the same but for our purpose it works
+                    AsyncAnthropicBedrock(
+                        base_url=self._base_url,
+                        timeout=self._timeout,
+                    ),
+                )
+
+    async def _initialize_client(self) -> None:
+        await self._client._client.aclose()
+        self._client = self._prepare_client()
+        await self._client.__aenter__()
+
+    async def _deinitialize_client(self) -> None:
+        await self._client.__aexit__(
+            None,
+            None,
+            None,
+        )
