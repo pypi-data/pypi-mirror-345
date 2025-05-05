04a2b2f347d07f8335997d1e2a9f4e43c1e7ede9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-05 04:05:45 +0000

    Add package diavgeia-api/0.0.4/diavgeia_api-0.0.4.tar.gz

---
+++ packages/diavgeia-api/diavgeia_api-0.0.4.tar.gz/diavgeia_api-0.0.4/diavgeia_api/models/search.py
@@ -0,0 +1,33 @@
+from typing import List
+from enum import Enum
+from pydantic import BaseModel
+from .decisions import Decision
+
+
+class SortOrder(str, Enum):
+    """Είδος ταξινόμησης."""
+
+    RECENT = (
+        "recent"  # Φθίνουσα ταξινόμηση ως προς την ημερομηνία τελευταίας τροποποίησης
+    )
+    RELATIVE = (
+        "relative"  # Φθίνουσα ταξινόμηση ως προς τη σχετικότητα των αποτελεσμάτων
+    )
+
+
+class SearchInfo(BaseModel):
+    """Information about the search results."""
+
+    query: str
+    page: int
+    size: int
+    actualSize: int
+    total: int
+    order: str
+
+
+class SearchResponse(BaseModel):
+    """Response model for search endpoints."""
+
+    decisions: List[Decision]
+    info: SearchInfo
