41af8ff40969dbab13fede7f7a7bd4e357ffa276
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-05 05:04:31 +0000

    Add package SdamMCKO/1.0.6/sdammcko-1.0.6.tar.gz

---
+++ packages/SdamMCKO/sdammcko-1.0.6.tar.gz/sdammcko-1.0.6/SdamMCKO/main.py
@@ -0,0 +1,18 @@
+def q_help(N:int) -> str:
+    """
+
+    """
+    try:
+        file = open('Guide.md', 'r', encoding='utf-8')
+        content = f"".join(file.read()).split("##")
+        print(content[N])
+    except:
+        
+        file =  open('SdamMCKO/Guide.md', 'r', encoding='utf-8')
+        content = f"".join(file.read()).split("##")
+        print(content[N])
+
+
+if __name__ == "__main__":
+
+    q_help(2)
