18c4f6b6f6a159380f98fbc7e03d1ab9018e0365
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-05 14:05:12 +0000

    Add package slamd/2.1.3/slamd-2.1.3-cp311-cp311-manylinux_2_28_x86_64.whl

---
+++ packages/slamd/slamd-2.1.3-cp311-cp311-manylinux_2_28_x86_64.whl/slamd/geom/__init__.py
@@ -0,0 +1,21 @@
+from ..bindings.geom import (
+    Box,
+    Arrows,
+    CameraFrustum,
+    Mesh,
+    Sphere,
+    Triad,
+)
+from .overrides import PointCloud, PolyLine
+
+
+__all__ = [
+    "Box",
+    "Arrows",
+    "CameraFrustum",
+    "PointCloud",
+    "PolyLine",
+    "Mesh",
+    "Sphere",
+    "Triad",
+]
---
+++ packages/slamd/slamd-2.1.3-cp311-cp311-manylinux_2_28_x86_64.whl/slamd/geom2d/__init__.py
@@ -0,0 +1,5 @@
+from ..bindings.geom2d import Image, PolyLine, Circles
+from .overrides import Points
+
+
+__all__ = ["Points", "Image", "PolyLine", "Circles"]
