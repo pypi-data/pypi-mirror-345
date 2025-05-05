c52d79146ca63ec154a2228d9880e6c8291a02f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-05 03:23:13 +0000

    Add package MetaRagTool/0.2.1/metaragtool-0.2.1.tar.gz

---
+++ packages/MetaRagTool/metaragtool-0.2.1.tar.gz/metaragtool-0.2.1/MetaRagTool/Constants.py
@@ -0,0 +1,19 @@
+﻿use_wandb = False
+local_mode = False
+trust_remote_code_SentenceTransformer = False
+
+# import os
+# WandbToken=os.getenv('WandbToken')
+# HFToken=os.getenv('HFToken')
+# GitHubToken=os.getenv('GitHubToken')
+
+
+WandbToken = ''
+HFToken = ''
+GitHubToken = ''
+
+API_KEY_GEMINI = ""
+API_KEY_OPENAI = ""
+API_KEY_OPENROUTER = ""
+
+
