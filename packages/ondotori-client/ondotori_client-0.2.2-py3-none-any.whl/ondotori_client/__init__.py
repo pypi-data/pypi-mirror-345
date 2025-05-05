eac9996d5faefb486c83d7fa2f0d107c69797c67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-05 15:05:45 +0000

    Add package ondotori-client/0.2.2/ondotori_client-0.2.2-py3-none-any.whl

---
+++ packages/ondotori-client/ondotori_client-0.2.2-py3-none-any.whl/ondotori_client/__init__.py
@@ -0,0 +1,12 @@
+# src/ondotori_client/__init__.py
+
+__version__ = "0.2.2"
+
+from .client import OndotoriClient, parse_current, parse_data
+
+__all__ = [
+    "OndotoriClient",
+    "parse_current",
+    "parse_data",
+    "__version__",
+]
