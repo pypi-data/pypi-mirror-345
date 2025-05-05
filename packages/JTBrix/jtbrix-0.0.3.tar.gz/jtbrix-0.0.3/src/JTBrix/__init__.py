7c6e1db95e6ca12787f07d9a6161ec104181e465
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-05 14:05:37 +0000

    Add package JTBrix/0.0.3/jtbrix-0.0.3.tar.gz

---
+++ packages/JTBrix/jtbrix-0.0.3.tar.gz/jtbrix-0.0.3/src/JTBrix/__init__.py
@@ -0,0 +1,23 @@
+"""Top-level package for JTBrix."""
+
+__author__ = """Amid Nayerhoda"""
+__email__ = 'Nayerhoda@infn.it'
+__version__ = '0.0.3'
+
+
+# Import core functionality to the top level
+from JTBrix.experiment import run_session  # Example: if you have a main entry point
+from JTBrix.questionnaire import screens   # Example: expose a core class
+from JTBrix.utils import port  # Example: utility functions
+from JTBrix.ui import main  # Example: if you have a UI component
+from JTBrix.screen_config import flow_config  # Example: if you have a configuration module
+from JTBrix.experiment.run_experiment import run_test
+
+__all__ = [
+    "run_session",
+    "screens",
+    "port",
+    "main",
+    "flow_config",
+    "run_test",
+]
