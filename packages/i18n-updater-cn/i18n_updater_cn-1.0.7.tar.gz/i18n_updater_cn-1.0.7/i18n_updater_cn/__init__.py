ed4faebea292f4af9e273cebfed5c0a7a13b32c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-05 15:05:43 +0000

    Add package i18n-updater-cn/1.0.7/i18n_updater_cn-1.0.7.tar.gz

---
+++ packages/i18n-updater-cn/i18n_updater_cn-1.0.7.tar.gz/i18n_updater_cn-1.0.7/i18n_updater_cn/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Minecraft 模组汉化包更新器
+
+一个用于下载、更新和转换 Minecraft 模组汉化包的工具
+基于 I18nUpdateMod3 项目重写的 Python 版本
+"""
+
+__version__ = '1.0.0'
+
+from .main import download_or_convert_language_pack
