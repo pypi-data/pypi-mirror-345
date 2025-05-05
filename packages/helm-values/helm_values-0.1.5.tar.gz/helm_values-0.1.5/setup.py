dede71643db82077a7cd112b2c6e49c732301aab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-05 15:05:41 +0000

    Add package helm-values/0.1.5/helm_values-0.1.5.tar.gz

---
+++ packages/helm-values/helm_values-0.1.5.tar.gz/helm_values-0.1.5/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="helm-values",
+    version="0.1.5",
+    py_modules=["values"],
+    install_requires=["pyyaml"],
+    author="fluffyspike",
+    description="Load and merge Helm values files for cdk8s",
+    python_requires=">=3",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
