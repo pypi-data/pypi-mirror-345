77d436ec3e709e226a841085a5df095b79bc5578
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-05 14:05:32 +0000

    Add package KeyisBClient-mmbp/0.0.0.1.22/keyisbclient_mmbp-0.0.0.1.22.tar.gz

---
+++ packages/KeyisBClient-mmbp/keyisbclient_mmbp-0.0.0.1.22.tar.gz/keyisbclient_mmbp-0.0.0.1.22/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+name = 'KeyisBClient_mmbp'
+filesName = 'KeyisBClient_mmbp'
+
+setup(
+    name=name,
+    version='0.0.0.1.22',
+    author="KeyisB",
+    author_email="keyisb.pip@gmail.com",
+    description=name,
+    long_description = 'GW and MMB Project libraries',
+    long_description_content_type= 'text/plain',
+    url=f"https://github.com/KeyisB/libs/tree/main/{name}",
+    include_package_data=True,
+    package_data = {'KeyisBClient_mmbp': ['gw_certs/*']}, # type: ignore
+    package_dir={'': f'{filesName}'.replace('-','_')},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.12',
+    license="MMB License v1.0",
+    install_requires = ['KeyisBClient', 'httpx'],
+)
