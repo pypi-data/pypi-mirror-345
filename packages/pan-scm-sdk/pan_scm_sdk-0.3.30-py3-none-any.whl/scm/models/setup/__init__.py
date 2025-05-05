26a20454635c8a679b7ee910eb0d73e47a648437
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-05 04:06:22 +0000

    Add package pan-scm-sdk/0.3.30/pan_scm_sdk-0.3.30-py3-none-any.whl

---
+++ packages/pan-scm-sdk/pan_scm_sdk-0.3.30-py3-none-any.whl/scm/models/setup/__init__.py
@@ -0,0 +1,39 @@
+from .device import (
+    DeviceCreateModel,
+    DeviceResponseModel,
+    DeviceUpdateModel,
+)
+from .folder import (
+    FolderCreateModel,
+    FolderResponseModel,
+    FolderUpdateModel,
+)
+from .snippet import (
+    FolderReference,
+    SnippetBaseModel,
+    SnippetCreateModel,
+    SnippetResponseModel,
+    SnippetUpdateModel,
+)
+from .variable import (
+    VariableCreateModel,
+    VariableResponseModel,
+    VariableUpdateModel,
+)
+
+__all__ = [
+    "DeviceCreateModel",
+    "DeviceResponseModel",
+    "DeviceUpdateModel",
+    "FolderCreateModel",
+    "FolderResponseModel",
+    "FolderUpdateModel",
+    "FolderReference",
+    "SnippetBaseModel",
+    "SnippetCreateModel",
+    "SnippetResponseModel",
+    "SnippetUpdateModel",
+    "VariableCreateModel",
+    "VariableResponseModel",
+    "VariableUpdateModel",
+]
