76f0e1e8b8c57db974e1c5c9cf9939200700835b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-05 15:05:30 +0000

    Add package ubi-config/3.2.1/ubi_config-3.2.1-py3-none-any.whl

---
+++ packages/ubi-config/ubi_config-3.2.1-py3-none-any.whl/ubiconfig/config_types/__init__.py
@@ -0,0 +1,76 @@
+from .content_sets import ContentSetsMapping
+from .flags import Flags
+from .modules import Modules
+from .packages import Packages
+
+
+class UbiConfig(object):
+    """Wrap all UBI related configurations
+    Examples to access different configurations:
+
+    Modules:
+      config.modules[0].whitelist[0].name
+    Packages:
+      config.packages.whitelist[0].name
+      config.packages.blacklist[0].name
+    ContentSets:
+      config.content_sets.rpm.input
+      config.content_sets.debuginfo.output"""
+
+    def __init__(self, cs, pkgs, mds, file_name, version, flags):
+        """
+        :param cs: :class:`~ubiconfig.config_types.content_sets.ContentSetsMapping`
+        :param pkgs: :class:`~ubiconfig.config_types.packages.Packages`
+        :param mds: :class:`~ubiconfig.config_types.modules.Modules`
+        :param filename: filename used as identifier
+        :param flags: :class:`~ubiconfig.config_types.flags.Flags`
+        """
+        self.content_sets = cs
+        self.packages = pkgs
+        self.modules = mds
+        self.file_name = file_name
+        self.version = version
+        self.flags = flags
+
+    def __repr__(self):
+        return self.file_name
+
+    @classmethod
+    def load_from_dict(cls, data, file_name, version=None):
+        """Create new instance of UbiConfig and load it from provided dictonary with
+        following format:
+
+        .. code-block:: json
+
+            {
+                "modules":  {},
+                "packages": {
+                    "include": ["package-name-.*"],
+                    "exclude": ["package-name-.*"],
+                    "arches": ["arch"]
+                },
+                "content_sets": {},
+            }
+
+        See also :meth:`ubiconfig.config_types.content_sets.ContentSetsMapping.load_from_dict`
+        :meth:`ubiconfig.config_types.modules.Modules.load_from_dict`
+        """
+        m_data = Modules.load_from_dict(data.get("modules", {}))
+        pkgs = data.get("packages", {})
+        pkgs_data = Packages(
+            pkgs.get("include", []), pkgs.get("exclude", []), data.get("arches", [])
+        )
+        cs_map = ContentSetsMapping.load_from_dict(data["content_sets"])
+
+        flags = Flags.load_from_dict(data.get("flags", {}))
+        # use the simplified file name
+        file_name = file_name.split("/")[-1]
+
+        return cls(
+            cs=cs_map,
+            pkgs=pkgs_data,
+            mds=m_data,
+            file_name=file_name,
+            version=version,
+            flags=flags,
+        )
---
+++ packages/ubi-config/ubi_config-3.2.1-py3-none-any.whl/ubiconfig/utils/config_validation.py
@@ -0,0 +1,16 @@
+import json
+import os
+
+from jsonschema import validate
+
+DEFAULT_SCHEMA = os.path.join(os.path.dirname(__file__), "config_schema.json")
+""" Default yaml schema used for validation of UbiConfig configuration files """
+
+
+def validate_config(data, schema=None):
+    """Validate the data according to the schema
+    If no schema is provided, :data:`DEFAULT_SCHEMA` is used"""
+    if schema is None:
+        with open(DEFAULT_SCHEMA) as f:
+            schema = json.load(f)
+    validate(data, schema)
