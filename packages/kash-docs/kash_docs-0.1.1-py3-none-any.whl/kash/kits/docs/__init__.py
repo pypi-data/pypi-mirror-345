57227b6b0ba69a3d65546ed741a97a35850cd8b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-05 14:05:21 +0000

    Add package kash-docs/0.1.1/kash_docs-0.1.1-py3-none-any.whl

---
+++ packages/kash-docs/kash_docs-0.1.1-py3-none-any.whl/kash/kits/docs/__init__.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+
+from kash.exec import import_and_register
+
+import_and_register(
+    __package__,
+    Path(__file__).parent,
+    [
+        "actions",
+        "concepts",
+        "docs",
+        "wiki",
+    ],
+)
