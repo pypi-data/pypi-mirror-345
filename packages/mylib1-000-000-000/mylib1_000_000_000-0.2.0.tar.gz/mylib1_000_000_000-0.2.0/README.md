85f3a0141259edf2af30158b09afc990256d3c10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-05 14:05:24 +0000

    Add package mylib1-000-000-000/0.2.0/mylib1_000_000_000-0.2.0.tar.gz

---
+++ packages/mylib1-000-000-000/mylib1_000_000_000-0.2.0.tar.gz/mylib1_000_000_000-0.2.0/README.md
@@ -0,0 +1,37 @@
+
+
+
+
+
+
+
+
+
+
+
+
+# MyLib
+
+مكتبة Python بسيطة جدًا.
+
+## الاستخدام
+
+```python
+
+import mylib
+
+print(mylib.hello("i"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ packages/mylib1-000-000-000/mylib1_000_000_000-0.2.0.tar.gz/mylib1_000_000_000-0.2.0/mylib/core.py
@@ -0,0 +1,37 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Greeter:
+
+    def __init__(self, name: str):
+
+        self.name = name
+
+    def greet(self) -> str:
+
+        return f"i . Hello, {self.name}!"
+
+
+
+
+
+
+
+
+
