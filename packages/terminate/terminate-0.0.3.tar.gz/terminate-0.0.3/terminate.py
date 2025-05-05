5b47b077904dc3b691ccba3d90df4ad8a649523f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-05 04:06:22 +0000

    Add package terminate/0.0.3/terminate-0.0.3.tar.gz

---
+++ packages/terminate/terminate-0.0.3.tar.gz/terminate-0.0.3/terminate.py
@@ -0,0 +1,45 @@
+from time import sleep
+
+# exit with status code 1
+def __stop()->None:exit(1);
+
+# exit or continue after answering prompt
+def __ask(q: str)->None:
+    if q=='n':pass;
+    elif q=='y':__stop();
+    else:print("Invalid Input!\n");a=input("exit? y/n\n");__ask(a);
+
+# wait for some time and then exit
+def __wait(t=5)->None:
+    try:
+        if type(t)!=int or t<0:__stop();
+        print("Press Ctrl+C to continue...")
+        for i in range(t):
+            if i==t-1:print("1");sleep(0.5);print("exiting...");sleep(0.5);
+            else:print(t-i);sleep(1);
+        __stop()
+    except KeyboardInterrupt:print("\n");
+
+# choose how to exit
+# r = "e": exit directly
+#   = "c": don't exit
+# 	= "w": wait and exit
+# 	= "a": ask to exit
+def retrn(r:str,e:str|TypeError|Exception)->None:
+    '''
+#### Function to handle exit
+- **r**: 
+    1. **'e'** - Exit directly
+    2. **'c'** - Continue
+    3. **'w'** - Wait for 5 seconds before exit
+    4. **'a'** - Ask prompt before exit
+- **e**: Error to print
+    '''
+    print(e,"\n")
+    match r:
+        case "e":__stop();
+        case 'c':pass;
+        case 'w':__wait();
+        case 'a':a=input("exit? y/n\n");__ask(a);
+        case _:print("Invalid argument: r => 'c'/'w'/'a'");a=input("exit? y/n\n");__ask(a);
+
