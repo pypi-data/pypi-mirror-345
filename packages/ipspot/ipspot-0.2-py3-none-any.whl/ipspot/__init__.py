bd64bdec6ef5bbbcd2467130ad6e8d04fef3aa92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-05 05:04:24 +0000

    Add package ipspot/0.2/ipspot-0.2-py3-none-any.whl

---
+++ packages/ipspot/ipspot-0.2-py3-none-any.whl/ipspot/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+"""ipspot modules."""
+from .params import IPSPOT_VERSION, IPv4API
+from .functions import get_private_ipv4, get_public_ipv4
+__version__ = IPSPOT_VERSION
---
+++ packages/ipspot/ipspot-0.2-py3-none-any.whl/ipspot/__main__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""ipspot main."""
+from .functions import main
+
+
+if __name__ == "__main__":
+    main()
