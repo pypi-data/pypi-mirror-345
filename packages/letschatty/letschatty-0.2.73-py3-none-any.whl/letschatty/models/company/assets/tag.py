cd88623a04d4036d1c2075faf905e70d5464ad01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-05 03:23:33 +0000

    Add package letschatty/0.2.73/letschatty-0.2.73-py3-none-any.whl

---
+++ packages/letschatty/letschatty-0.2.73-py3-none-any.whl/letschatty/models/company/assets/tag.py
@@ -0,0 +1,23 @@
+from ...base_models.chatty_asset_model import CompanyAssetModel, ChattyAssetPreview
+from pydantic import Field
+from typing import Optional, Any, ClassVar
+from pydantic import BaseModel
+from ...utils.types import StrObjectId
+
+class TagPreview(ChattyAssetPreview):
+    color: str
+
+    @classmethod
+    def get_projection(cls) -> dict[str, Any]:
+        return super().get_projection() | {"color": 1}
+
+class Tag(CompanyAssetModel):
+    name: str
+    description: str
+    color: str
+    is_event: bool
+    event_name: Optional[str] = Field(default=None)
+    preview_class: ClassVar[type[TagPreview]] = TagPreview
+    def get_preview(self) -> TagPreview:
+        return TagPreview(id=self.id, name=self.name, color=self.color, created_at=self.created_at, company_id=self.company_id)
+
