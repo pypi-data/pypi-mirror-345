1b28b023e05e898b069a8e1583aad746e50bd021
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-05 15:05:39 +0000

    Add package SdamMCKO/1.0.16/sdammcko-1.0.16.tar.gz

---
+++ packages/SdamMCKO/sdammcko-1.0.16.tar.gz/sdammcko-1.0.16/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+
+def readme():
+  with open('README.md', 'r',encoding="utf-8") as f:
+    return f.read()
+
+
+setup(
+  name='SdamMCKO',
+  version='1.0.16',
+  author='Fedor Tyulpin',
+  author_email='f.tyulpin@gmail.com',
+  description='A Python module for preparation for mcco',
+  long_description=readme(),
+  long_description_content_type='text/markdown',
+  packages=find_packages(),
+  install_requires=[],
+  classifiers=[
+    'Programming Language :: Python :: 3.13',
+    'Operating System :: OS Independent'
+  ],
+  keywords='converting numbers numeral systems',
+  python_requires='>=3.6',
+  package_data={
+        "SdamMCKO": ["*.md", "*.txt"]
+    },
+)
