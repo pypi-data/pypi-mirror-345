7ca2688c8ac546e9d0969dfd1cd3a4dc3ac6e85c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-05 04:05:45 +0000

    Add package grimoire-logger/1.0.0/grimoire_logger-1.0.0.tar.gz

---
+++ packages/grimoire-logger/grimoire_logger-1.0.0.tar.gz/grimoire_logger-1.0.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="grimoire_logger",
+    version="1.0.0",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    include_package_data=True,
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Lucas Signorelli",
+    description="A simple Python logger, that prints in a JSON format",
+    url="https://github.com/luquor/grimoire",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
