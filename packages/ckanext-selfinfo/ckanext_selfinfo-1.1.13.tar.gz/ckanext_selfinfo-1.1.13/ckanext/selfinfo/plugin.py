8aa432e896feb89efce7a421107ca4102628a963
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-05 15:05:42 +0000

    Add package ckanext-selfinfo/1.1.13/ckanext_selfinfo-1.1.13.tar.gz

---
+++ packages/ckanext-selfinfo/ckanext_selfinfo-1.1.13.tar.gz/ckanext_selfinfo-1.1.13/ckanext/selfinfo/plugin.py
@@ -0,0 +1,20 @@
+import ckan.plugins as plugins
+import ckan.plugins.toolkit as tk
+
+from .logic import action
+from . import cli
+
+
+@tk.blanket.config_declarations
+@tk.blanket.cli(cli.get_commands)
+@tk.blanket.actions(action.get_actions)
+@tk.blanket.blueprints
+class SelfinfoPlugin(plugins.SingletonPlugin):
+    plugins.implements(plugins.IConfigurer)
+
+    # IConfigurer
+
+    def update_config(self, config_):
+        tk.add_template_directory(config_, "templates")
+        tk.add_public_directory(config_, "public")
+        tk.add_resource("assets", "selfinfo")
