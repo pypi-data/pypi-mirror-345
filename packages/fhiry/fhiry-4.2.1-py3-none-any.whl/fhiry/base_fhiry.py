55cbf198c6a9f4b3c7226a5bb963492df189942c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-05 03:23:32 +0000

    Add package fhiry/4.2.1/fhiry-4.2.1-py3-none-any.whl

---
+++ packages/fhiry/fhiry-4.2.1-py3-none-any.whl/fhiry/base_fhiry.py
@@ -0,0 +1,191 @@
+"""
+ Copyright (c) 2020 Bell Eapen
+
+ This software is released under the MIT License.
+ https://opensource.org/licenses/MIT
+"""
+
+from typing import Any
+import pandas as pd
+import json
+
+
+
+class BaseFhiry(object):
+    def __init__(self, config_json=None):
+        self._df = None
+
+        # Codes from the FHIR datatype "coding"
+        # (f.e. element resource.code.coding or element resource.clinicalStatus.coding)
+        # are extracted to a col "codingcodes"
+        # (f.e. col resource.code.codingcodes or col resource.clinicalStatus.codingcodes)
+        # without other for analysis often not needed metadata like f.e. codesystem URI
+        # or FHIR extensions for coding entries.
+        # The full / raw object in col "coding" is deleted after this extraction.
+        # If you want to analyze more than the content of code and display from codings
+        # (like f.e. different codesystem URIs or further codes in extensions
+        # in the raw data/object), you can disable deletion of the raw source object "coding"
+        # (f.e. col "resource.code.coding") by setting property delete_col_raw_coding to False
+        self._delete_col_raw_coding = True
+        if config_json is not None:
+            try:
+                with open(config_json, "r") as f:  # config_json is a file path
+                    self.config = json.load(f)
+            except:
+                self.config = json.loads(config_json)  # config_json is a json string
+        else:
+            self.config = json.loads(
+                '{ "REMOVE": ["resource.text.div"], "RENAME": { "resource.id": "id" } }'
+            )
+
+    @property
+    def df(self):
+        return self._df
+
+    @property
+    def delete_col_raw_coding(self):
+        return self._delete_col_raw_coding
+
+    @delete_col_raw_coding.setter
+    def delete_col_raw_coding(self, delete_col_raw_coding):
+        self._delete_col_raw_coding = delete_col_raw_coding
+
+    def read_bundle_from_bundle_dict(self, bundle_dict):
+        return pd.json_normalize(bundle_dict["entry"])
+
+    def delete_unwanted_cols(self):
+        for col in self.config["REMOVE"]:
+            if col in self._df.columns:
+                del self._df[col]
+
+    def rename_cols(self):
+        self._df.rename(columns=self.config["RENAME"], inplace=True)
+
+    def process_df(self):
+        self.delete_unwanted_cols()
+        self.convert_object_to_list()
+        self.add_patient_id()
+        self.rename_cols()
+
+    def process_bundle_dict(self, bundle_dict):
+        self._df = self.read_bundle_from_bundle_dict(bundle_dict)
+        self.delete_unwanted_cols()
+        self.convert_object_to_list()
+        self.add_patient_id()
+        self.rename_cols()
+        return self._df
+
+    def convert_object_to_list(self):
+        """Convert object to a list of codes"""
+        for col in self._df.columns:
+            if "coding" in col:
+                codes = self._df.apply(lambda x: self.process_list(x[col]), axis=1)
+                self._df = pd.concat(
+                    [self._df, codes.to_frame(name=col + "codes")], axis=1
+                )
+                if self._delete_col_raw_coding:
+                    del self._df[col]
+            if "display" in col:
+                codes = self._df.apply(lambda x: self.process_list(x[col]), axis=1)
+                self._df = pd.concat(
+                    [self._df, codes.to_frame(name=col + "display")], axis=1
+                )
+                del self._df[col]
+
+    def add_patient_id(self):
+        """Create a patientId column with the resource.id if a Patient resource or with the resource.subject.reference if other resource type"""
+        try:
+            # PerformanceWarning: DataFrame is highly fragmented.  This is usually the result of calling `frame.insert` many times, which has poor performance.  Consider joining all columns at once using pd.concat(axis=1) instead. To get a de-fragmented frame, use `newframe = frame.copy()`
+            newframe = self._df.copy()
+            newframe["patientId"] = self._df.apply(
+                lambda x: (
+                    x["resource.id"]
+                    if x["resource.resourceType"] == "Patient"
+                    else self.check_subject_reference(x)
+                ),
+                axis=1,
+            )
+            self._df = newframe
+        except:
+            try:
+                newframe = self._df.copy()
+                newframe["patientId"] = self._df.apply(
+                    lambda x: (
+                        x["id"]
+                        if x["resourceType"] == "Patient"
+                        else self.check_subject_reference(x)
+                    ),
+                    axis=1,
+                )
+                self._df = newframe
+            except:
+                pass
+
+    def check_subject_reference(self, row):
+        try:
+            return row["resource.subject.reference"].replace("Patient/", "")
+        except:
+            return ""
+
+    def get_info(self):
+        if self._df is None:
+            return "Dataframe is empty"
+        return self._df.info()
+
+    def process_list(self, myList):
+        """Extracts the codes from a list of objects
+
+        Args:
+            myList (list): A list of objects
+
+        Returns:
+            list: A list of codes
+        """
+        myCodes = []
+        if isinstance(myList, list):
+            for entry in myList:
+                if "code" in entry:
+                    myCodes.append(entry["code"])
+                elif "display" in entry:
+                    myCodes.append(entry["display"])
+        return myCodes
+
+    def llm_query(self, query, llm, embed_model=None, verbose=True):
+        """Execute a query using llama_index
+
+        Args:
+            query (str): The natural language query
+            llm (Any): The Language Model
+            embed_model (str, optional): The embedding model string from HuggingFace. Defaults to None.
+            verbose (bool, optional): Verbose or not. Defaults to True.
+
+        Raises:
+            Exception: Llama_index not installed
+            Exception: Dataframe is empty
+
+        Returns:
+            Any: Results of the query
+        """
+        LLAMA_INDEX_ENABLED = False
+        try:
+            from llama_index.experimental.query_engine import PandasQueryEngine
+            from llama_index.core import Settings
+            from langchain_huggingface import HuggingFaceEmbeddings
+            LLAMA_INDEX_ENABLED = True
+        except:
+            pass
+        if not LLAMA_INDEX_ENABLED:
+            raise Exception("llama_index not installed")
+        if self._df is None:
+            raise Exception("Dataframe is empty")
+        if embed_model is None:
+            embed_model = HuggingFaceEmbeddings(model_name="BAAI/bge-small-en-v1.5")
+        else:
+            embed_model = HuggingFaceEmbeddings(model_name=embed_model)
+        Settings.llm = llm
+        Settings.embed_model = embed_model
+        query_engine = PandasQueryEngine(
+            df=self._df,
+            verbose=verbose,
+        )
+        return query_engine.query(query)
---
+++ packages/fhiry/fhiry-4.2.1-py3-none-any.whl/fhiry/parallel.py
@@ -0,0 +1,49 @@
+"""
+ Copyright (c) 2023 Bell Eapen
+
+ This software is released under the MIT License.
+ https://opensource.org/licenses/MIT
+"""
+
+import multiprocessing as mp
+import os
+import pandas as pd
+from . import Fhirndjson, Fhiry
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def process(folder, config_json=None):
+    logger.info("CPU count: {}".format(mp.cpu_count()))
+    pool = mp.Pool(mp.cpu_count())
+    f = Fhiry(config_json=config_json)
+    filenames = []
+    if os.path.isdir(folder):
+        for filename in os.listdir(folder):
+            if filename.endswith(".json"):
+                filenames.append(folder + "/" + filename)
+    else:
+        filenames.append(folder)
+
+    list_of_dataframes = pool.map(f.process_file, filenames)
+    pool.close()
+    return pd.concat(list_of_dataframes)
+
+
+def ndjson(folder, config_json=None):
+    logger.info("CPU count: {}".format(mp.cpu_count()))
+    pool = mp.Pool(mp.cpu_count())
+    f = Fhirndjson(config_json=config_json)
+    filenames = []
+
+    if os.path.isdir(folder):
+        for filename in os.listdir(folder):
+            if filename.endswith(".ndjson"):
+                filenames.append(folder + "/" + filename)
+    else:
+        filenames.append(folder)
+
+    list_of_dataframes = pool.map(f.process_file, filenames)
+    pool.close()
+    return pd.concat(list_of_dataframes)
