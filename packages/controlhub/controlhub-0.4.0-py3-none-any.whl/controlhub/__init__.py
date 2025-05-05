402511a73e123664176ff825ead7305a8050ddb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-05 15:05:42 +0000

    Add package controlhub/0.4.0/controlhub-0.4.0-py3-none-any.whl

---
+++ packages/controlhub/controlhub-0.4.0-py3-none-any.whl/controlhub/__init__.py
@@ -0,0 +1,41 @@
+from .desktop import (
+    open_file,
+    cmd,
+    run_program,
+    fullscreen,
+    switch_to_next_window,
+    switch_to_last_window,
+    reload_window,
+)
+from .keyboard import (
+    click,
+    move,
+    drag,
+    press,
+    hold,
+    write,
+    get_position,
+)
+from .web import open_url, download
+from .json_storage import data, JSONFile
+
+__all__ = [
+    "open_file",
+    "cmd",
+    "run_program",
+    "fullscreen",
+    "switch_to_next_window",
+    "switch_to_last_window",
+    "reload_window",
+    "click",
+    "move",
+    "drag",
+    "press",
+    "hold",
+    "write",
+    "get_position",
+    "open_url",
+    "download",
+    "data",
+    "JSONFile",
+]
