1eb9966b9795a5818772b77432a8124e0ff05f65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-05 03:23:26 +0000

    Add package horsetalk/0.25.7/horsetalk-0.25.7.tar.gz

---
+++ packages/horsetalk/horsetalk-0.25.7.tar.gz/horsetalk-0.25.7/out/horsetalk/quantity.pyi
@@ -0,0 +1,9 @@
+from _typeshed import Incomplete
+
+ureg: Incomplete
+Q_: Incomplete
+
+class HorsetalkQuantity(Q_):
+    REGEX: str
+    def __new__(cls, *args, **kwargs): ...
+    def __getattr__(self, attr): ...
