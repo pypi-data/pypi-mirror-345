84c8b5b2b053b94fe54b62ef9139263f516ee17d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-05 03:23:22 +0000

    Add package automa-bot/0.1.0/automa_bot-0.1.0.tar.gz

---
+++ packages/automa-bot/automa_bot-0.1.0.tar.gz/automa_bot-0.1.0/src/automa/bot/__init__.py
@@ -0,0 +1,20 @@
+from ._client import AsyncAutoma, Automa
+
+__all__ = [
+    "Automa",
+    "AsyncAutoma",
+]
+
+# Update the __module__ attribute for exported symbols so that
+# error messages point to this module instead of the module
+# it was originally defined in, e.g.
+# automa._exceptions.NotFoundError -> automa.NotFoundError
+__locals = locals()
+
+for __name in __all__:
+    if not __name.startswith("__"):
+        try:
+            __locals[__name].__module__ = "automa"
+        except (TypeError, AttributeError):
+            # Some of our exported symbols are builtins which we can't set attributes for.
+            pass
