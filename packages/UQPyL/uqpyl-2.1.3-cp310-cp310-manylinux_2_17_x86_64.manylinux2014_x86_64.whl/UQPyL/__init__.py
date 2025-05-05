d2a58188a9e19795e2a821a6c29528d464c26e11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-05 03:23:17 +0000

    Add package UQPyL/2.1.3/uqpyl-2.1.3-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/UQPyL/uqpyl-2.1.3-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/UQPyL/__init__.py
@@ -0,0 +1,13 @@
+from . import problems, surrogates, optimization, sensibility, DoE, utility
+
+__version__ = "2.1.3"
+__author__ = "wmtSky"
+
+__all__=[
+    "problems",
+    "surrogates",
+    "optimization",
+    "sensibility",
+    "DoE",
+    "utility"
+]
