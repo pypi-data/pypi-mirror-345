c63af208f4af1ec4cea0d65291404098e8a2b85b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-05 15:05:41 +0000

    Add package glik-sdk/0.0.1/glik-sdk-0.0.1.tar.gz

---
+++ packages/glik-sdk/glik-sdk-0.0.1.tar.gz/glik-sdk-0.0.1/setup.py
@@ -0,0 +1,55 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+requirements = [
+    "requests>=2.25.1",
+    "pydantic>=1.8.2",
+]
+
+setup(
+    name="glik-sdk",
+    version="0.0.1",
+    author="Glik",
+    author_email="engineer@glik.ai",
+    description="A Python SDK for integrating to the Glik Service-API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rivalz-glik/glik-sdk",
+    project_urls={
+        "Bug Tracker": "https://github.com/rivalz-glik/glik-sdk/issues",
+        "Documentation": "https://github.com/rivalz-glik/glik-sdk#readme",
+    },
+    license="MIT",
+    license_files = ["LICENSE"],
+    packages=["glik_sdk"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    python_requires=">=3.6",
+    install_requires=requirements,
+    extras_require={
+        "dev": [
+            "pytest>=6.0",
+            "pytest-cov>=2.0",
+            "black>=21.0",
+            "isort>=5.0",
+            "flake8>=3.9",
+            "mypy>=0.910",
+        ],
+    },
+    keywords="glik ai language-processing api sdk",
+    include_package_data=True,
+    zip_safe=False,
+)
