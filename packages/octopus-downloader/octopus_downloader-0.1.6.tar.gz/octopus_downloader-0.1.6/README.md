7c527284c75941b414ba71dd1ea6741ec6de8ad6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-05 05:04:24 +0000

    Add package octopus-downloader/0.1.6/octopus_downloader-0.1.6.tar.gz

---
+++ packages/octopus-downloader/octopus_downloader-0.1.6.tar.gz/octopus_downloader-0.1.6/README.md
@@ -0,0 +1,7 @@
+# Octopus的视频下载器
+
+## 更新发布方法
+```shell
+source .venv/bin/activate
+python upload_pypi.py
+```
---
+++ packages/octopus-downloader/octopus_downloader-0.1.6.tar.gz/octopus_downloader-0.1.6/octopus_downloader/utils/helper.py
@@ -0,0 +1,38 @@
+"""工具函数模块"""
+
+import os
+from dotenv import set_key
+
+
+def check_api_key(api_key: str) -> str:
+    """
+    检查 API 密钥是否有效
+
+    Args:
+        api_key (str): API 密钥名称
+
+    Returns:
+        str: 如果有效返回 API 密钥，否则返回 None
+    """
+
+    key = os.getenv(api_key, None)
+    if not key or len(key) < 1:
+        raise ValueError(f"{api_key} is not set in environment variables.")
+    
+    return key
+
+def set_api_key(api_key: str, value: str) -> None:
+    """
+    设置 API 密钥
+
+    Args:
+        api_key (str): API 密钥名称
+        value (str): API 密钥值
+    """
+    
+    if not api_key or not value:
+        raise ValueError("API key and value cannot be empty.")
+    
+    set_key(".env", api_key, value)
+    os.environ[api_key] = value
+    
