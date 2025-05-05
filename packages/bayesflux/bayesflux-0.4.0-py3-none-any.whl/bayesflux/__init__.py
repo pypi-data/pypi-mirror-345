1d533733b6422c9ef6d941bf84d8166152b08e34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-05 04:06:17 +0000

    Add package bayesflux/0.4.0/bayesflux-0.4.0-py3-none-any.whl

---
+++ packages/bayesflux/bayesflux-0.4.0-py3-none-any.whl/bayesflux/__init__.py
@@ -0,0 +1,6 @@
+"""
+BayesFlux: Bayesian Fast Linear algebra sUbspace eXtraction in JAX.
+"""
+
+from .generation import (GaussianInputOuputAndDerivativesSampler,
+                         InputOuputAndDerivativesSampler)
