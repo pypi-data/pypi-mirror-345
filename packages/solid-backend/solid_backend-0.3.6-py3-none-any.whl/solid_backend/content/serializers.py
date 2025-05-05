312f8d17bdbb40d58e0e342e38c1071a173252b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-05 04:06:02 +0000

    Add package solid-backend/0.3.6/solid_backend-0.3.6-py3-none-any.whl

---
+++ packages/solid-backend/solid_backend-0.3.6-py3-none-any.whl/solid_backend/content/serializers.py
@@ -0,0 +1,33 @@
+from importlib import import_module
+
+from django.conf import settings
+from rest_framework import serializers
+
+from solid_backend.utils.serializers import RecursiveSerializer
+
+from .models import TreeNode
+
+SERIALIZERS = {}
+
+if hasattr(settings, "PROFILES_SERIALIZERS"):
+    for field_name in settings.PROFILES_SERIALIZERS:
+        module, serializer = settings.PROFILES_SERIALIZERS[field_name]
+        SERIALIZERS[field_name] = getattr(import_module(module), serializer)
+
+
+class TreeNodeSerializer(serializers.ModelSerializer):
+    def build_nested_field(self, field_name, relation_info, nested_depth):
+        if SERIALIZERS.get(field_name) is not None:
+            return SERIALIZERS.get(field_name), {"many": True, "required": False}
+        return super(TreeNodeSerializer, self).build_nested_field(
+            field_name, relation_info, nested_depth
+        )
+
+    children = RecursiveSerializer(many=True, required=False)
+
+    class Meta:
+        model = TreeNode
+        fields = ("name", "info", "children") + tuple(
+            settings.PROFILES_SERIALIZERS.keys()
+        )
+        depth = 2
