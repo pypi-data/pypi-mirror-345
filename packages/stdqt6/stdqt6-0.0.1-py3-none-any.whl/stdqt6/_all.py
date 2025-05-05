6eba8215bdc784952f5900dd49a561b3271b8505
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-05 14:05:01 +0000

    Add package stdqt6/0.0.1/stdqt6-0.0.1-py3-none-any.whl

---
+++ packages/stdqt6/stdqt6-0.0.1-py3-none-any.whl/stdqt6/_all.py
@@ -0,0 +1,65 @@
+from PyQt6 import *
+from PyQt6.QtWidgets import *
+from PyQt6.QtCore import *
+from PyQt6.QtGui import *
+from PyQt6.QtMultimedia import *
+from PyQt6.QtNetwork import *
+from PyQt6.QtWebEngineWidgets import *
+from PyQt6.QtWebEngineCore import *
+from PyQt6.QtXml import *
+from PyQt6.QtWebSockets import *
+from PyQt6.QtWebChannel import *
+from PyQt6.QtTextToSpeech import *
+from PyQt6.QtTest import *
+from PyQt6.QtSvg import *
+from PyQt6.QtSql import *
+from PyQt6.QtSerialPort import *
+from PyQt6.QtSensors import *
+from PyQt6.QtRemoteObjects import *
+from PyQt6.QtQuick import *
+from PyQt6.QtQuickWidgets import *
+from PyQt6.QtQuick3D import *
+from PyQt6.QtQml import *
+from PyQt6.QtPrintSupport import *
+from PyQt6.QtPositioning import *
+from PyQt6.QtOpenGL import *
+from PyQt6.QtNfc import *
+from PyQt6.QtMultimediaWidgets import *
+from PyQt6.QtHelp import *
+from PyQt6.QtDesigner import *
+from PyQt6.QtDBus import *
+from PyQt6.QtBluetooth import *
+
+
+from PyQt6 import (
+    QtGui, 
+    QtWidgets, 
+    QtCore, 
+    QtMultimedia, 
+    QtNetwork, 
+    QtWebEngineWidgets, 
+    QtWebEngineCore, 
+    QtXml, 
+    QtWebSockets, 
+    QtWebChannel,
+    QtTextToSpeech,
+    QtDesigner,
+    QtHelp,
+    QtDBus,
+    QtBluetooth,
+    QtOpenGL,
+    QtNfc,
+    QtMultimediaWidgets,
+    QtPositioning,
+    QtPrintSupport,
+    QtQml,
+    QtQuick3D,
+    QtQuickWidgets,
+    QtQuick,
+    QtRemoteObjects,
+    QtSensors,
+    QtSerialPort,
+    QtTest,
+    QtSql,
+    QtSvg
+)
