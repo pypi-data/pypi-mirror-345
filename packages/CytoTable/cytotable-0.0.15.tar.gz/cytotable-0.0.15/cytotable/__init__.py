5d52c6c475b81fe0be102c30505921fa7fb9b30a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-05 03:23:08 +0000

    Add package CytoTable/0.0.15/cytotable-0.0.15.tar.gz

---
+++ packages/CytoTable/cytotable-0.0.15.tar.gz/cytotable-0.0.15/cytotable/__init__.py
@@ -0,0 +1,15 @@
+"""
+__init__.py for cytotable
+"""
+
+# note: version data is maintained by poetry-dynamic-versioning (do not edit)
+__version__ = "0.0.15"
+
+from .convert import convert
+from .exceptions import (
+    CytoTableException,
+    DatatypeException,
+    NoInputDataException,
+    SchemaException,
+)
+from .presets import config
