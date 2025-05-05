a8225cac136f3297871ad53e0b01cc8883aa1aaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-05 03:23:18 +0000

    Add package UQPyL/2.1.3/uqpyl-2.1.3-cp312-cp312-win_amd64.whl

---
+++ packages/UQPyL/uqpyl-2.1.3-cp312-cp312-win_amd64.whl/UQPyL/optimization/__init__.py
@@ -0,0 +1,5 @@
+from .single_objective import GA, PSO, CSA, DE, SCE_UA, ML_SCE_UA, ASMO, ABC, EGO
+from .multi_objective import NSGAII, MOEAD, MOASMO, RVEA
+# from .mathematics import Boxmin, Adam
+from .algorithmABC import Algorithm
+from .population import Population
