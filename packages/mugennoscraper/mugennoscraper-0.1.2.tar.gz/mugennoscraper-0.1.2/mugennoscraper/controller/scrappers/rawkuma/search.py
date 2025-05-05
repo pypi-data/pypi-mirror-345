fd013393fe93ba940bf6aa5b0733ba63a9aaee0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-05 14:04:51 +0000

    Add package mugennoscraper/0.1.2/mugennoscraper-0.1.2.tar.gz

---
+++ packages/mugennoscraper/mugennoscraper-0.1.2.tar.gz/mugennoscraper-0.1.2/mugennoscraper/controller/scrappers/rawkuma/search.py
@@ -0,0 +1,40 @@
+from bs4 import BeautifulSoup
+
+
+async def extract_titles(soup: BeautifulSoup) -> list[str]:
+    return [
+        title.get_text(strip=True)
+        for title in soup.select(".tt")  # ou ".bigor .tt" para ser mais específico
+    ]
+
+
+async def extract_links(soup: BeautifulSoup) -> list[str]:
+    return [
+        str(a["href"])
+        for a in soup.select("div.bs > div.bsx > a")
+        if a.has_attr("href")
+    ]
+
+
+async def extract_covers(soup: BeautifulSoup) -> list[str]:
+    return [
+        str(img["src"])  # No HTML fornecido, as imagens usam "src" não "data-src"
+        for img in soup.select(".limit img")  # Seleciona imagens dentro da div .limit
+        if img.has_attr("src")
+    ]
+
+
+async def extract_scores(soup: BeautifulSoup) -> list[str]:
+    return [
+        score.get_text(strip=True)
+        for score in soup.select(
+            ".numscore"
+        )  # A pontuação está na div com classe numscore
+    ]
+
+
+async def extract_chapters(soup: BeautifulSoup) -> list[str]:
+    return [
+        chapter.get_text(strip=True)
+        for chapter in soup.select(".epxs")  # Os capítulos estão na div com classe epxs
+    ]
