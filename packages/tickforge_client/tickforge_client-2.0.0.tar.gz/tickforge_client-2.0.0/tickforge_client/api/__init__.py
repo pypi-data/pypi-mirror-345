da027100cb228989c2948b4eb2455a18ad37aedf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-05 14:05:10 +0000

    Add package tickforge_client/2.0.0/tickforge_client-2.0.0.tar.gz

---
+++ packages/tickforge_client/tickforge_client-2.0.0.tar.gz/tickforge_client-2.0.0/tickforge_client/api/__init__.py
@@ -0,0 +1,15 @@
+# flake8: noqa
+
+# import apis into api package
+from tickforge_client.api.auth_api import AuthApi
+from tickforge_client.api.builds_api import BuildsApi
+from tickforge_client.api.db_api import DbApi
+from tickforge_client.api.default_api import DefaultApi
+from tickforge_client.api.git_api import GitApi
+from tickforge_client.api.github_api import GithubApi
+from tickforge_client.api.jobs_api import JobsApi
+from tickforge_client.api.metrics_api import MetricsApi
+from tickforge_client.api.redirects_api import RedirectsApi
+from tickforge_client.api.slack_api import SlackApi
+from tickforge_client.api.workspaces_api import WorkspacesApi
+
