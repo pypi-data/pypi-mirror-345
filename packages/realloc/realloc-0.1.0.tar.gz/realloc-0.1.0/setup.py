e0cf5545048798873db2ce01a17951871ed06b30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-05 04:05:55 +0000

    Add package realloc/0.1.0/realloc-0.1.0.tar.gz

---
+++ packages/realloc/realloc-0.1.0.tar.gz/realloc-0.1.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="realloc",
+    version="0.1.0",
+    description="A modular Python library for multi-account portfolio trade allocation",
+    author="Your Name",
+    author_email="you@example.com",
+    packages=find_packages(),
+    install_requires=[],
+    python_requires=">=3.8",
+    entry_points={
+        'console_scripts': [
+            'portfolio-cli=core.__main__:main',
+        ],
+    },
+    extras_require={
+        'dev': [
+            'pytest',
+            'pytest-cov',
+            'black',
+            'flake8',
+        ]
+    },
+    tests_require=[
+        'pytest',
+        'pytest-cov',
+        'hypothesis',
+    ],
+)
