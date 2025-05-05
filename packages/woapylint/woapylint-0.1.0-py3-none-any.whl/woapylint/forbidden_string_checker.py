bd07ad6405aa01243b5f70bf64a11b56bd27f76d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-05 14:04:49 +0000

    Add package woapylint/0.1.0/woapylint-0.1.0-py3-none-any.whl

---
+++ packages/woapylint/woapylint-0.1.0-py3-none-any.whl/woapylint/forbidden_string_checker.py
@@ -0,0 +1,19 @@
+from pylint.checkers import BaseChecker
+from astroid import nodes
+
+
+class ForbiddenStringChecker(BaseChecker):
+
+    name = "forbidden-string-checker"
+    msgs = {
+        "C9996": (
+            "Warning when u using field in your string '%s'",
+            "forbidden-string",
+            "Used field in your string",
+        ),
+    }
+
+    def visit_const(self, node: nodes.Const):
+        if isinstance(node.value, str):
+            if "field" in node.value.lower():
+                self.add_message("forbidden-string", node=node, args=(node.value,))
