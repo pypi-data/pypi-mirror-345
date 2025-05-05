8629e68659ca2a20add9f491d290370c3894fdee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-05 04:06:22 +0000

    Add package pan-scm-sdk/0.3.30/pan_scm_sdk-0.3.30.tar.gz

---
+++ packages/pan-scm-sdk/pan_scm_sdk-0.3.30.tar.gz/pan_scm_sdk-0.3.30/scm/config/setup/__init__.py
@@ -0,0 +1,13 @@
+# from .devices import Devices
+from .device import Device
+from .folder import Folder
+from .snippet import Snippet
+from .variable import Variable
+
+__all__ = [
+    "Device",
+    "Folder",
+    "Snippet",
+    "Variable",
+    # "Devices",
+]
