5769a0a0921110ec2da848083abef8985dce0838
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-05 04:06:12 +0000

    Add package nonebot-plugin-ehentai/0.1.0/nonebot_plugin_ehentai-0.1.0-py3-none-any.whl

---
+++ packages/nonebot-plugin-ehentai/nonebot_plugin_ehentai-0.1.0-py3-none-any.whl/nonebot_plugin_ehentai/config.py
@@ -0,0 +1,20 @@
+from nonebot.plugin import get_plugin_config
+from pydantic import BaseModel, Field
+
+
+class ScopedConfig(BaseModel):
+    base_api: str = "https://eh-arc-api.mhdy.icu"
+    """archive bot URL"""
+    apikey: str = ""
+    """archive bot apikey"""
+    pdf_pwd: str = ""
+    """pdf password"""
+    client: bool = True
+    """True NapCat/LLOB client, False Lagrange client"""
+
+
+class Config(BaseModel):
+    eh: ScopedConfig = Field(default_factory=ScopedConfig)
+
+
+config = get_plugin_config(Config).eh
