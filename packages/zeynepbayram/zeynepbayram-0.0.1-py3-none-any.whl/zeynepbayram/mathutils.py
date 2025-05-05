d0d05db0e3def311befd88981100a9d60c8f72e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-05 16:05:01 +0000

    Add package zeynepbayram/0.0.1/zeynepbayram-0.0.1-py3-none-any.whl

---
+++ packages/zeynepbayram/zeynepbayram-0.0.1-py3-none-any.whl/zeynepbayram/mathutils.py
@@ -0,0 +1,27 @@
+# src/zeynepbayram/mathutils.py
+
+def add(a: float, b: float) -> float:
+    """
+    Adds two numbers together.
+
+    Args:
+        a: The first number.
+        b: The second number.
+
+    Returns:
+        The sum of a and b.
+    """
+    return a + b
+
+def subtract(a: float, b: float) -> float:
+    """
+    Subtracts the second number from the first.
+
+    Args:
+        a: The number to subtract from.
+        b: The number to subtract.
+
+    Returns:
+        The result of a minus b.
+    """
+    return a - b
