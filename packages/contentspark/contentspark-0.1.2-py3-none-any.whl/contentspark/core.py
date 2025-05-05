f99f93a852457f92d14360989596a8a2c05b4194
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-05 15:04:54 +0000

    Add package contentspark/0.1.2/contentspark-0.1.2-py3-none-any.whl

---
+++ packages/contentspark/contentspark-0.1.2-py3-none-any.whl/contentspark/core.py
@@ -0,0 +1,49 @@
+from google.api_core.client_options import ClientOptions
+from google.cloud import discoveryengine_v1alpha as discoveryengine
+from typing import List
+
+def fetch_chat_response(
+    project_id: str,
+    location: str,
+    data_store_id: str,
+    search_query: str,
+) -> List[discoveryengine.SearchResponse]:
+    client_options = (
+        ClientOptions(api_endpoint=f"{location}-discoveryengine.googleapis.com")
+        if location != "global"
+        else None
+    )
+    client = discoveryengine.SearchServiceClient(client_options=client_options)
+
+    serving_config = client.serving_config_path(
+        project=project_id,
+        location=location,
+        data_store=data_store_id,
+        serving_config="default_config",
+    )
+
+    content_search_spec = discoveryengine.SearchRequest.ContentSearchSpec(
+        snippet_spec=discoveryengine.SearchRequest.ContentSearchSpec.SnippetSpec(
+            return_snippet=True
+        ),
+        summary_spec=discoveryengine.SearchRequest.ContentSearchSpec.SummarySpec(
+            summary_result_count=5,
+            include_citations=True,
+            ignore_adversarial_query=True,
+            ignore_non_summary_seeking_query=True,
+        ),
+    )
+
+    request = discoveryengine.SearchRequest(
+        serving_config=serving_config,
+        query=search_query,
+        content_search_spec=content_search_spec,
+        query_expansion_spec=discoveryengine.SearchRequest.QueryExpansionSpec(
+            condition=discoveryengine.SearchRequest.QueryExpansionSpec.Condition.AUTO
+        ),
+        spell_correction_spec=discoveryengine.SearchRequest.SpellCorrectionSpec(
+            mode=discoveryengine.SearchRequest.SpellCorrectionSpec.Mode.AUTO
+        ),
+    )
+
+    return client.search(request)
