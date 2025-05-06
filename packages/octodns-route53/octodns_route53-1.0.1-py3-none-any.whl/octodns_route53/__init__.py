c61494f4ead2869f84447a0b548f235103c12b31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-06 14:05:16 +0000

    Add package octodns-route53/1.0.1/octodns_route53-1.0.1-py3-none-any.whl

---
+++ packages/octodns-route53/octodns_route53-1.0.1-py3-none-any.whl/octodns_route53/__init__.py
@@ -0,0 +1,17 @@
+#
+#
+#
+
+from .provider import Route53Provider, Route53ProviderException
+from .record import Route53AliasRecord
+from .source import Ec2Source, ElbSource
+
+# TODO: remove __VERSION__ with the next major version release
+__version__ = __VERSION__ = '1.0.1'
+
+# quell warnings
+Ec2Source
+ElbSource
+Route53AliasRecord
+Route53Provider
+Route53ProviderException
