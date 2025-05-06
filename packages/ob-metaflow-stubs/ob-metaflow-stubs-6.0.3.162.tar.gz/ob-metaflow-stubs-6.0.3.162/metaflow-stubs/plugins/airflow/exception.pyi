5e8d04b53d1d992b0b78b60d03e73f7fd1b39386
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-06 08:05:21 +0000

    Add package ob-metaflow-stubs/6.0.3.162/ob-metaflow-stubs-6.0.3.162.tar.gz

---
+++ packages/ob-metaflow-stubs/ob-metaflow-stubs-6.0.3.162.tar.gz/ob-metaflow-stubs-6.0.3.162/metaflow-stubs/plugins/airflow/exception.pyi
@@ -0,0 +1,23 @@
+######################################################################################################
+#                                 Auto-generated Metaflow stub file                                  #
+# MF version: 2.15.10.1+obcheckpoint(0.2.1);ob(v1)                                                   #
+# Generated on 2025-05-05T21:26:32.705858                                                            #
+######################################################################################################
+
+from __future__ import annotations
+
+import metaflow
+import typing
+if typing.TYPE_CHECKING:
+    import metaflow.exception
+
+from ...exception import MetaflowException as MetaflowException
+
+class AirflowException(metaflow.exception.MetaflowException, metaclass=type):
+    def __init__(self, msg):
+        ...
+    ...
+
+class NotSupportedException(metaflow.exception.MetaflowException, metaclass=type):
+    ...
+
