357a3a3f9b4d50d45e48a5351539988a6e734c0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-06 04:06:02 +0000

    Add package gx-sqlalchemy-redshift/0.8.19/gx_sqlalchemy_redshift-0.8.19-py2.py3-none-any.whl

---
+++ packages/gx-sqlalchemy-redshift/gx_sqlalchemy_redshift-0.8.19-py2.py3-none-any.whl/sqlalchemy_redshift/__init__.py
@@ -0,0 +1,23 @@
+from importlib.metadata import version
+
+__version__ = version('gx-sqlalchemy-redshift')
+
+from sqlalchemy.dialects import registry  # noqa
+
+registry.register(
+    "redshift", "sqlalchemy_redshift.dialect",
+    "RedshiftDialect_psycopg2"
+)
+registry.register(
+    "redshift.psycopg2", "sqlalchemy_redshift.dialect",
+    "RedshiftDialect_psycopg2"
+)
+registry.register(
+    'redshift+psycopg2cffi', 'sqlalchemy_redshift.dialect',
+    'RedshiftDialect_psycopg2cffi',
+)
+
+registry.register(
+    "redshift+redshift_connector", "sqlalchemy_redshift.dialect",
+    "RedshiftDialect_redshift_connector"
+)
