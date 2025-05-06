b6d9e5e55ae409e54f0fb191713f8d416113196a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-06 06:05:57 +0000

    Add package xnatqa/0.0.19/xnatqa-0.0.19-py3-none-any.whl

---
+++ packages/xnatqa/xnatqa-0.0.19-py3-none-any.whl/xnatqa/__init__.py
@@ -0,0 +1,21 @@
+import os
+import json
+
+def get_files_with_extension(directory, extension):
+    files = []
+    for filename in os.listdir(directory):
+        if os.path.isfile(os.path.join(directory, filename)) and filename.endswith(extension):
+            files.append(filename)
+    return files
+
+def read_json_file(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            data = json.load(file)
+            return data
+    except FileNotFoundError:
+        print(f"Error: File not found: {file_path}")
+        return None
+    except json.JSONDecodeError:
+        print(f"Error: Invalid JSON format in file: {file_path}")
+        return None
