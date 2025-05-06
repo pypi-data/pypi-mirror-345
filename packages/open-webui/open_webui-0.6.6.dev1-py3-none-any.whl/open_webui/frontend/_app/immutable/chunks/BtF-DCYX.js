bc0ae3d5392a3bafe225d41c93dbb52944cd62f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-06 04:05:05 +0000

    Add package open-webui/0.6.6.dev1/open_webui-0.6.6.dev1-py3-none-any.whl

---
+++ packages/open-webui/open_webui-0.6.6.dev1-py3-none-any.whl/open_webui/frontend/_app/immutable/chunks/BtF-DCYX.js
@@ -0,0 +1,2 @@
+import{U as a,C as n}from"./BY77eygF.js";const t=(r,o)=>a.lang.round(n.parse(r)[o]);export{t as c};
+//# sourceMappingURL=BtF-DCYX.js.map
---
+++ packages/open-webui/open_webui-0.6.6.dev1-py3-none-any.whl/open_webui/frontend/_app/immutable/entry/start.BzyNAlXK.js
@@ -0,0 +1,2 @@
+import{l as o,a as r}from"../chunks/lE3Ktrvf.js";export{o as load_css,r as start};
+//# sourceMappingURL=start.BzyNAlXK.js.map
