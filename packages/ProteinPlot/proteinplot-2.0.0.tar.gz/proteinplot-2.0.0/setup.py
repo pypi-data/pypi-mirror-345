28f7f5441accd2ae806f3449ce1e8038a10e7d89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-06 06:05:43 +0000

    Add package ProteinPlot/2.0.0/proteinplot-2.0.0.tar.gz

---
+++ packages/ProteinPlot/proteinplot-2.0.0.tar.gz/proteinplot-2.0.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ProteinPlot",
+    version="2.0.0",
+    packages=find_packages(),
+    install_requires=['numpy', 'pandas', 'matplotlib', 'jupyter', 'plotly', 'seaborn', 'python'], 
+    author="Poku-Racz-Denes",
+    author_email="denes.dome@gmail.com",
+    description="Library for managing .pdb extension, plotting",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://protplot.readthedocs.io/en/latest/",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: Other/Proprietary License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",)
+
