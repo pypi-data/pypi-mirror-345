ef73ed12ae44196a7a555013eefc318bffccd553
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-06 06:05:34 +0000

    Add package e2b/1.4.0/e2b-1.4.0-py3-none-any.whl

---
+++ packages/e2b/e2b-1.4.0-py3-none-any.whl/e2b/sandbox/sandbox_api.py
@@ -0,0 +1,43 @@
+from abc import ABC
+from dataclasses import dataclass
+from typing import Optional, Dict
+from datetime import datetime
+from httpx import Limits
+
+
+@dataclass
+class SandboxInfo:
+    """Information about a sandbox."""
+
+    sandbox_id: str
+    """Sandbox ID."""
+    template_id: str
+    """Template ID."""
+    name: Optional[str]
+    """Template name."""
+    metadata: Dict[str, str]
+    """Saved sandbox metadata."""
+    started_at: datetime
+    """Sandbox start time."""
+    end_at: datetime
+    """Sandbox expiration date."""
+
+
+@dataclass
+class SandboxQuery:
+    """Query parameters for listing sandboxes."""
+
+    metadata: Optional[dict[str, str]] = None
+    """Filter sandboxes by metadata."""
+
+
+class SandboxApiBase(ABC):
+    _limits = Limits(
+        max_keepalive_connections=10,
+        max_connections=20,
+        keepalive_expiry=20,
+    )
+
+    @staticmethod
+    def _get_sandbox_id(sandbox_id: str, client_id: str) -> str:
+        return f"{sandbox_id}-{client_id}"
