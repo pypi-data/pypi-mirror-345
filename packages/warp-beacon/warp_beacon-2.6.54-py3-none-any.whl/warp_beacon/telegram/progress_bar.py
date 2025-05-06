626868dff9c370aec59a6a03e4752279fc88a0d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-06 14:05:23 +0000

    Add package warp-beacon/2.6.54/warp_beacon-2.6.54-py3-none-any.whl

---
+++ packages/warp-beacon/warp_beacon-2.6.54-py3-none-any.whl/warp_beacon/telegram/progress_bar.py
@@ -0,0 +1,85 @@
+import logging
+
+import hashlib
+
+from pyrogram.enums import ParseMode
+from pyrogram.errors.exceptions.bad_request_400 import MessageNotModified
+from pyrogram import Client
+
+class ProgressBar(object):
+	def __init__(self, client: Client) -> None:
+		self._next_threshold = 20
+		self.client = client
+		self.complete = False
+
+	def is_complete(self) -> bool:
+		return self.complete
+
+	def make_progress_bar(self, current: int, total: int, length: int = 10) -> str:
+		"""
+		Returns string
+		[████▌────] 55%
+		length — amount of characters in bar
+		"""
+		# fraction of completed job from 0.0 to 1.0
+		frac = current / total if total else 0
+		# how much "filled" cells
+		filled = int(frac * length)
+		# part between whole cells, optional may withdraw half
+		half_block = ''
+		if (frac * length) - filled >= 0.5:
+			half_block = '▌'  # or '▏', '▍' and etc.
+		# building bar
+		pbar = '█' * filled + half_block + '─' * (length - filled - len(half_block))
+		percent = frac * 100
+		return f"<b>[{pbar}] {round(percent)}%</b>"
+	
+	def make_emoji_progress_bar(self, current: int, total: int, length: int = 10) -> str:
+		"""
+		Returns string:
+		[🟩🟩🟩⬜️⬜️⬜️⬜️⬜️⬜️⬜️] 30%
+		length — common number of emoji cells
+		"""
+		frac = (current / total) if total else 0
+		filled_count = int(frac * length)
+		empty_count = length - filled_count
+		pbar = "🟩" * filled_count + "⬜️" * empty_count
+		percent = frac * 100
+		return f"[{pbar}] {round(percent)}%"
+
+	async def progress_callback(self, current: int, total: int, chat_id: int | str, message_id: int, operation: str, label: str = "") -> None:
+		percent = current * 100 / (total or 1)
+		if total == 0 or percent >= self._next_threshold:
+			#pbar = self.make_progress_bar(percent, 100, 25)
+			pbar = self.make_emoji_progress_bar(percent, 100, 14)
+			logging.info("[%s] Operation: %s %d%%", label or operation, operation, percent)
+			try:
+				#await self.client.edit_message_caption(chat_id, message_id, f"{pbar} <b>{operation}</b> {label}", ParseMode.HTML)
+				# we don't need to wait completion, waste of time and resources
+				task = self.client.loop.create_task(
+					self.client.edit_message_caption(chat_id, message_id, f"{pbar} <b>{operation}</b> {label}", ParseMode.HTML)
+				)
+				task.add_done_callback(
+					lambda t: (
+						(exc := t.exception()) and
+						(isinstance(exc, MessageNotModified)
+							and logging.warning("bad_request_400.MessageNotModified")
+							or logging.exception("Error in edit_message_caption", exc_info=exc))
+					)
+				)
+			#except MessageNotModified:
+				#logging.warning("bad_request_400.MessageNotModified")
+			except Exception as e:
+				logging.warning("An error occurred while setup task to update progress bar")
+				logging.exception(e)
+			if total > 0:
+				self._next_threshold += 20
+		if percent >= 100:
+			self.complete = True
+
+	@staticmethod
+	def make_hash(chat_id: str | int, message_id: int, algorithm: str = 'sha256') -> str:
+		s = f"{chat_id}:{message_id}"
+		# md5, sha1, sha256
+		h = hashlib.new(algorithm, s.encode('utf-8'))
+		return h.hexdigest()
