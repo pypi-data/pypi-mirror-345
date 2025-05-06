97fce04b86738f3405a6796b10b5d4a941a6d15c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-06 06:05:49 +0000

    Add package cfsign/0.0.2/cfsign-0.0.2.tar.gz

---
+++ packages/cfsign/cfsign-0.0.2.tar.gz/cfsign-0.0.2/cfsign/types/cloudfront_signature_types.py
@@ -0,0 +1,24 @@
+from typing import Literal, TypedDict
+
+__all__ = ("CfCookieDictKey", "CloudfrontSignatureDict", "CloudfrontSignatureQueryParams", "UrlQueryParamKey")
+
+CfCookieDictKey = Literal["CloudFront-Policy", "CloudFront-Signature", "CloudFront-Key-Pair-Id"]
+UrlQueryParamKey = Literal["Policy", "Signature", "Key-Pair-Id"]
+
+CloudfrontSignatureDict = TypedDict(
+    "CloudfrontSignatureDict",
+    {
+        "CloudFront-Policy": str,
+        "CloudFront-Signature": str,
+        "CloudFront-Key-Pair-Id": str,
+    },
+)
+
+CloudfrontSignatureQueryParams = TypedDict(
+    "CloudfrontSignatureQueryParams",
+    {
+        "Policy": str,
+        "Signature": str,
+        "Key-Pair-Id": str,
+    },
+)
