334ec40259f3026758899793abd9d6e51cb49074
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-06 03:21:07 +0000

    Add package pystolint/0.1.0/pystolint-0.1.0-py3-none-any.whl

---
+++ packages/pystolint/pystolint-0.1.0-py3-none-any.whl/pystolint/dto/report.py
@@ -0,0 +1,21 @@
+from collections.abc import Collection
+
+
+class ReportItem:
+    def __init__(self, file_path: str, line: int, column: int, message: str) -> None:
+        self.file_path = file_path
+        self.line = line
+        self.column = column
+        self.message = message
+
+    def __str__(self) -> str:
+        return f'{self.file_path}:{self.line}:{self.column}: {self.message}'
+
+
+class Report:
+    def __init__(self, items: list[ReportItem] | None = None, errors: Collection[str] | None = None) -> None:
+        self.items: list[ReportItem] = items or []
+        self.errors: set[str] = set(errors or ())
+
+    def __add__(self, other: 'Report') -> 'Report':
+        return Report(items=self.items + other.items, errors=self.errors | other.errors)
