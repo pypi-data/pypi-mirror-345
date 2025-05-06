c2faea3550c52bdf7162403023ef00058080d7db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-06 05:05:20 +0000

    Add package langchain-predictionguard/0.2.2/langchain_predictionguard-0.2.2-py3-none-any.whl

---
+++ packages/langchain-predictionguard/langchain_predictionguard-0.2.2-py3-none-any.whl/langchain_predictionguard/__init__.py
@@ -0,0 +1,6 @@
+"""Create controlled and compliant AI systems with PredictionGuard and LangChain"""
+from .PredictionGuard import PredictionGuard as PredictionGuard
+from .ChatPredictionGuard import ChatPredictionGuard as ChatPredictionGuard
+from .PredictionGuardEmbeddings import PredictionGuardEmbeddings as PredictionGuardEmbeddings
+
+__version__ = "0.1.1"
