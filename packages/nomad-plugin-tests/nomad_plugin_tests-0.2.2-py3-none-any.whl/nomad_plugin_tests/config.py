ee650c43c550691813267aa3c00fefca4f3f0a5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-06 05:05:21 +0000

    Add package nomad-plugin-tests/0.2.2/nomad_plugin_tests-0.2.2-py3-none-any.whl

---
+++ packages/nomad-plugin-tests/nomad_plugin_tests-0.2.2-py3-none-any.whl/nomad_plugin_tests/config.py
@@ -0,0 +1,11 @@
+TESTS_TO_RUN = {
+    "pynxtools": "tests/nomad",
+    "pynxtools_apm": "tests/test_nomad_examples.py",
+    "pynxtools_ellips": "tests/test_nomad_examples.py",
+    "pynxtools_em": "tests/test_nomad_examples.py",
+    "pynxtools_mpes": "tests/test_nomad_examples.py",
+    "pynxtools_stm": "tests/test_nomad_examples.py",
+    "pynxtools_spm": "tests/test_nomad_examples.py",
+    "pynxtools_xps": "tests/test_nomad_examples.py",
+    "electronicparsers": "tests",
+}
