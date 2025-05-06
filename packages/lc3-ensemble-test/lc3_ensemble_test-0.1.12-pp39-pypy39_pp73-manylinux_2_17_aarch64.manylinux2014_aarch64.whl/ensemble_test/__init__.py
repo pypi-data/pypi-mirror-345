f81978add3a9ce37945659c6385f0c8f4a004224
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-06 14:05:46 +0000

    Add package lc3-ensemble-test/0.1.12/lc3_ensemble_test-0.1.12-pp39-pypy39_pp73-manylinux_2_17_aarch64.manylinux2014_aarch64.whl

---
+++ packages/lc3-ensemble-test/lc3_ensemble_test-0.1.12-pp39-pypy39_pp73-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/ensemble_test/__init__.py
@@ -0,0 +1,9 @@
+"""
+A LC-3 simulator and unit tester, backed by lc3-ensemble.
+
+All of the contents of this package can be found 
+in the subpackages `ensemble_test.core` and `ensemble_test.autograder`.
+
+See `ensemble_test.core` for details about simulation,
+and `ensemble_test.autograder` for details about unit testing.
+"""
