1c899363f7629eccd91983fc3680c4ae39540939
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-06 07:05:54 +0000

    Add package ravenpy/0.18.2/ravenpy-0.18.2.tar.gz

---
+++ packages/ravenpy/ravenpy-0.18.2.tar.gz/ravenpy-0.18.2/tests/test_bias_correction.py
@@ -0,0 +1,63 @@
+import pytest
+import xarray as xr
+import xclim.sdba as sdba
+from xarray.coding.calendar_ops import convert_calendar
+
+
+# FIXME: This doesn't test Raven functionality; Should we move it to xclim?
+@pytest.mark.skip(reason="This test is not testing Raven functionality")
+class TestBiasCorrect:
+    def test_bias_correction(self, get_local_testdata):
+        ds_fut_sub = xr.open_dataset(
+            get_local_testdata(
+                "cmip5/nasa_nex-gddp-1.0_day_inmcm4_historical+rcp85_nex-gddp_2070-2071_subset.nc"
+            )
+        )
+        ds_ref_sub = xr.open_dataset(
+            get_local_testdata(
+                "cmip5/nasa_nex-gddp-1.0_day_inmcm4_historical+rcp45_nex-gddp_1971-1972_subset.nc"
+            )
+        )
+        ds_ref_sub = convert_calendar(ds_ref_sub, "noleap")
+
+        ds_his_sub = xr.open_dataset(
+            get_local_testdata("nrcan/NRCAN_1971-1972_subset.nc")
+        )
+        ds_his_sub = convert_calendar(ds_his_sub, "noleap")
+        group = sdba.Grouper("time.month")
+        # Train the model to find the correction factors
+        Adj = sdba.DetrendedQuantileMapping.train(
+            ref=ds_ref_sub["pr"],
+            hist=ds_his_sub["pr"],
+            nquantiles=50,
+            kind="+",
+            group=group,
+        )
+
+        # Apply the factors to the future data to bias-correct
+        Adj.adjust(ds_fut_sub["pr"], interp="linear")
+
+        # Repeat for temperature max
+        Adj = sdba.DetrendedQuantileMapping.train(
+            ref=ds_ref_sub["tasmax"],
+            hist=ds_his_sub["tasmax"],
+            nquantiles=50,
+            kind="+",
+            group=group,
+        )
+
+        # Apply the factors to the future data to bias-correct
+        Adj.adjust(ds_fut_sub["tasmax"], interp="linear")
+
+        # Repeat for tasmin
+        Adj = sdba.DetrendedQuantileMapping.train(
+            ref=ds_ref_sub["tasmin"],
+            hist=ds_his_sub["tasmin"],
+            nquantiles=50,
+            kind="+",
+            group=group,
+        )
+
+        Adj.adjust(ds_fut_sub["tasmin"], interp="linear")
+
+        # TODO: Add numerical check
---
+++ packages/ravenpy/ravenpy-0.18.2.tar.gz/ravenpy-0.18.2/tests/test_utils.py
@@ -0,0 +1,27 @@
+import pytest
+
+from ravenpy.config.utils import nc_specs
+
+
+def test_nc_specs(get_local_testdata):
+    f = get_local_testdata(
+        "raven-gr4j-cemaneige/Salmon-River-Near-Prince-George_meteo_daily.nc"
+    )
+    attrs = nc_specs(f, "PRECIP", station_idx=1, alt_names=("rain",))
+    assert "file_name_nc" in attrs
+
+
+def test_nc_specs_bad(bad_netcdf):
+    # Test with scalar elevation. Should normally have a station dimension, but not always the case.
+    attrs_s = nc_specs(bad_netcdf, "PRECIP", station_idx=1, alt_names=("rain",))
+    assert attrs_s["elevation"] == 1.0
+
+
+@pytest.mark.online
+def test_dap_specs():
+    # Link to THREDDS Data Server netCDF testdata
+    tds = "https://pavics.ouranos.ca/twitcher/ows/proxy/thredds/dodsC/birdhouse/testdata/raven"
+    fn = f"{tds}/raven-gr4j-cemaneige/Salmon-River-Near-Prince-George_meteo_daily.nc"
+
+    attrs = nc_specs(fn, "PRECIP", station_idx=1, alt_names=("rain",), engine="pydap")
+    assert "units" in attrs
