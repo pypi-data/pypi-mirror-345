fa799f0ccbe6868c607467582998e09111afbeb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-06 06:06:03 +0000

    Add package meu-pacote-python/1.0.0/meu_pacote_python-1.0.0.tar.gz

---
+++ packages/meu-pacote-python/meu_pacote_python-1.0.0.tar.gz/meu_pacote_python-1.0.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="meu_pacote_python",  # 🔁 troque pelo nome único no PyPI
+    version="1.0.0",
+    author="Igor Pompeo",
+    author_email="pompbass@gmail.com",
+    description="Exercícios de Python - Curso em Vídeo",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/igorpompeo/Python",
+    packages=find_packages(include=["Mundo01", "Mundo01.*"]),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+)
