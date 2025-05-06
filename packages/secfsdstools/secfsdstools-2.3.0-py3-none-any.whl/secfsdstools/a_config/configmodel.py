63a52928ecab2eb52763a5f88a4a95d7132fb0bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-06 07:05:38 +0000

    Add package secfsdstools/2.3.0/secfsdstools-2.3.0-py3-none-any.whl

---
+++ packages/secfsdstools/secfsdstools-2.3.0-py3-none-any.whl/secfsdstools/a_config/configmodel.py
@@ -0,0 +1,38 @@
+"""
+Base Model for configuration.
+"""
+
+import configparser
+import os
+from dataclasses import asdict, dataclass
+from typing import Optional
+
+
+@dataclass
+class Configuration:
+    """ Basic configuration settings """
+    download_dir: str
+    db_dir: str
+    parquet_dir: str
+    user_agent_email: str
+    rapid_api_key: Optional[str] = None
+    rapid_api_plan: Optional[str] = 'basic'
+    daily_download_dir: Optional[str] = None
+    auto_update: Optional[bool] = True
+    keep_zip_files: Optional[bool] = False
+    no_parallel_processing: Optional[bool] = False
+
+    config_parser: Optional[configparser.ConfigParser] = None
+    post_update_hook: Optional[str] = None
+    post_update_processes: Optional[str] = None
+
+    def __post_init__(self):
+        self.daily_download_dir = os.path.join(self.download_dir, "daily")
+
+    def get_dict(self):
+        """
+        returns the configuration as a dictionary
+        Returns:
+            the configuration as a dictionary
+        """
+        return dict(asdict(self))
