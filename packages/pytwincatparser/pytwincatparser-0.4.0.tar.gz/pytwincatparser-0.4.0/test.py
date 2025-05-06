3d524db1ef52f435a11ce6b2d7d914b5fe8f987c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-06 08:05:19 +0000

    Add package pytwincatparser/0.4.0/pytwincatparser-0.4.0.tar.gz

---
+++ packages/pytwincatparser/pytwincatparser-0.4.0.tar.gz/pytwincatparser-0.4.0/test.py
@@ -0,0 +1,13 @@
+from src.pytwincatparser.Twincat4024Strategy import Twincat4024Strategy
+from pathlib import Path   
+
+
+strategy = Twincat4024Strategy()
+
+    #print(strategy.load_objects(Path("TwincatFiles\Base\FB_Base.TcPOU")))
+    #print(strategy.load_objects(Path("TwincatFiles\Commands\ST_PmlCommand.TcDUT")))
+    #print(strategy.load_objects(Path("TwincatFiles\TwincatPlcProject.plcproj")))
+
+objects = strategy.load_objects(Path("TwincatFiles\moreFiles\Main.plcproj"))
+for obj in objects:
+    print(f"name:{obj.name} namespace:{obj.name_space} , parent:{obj.parent.name if obj.parent is not None else ""}, ident: {obj.get_identifier()}")
