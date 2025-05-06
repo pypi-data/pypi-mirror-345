59dd9f4b903acc5f72fa6cce8e3e354df14bb859
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-06 14:05:38 +0000

    Add package flaxdiff/0.2.9/flaxdiff-0.2.9-py3-none-any.whl

---
+++ packages/flaxdiff/flaxdiff-0.2.9-py3-none-any.whl/flaxdiff/samplers/euler.py
@@ -0,0 +1,56 @@
+import jax
+import jax.numpy as jnp
+from .common import DiffusionSampler
+from ..utils import RandomMarkovState
+
+class EulerSampler(DiffusionSampler):
+    # Basically a DDIM Sampler but parameterized as an ODE
+    def take_next_step(self, current_samples, reconstructed_samples, model_conditioning_inputs,
+                 pred_noise, current_step, state:RandomMarkovState, sample_model_fn, next_step=1) -> tuple[jnp.ndarray, RandomMarkovState]:
+        current_alpha, current_sigma = self.noise_schedule.get_rates(current_step)
+        next_alpha, next_sigma = self.noise_schedule.get_rates(next_step)
+
+        dt = next_sigma - current_sigma
+        
+        x_0_coeff = (current_alpha * next_sigma - next_alpha * current_sigma) / (dt)
+        dx = (current_samples - x_0_coeff * reconstructed_samples) / current_sigma
+        next_samples = current_samples + dx * dt
+        return next_samples, state
+
+class SimplifiedEulerSampler(DiffusionSampler):
+    """
+    This is for networks with forward diffusion of the form x_{t+1} = x_t + sigma_t * epsilon_t
+    """
+    def take_next_step(self, current_samples, reconstructed_samples, model_conditioning_inputs,
+                 pred_noise, current_step, state:RandomMarkovState, sample_model_fn, next_step=1) -> tuple[jnp.ndarray, RandomMarkovState]:
+        _, current_sigma = self.noise_schedule.get_rates(current_step)
+        _, next_sigma = self.noise_schedule.get_rates(next_step)
+
+        dt = next_sigma - current_sigma
+        
+        dx = (current_samples - reconstructed_samples) / current_sigma
+        next_samples = current_samples + dx * dt
+        return next_samples, state
+    
+class EulerAncestralSampler(DiffusionSampler):
+    """
+    Similar to EulerSampler but with ancestral sampling
+    """
+    def take_next_step(self, current_samples, reconstructed_samples, model_conditioning_inputs,
+                 pred_noise, current_step, state:RandomMarkovState, sample_model_fn, next_step=1) -> tuple[jnp.ndarray, RandomMarkovState]:
+        current_alpha, current_sigma = self.noise_schedule.get_rates(current_step)
+        next_alpha, next_sigma = self.noise_schedule.get_rates(next_step)
+
+        sigma_up = (next_sigma**2 * (current_sigma**2 - next_sigma**2) / current_sigma**2) ** 0.5
+        sigma_down = (next_sigma**2 - sigma_up**2) ** 0.5
+        
+        dt = sigma_down - current_sigma
+        
+        x_0_coeff = (current_alpha * next_sigma - next_alpha * current_sigma) / (next_sigma - current_sigma)
+        dx = (current_samples - x_0_coeff * reconstructed_samples) / current_sigma
+        
+        state, subkey = state.get_random_key()
+        dW = jax.random.normal(subkey, current_samples.shape) * sigma_up
+        
+        next_samples = current_samples + dx * dt + dW
+        return next_samples, state
