d3cc9864f7373797d964115ef5eb55cd06cffcab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-06 03:21:40 +0000

    Add package slamd/2.1.6/slamd-2.1.6-cp313-cp313-macosx_11_0_arm64.whl

---
+++ packages/slamd/slamd-2.1.6-cp313-cp313-macosx_11_0_arm64.whl/slamd/geom/overrides.py
@@ -0,0 +1,30 @@
+import numpy as np
+from ..bindings.geom import (
+    PointCloud as PointCloud_internal,
+    PolyLine as PolyLine_internal,
+)
+from .._utils.colors import Color
+from .._utils.handle_input import process_color, process_radii
+
+
+def PointCloud(
+    positions: np.ndarray,
+    colors: np.ndarray | tuple[int, int, int] = Color.black,
+    radii: np.ndarray | float = 1.0,
+    min_brightness: float = 1.0,
+):
+    n = positions.shape[0]
+    colors_np = process_color(colors, n)
+    radii_np = process_radii(radii, n)
+
+    return PointCloud_internal(positions, colors_np, radii_np, min_brightness)
+
+
+def PolyLine(
+    points: np.ndarray,
+    thickness: float = 1.0,
+    color: np.ndarray | tuple[int, int, int] = Color.red,
+    min_brightness: float = 1.0,
+):
+    color_np = process_color(color, 1).reshape(3)
+    return PolyLine_internal(points, thickness, color_np, min_brightness)
