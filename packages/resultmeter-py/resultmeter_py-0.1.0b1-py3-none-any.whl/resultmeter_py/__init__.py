d811a0c21455e7365ee780003f3e734b3d2969fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-06 15:06:32 +0000

    Add package resultmeter-py/0.1.0b1/resultmeter_py-0.1.0b1-py3-none-any.whl

---
+++ packages/resultmeter-py/resultmeter_py-0.1.0b1-py3-none-any.whl/resultmeter_py/__init__.py
@@ -0,0 +1,5 @@
+from typing import Any, Dict
+
+def record_usage(agent_id: str, customer_id: str, signal: str, extra: Dict[str, Any] = None) -> bool:
+    """Geçici telemetri iskeleti"""
+    return True
