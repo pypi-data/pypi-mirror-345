4bc658d6ae73f026e7fbb6ef41394ec764eba184
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-06 03:21:48 +0000

    Add package MiBetisCalculation/1.0.0/mibetiscalculation-1.0.0.tar.gz

---
+++ packages/MiBetisCalculation/mibetiscalculation-1.0.0.tar.gz/mibetiscalculation-1.0.0/FUNCTIONS/calculation.py
@@ -0,0 +1,18 @@
+def sum(n1,n2):
+    print(f"Sum result: {n1+2}")
+
+def minus(n1,n2):
+    print("Minus result: ",n1-n2)
+
+def multiply(n1,n2):
+    print("Multiply result: ",n1*n2)
+
+def divide(n1,n2):
+    print("Dvision result: ",n1/n2)
+
+def round(n1):
+    print(f"The rounded number is {round(n1)}")
+
+def exp(base,exp):
+    print(f"The result is {base**exp}")
+
---
+++ packages/MiBetisCalculation/mibetiscalculation-1.0.0.tar.gz/mibetiscalculation-1.0.0/v36_2_setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(
+    name = "MiBetisCalculation",
+    version = "1.0.0",
+    description = "Calculation Package",
+    author = "Javier",
+    author_email = "jprez@guidewire.com",
+    packages = ["FUNCTIONS"]
+    
+)
