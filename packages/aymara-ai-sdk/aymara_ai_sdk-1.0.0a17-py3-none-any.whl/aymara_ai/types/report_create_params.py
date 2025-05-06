0992fd4363a1d0cd1d05213eaaa0e4ad920fc013
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-06 07:05:41 +0000

    Add package aymara-ai-sdk/1.0.0a17/aymara_ai_sdk-1.0.0a17-py3-none-any.whl

---
+++ packages/aymara-ai-sdk/aymara_ai_sdk-1.0.0a17-py3-none-any.whl/aymara_ai/types/report_create_params.py
@@ -0,0 +1,16 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from typing import List, Optional
+from typing_extensions import Required, TypedDict
+
+__all__ = ["ReportCreateParams"]
+
+
+class ReportCreateParams(TypedDict, total=False):
+    eval_run_uuids: Required[List[str]]
+
+    is_sandbox: Optional[bool]
+
+    workspace_uuid: str
