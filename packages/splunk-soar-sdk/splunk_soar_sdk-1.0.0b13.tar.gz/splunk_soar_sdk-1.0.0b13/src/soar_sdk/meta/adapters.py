bf01f9718bb0dd25dd9124af60a6814dcbb224ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-06 08:05:19 +0000

    Add package splunk-soar-sdk/1.0.0b13/splunk_soar_sdk-1.0.0b13.tar.gz

---
+++ packages/splunk-soar-sdk/splunk_soar_sdk-1.0.0b13.tar.gz/splunk_soar_sdk-1.0.0b13/src/soar_sdk/meta/adapters.py
@@ -0,0 +1,23 @@
+import toml
+
+from .app import AppMeta
+
+
+class TOMLDataAdapter:
+    @staticmethod
+    def load_data(filepath: str) -> AppMeta:
+        with open(filepath) as f:
+            toml_data = toml.load(f)
+
+        soar_app_data = toml_data.get("tool", {}).get("soar", {}).get("app", {})
+        uv_app_data = toml_data.get("project", {})
+
+        return AppMeta(
+            **dict(
+                name=uv_app_data.get("name"),
+                description=uv_app_data.get("description"),
+                app_version=uv_app_data.get("version"),
+                license=uv_app_data.get("license"),
+                **soar_app_data,
+            )
+        )
