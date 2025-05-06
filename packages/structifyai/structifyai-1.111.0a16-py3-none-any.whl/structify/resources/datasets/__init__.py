152503ee4510c8434646570c0f98c99d93c627e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-06 07:05:26 +0000

    Add package structifyai/1.111.0a16/structifyai-1.111.0a16-py3-none-any.whl

---
+++ packages/structifyai/structifyai-1.111.0a16-py3-none-any.whl/structify/resources/datasets/__init__.py
@@ -0,0 +1,47 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from .datasets import (
+    DatasetsResource,
+    AsyncDatasetsResource,
+    DatasetsResourceWithRawResponse,
+    AsyncDatasetsResourceWithRawResponse,
+    DatasetsResourceWithStreamingResponse,
+    AsyncDatasetsResourceWithStreamingResponse,
+)
+from .evaluate import (
+    EvaluateResource,
+    AsyncEvaluateResource,
+    EvaluateResourceWithRawResponse,
+    AsyncEvaluateResourceWithRawResponse,
+    EvaluateResourceWithStreamingResponse,
+    AsyncEvaluateResourceWithStreamingResponse,
+)
+from .workflow import (
+    WorkflowResource,
+    AsyncWorkflowResource,
+    WorkflowResourceWithRawResponse,
+    AsyncWorkflowResourceWithRawResponse,
+    WorkflowResourceWithStreamingResponse,
+    AsyncWorkflowResourceWithStreamingResponse,
+)
+
+__all__ = [
+    "WorkflowResource",
+    "AsyncWorkflowResource",
+    "WorkflowResourceWithRawResponse",
+    "AsyncWorkflowResourceWithRawResponse",
+    "WorkflowResourceWithStreamingResponse",
+    "AsyncWorkflowResourceWithStreamingResponse",
+    "EvaluateResource",
+    "AsyncEvaluateResource",
+    "EvaluateResourceWithRawResponse",
+    "AsyncEvaluateResourceWithRawResponse",
+    "EvaluateResourceWithStreamingResponse",
+    "AsyncEvaluateResourceWithStreamingResponse",
+    "DatasetsResource",
+    "AsyncDatasetsResource",
+    "DatasetsResourceWithRawResponse",
+    "AsyncDatasetsResourceWithRawResponse",
+    "DatasetsResourceWithStreamingResponse",
+    "AsyncDatasetsResourceWithStreamingResponse",
+]
