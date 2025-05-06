abbc663ff9868ebe61969a8d234cac17439d0417
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-06 07:06:02 +0000

    Add package alitiq/2025.5.5/alitiq-2025.5.5-py3-none-any.whl

---
+++ packages/alitiq/alitiq-2025.5.5-py3-none-any.whl/alitiq/__init__.py
@@ -0,0 +1,8 @@
+"""easy access"""
+
+from alitiq.load_forecast import alitiqLoadAPI  # noqa
+from alitiq.models.load_forecast import LoadMeasurementForm  # noqa
+from alitiq.models.solar_power_forecast import PvMeasurementForm  # noqa
+from alitiq.models.solar_power_forecast import SolarPowerPlantModel  # noqa
+from alitiq.solar_power_forecast import alitiqSolarAPI  # noqa
+from alitiq.wind_power_forecast import alitiqWindAPI  # noqa
