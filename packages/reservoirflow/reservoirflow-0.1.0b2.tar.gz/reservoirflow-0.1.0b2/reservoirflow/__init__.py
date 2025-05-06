054d97396e807f9ed38c9f8092bba9aebda12ccb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-06 08:05:23 +0000

    Add package reservoirflow/0.1.0b2/reservoirflow-0.1.0b2.tar.gz

---
+++ packages/reservoirflow/reservoirflow-0.1.0b2.tar.gz/reservoirflow-0.1.0b2/reservoirflow/__init__.py
@@ -0,0 +1,27 @@
+"""
+ReservoirFlow
+=============
+
+ReservoirFlow: Reservoir Simulation and Engineering Library in Python developed by Zakariya Abugrin at Hiesab.
+
+- Check the `Documentation <https://reservoirflow.hiesab.com/>`_.
+"""
+
+__all__ = [
+    "fluids",
+    "grids",
+    "wells",
+    "models",
+    "solutions",
+    "scalers",
+    "utils",
+    "backends",
+    "FACTORS",
+    "UNITS",
+    "NOMENCLATURE",
+]
+
+from . import backends, fluids, grids, models, scalers, solutions, utils, wells
+from .base import FACTORS, NOMENCLATURE, UNITS
+
+__version__ = "0.1.0b2"
