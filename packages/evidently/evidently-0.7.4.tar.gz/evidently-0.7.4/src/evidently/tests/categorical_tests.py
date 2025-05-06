5d2497bfc000e4c96a567760edcb423a4fd2928b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-06 05:05:14 +0000

    Add package evidently/0.7.4/evidently-0.7.4.tar.gz

---
+++ packages/evidently/evidently-0.7.4.tar.gz/evidently-0.7.4/src/evidently/tests/categorical_tests.py
@@ -0,0 +1,46 @@
+from typing import List
+from typing import Union
+
+from evidently.core.metric_types import MetricCalculationBase
+from evidently.core.metric_types import MetricTest
+from evidently.core.metric_types import MetricTestResult
+from evidently.core.metric_types import SingleValue
+from evidently.core.metric_types import SingleValueTest
+from evidently.core.report import Context
+from evidently.legacy.tests.base_test import TestStatus
+
+
+class IsInMetricTest(MetricTest):
+    values: List[Union[int, str]]
+
+    def to_test(self) -> SingleValueTest:
+        def func(context: Context, metric: MetricCalculationBase, value: SingleValue) -> MetricTestResult:
+            check_value = value.value in self.values
+            return MetricTestResult(
+                id="is_in",
+                name=f"{metric.display_name()}: Value in list [{', '.join(str(x) for x in self.values)}]",
+                description=f"Actual value: {value.value}",
+                status=TestStatus.SUCCESS if check_value else TestStatus.FAIL,
+                metric_config=metric.to_metric_config(),
+                test_config=self.dict(),
+            )
+
+        return func
+
+
+class NotInMetricTest(MetricTest):
+    values: List[Union[int, str]]
+
+    def to_test(self) -> SingleValueTest:
+        def func(context: Context, metric: MetricCalculationBase, value: SingleValue) -> MetricTestResult:
+            check_value = value.value not in self.values
+            return MetricTestResult(
+                id="not_in",
+                name=f"{metric.display_name()}: Value not in list [{', '.join(str(x) for x in self.values)}]",
+                description=f"Actual value: {value.value}",
+                status=TestStatus.SUCCESS if check_value else TestStatus.FAIL,
+                metric_config=metric.to_metric_config(),
+                test_config=self.dict(),
+            )
+
+        return func
