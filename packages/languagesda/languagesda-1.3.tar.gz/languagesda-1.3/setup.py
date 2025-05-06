a4568273961d16b4e31ac0f225e8d1eba1eb46e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-06 03:21:49 +0000

    Add package languagesda/1.3/languagesda-1.3.tar.gz

---
+++ packages/languagesda/languagesda-1.3.tar.gz/languagesda-1.3/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(
+    name='languagesda',
+    version='1.3',
+    packages = ['languagesda'],
+    install_requires=[],
+    python_requires=">=3.11",
+    author="development",
+    author_email="agcs-development@rambler.ru"
+)
