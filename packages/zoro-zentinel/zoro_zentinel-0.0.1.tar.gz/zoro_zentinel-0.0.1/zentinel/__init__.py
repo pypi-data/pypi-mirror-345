32bf11df03aceddc36152556f5440ae7de87857f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-06 06:05:51 +0000

    Add package zoro-zentinel/0.0.1/zoro_zentinel-0.0.1.tar.gz

---
+++ packages/zoro-zentinel/zoro_zentinel-0.0.1.tar.gz/zoro_zentinel-0.0.1/zentinel/__init__.py
@@ -0,0 +1,10 @@
+"""Top-level package for zentinel."""
+
+import datetime
+
+
+__name__ = "zentinel"
+__version__ = "0.0.1"
+__author__ = "Zoro Data Platform"
+__email__ = ""
+__copyright__ = f"Copyright (C) {datetime.datetime.now().year} Zoro"
