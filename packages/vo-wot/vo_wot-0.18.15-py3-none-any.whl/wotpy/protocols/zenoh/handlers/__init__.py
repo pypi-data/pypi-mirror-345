78713e654dfe87c3d3ec57d2051f4bdec7c6839f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-06 05:05:00 +0000

    Add package vo-wot/0.18.15/vo_wot-0.18.15-py3-none-any.whl

---
+++ packages/vo-wot/vo_wot-0.18.15-py3-none-any.whl/wotpy/protocols/zenoh/handlers/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Entities that handle the Zenoh operations needed to support each of the Interaction verbs.
+
+.. autosummary::
+    :toctree: _handlers
+
+    wotpy.protocols.zenoh.handlers.action
+    wotpy.protocols.zenoh.handlers.base
+    wotpy.protocols.zenoh.handlers.event
+    wotpy.protocols.zenoh.handlers.ping
+    wotpy.protocols.zenoh.handlers.property
+    wotpy.protocols.zenoh.handlers.subs
+"""
