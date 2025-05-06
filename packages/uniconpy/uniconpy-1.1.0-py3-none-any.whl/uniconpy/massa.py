a328f589ff815b45ebda5f09f70138f8fb0b8687
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-06 07:05:56 +0000

    Add package uniconpy/1.1.0/uniconpy-1.1.0-py3-none-any.whl

---
+++ packages/uniconpy/uniconpy-1.1.0-py3-none-any.whl/uniconpy/massa.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+def kg_para_g(kg):
+    return kg * 1000
+
+def g_para_kg(g):
+    return g / 1000
+
+def lb_para_kg(lb):
+    return lb * 0.45359237
+
+def kg_para_lb(kg):
+    return kg / 0.45359237
+
+def kg_para_arroba(quilogramas):
+    return quilogramas / 15.0
+
+def arroba_para_kg(arrobas):
+    return arrobas * 15.0
+
+
+
+
---
+++ packages/uniconpy/uniconpy-1.1.0-py3-none-any.whl/uniconpy/volume.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+
+def litro_para_ml(l):
+    return l * 1000
+
+def ml_para_litro(ml):
+    return ml / 1000
+
+def litro_para_galao(l):
+    return l / 3.78541
+
+def galao_para_litro(gal):
+    return gal * 3.78541
