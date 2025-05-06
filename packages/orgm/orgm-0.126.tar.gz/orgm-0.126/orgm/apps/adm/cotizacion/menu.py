74c0ba62985e92f46a9ee9e68500289ae40a0e6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-06 08:05:22 +0000

    Add package orgm/0.126/orgm-0.126.tar.gz

---
+++ packages/orgm/orgm-0.126.tar.gz/orgm-0.126/orgm/apps/adm/cotizacion/menu.py
@@ -0,0 +1,15 @@
+from rich.console import Console
+from rich.prompt import Prompt
+import questionary
+import sys
+import subprocess
+
+from orgm.stuff.spinner import spinner
+
+console = Console()
+
+def menu():
+    """Menú interactivo para comandos de Cotizaciones."""
+    pass
+    
+
