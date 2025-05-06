4cdcd7918e74978463e78aef582af5bd88e214d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-06 04:05:05 +0000

    Add package sipua/0.4.0/sipua-0.4.0.tar.gz

---
+++ packages/sipua/sipua-0.4.0.tar.gz/sipua-0.4.0/src/sipua/__init__.py
@@ -0,0 +1,30 @@
+#
+# Copyright (C) Spacinov SAS
+# Distributed under the 2-clause BSD license
+#
+
+import importlib.metadata
+
+from .dialog import Dialog, DialogLayer
+from .transaction import (
+    ServerInviteTransaction,
+    ServerNonInviteTransaction,
+    ServerTransaction,
+    TransactionLayer,
+)
+from .transport import TransportAddress, TransportLayer
+from .utils import create_ack, create_response
+
+__all__ = [
+    "create_ack",
+    "create_response",
+    "Dialog",
+    "DialogLayer",
+    "ServerInviteTransaction",
+    "ServerNonInviteTransaction",
+    "ServerTransaction",
+    "TransactionLayer",
+    "TransportAddress",
+    "TransportLayer",
+]
+__version__ = importlib.metadata.version("sipua")
