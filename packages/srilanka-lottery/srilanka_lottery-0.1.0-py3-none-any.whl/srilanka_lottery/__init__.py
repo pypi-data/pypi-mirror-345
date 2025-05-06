0d45ba344a35855f90715e2b0c2f106a9262d308
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-06 04:05:17 +0000

    Add package srilanka-lottery/0.1.0/srilanka_lottery-0.1.0-py3-none-any.whl

---
+++ packages/srilanka-lottery/srilanka_lottery-0.1.0-py3-none-any.whl/srilanka_lottery/__init__.py
@@ -0,0 +1,10 @@
+from .scraper import (
+    scrape_nlb_result,
+    scrape_dlb_result,
+    scrape_dlb_lottery_names,
+    scrape_nlb_active_lottery_names,
+    scrape_nlb_latest_results,
+    scrape_dlb_latest_results
+)
+
+__version__ = "0.1.0"
