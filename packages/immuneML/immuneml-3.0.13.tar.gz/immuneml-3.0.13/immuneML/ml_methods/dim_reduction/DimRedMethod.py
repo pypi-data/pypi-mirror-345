f16b72938720ecdb0f347e8c3bf4c555e0430770
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-06 03:21:44 +0000

    Add package immuneML/3.0.13/immuneml-3.0.13.tar.gz

---
+++ packages/immuneML/immuneml-3.0.13.tar.gz/immuneml-3.0.13/immuneML/ml_methods/dim_reduction/DimRedMethod.py
@@ -0,0 +1,40 @@
+import abc
+from typing import List
+
+from immuneML.data_model.datasets.Dataset import Dataset
+
+
+class DimRedMethod:
+    """
+
+    Dimensionality reduction methods are algorithms which can be used to reduce the dimensionality
+    of encoded datasets, in order to uncover and analyze patterns present in the data.
+
+    These methods can be used in the :ref:`ExploratoryAnalysis` and :ref:`Clustering` instructions.
+    """
+
+    DOCS_TITLE = "Dimensionality reduction methods"
+
+    def __init__(self, name: str = None):
+        self.method = None
+        self.name = name
+
+    @abc.abstractmethod
+    def fit(self, dataset: Dataset):
+        pass
+
+    @abc.abstractmethod
+    def transform(self, dataset: Dataset):
+        pass
+
+    @abc.abstractmethod
+    def fit_transform(self, dataset: Dataset):
+        pass
+
+    @abc.abstractmethod
+    def get_dimension_names(self) -> List[str]:
+        pass
+
+    @classmethod
+    def get_title(cls):
+        return "Dimensionality Reduction"
