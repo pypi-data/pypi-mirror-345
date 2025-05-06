4080ae66b5bb2421ab184a7c342e379a6525b424
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-06 07:05:47 +0000

    Add package cog/0.14.8/cog-0.14.8.tar.gz

---
+++ packages/cog/cog-0.14.8.tar.gz/cog-0.14.8/pkg/docker/docker_command.go
@@ -0,0 +1,320 @@
+package docker
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
+	"strings"
+
+	"github.com/docker/cli/cli/config"
+	"github.com/docker/cli/cli/config/configfile"
+	"github.com/docker/cli/cli/config/types"
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/api/types/image"
+
+	"github.com/replicate/cog/pkg/docker/command"
+	"github.com/replicate/cog/pkg/util"
+	"github.com/replicate/cog/pkg/util/console"
+	"github.com/replicate/cog/pkg/util/slices"
+)
+
+var (
+	commandsRequiringPlatform = []string{"build", "run"}
+)
+
+type DockerCommand struct{}
+
+func NewDockerCommand() *DockerCommand {
+	return &DockerCommand{}
+}
+
+func (c *DockerCommand) Pull(ctx context.Context, image string, force bool) (*image.InspectResponse, error) {
+	console.Debugf("=== DockerCommand.Pull %s force:%t", image, force)
+
+	if !force {
+		inspect, err := c.Inspect(ctx, image)
+		if err == nil {
+			return inspect, nil
+		} else if !command.IsNotFoundError(err) {
+			// Log a warning if inspect fails for any reason other than not found.
+			// It's likely that pull will fail as well, but it's better to return that error
+			// so the caller can handle it appropriately than to fail silently here.
+			console.Warnf("failed to inspect image before pulling %q: %s", image, err)
+		}
+	}
+
+	err := c.exec(ctx, os.Stderr, "pull", image, "--platform", "linux/amd64")
+	if err != nil {
+		// A "not found" error message will be different depending on what flavor of engine and
+		// registry version we're hitting. This checks for both docker and OCI lingo.
+		if strings.Contains(err.Error(), "manifest unknown") || strings.Contains(err.Error(), "failed to resolve reference") {
+			return nil, &command.NotFoundError{Object: "manifest", Ref: image}
+		}
+		return nil, fmt.Errorf("failed to pull image %q: %w", image, err)
+	}
+
+	// pull succeeded, inspect the image again and return
+	inspect, err := c.Inspect(ctx, image)
+	if err != nil {
+		return nil, fmt.Errorf("failed to inspect image after pulling %q: %w", image, err)
+	}
+	return inspect, nil
+}
+
+func (c *DockerCommand) Push(ctx context.Context, image string) error {
+	console.Debugf("=== DockerCommand.Push %s", image)
+
+	return c.exec(ctx, os.Stderr, "push", image)
+}
+
+func (c *DockerCommand) LoadUserInformation(ctx context.Context, registryHost string) (*command.UserInfo, error) {
+	console.Debugf("=== DockerCommand.LoadUserInformation %s", registryHost)
+
+	conf := config.LoadDefaultConfigFile(os.Stderr)
+	credsStore := conf.CredentialsStore
+	if credsStore == "" {
+		authConf, err := loadAuthFromConfig(conf, registryHost)
+		if err != nil {
+			return nil, err
+		}
+		return &command.UserInfo{
+			Token:    authConf.Password,
+			Username: authConf.Username,
+		}, nil
+	}
+	credsHelper, err := loadAuthFromCredentialsStore(ctx, credsStore, registryHost)
+	if err != nil {
+		return nil, err
+	}
+	return &command.UserInfo{
+		Token:    credsHelper.Secret,
+		Username: credsHelper.Username,
+	}, nil
+}
+
+func (c *DockerCommand) CreateTarFile(ctx context.Context, image string, tmpDir string, tarFile string, folder string) (string, error) {
+	console.Debugf("=== DockerCommand.CreateTarFile %s %s %s %s", image, tmpDir, tarFile, folder)
+
+	args := []string{
+		"run",
+		"--rm",
+		"--volume",
+		tmpDir + ":/buildtmp",
+		image,
+		"/opt/r8/monobase/tar.sh",
+		"/buildtmp/" + tarFile,
+		"/",
+		folder,
+	}
+	if err := c.exec(ctx, os.Stderr, args...); err != nil {
+		return "", err
+	}
+	return filepath.Join(tmpDir, tarFile), nil
+}
+
+func (c *DockerCommand) CreateAptTarFile(ctx context.Context, tmpDir string, aptTarFile string, packages ...string) (string, error) {
+	console.Debugf("=== DockerCommand.CreateAptTarFile %s %s", aptTarFile, packages)
+
+	// This uses a hardcoded monobase image to produce an apt tar file.
+	// The reason being that this apt tar file is created outside the docker file, and it is created by
+	// running the apt.sh script on the monobase with the packages we intend to install, which produces
+	// a tar file that can be untarred into a docker build to achieve the equivalent of an apt-get install.
+	args := []string{
+		"run",
+		"--rm",
+		"--volume",
+		tmpDir + ":/buildtmp",
+		"r8.im/monobase:latest",
+		"/opt/r8/monobase/apt.sh",
+		"/buildtmp/" + aptTarFile,
+	}
+	args = append(args, packages...)
+	if err := c.exec(ctx, os.Stderr, args...); err != nil {
+		return "", err
+	}
+
+	return aptTarFile, nil
+}
+
+func (c *DockerCommand) Inspect(ctx context.Context, ref string) (*image.InspectResponse, error) {
+	console.Debugf("=== DockerCommand.Inspect %s", ref)
+	args := []string{
+		"image",
+		"inspect",
+		ref,
+	}
+	output, err := c.execCaptured(ctx, args...)
+	if err != nil {
+		if strings.Contains(err.Error(), "No such image") {
+			return nil, &command.NotFoundError{Object: "image", Ref: ref}
+		}
+		return nil, err
+	}
+
+	console.Debugf("=== DockerCommand.Inspect %s", output)
+
+	var resp []image.InspectResponse
+	if err := json.Unmarshal([]byte(output), &resp); err != nil {
+		return nil, fmt.Errorf("error unmarshaling inspect response: %w", err)
+	}
+
+	// There may be some Docker versions where a missing image
+	// doesn't return exit code 1, but progresses to output an
+	// empty list.
+	if len(resp) == 0 {
+		return nil, &command.NotFoundError{Ref: ref}
+	}
+	// inspect returns a list of manifests but we only care about the first
+	return &resp[0], nil
+}
+
+func (c *DockerCommand) ImageExists(ctx context.Context, ref string) (bool, error) {
+	console.Debugf("=== DockerCommand.ImageExists %s", ref)
+	_, err := c.Inspect(ctx, ref)
+	if err != nil {
+		if command.IsNotFoundError(err) {
+			return false, nil
+		}
+		return false, err
+	}
+	return true, nil
+}
+
+func (c *DockerCommand) ContainerLogs(ctx context.Context, containerID string, w io.Writer) error {
+	console.Debugf("=== DockerCommand.ContainerLogs %s", containerID)
+
+	args := []string{
+		"container",
+		"logs",
+		containerID,
+		"--follow",
+	}
+
+	return c.exec(ctx, w, args...)
+}
+
+func (c *DockerCommand) ContainerInspect(ctx context.Context, id string) (*container.InspectResponse, error) {
+	console.Debugf("=== DockerCommand.ContainerInspect %s", id)
+
+	args := []string{
+		"container",
+		"inspect",
+		id,
+	}
+
+	output, err := c.execCaptured(ctx, args...)
+	if err != nil {
+		if strings.Contains(err.Error(), "No such container") {
+			return nil, &command.NotFoundError{Object: "container", Ref: id}
+		}
+		return nil, err
+	}
+
+	var resp []*container.InspectResponse
+	if err = json.Unmarshal([]byte(output), &resp); err != nil {
+		return nil, err
+	}
+	if len(resp) == 0 {
+		return nil, &command.NotFoundError{Object: "container", Ref: id}
+	}
+
+	return resp[0], nil
+}
+
+func (c *DockerCommand) ContainerStop(ctx context.Context, containerID string) error {
+	console.Debugf("=== DockerCommand.ContainerStop %s", containerID)
+
+	if err := c.exec(ctx, os.Stderr, "container", "stop", "--time", "3", containerID); err != nil {
+		if strings.Contains(err.Error(), "No such container") {
+			err = &command.NotFoundError{Object: "container", Ref: containerID}
+		}
+		return fmt.Errorf("failed to stop container %q: %w", containerID, err)
+	}
+
+	return nil
+}
+
+func (c *DockerCommand) exec(ctx context.Context, w io.Writer, args ...string) error {
+	if slices.ContainsString(commandsRequiringPlatform, args[0]) && util.IsAppleSiliconMac(runtime.GOOS, runtime.GOARCH) {
+		args = append(args, "--platform", "linux/amd64")
+	}
+	dockerCmd := DockerCommandFromEnvironment()
+	cmd := exec.CommandContext(ctx, dockerCmd, args...)
+
+	// the ring buffer captures the last N bytes written to `w` so we have some context to return in an error
+	errbuf := util.NewRingBufferWriter(w, 1024)
+	cmd.Stdout = errbuf
+	cmd.Stderr = errbuf
+
+	console.Debug("$ " + strings.Join(cmd.Args, " "))
+	err := cmd.Run()
+	if err != nil {
+		if errors.Is(err, context.Canceled) {
+			return err
+		}
+		return fmt.Errorf("command failed: %s: %w", errbuf.String(), err)
+	}
+	return nil
+}
+
+func (c *DockerCommand) execCaptured(ctx context.Context, args ...string) (string, error) {
+	var out strings.Builder
+	err := c.exec(ctx, &out, args...)
+	if err != nil {
+		return "", err
+	}
+	return out.String(), nil
+}
+
+func loadAuthFromConfig(conf *configfile.ConfigFile, registryHost string) (types.AuthConfig, error) {
+	return conf.AuthConfigs[registryHost], nil
+}
+
+func loadAuthFromCredentialsStore(ctx context.Context, credsStore string, registryHost string) (*CredentialHelperInput, error) {
+	var out strings.Builder
+	binary := DockerCredentialBinary(credsStore)
+	cmd := exec.CommandContext(ctx, binary, "get")
+	cmd.Env = os.Environ()
+	cmd.Stdout = &out
+	cmd.Stderr = &out
+	stdin, err := cmd.StdinPipe()
+	if err != nil {
+		return nil, err
+	}
+	defer stdin.Close()
+	console.Debug("$ " + strings.Join(cmd.Args, " "))
+	err = cmd.Start()
+	if err != nil {
+		return nil, err
+	}
+	_, err = io.WriteString(stdin, registryHost)
+	if err != nil {
+		return nil, err
+	}
+	err = stdin.Close()
+	if err != nil {
+		return nil, err
+	}
+	err = cmd.Wait()
+	if err != nil {
+		return nil, fmt.Errorf("exec wait error: %w", err)
+	}
+
+	var config CredentialHelperInput
+	err = json.Unmarshal([]byte(out.String()), &config)
+	if err != nil {
+		return nil, err
+	}
+
+	return &config, nil
+}
+
+func DockerCredentialBinary(credsStore string) string {
+	return "docker-credential-" + credsStore
+}
---
+++ packages/cog/cog-0.14.8.tar.gz/cog-0.14.8/pkg/image/build.go
@@ -0,0 +1,440 @@
+package image
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+	"time"
+
+	"github.com/getkin/kin-openapi/openapi3"
+	"github.com/google/go-containerregistry/pkg/name"
+	"github.com/google/go-containerregistry/pkg/v1/remote"
+
+	"github.com/replicate/cog/pkg/config"
+	"github.com/replicate/cog/pkg/docker"
+	"github.com/replicate/cog/pkg/docker/command"
+	"github.com/replicate/cog/pkg/dockercontext"
+	"github.com/replicate/cog/pkg/dockerfile"
+	"github.com/replicate/cog/pkg/dockerignore"
+	"github.com/replicate/cog/pkg/global"
+	"github.com/replicate/cog/pkg/util/console"
+	"github.com/replicate/cog/pkg/weights"
+)
+
+const dockerignoreBackupPath = ".dockerignore.cog.bak"
+const weightsManifestPath = ".cog/cache/weights_manifest.json"
+const bundledSchemaFile = ".cog/openapi_schema.json"
+const bundledSchemaPy = ".cog/schema.py"
+
+var errGit = errors.New("git error")
+
+// Build a Cog model from a config
+//
+// This is separated out from docker.Build(), so that can be as close as possible to the behavior of 'docker build'.
+func Build(ctx context.Context, cfg *config.Config, dir, imageName string, secrets []string, noCache, separateWeights bool, useCudaBaseImage string, progressOutput string, schemaFile string, dockerfileFile string, useCogBaseImage *bool, strip bool, precompile bool, fastFlag bool, annotations map[string]string, localImage bool, dockerCommand command.Command) error {
+	console.Infof("Building Docker image from environment in cog.yaml as %s...", imageName)
+	if fastFlag {
+		console.Info("Fast build enabled.")
+	}
+
+	// remove bundled schema files that may be left from previous builds
+	_ = os.Remove(bundledSchemaFile)
+	_ = os.Remove(bundledSchemaPy)
+
+	if err := checkCompatibleDockerIgnore(dir); err != nil {
+		return err
+	}
+
+	var cogBaseImageName string
+
+	if dockerfileFile != "" {
+		dockerfileContents, err := os.ReadFile(dockerfileFile)
+		if err != nil {
+			return fmt.Errorf("Failed to read Dockerfile at %s: %w", dockerfileFile, err)
+		}
+		if err := docker.Build(ctx, dir, string(dockerfileContents), imageName, secrets, noCache, progressOutput, config.BuildSourceEpochTimestamp, dockercontext.StandardBuildDirectory, nil); err != nil {
+			return fmt.Errorf("Failed to build Docker image: %w", err)
+		}
+	} else {
+		generator, err := dockerfile.NewGenerator(cfg, dir, fastFlag, dockerCommand, localImage)
+		if err != nil {
+			return fmt.Errorf("Error creating Dockerfile generator: %w", err)
+		}
+		contextDir, err := generator.BuildDir()
+		if err != nil {
+			return err
+		}
+		buildContexts, err := generator.BuildContexts()
+		if err != nil {
+			return err
+		}
+		defer func() {
+			if err := generator.Cleanup(); err != nil {
+				console.Warnf("Error cleaning up Dockerfile generator: %s", err)
+			}
+		}()
+		generator.SetStrip(strip)
+		generator.SetPrecompile(precompile)
+		generator.SetUseCudaBaseImage(useCudaBaseImage)
+		if useCogBaseImage != nil {
+			generator.SetUseCogBaseImage(*useCogBaseImage)
+		}
+
+		if generator.IsUsingCogBaseImage() {
+			cogBaseImageName, err = generator.BaseImage()
+			if err != nil {
+				return fmt.Errorf("Failed to get cog base image name: %s", err)
+			}
+		}
+
+		if separateWeights {
+			weightsDockerfile, runnerDockerfile, dockerignore, err := generator.GenerateModelBaseWithSeparateWeights(ctx, imageName)
+			if err != nil {
+				return fmt.Errorf("Failed to generate Dockerfile: %w", err)
+			}
+
+			if err := backupDockerignore(); err != nil {
+				return fmt.Errorf("Failed to backup .dockerignore file: %w", err)
+			}
+
+			weightsManifest, err := generator.GenerateWeightsManifest(ctx)
+			if err != nil {
+				return fmt.Errorf("Failed to generate weights manifest: %w", err)
+			}
+			cachedManifest, _ := weights.LoadManifest(weightsManifestPath)
+			changed := cachedManifest == nil || !weightsManifest.Equal(cachedManifest)
+			if changed {
+				if err := buildWeightsImage(ctx, dir, weightsDockerfile, imageName+"-weights", secrets, noCache, progressOutput, contextDir, buildContexts); err != nil {
+					return fmt.Errorf("Failed to build model weights Docker image: %w", err)
+				}
+				err := weightsManifest.Save(weightsManifestPath)
+				if err != nil {
+					return fmt.Errorf("Failed to save weights hash: %w", err)
+				}
+			} else {
+				console.Info("Weights unchanged, skip rebuilding and use cached image...")
+			}
+
+			if err := buildRunnerImage(ctx, dir, runnerDockerfile, dockerignore, imageName, secrets, noCache, progressOutput, contextDir, buildContexts); err != nil {
+				return fmt.Errorf("Failed to build runner Docker image: %w", err)
+			}
+		} else {
+			dockerfileContents, err := generator.GenerateDockerfileWithoutSeparateWeights(ctx)
+			if err != nil {
+				return fmt.Errorf("Failed to generate Dockerfile: %w", err)
+			}
+			if err := docker.Build(ctx, dir, dockerfileContents, imageName, secrets, noCache, progressOutput, config.BuildSourceEpochTimestamp, contextDir, buildContexts); err != nil {
+				return fmt.Errorf("Failed to build Docker image: %w", err)
+			}
+		}
+	}
+
+	var schemaJSON []byte
+	if schemaFile != "" {
+		console.Infof("Validating model schema from %s...", schemaFile)
+		data, err := os.ReadFile(schemaFile)
+		if err != nil {
+			return fmt.Errorf("Failed to read schema file: %w", err)
+		}
+
+		schemaJSON = data
+	} else {
+		console.Info("Validating model schema...")
+		schema, err := GenerateOpenAPISchema(ctx, imageName, cfg.Build.GPU)
+		if err != nil {
+			return fmt.Errorf("Failed to get type signature: %w", err)
+		}
+
+		data, err := json.Marshal(schema)
+		if err != nil {
+			return fmt.Errorf("Failed to convert type signature to JSON: %w", err)
+		}
+
+		schemaJSON = data
+	}
+
+	// save open_api schema file
+	if err := os.WriteFile(bundledSchemaFile, schemaJSON, 0o644); err != nil {
+		return fmt.Errorf("failed to store bundled schema file %s: %w", bundledSchemaFile, err)
+	}
+
+	loader := openapi3.NewLoader()
+	loader.IsExternalRefsAllowed = true
+	doc, err := loader.LoadFromData(schemaJSON)
+	if err != nil {
+		return fmt.Errorf("Failed to load model schema JSON: %w", err)
+	}
+	err = doc.Validate(loader.Context)
+	if err != nil {
+		return fmt.Errorf("Model schema is invalid: %w\n\n%s", err, string(schemaJSON))
+	}
+
+	console.Info("Adding labels to image...")
+
+	// We used to set the cog_version and config labels in Dockerfile, because we didn't require running the
+	// built image to get those. But, the escaping of JSON inside a label inside a Dockerfile was gnarly, and
+	// doesn't seem to be a problem here, so do it here instead.
+	configJSON, err := json.Marshal(cfg)
+	if err != nil {
+		return fmt.Errorf("Failed to convert config to JSON: %w", err)
+	}
+
+	pipFreeze, err := GeneratePipFreeze(ctx, imageName, fastFlag)
+	if err != nil {
+		return fmt.Errorf("Failed to generate pip freeze from image: %w", err)
+	}
+
+	labels := map[string]string{
+		command.CogVersionLabelKey:               global.Version,
+		command.CogConfigLabelKey:                string(bytes.TrimSpace(configJSON)),
+		global.LabelNamespace + "openapi_schema": string(schemaJSON),
+		global.LabelNamespace + "pip_freeze":     pipFreeze,
+		// Mark the image as having an appropriate init entrypoint. We can use this
+		// to decide how/if to shim the image.
+		global.LabelNamespace + "has_init": "true",
+	}
+
+	if cogBaseImageName != "" {
+		labels[global.LabelNamespace+"cog-base-image-name"] = cogBaseImageName
+
+		ref, err := name.ParseReference(cogBaseImageName)
+		if err != nil {
+			return fmt.Errorf("Failed to parse cog base image reference: %w", err)
+		}
+
+		img, err := remote.Image(ref)
+		if err != nil {
+			return fmt.Errorf("Failed to fetch cog base image: %w", err)
+		}
+
+		layers, err := img.Layers()
+		if err != nil {
+			return fmt.Errorf("Failed to get layers for cog base image: %w", err)
+		}
+
+		if len(layers) == 0 {
+			return fmt.Errorf("Cog base image has no layers: %s", cogBaseImageName)
+		}
+
+		lastLayerIndex := len(layers) - 1
+		layerLayerDigest, err := layers[lastLayerIndex].DiffID()
+		if err != nil {
+			return fmt.Errorf("Failed to get last layer digest for cog base image: %w", err)
+		}
+
+		lastLayer := layerLayerDigest.String()
+		console.Debugf("Last layer of the cog base image: %s", lastLayer)
+
+		labels[global.LabelNamespace+"cog-base-image-last-layer-sha"] = lastLayer
+		labels[global.LabelNamespace+"cog-base-image-last-layer-idx"] = fmt.Sprintf("%d", lastLayerIndex)
+	}
+
+	if commit, err := gitHead(ctx, dir); commit != "" && err == nil {
+		labels["org.opencontainers.image.revision"] = commit
+	} else {
+		console.Info("Unable to determine Git commit")
+	}
+
+	if tag, err := gitTag(ctx, dir); tag != "" && err == nil {
+		labels["org.opencontainers.image.version"] = tag
+	} else {
+		console.Info("Unable to determine Git tag")
+	}
+
+	for key, val := range annotations {
+		labels[key] = val
+	}
+
+	if err := docker.BuildAddLabelsAndSchemaToImage(ctx, imageName, labels, bundledSchemaFile, bundledSchemaPy); err != nil {
+		return fmt.Errorf("Failed to add labels to image: %w", err)
+	}
+	return nil
+}
+
+func BuildBase(ctx context.Context, cfg *config.Config, dir string, useCudaBaseImage string, useCogBaseImage *bool, progressOutput string) (string, error) {
+	// TODO: better image management so we don't eat up disk space
+	// https://github.com/replicate/cog/issues/80
+	imageName := config.BaseDockerImageName(dir)
+
+	console.Info("Building Docker image from environment in cog.yaml...")
+	command := docker.NewDockerCommand()
+	generator, err := dockerfile.NewGenerator(cfg, dir, false, command, false)
+	if err != nil {
+		return "", fmt.Errorf("Error creating Dockerfile generator: %w", err)
+	}
+	contextDir, err := generator.BuildDir()
+	if err != nil {
+		return "", err
+	}
+	buildContexts, err := generator.BuildContexts()
+	if err != nil {
+		return "", err
+	}
+	defer func() {
+		if err := generator.Cleanup(); err != nil {
+			console.Warnf("Error cleaning up Dockerfile generator: %s", err)
+		}
+	}()
+
+	generator.SetUseCudaBaseImage(useCudaBaseImage)
+	if useCogBaseImage != nil {
+		generator.SetUseCogBaseImage(*useCogBaseImage)
+	}
+
+	dockerfileContents, err := generator.GenerateModelBase(ctx)
+	if err != nil {
+		return "", fmt.Errorf("Failed to generate Dockerfile: %w", err)
+	}
+	if err := docker.Build(ctx, dir, dockerfileContents, imageName, []string{}, false, progressOutput, config.BuildSourceEpochTimestamp, contextDir, buildContexts); err != nil {
+		return "", fmt.Errorf("Failed to build Docker image: %w", err)
+	}
+	return imageName, nil
+}
+
+func isGitWorkTree(ctx context.Context, dir string) bool {
+	ctx, cancel := context.WithTimeout(ctx, 3*time.Second)
+	defer cancel()
+
+	out, err := exec.CommandContext(ctx, "git", "-C", dir, "rev-parse", "--is-inside-work-tree").Output()
+	if err != nil {
+		return false
+	}
+
+	return strings.TrimSpace(string(out)) == "true"
+}
+
+func gitHead(ctx context.Context, dir string) (string, error) {
+	if v, ok := os.LookupEnv("GITHUB_SHA"); ok && v != "" {
+		return v, nil
+	}
+
+	if isGitWorkTree(ctx, dir) {
+		ctx, cancel := context.WithTimeout(ctx, 3*time.Second)
+		defer cancel()
+
+		out, err := exec.CommandContext(ctx, "git", "-C", dir, "rev-parse", "HEAD").Output()
+		if err != nil {
+			return "", err
+		}
+
+		return string(bytes.TrimSpace(out)), nil
+	}
+
+	return "", fmt.Errorf("Failed to find HEAD commit: %w", errGit)
+}
+
+func gitTag(ctx context.Context, dir string) (string, error) {
+	if v, ok := os.LookupEnv("GITHUB_REF_NAME"); ok && v != "" {
+		return v, nil
+	}
+
+	if isGitWorkTree(ctx, dir) {
+		ctx, cancel := context.WithTimeout(ctx, 3*time.Second)
+		defer cancel()
+
+		out, err := exec.CommandContext(ctx, "git", "-C", dir, "describe", "--tags", "--dirty").Output()
+		if err != nil {
+			return "", err
+		}
+
+		return string(bytes.TrimSpace(out)), nil
+	}
+
+	return "", fmt.Errorf("Failed to find ref name: %w", errGit)
+}
+
+func buildWeightsImage(ctx context.Context, dir, dockerfileContents, imageName string, secrets []string, noCache bool, progressOutput string, contextDir string, buildContexts map[string]string) error {
+	if err := makeDockerignoreForWeightsImage(); err != nil {
+		return fmt.Errorf("Failed to create .dockerignore file: %w", err)
+	}
+	if err := docker.Build(ctx, dir, dockerfileContents, imageName, secrets, noCache, progressOutput, config.BuildSourceEpochTimestamp, contextDir, buildContexts); err != nil {
+		return fmt.Errorf("Failed to build Docker image for model weights: %w", err)
+	}
+	return nil
+}
+
+func buildRunnerImage(ctx context.Context, dir, dockerfileContents, dockerignoreContents, imageName string, secrets []string, noCache bool, progressOutput string, contextDir string, buildContexts map[string]string) error {
+	if err := writeDockerignore(dockerignoreContents); err != nil {
+		return fmt.Errorf("Failed to write .dockerignore file with weights included: %w", err)
+	}
+	if err := docker.Build(ctx, dir, dockerfileContents, imageName, secrets, noCache, progressOutput, config.BuildSourceEpochTimestamp, contextDir, buildContexts); err != nil {
+		return fmt.Errorf("Failed to build Docker image: %w", err)
+	}
+	if err := restoreDockerignore(); err != nil {
+		return fmt.Errorf("Failed to restore backup .dockerignore file: %w", err)
+	}
+	return nil
+}
+
+func makeDockerignoreForWeightsImage() error {
+	if err := backupDockerignore(); err != nil {
+		return fmt.Errorf("Failed to backup .dockerignore file: %w", err)
+	}
+
+	if err := writeDockerignore(dockerfile.DockerignoreHeader); err != nil {
+		return fmt.Errorf("Failed to write .dockerignore file: %w", err)
+	}
+	return nil
+}
+
+func writeDockerignore(contents string) error {
+	// read existing file contents from .dockerignore.cog.bak if it exists, and append to the new contents
+	if _, err := os.Stat(dockerignoreBackupPath); err == nil {
+		existingContents, err := os.ReadFile(dockerignoreBackupPath)
+		if err != nil {
+			return err
+		}
+		contents = string(existingContents) + "\n" + contents
+	}
+
+	return os.WriteFile(".dockerignore", []byte(contents), 0o644)
+}
+
+func backupDockerignore() error {
+	if _, err := os.Stat(".dockerignore"); err != nil {
+		if os.IsNotExist(err) {
+			// .dockerignore file does not exist, nothing to backup
+			return nil
+		}
+		return err
+	}
+
+	// rename the .dockerignore file to a new name
+	return os.Rename(".dockerignore", dockerignoreBackupPath)
+}
+
+func restoreDockerignore() error {
+	if err := os.Remove(".dockerignore"); err != nil {
+		return err
+	}
+
+	if _, err := os.Stat(dockerignoreBackupPath); err != nil {
+		if os.IsNotExist(err) {
+			// .dockerignore backup file does not exist, nothing to restore
+			return nil
+		}
+		return err
+	}
+
+	return os.Rename(dockerignoreBackupPath, ".dockerignore")
+}
+
+func checkCompatibleDockerIgnore(dir string) error {
+	matcher, err := dockerignore.CreateMatcher(dir)
+	if err != nil {
+		return err
+	}
+	// If the matcher is nil and we don't have an error, we don't have a .dockerignore to scan.
+	if matcher == nil {
+		return nil
+	}
+	if matcher.MatchesPath(".cog") {
+		return errors.New("The .cog tmp path cannot be ignored by docker in .dockerignore.")
+	}
+	return nil
+}
