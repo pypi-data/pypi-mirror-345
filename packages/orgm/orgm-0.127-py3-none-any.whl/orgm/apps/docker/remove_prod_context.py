0056680323a9a57c552cd56e91affecd9135b8ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-06 08:05:24 +0000

    Add package orgm/0.127/orgm-0.127-py3-none-any.whl

---
+++ packages/orgm/orgm-0.127-py3-none-any.whl/orgm/apps/docker/remove_prod_context.py
@@ -0,0 +1,13 @@
+import os
+from orgm.apps.docker.local_env import load_local_env, require_vars
+from orgm.apps.docker.cmd import _docker_cmd
+from rich.console import Console
+
+console = Console()
+
+def remove_prod_context():
+    """Elimina el contexto Docker 'prod'."""
+    load_local_env()
+
+    console.print("[bold green]Eliminando contexto prod...[/bold green]")
+    _docker_cmd(["docker", "context", "rm", "prod"])
