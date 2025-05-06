ae3f87fbb84631cbdd75d53a623a9255811a37e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-06 08:05:23 +0000

    Add package otpcr/81/otpcr-81.tar.gz

---
+++ packages/otpcr/otpcr-81.tar.gz/otpcr-81/otpcr/modules/tbl.py
@@ -0,0 +1,63 @@
+# This file is placed in the Public Domain.
+
+
+"lookup tables"
+
+
+NAMES = {
+    "ask": "llm",
+    "cfg": "irc",
+    "dis": "mdl",
+    "dne": "tdo",
+    "dpl": "rss",
+    "eml": "mbx",
+    "err": "err",
+    "exp": "rss",
+    "flt": "flt",
+    "fnd": "fnd",
+    "imp": "rss",
+    "log": "log",
+    "man": "man",
+    "mbx": "mbx",
+    "mod": "mod",
+    "mre": "irc",
+    "nme": "rss",
+    "now": "mdl",
+    "pwd": "irc",
+    "rem": "rss",
+    "req": "req",
+    "res": "rss",
+    "rss": "rss",
+    "slg": "slg",
+    "syn": "rss",
+    "tdo": "tdo",
+    "thr": "thr",
+    "tmr": "tmr",
+    "upt": "upt",
+    "ver": "ver"
+}
+
+
+MD5 = {
+    "err": "2aca6bd937ceb5aa2b4233f1700804b1",
+    "flt": "8895b1a059d3ab3b620c4e55c717faba",
+    "fnd": "59349ef1f4e3aa90de47145c871cbfe2",
+    "irc": "f6e2b02ba3ef5439e36cd099abded16c",
+    "llm": "984a197166d2636cf878a883ea1d5e39",
+    "log": "790205d2a19990c2de9467a5de14c8b0",
+    "man": "50a756109d168978001cd8588384e78c",
+    "mbx": "3e667cfb8304388afa9687b141c2ac67",
+    "mdl": "f0e1ace28851469439ff43854749a490",
+    "mod": "82d9b50b3a2a446fbb857de7192b8f28",
+    "req": "a0013d5e8582b1cb56854d17e9c66a95",
+    "rss": "116015efdbf66d42c4072cb616c27c75",
+    "rst": "897028501a9dd600def7aa3d0977fef5",
+    "slg": "b8d0bfca8f89e41420c42348d8b51d17",
+    "tbl": "d41d8cd98f00b204e9800998ecf8427e",
+    "tdo": "554977aeaed9897b19b85eb8addb7ae1",
+    "thr": "b04ea6fc90c5d38341cd4a2dcbfd3511",
+    "tmr": "02f46ebc284eb1d0100d22bf8d5fec0b",
+    "upt": "6f6fdb1397b57db6e68498e03b793d7a",
+    "ver": "ba6cfe6e26cbaff110fb7afa356c5a7f",
+    "web": "39acce3a18648cca02e08a7159b0c01e",
+}
