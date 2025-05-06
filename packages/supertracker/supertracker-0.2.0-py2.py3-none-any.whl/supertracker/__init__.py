40972215f8b43530ca8ba8419f517470670cbbeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-06 15:06:21 +0000

    Add package supertracker/0.2.0/supertracker-0.2.0-py2.py3-none-any.whl

---
+++ packages/supertracker/supertracker-0.2.0-py2.py3-none-any.whl/supertracker/__init__.py
@@ -0,0 +1,24 @@
+"""Top-level package for supertracker."""
+
+__author__ = """Syahvan Alviansyah"""
+__email__ = "syahvanalviansyah91@gmail.com"
+__version__ = "0.2.0"
+
+from supertracker.detection.core import Detections
+from supertracker.detection.utils import box_iou_batch
+from supertracker.bytetrack import matching
+from supertracker.bytetrack.kalman_filter import KalmanFilter
+from supertracker.bytetrack.single_object_track import STrack, TrackState
+from supertracker.bytetrack.utils import IdCounter
+from supertracker.bytetrack.core import ByteTrack
+
+__all__ = [
+    "Detections",
+    "box_iou_batch",
+    "matching",
+    "KalmanFilter",
+    "STrack",
+    "TrackState",
+    "IdCounter",
+    "ByteTrack",
+]
