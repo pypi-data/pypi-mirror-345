8df86b350fa037aae21a1889942f8326db626334
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-06 05:05:17 +0000

    Add package xnatqa/0.0.18/xnatqa-0.0.18.tar.gz

---
+++ packages/xnatqa/xnatqa-0.0.18.tar.gz/xnatqa-0.0.18/xnatqa/xnatqa.py
@@ -0,0 +1,22 @@
+import os
+import argparse
+from xnatqa.tag import tag_scans
+
+def main():
+
+    # parse input arguments
+    parser = argparse.ArgumentParser(description="XNAT QA Workflow")
+    parser.add_argument("--dicom_dir", default="/input", help = "where the DICOMs are located", required=True)
+    parser.add_argument("--experiment", default = "", required=True)
+    parser.add_argument("--dryrun", default = "", action='store_true', help="Run in dry run mode: No upload to XNAT")
+
+    args, unknown_args = parser.parse_known_args()
+    dicom_dir  = os.path.join(args.dicom_dir, 'SCANS')
+    experiment = args.experiment
+    dryrun     = args.dryrun
+
+    # tag all scans in this session
+    tag_scans(dicom_dir, experiment, dryrun)
+
+if __name__ == "__main__":
+    main()
