8e3cc6a31a5f11c58be8de888b2defeca135f86e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-06 05:05:36 +0000

    Add package authentikate/0.7.0/authentikate-0.7.0.tar.gz

---
+++ packages/authentikate/authentikate-0.7.0.tar.gz/authentikate-0.7.0/authentikate/strawberry/extension.py
@@ -0,0 +1,53 @@
+from typing import AsyncIterator, Iterator, Union
+from strawberry.extensions import SchemaExtension
+from kante.context import WsContext, HttpContext
+from authentikate.vars import set_token
+
+from authentikate.utils import authenticate_token_or_none, authenticate_header_or_none
+
+class AuthentikateExtension(SchemaExtension):
+    """ This is the extension class for the authentikate extension """
+    
+    
+    async def on_operation(self) -> Union[AsyncIterator[None], Iterator[None]]:
+        """ Set the token in the context variable """
+        
+        context = self.execution_context.context
+        
+        
+        
+        if isinstance(context, WsContext):
+            # WebSocket context
+            # Do something with the WebSocket context
+            
+            token = authenticate_token_or_none(
+                context.connection_params.get("token", ""),
+            )
+            set_token(token)
+            
+        
+        elif isinstance(context, HttpContext):
+            # HTTP context
+            # Do something with the HTTP context
+            token = authenticate_header_or_none(
+                context.headers,
+            )
+            
+            
+            set_token(token)
+            
+        else:
+            raise ValueError("Unknown context type. Cannot determine if it's WebSocket or HTTP.")
+           
+        
+        yield 
+        
+        
+        set_token(None)
+        
+        return 
+        
+       
+       
+
+        
