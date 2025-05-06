6d0346d87a9568500f9159941e038ef10eecb7ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-06 05:05:10 +0000

    Add package parsomics-plugin-interpro/1.0.5/parsomics_plugin_interpro-1.0.5-py3-none-any.whl

---
+++ packages/parsomics-plugin-interpro/parsomics_plugin_interpro-1.0.5-py3-none-any.whl/parsomics_plugin_interpro/validated_file.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+from typing import ClassVar
+
+from parsomics_core.entities.files.validated_file import ValidatedFileWithGenome
+
+
+class InterproTsvValidatedFile(ValidatedFileWithGenome):
+    _VALID_FILE_TERMINATIONS: ClassVar[list[str]] = [
+        "interpro_out.tsv",
+    ]
+
+    @property
+    def genome_name(self) -> str:
+        path_obj = Path(self.path)
+        return "_".join(path_obj.name.split("_")[:-2])
