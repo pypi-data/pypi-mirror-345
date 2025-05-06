f49bd238baa46c877143cf1b56f3801c0fedf9ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-06 05:05:00 +0000

    Add package model-auditor/0.1.0/model_auditor-0.1.0-py3-none-any.whl

---
+++ packages/model-auditor/model_auditor-0.1.0-py3-none-any.whl/model_auditor/plotting/schemas.py
@@ -0,0 +1,40 @@
+from typing import Optional
+from dataclasses import dataclass, field
+
+
+@dataclass
+class PlotterData:
+    labels: list = field(default_factory=list)
+    ids: list = field(default_factory=list)
+    parents: list = field(default_factory=list)
+    values: list = field(default_factory=list)
+    colors: list = field(default_factory=list)
+
+    def add(self, label: str, id: str, parent: str, value: int, color: float) -> None:
+        self.labels.append(label)
+        self.ids.append(id)
+        self.parents.append(parent)
+        self.values.append(value)
+        self.colors.append(color)
+
+
+@dataclass
+class HItem:
+    """Hierarchy Item"""
+
+    name: str
+    query: Optional[str] = None
+
+
+@dataclass
+class HLevel:
+    """Hierarchy level"""
+
+    items: list[HItem] = field(default_factory=list)
+
+
+@dataclass
+class Hierarchy:
+    """Hierarchy container"""
+
+    levels: list[HLevel] = field(default_factory=list)
