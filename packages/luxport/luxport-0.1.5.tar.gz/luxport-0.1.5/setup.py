7640b0634ec52e22622158178b0ee662bced5520
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-06 05:05:10 +0000

    Add package luxport/0.1.5/luxport-0.1.5.tar.gz

---
+++ packages/luxport/luxport-0.1.5.tar.gz/luxport-0.1.5/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="luxport",
+    version="0.1.5",
+    packages=find_packages(),
+    install_requires=[
+        "requests>=2.25.0",
+        "tqdm>=4.62.0",
+        "jsonschema>=4.0.0",
+    ],
+    entry_points={
+        'console_scripts': [
+            'luxport=luxport.luxport.cli:main',
+        ],
+    },
+    author="William J.B. Mattingly",
+    author_email="william.mattingly@yale.edu",
+    description="A utility for exporting IIIF manifest data to ZIP files",
+    keywords="iiif, manifest, export, yale, library",
+    python_requires=">=3.7",
+) 
