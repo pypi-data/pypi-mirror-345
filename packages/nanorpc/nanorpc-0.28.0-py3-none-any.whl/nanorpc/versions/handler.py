225b930068ca251cc8da9ea7699c011d3044792c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-06 03:21:28 +0000

    Add package nanorpc/0.28.0/nanorpc-0.28.0-py3-none-any.whl

---
+++ packages/nanorpc/nanorpc-0.28.0-py3-none-any.whl/nanorpc/versions/handler.py
@@ -0,0 +1,48 @@
+from enum import IntEnum
+from nanorpc.versions import nano_to
+from typing import Dict, Any
+
+import importlib
+
+
+class NodeVersion(IntEnum):
+    # version modules need follow strict naming convention
+    NANO_TO = 999
+    V23_3 = 1  # v23_3.py
+    V24_0 = 2  # v24_0.py
+    V25_0 = 3  # v25_0.py
+    V25_1 = 4  # no changes
+    V26_0 = 5  # no changes
+    V26_1 = 6  # no changes
+    UNDOCUMENTED = 7  # undocumented.py
+    V27_1 = 8  # v27_1.py
+    V28_0 = 9  # v28_0.py
+
+
+def _import_version_module(version: NodeVersion) -> Any:
+    module_name = f"nanorpc.versions.{version.name.lower()}"
+    try:
+        return importlib.import_module(module_name)
+    except ImportError:
+        return None
+
+
+def _update_commands(commands: Dict[str, Any], module: Any) -> None:
+    if module:
+        commands.update(getattr(module, "COMMANDS", {}))
+        for command in getattr(module, "REMOVALS", []):
+            commands.pop(command, None)
+
+
+def get_commands_for_version(node_version: NodeVersion) -> Dict[str, Any]:
+    if node_version == NodeVersion.NANO_TO:
+        return nano_to.COMMANDS
+
+    commands = {}
+    for version in NodeVersion:
+        if version > node_version:
+            continue
+        module = _import_version_module(version)
+        _update_commands(commands, module)
+
+    return commands
---
+++ packages/nanorpc/nanorpc-0.28.0-py3-none-any.whl/nanorpc/versions/v25_0.py
@@ -0,0 +1,14 @@
+### Non Backwards compatibale changes
+### accounts_balances (in case of any error returns 'errors' instead of 'error')
+### accounts_frontiers (in case of any error returns 'errors' instead of 'error')
+### accounts_representatives (in case of any error returns 'errors' instead of 'error')
+
+### Changed Commands
+# accounts_balances (added optional 'include_only_confirmed')
+
+COMMANDS = {
+    "accounts_balances": {
+        "required": ["accounts"],
+        "optional": ["include_only_confirmed"]
+    }
+}
