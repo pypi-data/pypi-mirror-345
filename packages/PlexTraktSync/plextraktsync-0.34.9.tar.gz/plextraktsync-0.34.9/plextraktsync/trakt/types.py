a6326b0e14bc5bcb5da59d9279d44dfdf17f247d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-06 06:06:09 +0000

    Add package PlexTraktSync/0.34.9/plextraktsync-0.34.9.tar.gz

---
+++ packages/PlexTraktSync/plextraktsync-0.34.9.tar.gz/plextraktsync-0.34.9/plextraktsync/trakt/types.py
@@ -0,0 +1,14 @@
+from __future__ import annotations
+
+from typing import TypedDict, Union
+
+from trakt.movies import Movie
+from trakt.tv import TVEpisode, TVSeason, TVShow
+
+TraktMedia = Union[Movie, TVShow, TVSeason, TVEpisode]
+TraktPlayable = Union[Movie, TVEpisode]
+
+
+class TraktLikedList(TypedDict):
+    listid: int
+    listname: str
