159e7bee9a338cd86ae9be38fd4c712e9997d53a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-06 14:05:09 +0000

    Add package xetmap/1.2/xetmap-1.2.tar.gz

---
+++ packages/xetmap/xetmap-1.2.tar.gz/xetmap-1.2/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+if __name__ == '__main__':
+
+    setup(
+        name='xetmap',
+        description='Libreria que te proporciona direcciones reales de Estados Unidos',
+        license='MIT',
+        url='https://github.com/MrXetwy21/XetMap',
+        version='1.2',
+        author='MrXetwy21',
+        author_email='Xetwy21@gmail.com',
+        packages=find_packages(),
+        long_description=open('README.md', encoding='utf-8').read(),
+        long_description_content_type='text/markdown',
+        classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Software Development :: Testing",
+        "Topic :: Utilities"
+        ],
+        install_requires=["names"],
+        python_requires='>=3.10',
+        
+    )
+
