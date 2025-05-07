44f86e601277a66da6158e57f6d5cd2b947903c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-07 06:05:06 +0000

    Add package otlp-proto/0.9.1/otlp_proto-0.9.1-pp311-pypy311_pp73-musllinux_1_2_armv7l.whl

---
+++ packages/otlp-proto/otlp_proto-0.9.1-pp311-pypy311_pp73-musllinux_1_2_armv7l.whl/otlp_proto/__init__.pyi
@@ -0,0 +1,10 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Sequence
+
+if TYPE_CHECKING:
+    import opentelemetry.sdk.trace
+
+def encode_spans(
+    sdk_spans: Sequence[opentelemetry.sdk.trace.ReadableSpan],
+) -> bytes: ...
