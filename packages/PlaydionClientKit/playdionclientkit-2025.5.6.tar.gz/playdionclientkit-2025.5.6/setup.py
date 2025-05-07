0946f9573a586d09c3bbba6c724670c415f0c09f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-07 04:06:05 +0000

    Add package PlaydionClientKit/2025.5.6/playdionclientkit-2025.5.6.tar.gz

---
+++ packages/PlaydionClientKit/playdionclientkit-2025.5.6.tar.gz/playdionclientkit-2025.5.6/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="PlaydionClientKit",
+    version="2025.5.6",
+    author="Adrian Albrecht",
+    author_email="adriandevprojects@gmail.com",
+    packages=setuptools.find_packages(),
+    url="https://github.com/AdrianDevProjects/ClientKit",
+    license="GPL-3.0",
+    description="ClientKit for Playdion Client",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
