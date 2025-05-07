4b228f647e34b281a2601e6d33ca018fbb8297e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-07 04:05:55 +0000

    Add package ifgen/4.2.4/ifgen-4.2.4.tar.gz

---
+++ packages/ifgen/ifgen-4.2.4.tar.gz/ifgen-4.2.4/ifgen/__init__.py
@@ -0,0 +1,13 @@
+# =====================================
+# generator=datazen
+# version=3.2.1
+# hash=f39ca0381d0cf8572f14cc8b29b7c2fc
+# =====================================
+
+"""
+Useful defaults and other package metadata.
+"""
+
+DESCRIPTION = "An interface generator for distributed computing."
+PKG_NAME = "ifgen"
+VERSION = "4.2.4"
