aaa98be6fd338ea2268421d161956912d917637f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-07 03:21:50 +0000

    Add package ScreenX/0.0.5/ScreenX-0.0.5.tar.gz

---
+++ packages/ScreenX/ScreenX-0.0.5.tar.gz/ScreenX-0.0.5/ScreenX/__init__.py
@@ -0,0 +1,12 @@
+from .bots import (
+    div_identifier,
+    element_miner,
+    Romulus,
+    Marcius,
+    navigator
+)
+
+from ._dividentifiers import (
+    implicit,
+    explicit
+    )
---
+++ packages/ScreenX/ScreenX-0.0.5.tar.gz/ScreenX-0.0.5/ScreenX/bots.py
@@ -0,0 +1,41 @@
+import requests
+from ._dividentifiers import extract_elements
+
+
+def div_identifier(header,content, div_class):
+    try:
+        element = extract_elements(Main_header = header, webcontent=content, div_class=div_class).json()['webpage_elements']
+        return element
+    except Exception as e:
+        print(f"Error: {e}")
+        return None
+
+def element_miner(header, content, xpath, selector):
+    try:
+        element = extract_elements(Main_header = header, webcontent=content, xpath=xpath).json()[selector]
+        return element
+    except Exception as e:
+        print(f"Error: {e}")
+        return None
+
+##################### Old packages ############################
+def Romulus(header, html, div_class):
+    if html is None:
+        element_name = extract_elements(Main_header = header, webcontent=html, div_class=div_class).json()['webpage_elements']
+        return element_name['div5']
+    else:
+        raise ValueError("Invalid HTML content")
+
+def Marcius(header, webelements, div_class):
+    if webelements is not None:
+        element_name = extract_elements(Main_header = header, webcontent=webelements, div_class=div_class).json()['webpage_elements']
+        return element_name['div6']
+    else:
+        raise ValueError("Invalid HTML content")
+    
+def navigator(header, elements, div_class):
+    if elements is not None:
+        element_name = extract_elements(Main_header = header, webcontent=elements, div_class=div_class).json()['webpage_elements']
+        return element_name['div7']
+    else:
+        raise ValueError("Invalid HTML content")
