3059505fab25965075bac4f375cbc171072f971c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-07 03:21:38 +0000

    Add package abhello/0.1/abhello-0.1.tar.gz

---
+++ packages/abhello/abhello-0.1.tar.gz/abhello-0.1/abhello/main.py
@@ -0,0 +1,3 @@
+def hello():
+    print("Hello from abhello!")
+    return "Hello from abhello!"
---
+++ packages/abhello/abhello-0.1.tar.gz/abhello-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "abhello",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+
+    ],
+)
