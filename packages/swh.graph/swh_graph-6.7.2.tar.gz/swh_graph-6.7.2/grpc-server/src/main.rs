146bbbfee98d3b985194c74d59249db3147ea77c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-07 04:05:47 +0000

    Add package swh.graph/6.7.2/swh_graph-6.7.2.tar.gz

---
+++ packages/swh.graph/swh_graph-6.7.2.tar.gz/swh_graph-6.7.2/grpc-server/src/main.rs
@@ -0,0 +1,115 @@
+// Copyright (C) 2023  The Software Heritage developers
+// See the AUTHORS file at the top-level directory of this distribution
+// License: GNU General Public License version 3, or any later version
+// See top-level LICENSE file for more information
+
+use anyhow::{Context, Result};
+use clap::Parser;
+use std::collections::HashSet;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::path::PathBuf;
+
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::util::SubscriberInitExt;
+
+use swh_graph::graph::*;
+use swh_graph::views::Subgraph;
+
+#[derive(Parser, Debug)]
+#[command(about = "gRPC server for the compressed Software Heritage graph", long_about = None)]
+struct Args {
+    graph_path: PathBuf,
+    #[arg(long, default_value = "[::]:50091")]
+    bind: std::net::SocketAddr,
+    #[arg(long)]
+    /// A line-separated list of node SWHIDs to exclude from the graph
+    masked_nodes: Option<PathBuf>,
+    #[arg(long)]
+    /// Defaults to `localhost:8125` (or whatever is configured by the `STATSD_HOST`
+    /// and `STATSD_PORT` environment variables).
+    statsd_host: Option<String>,
+}
+
+pub fn main() -> Result<()> {
+    let args = Args::parse();
+
+    let fmt_layer = tracing_subscriber::fmt::layer();
+    let filter_layer = tracing_subscriber::EnvFilter::try_from_default_env()
+        .or_else(|_| tracing_subscriber::EnvFilter::try_new("info"))
+        .unwrap();
+
+    let logger = tracing_subscriber::registry();
+
+    #[cfg(feature = "sentry")]
+    let (_guard, sentry_layer) = swh_graph_grpc_server::sentry::setup();
+
+    #[cfg(feature = "sentry")]
+    let logger = logger.with(sentry_layer);
+
+    logger
+        .with(filter_layer)
+        .with(fmt_layer)
+        .try_init()
+        .context("Could not initialize logging")?;
+
+    let statsd_client = swh_graph_grpc_server::statsd::statsd_client(args.statsd_host)?;
+
+    log::info!("Loading graph");
+    let graph = SwhBidirectionalGraph::new(args.graph_path)
+        .context("Could not load graph")?
+        .init_properties()
+        .load_properties(|properties| {
+            properties
+                .load_maps::<swh_graph::mph::DynMphf>()
+                .context("Could not load maps")?
+                .opt_load_timestamps()
+                .context("Could not load timestamp properties")?
+                .opt_load_persons()
+                .context("Could not load person properties")?
+                .opt_load_contents()
+                .context("Could not load content properties")?
+                .opt_load_strings()
+                .context("Could not load string properties")?
+                .load_label_names()
+                .context("Could not load label names")
+        })?
+        .load_labels()
+        .context("Could not load labels")?;
+
+    let mut masked_nodes = HashSet::new();
+    if let Some(masked_nodes_path) = args.masked_nodes {
+        log::info!("Loading masked nodes");
+        let f = File::open(&masked_nodes_path)
+            .map(BufReader::new)
+            .with_context(|| {
+                format!(
+                    "Could not open masked nodes file {}",
+                    masked_nodes_path.display()
+                )
+            })?;
+
+        for line in f.lines() {
+            let line = line.context("Could not deserialize line")?;
+            match graph.properties().node_id_from_string_swhid(&line) {
+                Ok(node) => {
+                    masked_nodes.insert(node);
+                }
+                Err(e) => log::warn!("Unknown node {line}: {e}"),
+            }
+        }
+    }
+
+    let graph = Subgraph::with_node_filter(graph, move |node| !masked_nodes.contains(&node));
+
+    // can't use #[tokio::main] because Sentry must be initialized before we start the tokio runtime
+    tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()
+        .unwrap()
+        .block_on(async {
+            log::info!("Starting server");
+            swh_graph_grpc_server::serve(graph, args.bind, statsd_client).await
+        })?;
+    Ok(())
+}
