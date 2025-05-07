bf6532a942ee9552ffe9a3d54f010bab8b0a512d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-07 04:06:08 +0000

    Add package aiowiserbyfeller/1.0.0b12/aiowiserbyfeller-1.0.0b12-py3-none-any.whl

---
+++ packages/aiowiserbyfeller/aiowiserbyfeller-1.0.0b12-py3-none-any.whl/aiowiserbyfeller/load/__init__.py
@@ -0,0 +1,12 @@
+"""Wiser by Feller load submodule"""
+
+from .load import Load
+from .on_off import OnOff
+from .dim import Dim
+from .dali import Dali
+from .dali_tw import DaliTw
+from .dali_rgbw import DaliRgbw
+from .motor import Motor
+from .hvac import Hvac
+
+__all__ = ["Load", "OnOff", "Dim", "Dali", "DaliTw", "DaliRgbw", "Motor", "Hvac"]
