6508a82a49d6f6059d9b8e1df675759734afe398
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-07 04:05:47 +0000

    Add package igua/0.1.0.post1/igua-0.1.0.post1-cp39-cp39-win_amd64.whl

---
+++ packages/igua/igua-0.1.0.post1-cp39-cp39-win_amd64.whl/igua/hca/lib.rs
@@ -0,0 +1,18 @@
+extern crate kodama;
+extern crate pyo3;
+extern crate numpy;
+extern crate rayon;
+
+use pyo3::prelude::*;
+
+mod clustering;
+mod distance;
+
+#[pymodule]
+#[pyo3(name = "hca")]
+pub fn init<'py>(_py: Python<'py>, m: &Bound<'py, PyModule>) -> PyResult<()> {
+    m.add("__package__", "igua")?;
+    m.add_function(wrap_pyfunction!(distance::manhattan, m)?)?;
+    m.add_function(wrap_pyfunction!(clustering::linkage, m)?)?;
+    Ok(())
+}
