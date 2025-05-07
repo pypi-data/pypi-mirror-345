0d5da1dc99ab6a86481ce68c6736b164fbabb270
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-07 05:07:28 +0000

    Add package simstring-fast/0.6.0/simstring_fast-0.6.0-cp310-cp310-manylinux_2_5_i686.manylinux1_i686.manylinux_2_17_i686.manylinux2014_i686.whl

---
+++ packages/simstring-fast/simstring_fast-0.6.0-cp310-cp310-manylinux_2_5_i686.manylinux1_i686.manylinux_2_17_i686.manylinux2014_i686.whl/simstring/measure/overlap.py
@@ -0,0 +1,49 @@
+import math
+from typing import Iterable
+
+
+class OverlapMeasure:
+    def __init__(self, db=None, maxsize: int = 100) -> None:
+        super().__init__()
+        if db:
+            self.maxsize = db.max_feature_size()
+        else:
+            self.maxsize = maxsize
+
+    def min_feature_size(self, query_size: int, alpha: float) -> int:
+        # return 1 # Not sure the below isn't sufficient
+        return math.floor(query_size * alpha) or 1
+
+    def max_feature_size(self, query_size: int, alpha: float) -> int:
+        return self.maxsize
+
+    def minimum_common_feature_count(
+        self, query_size: int, y_size: int, alpha: float
+    ) -> int:
+        return int(math.ceil(alpha * min(query_size, y_size)))
+
+    def similarity(self, X: Iterable[str], Y: Iterable[str]) -> int:
+        return min(len(set(X)), len(set(Y)))
+
+
+class LeftOverlapMeasure:
+    def __init__(self, db=None, maxsize: int = 100) -> None:
+        super().__init__()
+        if db:
+            self.maxsize = db.max_feature_size()
+        else:
+            self.maxsize = maxsize
+
+    def min_feature_size(self, query_size: int, alpha: float) -> int:
+        return math.floor(query_size * alpha) or 1
+
+    def max_feature_size(self, query_size: int, alpha: float) -> int:
+        return self.maxsize
+
+    def minimum_common_feature_count(
+        self, query_size: int, y_size: int, alpha: float
+    ) -> int:
+        return math.floor(query_size * alpha) or 1
+
+    def similarity(self, X: Iterable[str], Y: Iterable[str]) -> float:
+        return 1 - len(set(X) - set(Y)) / len(set(X))
