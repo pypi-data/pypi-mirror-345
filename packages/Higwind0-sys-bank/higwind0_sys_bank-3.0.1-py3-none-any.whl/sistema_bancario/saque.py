1c78f579f5d0f8851ffbee74af0ddf8805a544a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-07 06:06:16 +0000

    Add package Higwind0-sys-bank/3.0.1/higwind0_sys_bank-3.0.1-py3-none-any.whl

---
+++ packages/Higwind0-sys-bank/higwind0_sys_bank-3.0.1-py3-none-any.whl/sistema_bancario/saque.py
@@ -0,0 +1,17 @@
+from sistema_bancario.transacao import Transacao
+
+
+class Saque(Transacao):
+    def __init__(self, valor):
+        self._valor = valor
+
+    @property
+    def valor(self):
+        return self._valor
+
+    def registrar(self, conta):
+        sucesso_transacao = conta.sacar(self._valor)
+
+        if sucesso_transacao:
+            conta.historico.adicionar_transacao(self)
+            return True
