83fe22d923174357f8dda8b68d836fd6924ce55c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-07 04:05:54 +0000

    Add package LingmoUIPy/3.1.3b2025050608/lingmouipy-3.1.3b2025050608-cp312-cp312-win_amd64.whl

---
+++ packages/LingmoUIPy/lingmouipy-3.1.3b2025050608-cp312-cp312-win_amd64.whl/LingmoUIPy/lib/cmake/LingmoUI3/LingmoUITargets-release.cmake
@@ -0,0 +1,41 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Release".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "LingmoUI3.GraphicalEffects" for configuration "Release"
+set_property(TARGET LingmoUI3.GraphicalEffects APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(LingmoUI3.GraphicalEffects PROPERTIES
+  IMPORTED_IMPLIB_RELEASE "D:/a/LingmoUI/LingmoUI/build/lib.win-amd64-cpython-312/LingmoUIPy/qml/LingmoUI/GraphicalEffects/LingmoUI3.GraphicalEffects.lib"
+  IMPORTED_LOCATION_RELEASE "D:/a/LingmoUI/LingmoUI/build/lib.win-amd64-cpython-312/LingmoUIPy/qml/LingmoUI/GraphicalEffects/LingmoUI3.GraphicalEffects.dll"
+  )
+
+list(APPEND _cmake_import_check_targets LingmoUI3.GraphicalEffects )
+list(APPEND _cmake_import_check_files_for_LingmoUI3.GraphicalEffects "D:/a/LingmoUI/LingmoUI/build/lib.win-amd64-cpython-312/LingmoUIPy/qml/LingmoUI/GraphicalEffects/LingmoUI3.GraphicalEffects.lib" "D:/a/LingmoUI/LingmoUI/build/lib.win-amd64-cpython-312/LingmoUIPy/qml/LingmoUI/GraphicalEffects/LingmoUI3.GraphicalEffects.dll" )
+
+# Import target "LingmoUI3" for configuration "Release"
+set_property(TARGET LingmoUI3 APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(LingmoUI3 PROPERTIES
+  IMPORTED_IMPLIB_RELEASE "D:/a/LingmoUI/LingmoUI/build/lib.win-amd64-cpython-312/LingmoUIPy/qml/LingmoUI/LingmoUI3.lib"
+  IMPORTED_LINK_DEPENDENT_LIBRARIES_RELEASE "Qt6::Qml;Qt6::Quick;Qt6::QuickControls2;Qt6::Core5Compat"
+  IMPORTED_LOCATION_RELEASE "D:/a/LingmoUI/LingmoUI/build/lib.win-amd64-cpython-312/LingmoUIPy/qml/LingmoUI/LingmoUI3.dll"
+  )
+
+list(APPEND _cmake_import_check_targets LingmoUI3 )
+list(APPEND _cmake_import_check_files_for_LingmoUI3 "D:/a/LingmoUI/LingmoUI/build/lib.win-amd64-cpython-312/LingmoUIPy/qml/LingmoUI/LingmoUI3.lib" "D:/a/LingmoUI/LingmoUI/build/lib.win-amd64-cpython-312/LingmoUIPy/qml/LingmoUI/LingmoUI3.dll" )
+
+# Import target "LingmoUI3CompatibleModule" for configuration "Release"
+set_property(TARGET LingmoUI3CompatibleModule APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(LingmoUI3CompatibleModule PROPERTIES
+  IMPORTED_IMPLIB_RELEASE "D:/a/LingmoUI/LingmoUI/build/lib.win-amd64-cpython-312/LingmoUIPy/qml/LingmoUI/CompatibleModule/LingmoUI3CompatibleModule.lib"
+  IMPORTED_LINK_DEPENDENT_LIBRARIES_RELEASE "Qt6::Qml;Qt6::Quick;Qt6::QuickControls2;Qt6::Core5Compat"
+  IMPORTED_LOCATION_RELEASE "D:/a/LingmoUI/LingmoUI/build/lib.win-amd64-cpython-312/LingmoUIPy/qml/LingmoUI/CompatibleModule/LingmoUI3CompatibleModule.dll"
+  )
+
+list(APPEND _cmake_import_check_targets LingmoUI3CompatibleModule )
+list(APPEND _cmake_import_check_files_for_LingmoUI3CompatibleModule "D:/a/LingmoUI/LingmoUI/build/lib.win-amd64-cpython-312/LingmoUIPy/qml/LingmoUI/CompatibleModule/LingmoUI3CompatibleModule.lib" "D:/a/LingmoUI/LingmoUI/build/lib.win-amd64-cpython-312/LingmoUIPy/qml/LingmoUI/CompatibleModule/LingmoUI3CompatibleModule.dll" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
