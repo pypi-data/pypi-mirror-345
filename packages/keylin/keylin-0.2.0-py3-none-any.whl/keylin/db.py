9583108acf2dd3c863c132f499e36af0133cd27c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-07 03:21:40 +0000

    Add package keylin/0.2.0/keylin-0.2.0-py3-none-any.whl

---
+++ packages/keylin/keylin-0.2.0-py3-none-any.whl/keylin/db.py
@@ -0,0 +1,18 @@
+from fastapi import Depends
+from fastapi_users.db import SQLAlchemyUserDatabase
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+
+from .config import settings
+from .models import User
+
+engine = create_async_engine(settings.DATABASE_URL, echo=True)
+async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
+
+
+async def get_async_session() -> AsyncSession:
+    async with async_session_maker() as session:
+        yield session
+
+
+async def get_user_db(session: AsyncSession = Depends(get_async_session)):
+    yield SQLAlchemyUserDatabase(session, User)
