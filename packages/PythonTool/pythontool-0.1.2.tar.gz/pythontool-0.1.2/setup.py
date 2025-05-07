4e2683346eb3bdd8a271aea1d5fefd961dd293d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-07 05:06:26 +0000

    Add package PythonTool/0.1.2/pythontool-0.1.2.tar.gz

---
+++ packages/PythonTool/pythontool-0.1.2.tar.gz/pythontool-0.1.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+import os
+
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='PythonTool',
+    version='0.1.2',
+    author='Zhu Chongjing',
+    author_email='tommy1008@dingtalk.com',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=[
+        'manim',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
