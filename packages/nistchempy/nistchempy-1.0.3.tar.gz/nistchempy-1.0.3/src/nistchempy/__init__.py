349d29d1c9734f35e76067bfcf067f3f64ea0d3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-07 04:05:55 +0000

    Add package nistchempy/1.0.3/nistchempy-1.0.3.tar.gz

---
+++ packages/nistchempy/nistchempy-1.0.3.tar.gz/nistchempy-1.0.3/src/nistchempy/__init__.py
@@ -0,0 +1,18 @@
+'''This package is a Python interface for the NIST Chemistry WebBook database
+that provides additional data for the efficient compound search and 
+automatic retrievement of the stored physico-chemical data
+
+'''
+
+__version__ = '1.0.3'
+__updated__ = 'May 06, 2025'
+__license__ = 'MIT'
+
+
+from nistchempy.requests import RequestConfig
+from nistchempy.compound_list import get_all_data
+from nistchempy.compound import get_compound
+from nistchempy.search import run_search, NistSearchParameters
+from nistchempy.search import get_search_parameters, print_search_parameters
+
+
