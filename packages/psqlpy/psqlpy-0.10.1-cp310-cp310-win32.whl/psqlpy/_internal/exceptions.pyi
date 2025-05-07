1ebcd341e86b20a5138d132695f1f97d8df9f318
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-07 05:06:03 +0000

    Add package psqlpy/0.10.1/psqlpy-0.10.1-cp310-cp310-win32.whl

---
+++ packages/psqlpy/psqlpy-0.10.1-cp310-cp310-win32.whl/psqlpy/_internal/exceptions.pyi
@@ -0,0 +1,93 @@
+class RustPSQLDriverPyBaseError(Exception):
+    """Base PSQL-Rust-Engine exception."""
+
+class BaseConnectionPoolError(RustPSQLDriverPyBaseError):
+    """Base error for all Connection Pool errors."""
+
+class ConnectionPoolBuildError(BaseConnectionPoolError):
+    """Error for errors in building connection pool."""
+
+class ConnectionPoolConfigurationError(BaseConnectionPoolError):
+    """Error in connection pool configuration."""
+
+class ConnectionPoolExecuteError(BaseConnectionPoolError):
+    """Error in connection pool execution."""
+
+class BaseConnectionError(RustPSQLDriverPyBaseError):
+    """Base error for Connection errors."""
+
+class ConnectionExecuteError(BaseConnectionError):
+    """Error in connection execution."""
+
+class ConnectionClosedError(BaseConnectionError):
+    """Error if underlying connection is already closed."""
+
+class BaseTransactionError(RustPSQLDriverPyBaseError):
+    """Base error for all transaction errors."""
+
+class TransactionBeginError(BaseTransactionError):
+    """Error in transaction begin."""
+
+class TransactionCommitError(BaseTransactionError):
+    """Error in transaction commit."""
+
+class TransactionRollbackError(BaseTransactionError):
+    """Error in transaction rollback."""
+
+class TransactionSavepointError(BaseTransactionError):
+    """Error in transaction savepoint."""
+
+class TransactionExecuteError(BaseTransactionError):
+    """Error in transaction execution."""
+
+class TransactionClosedError(BaseTransactionError):
+    """Error if underlying connection is already closed."""
+
+class BaseCursorError(RustPSQLDriverPyBaseError):
+    """Base error for Cursor errors."""
+
+class CursorStartError(BaseCursorError):
+    """Error in cursor declare."""
+
+class CursorCloseError(BaseCursorError):
+    """Error in cursor close."""
+
+class CursorFetchError(BaseCursorError):
+    """Error in cursor fetch (any fetch)."""
+
+class CursorClosedError(BaseCursorError):
+    """Error if underlying connection is already closed."""
+
+class UUIDValueConvertError(RustPSQLDriverPyBaseError):
+    """Error if it's impossible to convert py string UUID into rust UUID."""
+
+class MacAddrConversionError(RustPSQLDriverPyBaseError):
+    """Error if cannot convert MacAddr string value to rust type."""
+
+class RustToPyValueMappingError(RustPSQLDriverPyBaseError):
+    """Error if it is not possible to covert rust type to python.
+
+    You can get it if you database contains data type that it not
+    supported by this library.
+
+    It's better to handle this exception.
+    """
+
+class PyToRustValueMappingError(RustPSQLDriverPyBaseError):
+    """Error if it is not possible to covert python type to rust.
+
+    You can get this exception when executing queries with parameters.
+    So, if there are no parameters for the query, don't handle this error.
+    """
+
+class BaseListenerError(RustPSQLDriverPyBaseError):
+    """Base error for all Listener errors."""
+
+class ListenerStartError(BaseListenerError):
+    """Error if listener start failed."""
+
+class ListenerClosedError(BaseListenerError):
+    """Error if listener manipulated but it's closed."""
+
+class ListenerCallbackError(BaseListenerError):
+    """Error if callback passed to listener isn't a coroutine."""
