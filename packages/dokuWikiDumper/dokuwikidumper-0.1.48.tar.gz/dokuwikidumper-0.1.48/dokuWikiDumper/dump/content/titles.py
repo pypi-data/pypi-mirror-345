e33bdadc5aec96f9f581fb91f1986bb2c0ff9ef2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-07 05:07:24 +0000

    Add package dokuWikiDumper/0.1.48/dokuwikidumper-0.1.48.tar.gz

---
+++ packages/dokuWikiDumper/dokuwikidumper-0.1.48.tar.gz/dokuwikidumper-0.1.48/dokuWikiDumper/dump/content/titles.py
@@ -0,0 +1,128 @@
+import os
+import urllib.parse as urlparse
+from bs4 import BeautifulSoup
+import requests
+from dokuWikiDumper.exceptions import ActionIndexDisabled
+from dokuWikiDumper.utils.util import print_with_lock as print
+from dokuWikiDumper.utils.config import running_config
+
+def getTitles(url, ns=None, session: requests.Session=None, useOldMethod=None):
+    """Get titles given a doku.php URL and an (optional) namespace
+
+    :param `useOldMethod`: `bool|None`. `None` will auto-detect if ajax api is enabled"""
+
+
+    titles = []
+    ajax = urlparse.urljoin(url, 'lib/exe/ajax.php')
+    params = {'call': 'index'}
+    if ns:
+        params['idx'] = ns
+    else:
+        print('Finding titles')
+    ns = ns or ''
+    depth = len(ns.split(':'))
+    if ns:
+        print('%sLooking in namespace %s' % (' ' * depth, ns))
+    
+
+    r = None
+    if useOldMethod is None:
+    # Don't know if ajax api is enabled
+        try:
+            print('Trying AJAX API (~15s...)')
+            # use requests directly to avoid the session retry
+            r = requests.post(ajax, data=params, timeout=15, # a timeout to avoid hanging
+                headers=session.headers, proxies=session.proxies,
+                verify=session.verify,cookies=session.cookies)
+            r.raise_for_status()
+            # ajax API OK
+            useOldMethod = False
+        except requests.exceptions.RequestException as e:
+            useOldMethod = True
+            print(str(e))
+
+        if r and (r.status_code != 200 or "AJAX call 'index' unknown!" in r.text):
+            useOldMethod = True
+
+    assert useOldMethod is not None
+    if useOldMethod is True:
+        print('AJAX API not enabled? Using old method...')
+        return getTitlesOld(url, ns=None, session=session)
+    
+    assert useOldMethod is False
+    r = session.post(ajax, data=params) if r is None else r # reuse the previous Response if possible
+    soup = BeautifulSoup(r.text, running_config.html_parser)
+    for a in soup.findAll('a', href=True):
+        if a.has_attr('title'):
+            title = a['title']
+        elif a.has_attr('data-wiki-id'): 
+            # for https://wiki.eternal-twin.net/start?do=index
+            # (log: https://cdn.digitaldragon.dev/wikibot/jobs/4efda8bd-3bd2-4f59-81ef-8e37cf574431/log.txt)
+            title = a['data-wiki-id']
+        else:
+            query = urlparse.parse_qs(urlparse.urlparse(a['href']).query)
+            title = (query['idx' if 'idx' in query else 'id'])[0]
+        if 'idx_dir' in a['class']:
+            titles += getTitles(url=url, ns=title, session=session, useOldMethod=useOldMethod)
+        else:
+            titles.append(title)
+    # time.sleep(1.5)
+    print('%sFound %d title(s) in namespace %s' %
+          (' ' * depth, len(titles), ns or '(all)'))
+    return titles
+
+
+def getTitlesOld(url, ns=None, ancient=False, session:requests.Session=None):
+    """Get titles using the doku.php?do=index"""
+
+    titles = []
+    params = {'do': 'index'}
+
+    if ns:
+        params['idx'] = ns
+    ns = ns or ''
+    depth = len(ns.split(':'))
+
+    r = session.get(url, params=params)
+    soup = BeautifulSoup(r.text, running_config.html_parser).findAll('ul', {'class': 'idx'})[0]
+    attr = 'text' if ancient else 'title'
+
+    if ns:
+        print('%sSearching in namespace %s' % (' ' * depth, ns))
+
+        def match(href):
+            if not href:
+                return False
+            qs = urlparse.urlparse(href).query
+            qs = urlparse.parse_qs(qs)
+            return 'idx' in qs and qs['idx'][0] in (ns, ':' + ns)
+        try:
+            result = soup.findAll(
+            'a', {
+                'class': 'idx_dir', 'href': match})[0].findAllPrevious('li')[0].findAll(
+            'a', {
+                'href': lambda x: x and not match(x)})
+        except:
+            if 'Command disabled: index' in r.text:
+                raise ActionIndexDisabled
+            
+            raise
+
+    else:
+        print('Finding titles (?do=index)')
+        result = soup.findAll('a')
+
+    for a in result:
+        query = urlparse.parse_qs(urlparse.urlparse(a['href']).query)
+        if (
+            a.has_attr('class')
+            and ('idx_dir' in a['class'])
+        ):
+            titles += getTitlesOld(url, query['idx'][0], session=session)
+        else:
+            titles.append(query['id'][0])
+
+    print('%sFound %d title(s) in namespace %s' %
+          (' ' * depth, len(titles), ns or '(all)'))
+
+    return titles
---
+++ packages/dokuWikiDumper/dokuwikidumper-0.1.48.tar.gz/dokuwikidumper-0.1.48/dokuWikiDumper/dump/media/media.py
@@ -0,0 +1,154 @@
+import os
+import re
+import threading
+import time
+import urllib.parse as urlparse
+
+from bs4 import BeautifulSoup
+import requests
+
+from dokuWikiDumper.utils.util import smkdirs, uopen
+from dokuWikiDumper.utils.util import print_with_lock as print
+from dokuWikiDumper.utils.config import running_config
+
+
+
+sub_thread_error = None
+
+
+def getFiles(url, ns: str = '',  dumpDir: str = '', session: requests.Session=None):
+    """ Return a list of media filenames of a wiki """
+
+    if dumpDir and os.path.exists(dumpDir + '/dumpMeta/files.txt'):
+        with uopen(dumpDir + '/dumpMeta/files.txt', 'r') as f:
+            files = f.read().splitlines()
+            if files[-1] == '--END--':
+                print('Loaded %d files from %s' %
+                      (len(files) - 1, dumpDir + '/dumpMeta/files.txt'))
+                return files[:-1]  # remove '--END--'
+
+    files = set()
+    ajax = urlparse.urljoin(url, 'lib/exe/ajax.php')
+    medialist = BeautifulSoup(
+        session.post(ajax, {
+            'call': 'medialist',
+            'ns': ns,
+            'do': 'media'
+        }).text, running_config.html_parser)
+    medians = BeautifulSoup(
+        session.post(ajax, {
+            'call': 'medians',
+            'ns': ns,
+            'do': 'media'
+        }).text, running_config.html_parser)
+    imagelinks = medialist.findAll(
+        'a',
+        href=lambda x: x and re.findall(
+            '[?&](media|image)=',
+            x))
+    for a in imagelinks:
+        query = urlparse.parse_qs(urlparse.urlparse(a['href']).query)
+        key = 'media' if 'media' in query else 'image'
+        files.add(query[key][0])
+    files = list(files)
+    namespacelinks = medians.findAll('a', {'class': 'idx_dir', 'href': True})
+    for a in namespacelinks:
+        query = urlparse.parse_qs(urlparse.urlparse(a['href']).query)
+        files += getFiles(url, query['ns'][0], session=session)
+    print('Found %d files in namespace %s' % (len(files), ns or '(all)'))
+
+    if dumpDir:
+        smkdirs(dumpDir + '/dumpMeta')
+        with uopen(dumpDir + '/dumpMeta/files.txt', 'w') as f:
+            f.write('\n'.join(files))
+            f.write('\n--END--\n')
+
+    return files
+
+
+def dumpMedia(base_url: str = '', dumpDir: str = '', session=None, threads: int = 1, ignore_errors: bool = False):
+    if not dumpDir:
+        raise ValueError('dumpDir must be set')
+
+    smkdirs(dumpDir + '/media')
+    # smkdirs(dumpDir + '/media_attic')
+    # smkdirs(dumpDir + '/media_meta')
+
+    fetch = urlparse.urljoin(base_url, 'lib/exe/fetch.php')
+    # media_repo = urlparse.urljoin(base_url, '_media')
+
+    files = getFiles(base_url, dumpDir=dumpDir, session=session)
+    def try_download(*args, **kwargs):
+        try:
+            download(*args, **kwargs)
+        except Exception as e:
+            if not ignore_errors:
+                global sub_thread_error
+                sub_thread_error = e
+                raise e
+            print(threading.current_thread().name, 'Error in sub thread: (', e, ') ignored')
+    index_of_title = -1
+    for title in files:
+        index_of_title += 1
+        while threading.active_count() > threads:
+            time.sleep(0.1)
+        if sub_thread_error:
+            raise sub_thread_error
+        print('Media: (%d/%d): [[%s]] ...' % (index_of_title+1, len(files), title))
+
+        def download(title, session: requests.Session):
+            child_path = title.replace(':', '/')
+            child_path = child_path.lstrip('/')
+            child_path = '/'.join(child_path.split('/')[:-1])
+            smkdirs(dumpDir + '/media/' + child_path)
+            file = dumpDir + '/media/' + title.replace(':', '/')
+            local_size = -1
+            if os.path.exists(file):
+                local_size = os.path.getsize(file)
+            with session.get(fetch, params={'media': title},
+                            stream=True, headers={'Referer': base_url}
+                            ) as r:
+                r.raise_for_status()
+
+                if local_size == -1:  # file does not exist
+                    to_download = True
+                else:
+                    remote_size = int(r.headers.get('Content-Length', -2))
+                    if local_size == remote_size:  # file exists and is complete
+                        print(threading.current_thread().name,
+                              'File [[%s]] exists (%d bytes)' % (title, local_size))
+                        to_download = False
+                    elif remote_size == -2:
+                        print(threading.current_thread().name,
+                          'File [[%s]] cannot get remote size ("Content-Length" missing), ' % title +
+                          'will re-download anyway')
+                        to_download = True
+                    else:
+                        to_download = True  # file exists but is incomplete
+
+                if to_download:
+                    with open(file, 'wb') as f:
+                        for chunk in r.iter_content(chunk_size=8192):
+                            f.write(chunk)
+                        print(threading.current_thread().name,
+                              'File [[%s]] Done' % title)
+                # modify mtime based on Last-Modified header
+                last_modified = r.headers.get('Last-Modified', None)
+                if last_modified:
+                    mtime = time.mktime(time.strptime(
+                        last_modified, '%a, %d %b %Y %H:%M:%S %Z'))
+                    atime = os.stat(file).st_atime
+                    # atime is not modified
+                    os.utime(file, times=(atime, mtime))
+                    # print(atime, mtime)
+
+            # time.sleep(1.5)
+
+        t = threading.Thread(target=try_download, daemon=True,
+                             args=(title, session))
+        t.start()
+
+    while threading.active_count() > 1:
+        time.sleep(2)
+        print('Waiting for %d threads to finish' %
+              (threading.active_count() - 1), end='\r')
