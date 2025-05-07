acb77291f095448ae1536f51562c80d984374002
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-07 04:06:00 +0000

    Add package dify_plugin/0.2.2/dify_plugin-0.2.2.tar.gz

---
+++ packages/dify_plugin/dify_plugin-0.2.2.tar.gz/dify_plugin-0.2.2/dify_plugin/interfaces/endpoint/__init__.py
@@ -0,0 +1,33 @@
+from abc import ABC, abstractmethod
+from collections.abc import Mapping
+
+from werkzeug import Request, Response
+
+from dify_plugin.core.runtime import Session
+
+
+class Endpoint(ABC):
+    def __init__(self, session: Session) -> None:
+        self.session = session
+
+    ############################################################
+    #        Methods that can be implemented by plugin         #
+    ############################################################
+
+    @abstractmethod
+    def _invoke(self, r: Request, values: Mapping, settings: Mapping) -> Response:
+        """
+        Invokes the endpoint with the given request.
+
+        To be implemented by subclasses.
+        """
+
+    ############################################################
+    #                 For executor use only                    #
+    ############################################################
+
+    def invoke(self, r: Request, values: Mapping, settings: Mapping) -> Response:
+        """
+        Invokes the endpoint with the given request.
+        """
+        return self._invoke(r, values, settings)
