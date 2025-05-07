ddd8936392543580909c2339bfac2551b9b21013
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-07 03:21:39 +0000

    Add package andromeda-api/0.1.0/andromeda_api-0.1.0-py3-none-any.whl

---
+++ packages/andromeda-api/andromeda_api-0.1.0-py3-none-any.whl/client/inference.py
@@ -0,0 +1,27 @@
+
+import os
+import requests
+from dotenv import load_dotenv
+
+class InferenceClient:
+    def __init__(self, base_url="https://api.andromeda.prosilico.com"):
+        load_dotenv()
+        self.base_url = os.getenv("INFERENCE_URL", base_url)
+        self.token = os.getenv("INFERENCE_API_TOKEN")
+
+        if not self.token:
+            raise ValueError("Token not found in .env file (INFERENCE_API_TOKEN)")
+
+    def predict(self, molecule: str, **kwargs):
+        url = f"{self.base_url}/predict"
+        headers = {
+            "Authorization": f"Bearer {self.token}",
+            "Content-Type": "application/json"
+        }
+
+        payload = {"molecule": molecule}
+        payload.update(kwargs)
+
+        response = requests.post(url, json=payload, headers=headers)
+        response.raise_for_status()
+        return response.json()
