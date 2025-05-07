1eca8d4b799bf0fcf99f762aa268ca8a4048bc42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-07 05:07:30 +0000

    Add package simstring-fast/0.6.0/simstring_fast-0.6.0-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/simstring-fast/simstring_fast-0.6.0-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/simstring/feature_extractor/word_ngram.py
@@ -0,0 +1,16 @@
+from .base import BaseFeatureExtractor, SENTINAL_CHAR
+
+
+class WordNgramFeatureExtractor(BaseFeatureExtractor):
+    def __init__(self, n=2, splitter=" "):
+        self.n = n
+        self.splitter = splitter
+
+    def features(self, text: str) -> list[str]:
+        # Split text by white space.
+        # If you want to extract words from text in more complicated way or using your favorite library like NLTK, please implement in your own.
+        words = text.split(self.splitter)
+        return self._words_ngram(words, self.n, SENTINAL_CHAR)
+
+    def __define__(self) -> str:
+        return f"WordNgramFeatureExtractor({self.n},{self.splitter})"
