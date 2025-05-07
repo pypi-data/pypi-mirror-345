0dc94ccba71c5b49f19380de12cb5af506041275
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-07 05:06:46 +0000

    Add package prasanth/0.1.1/prasanth-0.1.1.tar.gz

---
+++ packages/prasanth/prasanth-0.1.1.tar.gz/prasanth-0.1.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="prasanth",
+    version="0.1.1",
+    packages=find_packages(),
+    author="Your Name",
+    author_email="your@email.com",
+    description="My first Python library!",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/prasanth",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires=">=3.7",
+)
