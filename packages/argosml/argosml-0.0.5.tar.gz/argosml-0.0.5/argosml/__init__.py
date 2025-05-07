e67501f2c66eb7e6e14925607650023770478154
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-07 03:22:03 +0000

    Add package argosml/0.0.5/argosml-0.0.5.tar.gz

---
+++ packages/argosml/argosml-0.0.5.tar.gz/argosml-0.0.5/argosml/__init__.py
@@ -0,0 +1,9 @@
+from .tracker import init, log_run, train_and_log_run
+
+# Make these functions available directly as module attributes
+__all__ = ['init', 'log_run', 'train_and_log_run']
+
+# Expose the functions at the module level
+init = init
+log_run = log_run
+train_and_log_run = train_and_log_run
