6df7a4e6f93e58cf7558fc657f50a22548d2fd4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-07 05:07:26 +0000

    Add package pixellab/1.0.4/pixellab-1.0.4-py3-none-any.whl

---
+++ packages/pixellab/pixellab-1.0.4-py3-none-any.whl/pixellab/models/keypoint.py
@@ -0,0 +1,29 @@
+from typing import Literal, TypedDict
+
+SkeletonLabel = Literal[
+    "NOSE",
+    "NECK",
+    "RIGHT SHOULDER",
+    "RIGHT ELBOW",
+    "RIGHT ARM",
+    "LEFT SHOULDER",
+    "LEFT ELBOW",
+    "LEFT ARM",
+    "RIGHT HIP",
+    "RIGHT KNEE",
+    "RIGHT LEG",
+    "LEFT HIP",
+    "LEFT KNEE",
+    "LEFT LEG",
+    "RIGHT EYE",
+    "LEFT EYE",
+    "RIGHT EAR",
+    "LEFT EAR",
+]
+
+
+class Keypoint(TypedDict):
+    x: float
+    y: float
+    label: SkeletonLabel
+    z_index: float = 0.0
