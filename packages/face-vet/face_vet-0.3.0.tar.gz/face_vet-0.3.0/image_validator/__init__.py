315375b21c7f1afa94e474db8a36098eeecf78be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-07 03:21:50 +0000

    Add package face-vet/0.3.0/face_vet-0.3.0.tar.gz

---
+++ packages/face-vet/face_vet-0.3.0.tar.gz/face_vet-0.3.0/image_validator/__init__.py
@@ -0,0 +1,2 @@
+# # image_validator/__init__.py
+from .core import analyze_image
---
+++ packages/face-vet/face_vet-0.3.0.tar.gz/face_vet-0.3.0/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='face-vet',
+    version='0.3.0',
+    description='A package to detect fake or low-quality images using OpenCV and Tesseract',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Vinayak Gaikwad',
+    author_email='vinayak.gaikwad@fdplfinance.com',
+    packages=find_packages(),
+    install_requires=[
+        'opencv-python',
+        'pytesseract',
+        'numpy',
+        'Pillow',
+        'setuptools',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    include_package_data=True,
+    package_data={
+        'image_validator': ['models/*.xml', 'models/*.dat'],
+    },
+
+    python_requires='>=3.6',
+)
