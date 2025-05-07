2e474c4b376d452daf72e3c7c327aa1d5e8172c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-07 03:21:37 +0000

    Add package scilens/0.3.3/scilens-0.3.3-py3-none-any.whl

---
+++ packages/scilens/scilens-0.3.3-py3-none-any.whl/scilens/readers/cols_dataset.py
@@ -0,0 +1,47 @@
+_E='charts'
+_D='x_index'
+_C='csv_col_index'
+_B='curves'
+_A=None
+import logging
+from dataclasses import dataclass,field
+from scilens.components.compare_models import CompareGroup
+from scilens.components.compare_floats import CompareFloats
+from scilens.config.models.reader_format_cols_curve import ReaderCurveParserNameConfig
+@dataclass
+class ColsDataset:cols_count:int=0;rows_count:int=0;names:list[str]=field(default_factory=lambda:[]);numeric_col_indexes:list[int]=field(default_factory=lambda:[]);data:list[list[float]]=field(default_factory=lambda:[]);origin_line_nb:list[int]=field(default_factory=lambda:[])
+@dataclass
+class ColsCurves:type:str;info:dict;curves:dict
+def cols_dataset_get_curves_col_x(cols_dataset,col_x):
+	I='title';B=col_x;A=cols_dataset;E={}
+	if isinstance(B,int):
+		C=B-1
+		if C<0 or C>=A.cols_count:raise Exception('curve parser col_x: col_index is out of range.')
+	if isinstance(B,str):B=[B]
+	if isinstance(B,list):
+		G=[A for(A,C)in enumerate(A.names)if C in B]
+		if len(G)==0:return _A,E
+		C=G[0]
+	E[_D]=C;J=[B for(A,B)in enumerate(A.numeric_col_indexes)if A!=C];F=[];H=[]
+	for D in J:B=A.data[C];K=A.data[D];L={I:A.names[D],'short_title':A.names[D],'series':[[B[A],K[A]]for A in range(A.rows_count)],_C:D};F+=[L];M={I:A.names[D],'type':'simple','xaxis':A.names[C],'yaxis':A.names[D],_B:[len(F)-1]};H+=[M]
+	return{_B:F,_E:H},E
+def compare(group,compare_floats,reader_test,reader_ref,cols_curve):
+	J='Errors limit reached';F=reader_ref;E=group;C=cols_curve;A=reader_test;logging.debug(f"compare cols: {E.name}")
+	if len(A.numeric_col_indexes)!=len(F.numeric_col_indexes):E.error=f"Number Float columns indexes are different: {len(A.numeric_col_indexes)} != {len(F.numeric_col_indexes)}";return
+	D=[''for A in range(A.cols_count)];K=_A;G=_A
+	if C and C.type==ReaderCurveParserNameConfig.COL_X:L=C.info[_D];K=A.data[L];G=A.names[L]
+	H=False
+	for B in range(A.cols_count):
+		if B not in A.numeric_col_indexes:continue
+		if H:D[B]=J;continue
+		P=A.data[B];Q=F.data[B];logging.debug(f"compare cols: {A.names[B]}");M,R,T=compare_floats.add_group_and_compare_vectors(A.names[B],E,{'info_prefix':G}if G else _A,P,Q,info_vector=K)
+		if R:H=True;D[B]=J;continue
+		if M.total_errors>0:D[B]=f"{M.total_errors} comparison errors"
+	if C:
+		for N in C.curves[_E]:
+			O=0
+			for S in N[_B]:
+				I=C.curves[_B][S]
+				if D[I[_C]]:I['comparison_error']=D[I[_C]];O+=1
+			N['comparison']={'curves_nb_with_error':O}
+	E.error=J if H else _A;E.info={'cols_has_error':D}
