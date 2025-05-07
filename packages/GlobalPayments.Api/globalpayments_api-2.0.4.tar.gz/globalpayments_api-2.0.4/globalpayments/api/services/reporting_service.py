b39ff332338903f3725907434e72f22b153609e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-07 05:07:53 +0000

    Add package GlobalPayments.Api/2.0.4/globalpayments_api-2.0.4.tar.gz

---
+++ packages/GlobalPayments.Api/globalpayments_api-2.0.4.tar.gz/globalpayments_api-2.0.4/globalpayments/api/services/reporting_service.py
@@ -0,0 +1,18 @@
+import globalpayments as gp
+from globalpayments.api.entities.enums import ReportType
+
+
+class ReportingService(object):
+    @staticmethod
+    def activity():
+        return gp.api.builders.TransactionReportBuilder(ReportType.Activity)
+
+    @staticmethod
+    def transaction_detail(transaction_id=None):
+        return gp.api.builders.TransactionReportBuilder(
+            ReportType.TransactionDetail
+        ).with_transaction_id(transaction_id)
+
+    @staticmethod
+    def find_transactions():
+        return gp.api.builders.TransactionReportBuilder(ReportType.FindTransactions)
