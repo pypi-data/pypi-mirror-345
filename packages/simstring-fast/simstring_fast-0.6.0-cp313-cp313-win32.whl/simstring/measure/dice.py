dfde23aca862b326db8950e9d8af873b2b18a67a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-07 05:07:30 +0000

    Add package simstring-fast/0.6.0/simstring_fast-0.6.0-cp313-cp313-win32.whl

---
+++ packages/simstring-fast/simstring_fast-0.6.0-cp313-cp313-win32.whl/simstring/measure/dice.py
@@ -0,0 +1,18 @@
+import math
+from typing import Iterable
+
+
+class DiceMeasure:
+    def min_feature_size(self, query_size: int, alpha: float) -> int:
+        return int(math.ceil(alpha * 1.0 / (2 - alpha) * query_size))
+
+    def max_feature_size(self, query_size: int, alpha: float) -> int:
+        return int(math.floor((2 - alpha) * query_size * 1.0 / alpha))
+
+    def minimum_common_feature_count(
+        self, query_size: int, y_size: int, alpha: float
+    ) -> int:
+        return int(math.ceil(0.5 * alpha * query_size * y_size))
+
+    def similarity(self, X: Iterable[str], Y: Iterable[str]) -> float:
+        return len(set(X) & set(Y)) * 2.0 / (len(set(X)) + len(set(Y)))
