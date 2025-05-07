0bd76d869318fee5f99b7da6d5a747191b7c0553
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-05-07 03:22:11 +0000

    Add package ScreenX1/0.0.4/ScreenX1-0.0.4.tar.gz

---
+++ packages/ScreenX1/ScreenX1-0.0.4.tar.gz/ScreenX1-0.0.4/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    # name="ScrapeX",
+    name="ScreenX1",
+    version="0.0.4",
+    description='''The Ultimate AI-Powered Web Automation & Scraping Bot''',
+    # package_dir={"": "ScreenX"},
+    packages=find_packages(),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    # url="https://github.com/ArjanCodes/2023-package",
+    author="David C",
+    author_email="structureddatadrive@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["selenium >= 4.24.0","pandas>=2.2.2"],
+    extras_require={
+        "dev": ["pytest>=7.0", "twine>=4.0.2"],
+    },
+    python_requires=">=3.6",
+)
